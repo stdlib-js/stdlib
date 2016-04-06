3514b33a950114861190f32cda571045e84fba49
kgryte <kgryte@gmail.com>
2016-04-05 22:43:33 -0700

    Remove SOURCE_DIR export

--- tools/ci/travis.sh
+++ tools/ci/travis.sh
@@ -8,7 +8,7 @@
 # VARIABLES #
 
 # Get the directory of this script:
-export SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Define a heartbeat interval to prevent Travis CI from prematurely ending due to long running commands:
 export HEARTBEAT_INTERVAL=30s
