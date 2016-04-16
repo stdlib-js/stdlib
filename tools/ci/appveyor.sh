c70d1cddc639b111ec7d0cb90b3e2be967e88f9b
kgryte <kgryte@gmail.com>
2016-04-16 05:07:31 -0700

    Remove browser tests from file

--- tools/ci/appveyor.sh
+++ tools/ci/appveyor.sh
@@ -88,9 +88,6 @@ start_heartbeat
 echo 'Running tests...'
 make test >> $CI_LOG_PATH 2>&1
 
-# echo 'Running browser tests...'
-# make test-browsers >> $CI_LOG_PATH 2>&1
-
 # TODO: uncomment once https://github.com/ForbesLindesay/cmd-shim/issues/17 is resolved.
 # echo 'Generating test coverage report...'
 # make test-cov >> $CI_LOG_PATH 2>&1
