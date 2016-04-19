862f117c8e6b19a03ed3ccc0369f2eee578ff335
kgryte <kgryte@gmail.com>
2016-04-18 21:50:28 -0700

    Rename var name

--- tools/snippets/test/test.js
+++ tools/snippets/test/test.js
@@ -3,14 +3,14 @@
 // MODULES //
 
 var tape = require( 'tape' );
-var fcn = require( './../lib' );
+var TODO = require( './../lib' );
 
 
 // TESTS //
 
 tape( 'main export is a function', function test( t ) {
 	t.ok( true, __filename );
-	t.equal( typeof fcn, 'function', 'main export is a function' );
+	t.equal( typeof TODO, 'function', 'main export is a function' );
 	t.end();
 });
 
