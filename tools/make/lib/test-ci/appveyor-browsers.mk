4dc92300cfd4bbafb9e5feb84682e07e31e2743e
kgryte <kgryte@gmail.com>
2016-05-20 23:22:18 -0700

    Update recipe

--- tools/make/lib/test-ci/appveyor-browsers.mk
+++ tools/make/lib/test-ci/appveyor-browsers.mk
@@ -6,7 +6,7 @@
 # This target runs browser tests using a headless browser on Appveyor.
 
 test-browsers-appveyor: node_modules
-	make -f $(THIS_FILE) test-browsers
+	@$(MAKE) -f $(THIS_FILE) test-browsers
 
 
 .PHONY: test-browsers-appveyor
