7b13a4e759e19935207c5eec92be213a0efca6b1
kgryte <kgryte@gmail.com>
2016-05-21 00:02:11 -0700

    Add recipe for finding Markdown files

---
+++ tools/make/lib/find/markdown.mk
@@ -0,0 +1,36 @@
+
+# VARIABLES #
+
+# On Mac OSX, in order to use `|` and other regular expression operators, we need to use enhanced regular expression syntax (-E); see https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man7/re_format.7.html#//apple_ref/doc/man/7/re_format.
+
+ifeq ($(KERNEL), Darwin)
+	MARKDOWN_FILES ?= $(shell find -E $(ROOT) \
+		-name "$(MARKDOWN_PATTERN)" \
+		-regex "$(MARKDOWN_FILTER)" \
+		-not -path "$(NODE_MODULES)/*" \
+		-not -path "$(BUILD_DIR)/*" \
+		-not -path "$(REPORTS_DIR)/*" \
+	)
+else
+	MARKDOWN_FILES ?= $(shell find $(ROOT) \
+		-name "$(MARKDOWN_PATTERN)" \
+		-regextype posix-extended \
+		-regex "$(MARKDOWN_FILTER)" \
+		-not -path "$(NODE_MODULES)/*" \
+		-not -path "$(BUILD_DIR)/*" \
+		-not -path "$(REPORTS_DIR)/*" \
+	)
+endif
+
+
+# TARGETS #
+
+# List all Markdown files.
+#
+# This target prints a list of all Makdown files, excluding the `node_modules`, `build`, and `reports` directories.
+
+list-markdown-files:
+	@printf '%s\n' $(MARKDOWN_FILES)
+
+
+.PHONY: list-markdown-files
