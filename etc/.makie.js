446e0a34cd30619618d0292f975b4bb775fd0f2b
kgryte <kgryte@gmail.com>
2016-05-28 13:25:04 -0700

    Add makie configuration file

---
+++ etc/.makie.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// MODULES //
+
+var path = require( 'path' );
+
+
+// VARIABLES //
+
+var prefix = path.resolve( __dirname, '../tools/misc' );
+
+
+// CONFIG //
+
+var config = {};
+var plugins = {};
+
+config.plugins = plugins;
+
+
+// PLUGINS //
+
+plugins[ 'repl' ] = path.join( prefix, 'makie-repl' );
+plugins[ 'examples' ] = path.join( prefix, 'makie-examples' );
+plugins[ 'test' ] = path.join( prefix, 'makie-test' );
+plugins[ 'test-summary' ] = path.join( prefix, 'makie-test-summary' );
+plugins[ 'test-cov' ] = path.join( prefix, 'makie-test-cov' );
+plugins[ 'view-cov' ] = path.join( prefix, 'makie-view-cov' );
+plugins[ 'lint-markdown' ] = path.join( prefix, 'makie-lint-markdown' );
+plugins[ 'list-modules' ] = path.join( prefix, 'makie-list-modules' );
+
+
+// EXPORTS //
+
+module.exports = config;
