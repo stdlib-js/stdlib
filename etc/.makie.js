d35e92350a8526f31647304c1cda666dd41fe785
kgryte <kgryte@gmail.com>
2016-06-09 20:32:17 -0700

    Add makie plugin for complexity

--- etc/.makie.js
+++ etc/.makie.js
@@ -30,6 +30,8 @@ plugins[ 'view-cov' ] = path.join( prefix, 'makie-view-cov' );
 plugins[ 'benchmark' ] = path.join( prefix, 'makie-benchmark' );
 plugins[ 'lint-javascript' ] = path.join( prefix, 'makie-lint-javascript' );
 plugins[ 'lint-markdown' ] = path.join( prefix, 'makie-lint-markdown' );
+plugins[ 'complexity' ] = path.join( prefix, 'makie-complexity' );
+plugins[ 'view-complexity' ] = path.join( prefix, 'makie-view-complexity' );
 plugins[ 'list-modules' ] = path.join( prefix, 'makie-list-modules' );
 
 
--- tools/misc/makie/README.md
+++ tools/misc/makie/README.md
@@ -20,6 +20,7 @@ This utility provides support for executing a subset of `Makefile` commands from
 * `benchmark`
 * `lint-javascript`
 * `lint-markdown`
+* `complexity`
 * `list-modules`
 
 When executing a command, the current working directory is used as a filter. Thus, when invoking the `test` command, __only__ those tests in the current working directory and below will be run.
