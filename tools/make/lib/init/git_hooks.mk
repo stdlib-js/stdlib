7a588bb4e01c2240b1b4e3fe49ddf3bce569e693
kgryte <kgryte@gmail.com>
2016-06-16 15:57:56 -0700

    Create separate Makefile for initializing git hooks

---
+++ tools/make/lib/init/git_hooks.mk
@@ -0,0 +1,35 @@
+
+# VARIABLES #
+
+# Define the command to copy files:
+CP ?= cp
+
+# Define the command for setting executable permissions:
+MAKE_EXECUTABLE ?= chmod +x
+
+# Define the directory from which to copy git hooks:
+GIT_HOOKS_DIR ?= $(TOOLS_DIR)/git/hooks
+
+# Define a list of hooks:
+GIT_HOOKS ?= $(shell find $(GIT_HOOKS_DIR) -type f | xargs -n 1 basename)
+
+# Define the destination directory for git hooks:
+GIT_HOOKS_OUT ?= $(ROOT_DIR)/.git/hooks
+
+
+# TARGETS #
+
+# Installs git hooks.
+#
+# This target installs [git hooks][1].
+#
+# [1]: https://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks
+
+init-git-hooks:
+	@for file in $(GIT_HOOKS); do \
+		$(CP) $(GIT_HOOKS_DIR)/$$file $(GIT_HOOKS_OUT)/$$file; \
+		$(MAKE_EXECUTABLE) $(GIT_HOOKS_OUT)/$$file; \
+	done
+
+.PHONY: init-git-hooks
+
