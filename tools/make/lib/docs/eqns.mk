829c7c6fba4a619eac4e3762c4e7df55054c30eb
kgryte <kgryte@gmail.com>
2016-06-18 16:06:44 -0700

    Ensure proper escaping

--- tools/make/lib/docs/eqns.mk
+++ tools/make/lib/docs/eqns.mk
@@ -11,10 +11,10 @@ GIT_ADD ?= $(GIT) add
 GIT_COMMIT ?= $(GIT) commit
 
 # Define the commit message for processing Markdown equations:
-GIT_COMMIT_MESSAGE_EQUATIONS ?= '"Process Markdown equations"'
+GIT_COMMIT_MESSAGE_EQUATIONS ?= 'Process Markdown equations'
 
 # Define the commit message for inserting resource URLs into HTML equation elements:
-GIT_COMMIT_MESSAGE_SRC_URLS ?= '"Insert src urls into equation elements"'
+GIT_COMMIT_MESSAGE_SRC_URLS ?= 'Insert src urls into equation elements'
 
 # Define the command to commit staged files after processing Markdown equations:
 GIT_COMMIT_EQUATIONS ?= $(GIT_COMMIT) -m $(GIT_COMMIT_MESSAGE_EQUATIONS)
@@ -23,7 +23,7 @@ GIT_COMMIT_EQUATIONS ?= $(GIT_COMMIT) -m $(GIT_COMMIT_MESSAGE_EQUATIONS)
 GIT_COMMIT_SRC_URLS ?= $(GIT_COMMIT) -m $(GIT_COMMIT_MESSAGE_SRC_URLS)
 
 # Define the path relative to a processed Markdown file for storing equation resources:
-EQUATION_RESOURCES_PATH ?= './docs/img/'
+EQUATION_RESOURCES_PATH ?= ./docs/img/
 
 # Define the path to the [remark][1] executable.
 #
@@ -43,17 +43,17 @@ REMARK_LOCAL_PLUGINS_DIR ?= $(TOOLS_DIR)/remark/plugins
 # Define the path to a plugin which processes Markdown equation comments:
 REMARK_HTML_EQUATIONS_PLUGIN ?= $(REMARK_LOCAL_PLUGINS_DIR)/remark-html-equations
 REMARK_HTML_EQUATIONS_PLUGIN_SETTINGS ?=
-REMARK_HTML_EQUATIONS_PLUGIN_FLAGS ?= '--use '"'""$(REMARK_HTML_EQUATIONS_PLUGIN)$(REMARK_HTML_EQUATIONS_PLUGIN_SETTINGS)""'"
+REMARK_HTML_EQUATIONS_PLUGIN_FLAGS ?= --use "'""$(REMARK_HTML_EQUATIONS_PLUGIN)$(REMARK_HTML_EQUATIONS_PLUGIN_SETTINGS)""'"
 
 # Define the path to a plugin which creates SVG equations from Markdown equation comments:
 REMARK_SVG_EQUATIONS_PLUGIN ?= $(REMARK_LOCAL_PLUGINS_DIR)/remark-svg-equations
-REMARK_SVG_EQUATIONS_PLUGIN_SETTINGS ?= '="dir":''"'"$(EQUATION_RESOURCES_PATH)"'"'
-REMARK_SVG_EQUATIONS_PLUGIN_FLAGS ?= '--use '"'""$(REMARK_SVG_EQUATIONS_PLUGIN)$(REMARK_SVG_EQUATIONS_PLUGIN_SETTINGS)""'"
+REMARK_SVG_EQUATIONS_PLUGIN_SETTINGS ?= \='"'dir'"'\:'"'"$(EQUATION_RESOURCES_PATH)"'"'
+REMARK_SVG_EQUATIONS_PLUGIN_FLAGS ?= --use "'""$(REMARK_SVG_EQUATIONS_PLUGIN)$(REMARK_SVG_EQUATIONS_PLUGIN_SETTINGS)""'"
 
 # Define the path to a plugin which inserts resource URLs into Markdown HTML equation elements:
 REMARK_EQUATION_SRC_URLS_PLUGIN ?= $(REMARK_LOCAL_PLUGINS_DIR)/remark-html-equation-src-urls
-REMARK_EQUATION_SRC_URLS_PLUGIN_SETTINGS ?= '="dir":''"'"$(EQUATION_RESOURCES_PATH)"'"'
-REMARK_EQUATION_SRC_URLS_PLUGIN_FLAGS ?= '--use '"'""$(REMARK_EQUATION_SRC_URLS_PLUGIN)$(REMARK_EQUATION_SRC_URLS_PLUGIN_SETTINGS)""'"
+REMARK_EQUATION_SRC_URLS_PLUGIN_SETTINGS ?= \='"'dir'"'\:'"'"$(EQUATION_RESOURCES_PATH)"'"'
+REMARK_EQUATION_SRC_URLS_PLUGIN_FLAGS ?= --use "'""$(REMARK_EQUATION_SRC_URLS_PLUGIN)$(REMARK_EQUATION_SRC_URLS_PLUGIN_SETTINGS)""'"
 
 # Define Markdown extensions:
 REMARK_EXT ?= md
