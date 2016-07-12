6a046f8c27d860b8ac6ee5fee3dddeb4dda8fea3
kgryte <kgryte@gmail.com>
2016-07-12 13:33:38 +0100

    Wrap setting invalid value in try/catch and set `x` and `y` at instantiation

--- workshops/numeric-computing/solutions/plotting.1.js
+++ workshops/numeric-computing/solutions/plotting.1.js
@@ -3,16 +3,21 @@
 var Plot = require( '@stdlib/plot/plot' );
 
 // To create a `plot` instance:
-var plot = new Plot();
+var plot = new Plot({
+	'x': [[1,2,3]],
+	'y': [[1,0,1]]
+});
 
 // To set properties:
-plot.x = [[1,2,3]];
-plot.y = [[1,0,1]];
 plot.xLabel = 'index';
 plot.yLabel = 'value';
 plot.title = 'Pretty plot';
 plot.symbols = 'closed-circle';
 
 // If provided an invalid property value, an `error` is thrown.
-plot.x = null;
-// throws <Error>
+try {
+	plot.x = null;
+	console.error( 'Something went wrong. Should not reach this line.' );
+} catch ( err ) {
+	console.log( err.message );
+}
