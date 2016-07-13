b03b013bc6e48ea584771bf1bf80f63884341a3f
kgryte <kgryte@gmail.com>
2016-07-13 18:39:14 +0100

    Add text generation solution

---
+++ workshops/numeric-computing/solutions/simulating_markov_chains.4.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var path = require( 'path' );
+var readFileSync = require( '@stdlib/utils/fs/read-file' ).sync;
+var createRandom = require( '@stdlib/math/base/random/randu' ).factory;
+var floor = require( '@stdlib/math/base/special/floor' );
+
+
+// FIXTURES //
+
+var DB = require( './../fixtures/moby-dick/db.json' );
+
+var filepath = path.resolve( __dirname, '../fixtures/moby-dick/list.txt' );
+var LIST = readFileSync( filepath, {
+	'encoding': 'utf8'
+});
+LIST = LIST.split( /\r?\n/ );
+
+// Create a seeded PRNG:
+var randu = createRandom({
+	'seed': 123456
+});
+
+/**
+* Generates text.
+*
+* @private
+* @param {PositiveInteger} len - text length (number of words)
+* @returns {string} text
+*/
+function generate( len ) {
+	var words;
+	var list;
+	var w1;
+	var w2;
+	var w3;
+	var i;
+	var j;
+
+	i = floor( randu()*LIST.length );
+	w1 = LIST[ i ];
+	w2 = LIST[ i+1 ];
+	words = [];
+	for ( i = 0; i < len; i++ ) {
+		words.push( w1 );
+		list = DB[ w1+' '+w2 ];
+		j = floor( randu()*list.length );
+		w3 = list[ j ];
+		w1 = w2;
+		w2 = w3;
+	}
+	words.push( w2 );
+	return words.join( ' ' );
+} // end FUNCTION generate()
+
+/**
+* Main script.
+*
+* @private
+*/
+function main() {
+	var maxLength;
+	var text;
+	var len;
+	var i;
+
+	maxLength = 25;
+	for ( i = 0; i < 10; i++ ) {
+		len = floor( randu()*maxLength );
+		text = generate( len );
+		console.log( text );
+	}
+}
+
+main();
