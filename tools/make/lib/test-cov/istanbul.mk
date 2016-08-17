cebb2f14c7b0be0a4794df8e7db94c5af05e33f5
kgryte <kgryte@gmail.com>
2016-08-17 00:29:56 -0700

    Modify istanbul recipe to run tests on a per-directory basis

--- tools/make/lib/test-cov/istanbul.mk
+++ tools/make/lib/test-cov/istanbul.mk
@@ -44,9 +44,14 @@ JAVASCRIPT_TEST_COV_FLAGS ?= --no-default-excludes \
 # [1]: https://github.com/gotwarlost/istanbul
 
 test-istanbul: $(NODE_MODULES)
-	NODE_ENV=$(NODE_ENV_TEST) \
-	NODE_PATH=$(NODE_PATH_TEST) \
-	$(JAVASCRIPT_TEST_COV) $(JAVASCRIPT_TEST_COV_FLAGS) $(JAVASCRIPT_TEST) -- $(JAVASCRIPT_TEST_FLAGS) $(TESTS)
+	for dir in $(TESTS_DIRS); do \
+		echo ''; \
+		echo "Running tests in directory: $$dir"; \
+		echo ''; \
+		NODE_ENV=$(NODE_ENV_TEST) \
+		NODE_PATH=$(NODE_PATH_TEST) \
+		$(JAVASCRIPT_TEST_COV) $(JAVASCRIPT_TEST_COV_FLAGS) $(JAVASCRIPT_TEST) -- $(JAVASCRIPT_TEST_FLAGS) $$dir/**/$(TESTS_PATTERN); \
+	done
 
 .PHONY: test-istanbul
 
