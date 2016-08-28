2e309d0e412a0d642f80cc05af9ec5e1eb9012cd
kgryte <kgryte@gmail.com>
2016-08-28 02:58:34 -0500

    Expose codecov flags

--- tools/make/lib/coverage-service/codecov.mk
+++ tools/make/lib/coverage-service/codecov.mk
@@ -8,6 +8,9 @@ CAT_FLAGS ?=
 # Define the path to the Codecov executable:
 CODECOV ?= $(BIN_DIR)/codecov
 
+# Define the command-line options to be used when reporting coverage statistics:
+CODECOV_FLAGS ?=
+
 
 # TARGETS #
 
@@ -22,6 +25,6 @@ CODECOV ?= $(BIN_DIR)/codecov
 
 coverage-codecov:
 	$(QUIET) $(NPM) install codecov
-	$(QUIET) $(CAT) $(CAT_FLAGS) $(LCOV_INFO) | $(CODECOV)
+	$(QUIET) $(CAT) $(CAT_FLAGS) $(LCOV_INFO) | $(CODECOV) $(CODECOV_FLAGS)
 
 .PHONY: coverage-codecov
