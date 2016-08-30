04f9055129e5fa9e5a2aead0004e52635a603b3e
kgryte <kgryte@gmail.com>
2016-08-30 11:12:12 -0700

    Set shell to bash when running Codecov upload script

--- tools/make/lib/coverage-service/codecov.mk
+++ tools/make/lib/coverage-service/codecov.mk
@@ -4,10 +4,6 @@
 # Define the command for `node`:
 NODE ?= node
 
-# Define the command for `cat`:
-CAT ?= cat
-CAT_FLAGS ?=
-
 # Define the path to the [Codecov][1] executable.
 #
 # [1]: https://github.com/codecov/codecov-bash
@@ -16,9 +12,11 @@ CODECOV ?= bash <(curl -s https://codecov.io/bash)
 
 # Define the command-line options to be used when reporting coverage statistics:
 CODECOV_FLAGS ?= \
+	-f "$(LCOV_INFO)" \
 	-F $(CI_SERVICE) \
 	-F $(shell echo $(KERNEL) | tr '[:upper:]' '[:lower:]') \
-	-F $(shell $(NODE) --version | tr '\.' '_' | (printf 'node_' && $(CAT)))
+	-F $(shell $(NODE) --version | tr '\.' '_' | (printf 'node_' && $(CAT))) \
+	-Z
 
 ifdef COVERAGE_NAME
 	CODECOV_FLAGS := $(CODECOV_FLAGS) -F $(COVERAGE_NAME)
@@ -36,8 +34,9 @@ CODECOV_CONF ?= $(ROOT)/.codecov.yml
 #
 # [1]: https://codecov.io/
 
+coverage-codecov: SHELL := /bin/bash
 coverage-codecov:
-	$(QUIET) $(CAT) $(CAT_FLAGS) $(LCOV_INFO) | $(CODECOV) $(CODECOV_FLAGS) || echo "Failed to upload coverage reports to Codecov. :("
+	$(QUIET) $(CODECOV) $(CODECOV_FLAGS) || echo 'Failed to upload coverage reports to Codecov.'
 
 .PHONY: coverage-codecov
 
