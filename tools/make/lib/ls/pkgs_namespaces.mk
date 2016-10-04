1eba0dfc779058566591be163ed5327f1c87547d
kgryte <kgryte@gmail.com>
2016-10-03 19:17:15 -0700

    Add recipe to list namespaces

---
+++ tools/make/lib/ls/pkgs_namespaces.mk
@@ -0,0 +1,27 @@
+
+# VARIABLES #
+
+# Define the command for `node`:
+NODE ?= node
+
+# Define the root directory from which to search for packages:
+PACKAGES_DIR ?= $(ROOT_DIR)/lib/node_modules
+
+# Define the path of the executable:
+LIST_PACKAGE_NAMESPACES ?= $(TOOLS_DIR)/ls/namespaces/bin/cli
+
+# Define the command flags:
+LIST_PACKAGE_NAMESPACES_FLAGS ?= \
+	--dir $(PACKAGES_DIR)
+
+
+# TARGETS #
+
+# List all package namespaces.
+#
+# This target prints a list of all package namespaces.
+
+list-pkgs-namespaces: $(LIST_PACKAGE_NAMESPACES) $(NODE_MODULES)
+	$(QUIET) $(NODE) $(LIST_PACKAGE_NAMESPACES) $(LIST_PACKAGE_NAMESPACES_FLAGS)
+
+.PHONY: list-pkgs-namespaces
