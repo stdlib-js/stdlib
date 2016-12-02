d761cd325ca00cb2a28b35296680f855462727d4
kgryte <kgryte@gmail.com>
2016-12-01 21:42:46 -0600

    Add example

---
+++ tools/test/browser-build/examples/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var join = require( 'path' ).join;
+var resolve = require( 'path' ).resolve;
+var mkdirp = require( 'mkdirp' ).sync;
+var build = require( './../lib' );
+
+var root = join( __dirname, 'fixtures' );
+var out = resolve( __dirname, '../build' );
+
+mkdirp( out );
+
+var opts = {
+    'pattern': 'index.js',
+    'bundle': 'test.js',
+    'html': 'tests.html'
+};
+
+build( root, out, opts, clbk );
+
+function clbk( error, bool ) {
+    if ( error ) {
+        throw error;
+    }
+    if ( bool ) {
+        console.log( 'Success!' );
+    } else {
+        console.log( 'No generated assets.' );
+    }
+}
