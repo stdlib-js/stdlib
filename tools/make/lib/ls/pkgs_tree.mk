4fae6969a53addf3db9bc6520744fe9fdb9ed1fc
kgryte <kgryte@gmail.com>
2016-12-02 19:17:53 -0600

    Update recipe

--- tools/make/lib/ls/pkgs_tree.mk
+++ tools/make/lib/ls/pkgs_tree.mk
@@ -11,8 +11,7 @@ PACKAGES_DIR ?= $(ROOT_DIR)/lib/node_modules
 LIST_PACKAGE_TREE ?= $(TOOLS_DIR)/pkgs/tree/bin/cli
 
 # Define the command flags:
-LIST_PACKAGE_TREE_FLAGS ?= \
-	--dir $(PACKAGES_DIR)
+LIST_PACKAGE_TREE_FLAGS ?=
 
 
 # TARGETS #
@@ -22,6 +21,6 @@ LIST_PACKAGE_TREE_FLAGS ?= \
 # This target prints a list of all packages as a tree.
 
 list-pkgs-tree: $(LIST_PACKAGE_TREE) $(NODE_MODULES)
-	$(QUIET) $(NODE) $(LIST_PACKAGE_TREE) $(LIST_PACKAGE_TREE_FLAGS)
+	$(QUIET) $(NODE) $(LIST_PACKAGE_TREE) $(LIST_PACKAGE_TREE_FLAGS) $(PACKAGES_DIR)
 
 .PHONY: list-pkgs-tree
