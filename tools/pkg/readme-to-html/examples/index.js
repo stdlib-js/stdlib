990b2f6f598aef60a8f8c55756d7b090c740cd6d
kgryte <kgryte@gmail.com>
2016-12-03 12:39:35 -0600

    Add example

---
+++ tools/pkg/readme-to-html/examples/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var resolve = require( 'path' ).resolve;
+var convert = require( './../lib' );
+
+var fpath = resolve( __dirname, '..', 'README.md' );
+var opts = {
+	'title': 'Beep',
+	'source': 'https://github.com/stdlib-js/stdlib/develop/tools/pkg/readme-to-html/lib/index.js',
+	'append': '<script type="text/javascript>console.log("Beep!");</script>'
+};
+
+convert( fpath, opts, onFinish );
+
+function onFinish( error, html ) {
+	if ( error ) {
+		throw error;
+	}
+	console.log( html );
+}
