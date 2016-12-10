6c7213625707c7760e9abb70f5fa0375e0b31bf3
kgryte <kgryte@gmail.com>
2016-12-10 00:36:17 -0800

    Add recipe to lint package.json files

---
+++ tools/make/lib/lint/package_json.mk
@@ -0,0 +1,24 @@
+
+# VARIABLES #
+
+# Define the command for setting executable permissions:
+MAKE_EXECUTABLE ?= chmod +x
+
+# Define the path of the linter executable:
+PACKAGE_JSON_LINTER ?= $(TOOLS_DIR)/lint/pkg-json/bin/cli
+
+# Define the command-line options to be used when invoking the executable:
+PACKAGE_JSON_LINTER_FLAGS ?=
+
+
+# TARGETS #
+
+# Lint package.json.
+#
+# This target lints package.json files.
+
+lint-pkg-json:
+	$(QUIET) $(MAKE_EXECUTABLE) $(PACKAGE_JSON_LINTER)
+	$(QUIET) $(PACKAGE_JSON_LINTER) $(PACKAGE_JSON_LINTER_FLAGS) $(ROOT_DIR)
+
+.PHONY: lint-pkg-json
