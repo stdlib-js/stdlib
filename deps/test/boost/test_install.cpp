40aad0dbcb2bc35aaf628930c243696641abd7dc
kgryte <kgryte@gmail.com>
2016-12-16 18:14:54 -0800

    Add script to test install

---
+++ deps/test/boost/test_install.cpp
@@ -0,0 +1,13 @@
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+}
