8c28da31cd49a30b3596a0f8706ed9e581d567d4
kgryte <kgryte@gmail.com>
2016-12-16 17:31:57 -0800

    Add recipes for installing Boost

--- tools/make/lib/deps/boost.mk
+++ tools/make/lib/deps/boost.mk
@@ -1,6 +1,9 @@
 
 # VARIABLES #
 
+# Define the command for recursively creating directories (WARNING: portability issues on some systems!):
+MKDIR_RECURSIVE ?= mkdir -p
+
 # Define the path to an executable for downloading a remote resource:
 DEPS_DOWNLOAD_BIN ?= $(TOOLS_DIR)/scripts/download
 
@@ -16,27 +19,77 @@ DEPS_BOOST_URL ?= https://sourceforge.net/projects/boost/files/boost/$(DEPS_BOOS
 # Determine the basename for the Boost download:
 deps_boost_basename := $(notdir $(DEPS_BOOST_URL))
 
-# Define the output filepath when downloading Boost:
-DEPS_BOOST_DOWNLOAD_OUT ?= $(DEPS_DIR)/$(deps_boost_basename)
+# Define the output path when downloading Boost:
+DEPS_BOOST_DOWNLOAD_OUT ?= $(DEPS_TMP_DIR)/$(deps_boost_basename)
+
+# Define the output path when building Boost:
+DEPS_BOOST_BUILD_OUT ?= $(DEPS_BUILD_DIR)/boost_$(deps_boost_version_slug)
 
 
 # TARGETS #
 
-# Downloads Boost.
+# Download.
 #
-# This target downloads Boost to the `build` directory.
+# This target downloads a Boost distribution.
 
-deps-download-boost: $(DEPS_DIR)
+$(DEPS_BOOST_DOWNLOAD_OUT): $(DEPS_TMP_DIR)
+	$(QUIET) echo 'Downloading Boost...' >&2
 	$(QUIET) $(DEPS_DOWNLOAD_BIN) $(DEPS_BOOST_URL) $(DEPS_BOOST_DOWNLOAD_OUT)
+
+
+# Extract.
+#
+# This target extracts a gzipped tar archive.
+#
+# TODO: verify checksum before extracting.
+
+$(DEPS_BOOST_BUILD_OUT): $(DEPS_BOOST_DOWNLOAD_OUT) $(DEPS_BUILD_DIR)
+	$(QUIET) echo 'Extracting Boost...' >&2
+	$(QUIET) tar -zxf $(DEPS_BOOST_DOWNLOAD_OUT) -C $(DEPS_BUILD_DIR)
+
+
+# Download Boost.
+#
+# This target downloads a Boost distribution.
+
+deps-download-boost: $(DEPS_BOOST_DOWNLOAD_OUT)
 
 .PHONY: deps-download-boost
 
 
-# Extracts Boost.
+# Extract Boost.
 #
-# This targets extracts a Boost download.
+# This target extracts a Boost download.
 
-deps-extract-boost: $(DEPS_BOOST_DOWNLOAD_OUT)
-	$(QUIET)
+deps-extract-boost: $(DEPS_BOOST_BUILD_OUT)
 
 .PHONY: deps-extract-boost
+
+
+# Test install.
+#
+# This target tests a Boost installation.
+
+deps-test-boost: $(DEPS_BOOST_BUILD_OUT)
+
+
+.PHONY: deps-test-boost
+
+
+# Install Boost.
+#
+# This target installs Boost.
+
+deps-install-boost: deps-download-boost deps-extract-boost
+
+.PHONY: deps-install-boost
+
+
+# Clean Boost.
+#
+# This target removes a Boost distribution (but does not remove a Boost download if one exists).
+
+clean-deps-boost:
+	$(QUIET) $(DELETE) $(DELETE_FLAGS) $(DEPS_BOOST_BUILD_OUT)
+
+.PHONY: clean-deps-boost
--- tools/make/lib/deps/deps.mk
+++ tools/make/lib/deps/deps.mk
@@ -1,5 +1,63 @@
 
+# VARIABLES #
+
+# Define the command for recursively creating directories (WARNING: portability issues on some systems!):
+MKDIR_RECURSIVE ?= mkdir -p
+
+# Define the path for saving dependency downloads:
+DEPS_TMP_DIR ?= $(DEPS_DIR)/tmp
+
+# Define the path for building dependencies:
+DEPS_BUILD_DIR ?= $(DEPS_DIR)/build
+
+
 # DEPENDENCIES #
 
 include $(TOOLS_MAKE_LIB_DIR)/deps/boost.mk
 
+
+# TARGETS #
+
+# Create directory for temporary files.
+#
+# This target creates a directory for storing vendor dependency downloads.
+
+$(DEPS_TMP_DIR):
+	$(QUIET) $(MKDIR_RECURSIVE) $(DEPS_TMP_DIR)
+
+
+# Create directory for dependency builds.
+#
+# This target creates a directory for storing vendor dependencies.
+
+$(DEPS_BUILD_DIR):
+	$(QUIET) $(MKDIR_RECURSIVE) $(DEPS_BUILD_DIR)
+
+
+# Clean vendor dependencies.
+#
+# This target removes vendor dependencies.
+
+clean-deps: clean-deps-downloads clean-deps-build
+
+.PHONY: clean-deps
+
+
+# Clean builds.
+#
+# This target removes vendor dependency builds.
+
+clean-deps-build:
+	$(QUIET) $(DELETE) $(DELETE_FLAGS) $(DEPS_BUILD_DIR)
+
+.PHONY: clean-deps-build
+
+
+# Clean downloads.
+#
+# This target removes vendor dependency downloads.
+
+clean-deps-downloads:
+	$(QUIET) $(DELETE) $(DELETE_FLAGS) $(DEPS_TMP_DIR)
+
+.PHONY: clean-deps-downloads
