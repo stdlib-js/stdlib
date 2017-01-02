d038ade5b51a9953e1852b0570a5c459f7aff670
kgryte <kgryte@gmail.com>
2017-01-02 09:57:23 -0800

    Add overrides

--- etc/eslint/.eslintrc.tests.js
+++ etc/eslint/.eslintrc.tests.js
@@ -16,6 +16,13 @@ var defaults = require( './.eslintrc.js' );
 var eslint = copy( defaults );
 
 /**
+* Allow empty functions.
+*
+* @private
+*/
+eslint.rules[ 'no-empty-function' ] = 'off';
+
+/**
 * Warn when using `String`, `Number`, and `Boolean` in place of primitives.
 *
 * @private
@@ -23,11 +30,87 @@ var eslint = copy( defaults );
 eslint.rules[ 'no-new-wrappers' ] = 'warn';
 
 /**
+* Override the limit for source code lines in a file.
+*
+* @private
+*/
+eslint.rules[ 'max-lines' ] = [ 'warn', {
+	'max': 1000,
+	'skipBlankLines': true,
+	'skipComments': true
+}];
+
+/**
+* Override list of restricted syntax.
+*
+* @private
+*/
+eslint.rules[ 'no-restricted-syntax' ] = [ 'error',
+	'ArrowFunctionExpression',
+	'ClassBody',
+	'ClassDeclaration',
+	'ClassExpression',
+	'DebuggerStatement',
+	'ExperimentalRestProperty',
+	'ExperimentalSpreadProperty',
+	// 'FunctionExpression',
+	'LabeledStatement',
+	'RestElement',
+	'SpreadElement',
+	'TaggedTemplateExpression',
+	'TemplateElement',
+	'TemplateLiteral',
+	'WithStatement',
+	'YieldExpression',
+	'JSXIdentifier',
+	'JSXNamespacedName',
+	'JSXMemberExpression',
+	'JSXEmptyExpression',
+	'JSXExpressionContainer',
+	'JSXElement',
+	'JSXClosingElement',
+	'JSXOpeningElement',
+	'JSXAttribute',
+	'JSXSpreadAttribute',
+	'JSXText',
+	'ExportDefaultDeclaration',
+	'ExportNamedDeclaration',
+	'ExportAllDeclaration',
+	'ExportSpecifier',
+	'ImportDeclaration',
+	'ImportSpecifier',
+	'ImportDefaultSpecifier',
+	'ImportNamespaceSpecifier'
+];
+
+/**
+* Allow one property objects to be expressed on the same line.
+*
+* @private
+* @example
+* // Okay...
+* var obj = { 'foo': 'bar' };
+*/
+eslint.rules[ 'object-curly-newline' ] = [ 'error', {
+	'ObjectExpression': {
+		'minProperties': 2
+	},
+	'ObjectPattern': 'never'
+}];
+
+/**
 * Do not require JSDoc comments.
 *
 * @private
 */
 eslint.rules[ 'require-jsdoc' ] = 'off';
+
+/**
+* Allow the use of `undefined`.
+*
+* @private
+*/
+eslint.rules[ 'no-undefined' ] = 'off';
 
 
 // EXPORTS //
