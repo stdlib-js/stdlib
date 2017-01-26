04ce2fb19766cccb36336555fdf5213ba892dd08
kgryte <kgryte@gmail.com>
2017-01-26 05:23:19 -0800

    Add recipe to list add-ons

---
+++ tools/make/lib/ls/pkgs_addons.mk
@@ -0,0 +1,26 @@
+
+# VARIABLES #
+
+# Define the command for `node`:
+NODE ?= node
+
+# Define the root directory from which to search for packages:
+PACKAGES_DIR ?= $(ROOT_DIR)/lib/node_modules
+
+# Define the path of the executable:
+LIST_PACKAGE_ADDONS ?= $(TOOLS_DIR)/pkgs/addons/bin/cli
+
+# Define the command flags:
+LIST_PACKAGE_ADDONS_FLAGS ?=
+
+
+# TARGETS #
+
+# List all add-ons.
+#
+# This target prints a list of all add-ons.
+
+list-pkgs-addons: $(LIST_PACKAGE_ADDONS) $(NODE_MODULES)
+	$(QUIET) $(NODE) $(LIST_PACKAGE_ADDONS) $(LIST_PACKAGE_ADDONS_FLAGS) $(PACKAGES_DIR)
+
+.PHONY: list-pkgs-addons
