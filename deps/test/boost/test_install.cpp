20bfb2c6c8c3d43b6b74e0819381c51964fd4c6d
kgryte <kgryte@gmail.com>
2017-02-15 14:29:37 -0800

    Update formatting

--- deps/test/boost/test_install.cpp
+++ deps/test/boost/test_install.cpp
@@ -8,6 +8,5 @@ int main()
     using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
 
-    std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+    std::for_each( in(std::cin), in(), std::cout << (_1 * 3) << " " );
 }
