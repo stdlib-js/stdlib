93100c9938e95d55766fb75687edbd476a2ac1ab
kgryte <kgryte@gmail.com>
2017-03-02 13:32:24 -0800

    Update benchmark lint config

--- etc/eslint/.eslintrc.benchmarks.js
+++ etc/eslint/.eslintrc.benchmarks.js
@@ -23,6 +23,60 @@ var eslint = copy( defaults );
 eslint.rules[ 'no-new-wrappers' ] = 'warn';
 
 /**
+* Override the limit for source code lines in a file.
+*
+* @private
+*/
+eslint.rules[ 'max-lines' ] = [ 'warn', {
+	'max': 1000,
+	'skipBlankLines': true,
+	'skipComments': true
+}];
+
+/**
+* Override list of restricted syntax.
+*
+* @private
+*/
+eslint.rules[ 'no-restricted-syntax' ] = [ 'error',
+	'ArrowFunctionExpression',
+	'ClassBody',
+	'ClassDeclaration',
+	'ClassExpression',
+	'DebuggerStatement',
+	'ExperimentalRestProperty',
+	'ExperimentalSpreadProperty',
+	// 'FunctionExpression',
+	'LabeledStatement',
+	'RestElement',
+	'SpreadElement',
+	'TaggedTemplateExpression',
+	'TemplateElement',
+	'TemplateLiteral',
+	'WithStatement',
+	'YieldExpression',
+	'JSXIdentifier',
+	'JSXNamespacedName',
+	'JSXMemberExpression',
+	'JSXEmptyExpression',
+	'JSXExpressionContainer',
+	'JSXElement',
+	'JSXClosingElement',
+	'JSXOpeningElement',
+	'JSXAttribute',
+	'JSXSpreadAttribute',
+	'JSXText',
+	'ExportDefaultDeclaration',
+	'ExportNamedDeclaration',
+	'ExportAllDeclaration',
+	'ExportSpecifier',
+	'ImportDeclaration',
+	'ImportSpecifier',
+	'ImportDefaultSpecifier',
+	'ImportNamespaceSpecifier'
+];
+
+/**
 * Do not require JSDoc comments.
 *
 * @private
