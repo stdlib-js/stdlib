23bd9c6ffd8699c14e40cd1a2611b1c2e9219f73
kgryte <kgryte@gmail.com>
2017-03-13 14:48:15 -0700

    Add validation snippet

---
+++ tools/snippets/lib/validate.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// MODULES //
+
+var isObject = require( '@stdlib/utils/is-plain-object' );
+var hasOwnProp = require( '@stdlib/utils/has-own-property' );
+
+
+// MAIN //
+
+/**
+* Validates function options.
+*
+* @private
+* @param {Object} opts - destination object
+* @param {Options} options - function options
+* @returns {(Error|null)} null or an error object
+*
+* @example
+* var opts = {};
+* var options = {};
+* var err = validate( opts, options );
+* if ( err ) {
+*     throw err;
+* }
+*/
+function validate( opts, options ) {
+	if ( !isObject( options ) ) {
+		return new TypeError( 'invalid input argument. Options must be an object. Value: `' + options + '`.' );
+	}
+	if ( hasOwnProp( options, 'TODO' ) ) {
+		opts.TODO = options.TODO;
+		if ( !isObject( opts.TODO ) ) {
+			return new TypeError( 'invalid option. `TODO` option must be a TODO. Option: `' + opts.TODO + '`.' );
+		}
+	}
+	return null;
+} // end FUNCTION validate()
+
+
+// EXPORTS //
+
+module.exports = validate;
