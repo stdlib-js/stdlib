d1211596cfd7b252604b6a830ea80ff18cc016ce
kgryte <kgryte@gmail.com>
2017-05-19 12:29:44 -0700

    Add support for linting Python

--- etc/.makie.js
+++ etc/.makie.js
@@ -28,6 +28,7 @@ plugins[ 'benchmark' ] = path.join( prefix, 'makie-benchmark' );
 plugins[ 'benchmark-lang' ] = path.join( prefix, 'makie-benchmark-lang' );
 plugins[ 'lint-javascript' ] = path.join( prefix, 'makie-lint-javascript' );
 plugins[ 'lint-markdown' ] = path.join( prefix, 'makie-lint-markdown' );
+plugins[ 'lint-python' ] = path.join( prefix, 'makie-lint-python' );
 plugins[ 'complexity' ] = path.join( prefix, 'makie-complexity' );
 plugins[ 'view-complexity' ] = path.join( prefix, 'makie-view-complexity' );
 plugins[ 'list-pkgs' ] = path.join( prefix, 'makie-list-pkgs' );
