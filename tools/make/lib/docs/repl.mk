5939bf581ddb3f1f710576b553e29529d318807d
kgryte <kgryte@gmail.com>
2017-06-28 08:31:11 -0700

    Refactor REPL recipes into separate targets

--- tools/make/lib/docs/repl.mk
+++ tools/make/lib/docs/repl.mk
@@ -2,16 +2,38 @@
 # VARIABLES #
 
 # Define the path to the executable for generating REPL help docs:
-REPL_HELP ?= $(SRC_DIR)/@stdlib/namespace/scripts/build.js
+REPL_HELP ?= $(SRC_DIR)/@stdlib/repl/help/scripts/build.js
+
+# Define the path to the executable for aggregating REPL examples:
+REPL_EXAMPLES ?= $(SRC_DIR)/@stdlib/repl/example/scripts/build.js
 
 
 # TARGETS #
+
+# Generate REPL docs.
+#
+# This target generates REPL documentation.
+
+repl-docs: repl-help repl-examples
+
+.PHONY: repl-docs
+
 
 # Generate REPL help.
 #
 # This target generates REPL help documentation.
 
-repl-help: $(NODE_MODULES)
+repl-help: $(NODE_MODULES) $(REPL_HELP)
 	$(QUIET) $(NODE) $(REPL_HELP)
 
 .PHONY: repl-help
+
+
+# Aggregate REPL examples.
+#
+# This target aggregates REPL examples.
+
+repl-examples: $(NODE_MODULES) $(REPL_EXAMPLES)
+	$(QUIET) $(NODE) $(REPL_EXAMPLES)
+
+.PHONY: repl-examples
