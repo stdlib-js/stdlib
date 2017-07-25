212b95f967f40cd5c7160d1fa27721204b66ae6f
kgryte <kgryte@gmail.com>
2017-07-25 00:42:17 -0700

    Add recipe to resolving a link id

--- tools/make/lib/links/links.mk
+++ tools/make/lib/links/links.mk
@@ -1,11 +1,18 @@
 
 # VARIABLES #
 
-# Define the command for returning a citation reference:
+# Define the command for inserting a link into the database:
 LINKS_INSERT ?= $(NODE) $(TOOLS_DIR)/links/create/bin/cli
 
 # Define the command-line options to be used when executing the command:
 LINKS_INSERT_FLAGS ?= \
+	--database $(ROOT_DIR)/docs/links/database.json
+
+# Define the command for resolving an id from a URI:
+LINKS_URI2ID ?= $(NODE) $(TOOLS_DIR)/links/uri2id/bin/cli
+
+# Define the command-line options to be used when executing the command:
+LINKS_URI2ID_FLAGS ?= \
 	--database $(ROOT_DIR)/docs/links/database.json
 
 
@@ -19,3 +26,12 @@ links-insert: $(NODE_MODULES)
 	$(QUIET) $(LINKS_INSERT) $(LINKS_INSERT_FLAGS)
 
 .PHONY: links-insert
+
+# Resolve a link id.
+#
+# This target resolves a database link identifier from a URI.
+
+links-uri2id: $(NODE_MODULES)
+	$(QUIET) $(LINKS_URI2ID) $(LINKS_URI2ID_FLAGS)
+
+.PHONY: links-uri2id
