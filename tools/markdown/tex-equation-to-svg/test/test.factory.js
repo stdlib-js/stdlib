969717d306d00d726750b6b420648a554259c807
kgryte <kgryte@gmail.com>
2017-07-30 11:55:35 -0700

    Fix tests

--- tools/markdown/tex-equation-to-svg/test/test.factory.js
+++ tools/markdown/tex-equation-to-svg/test/test.factory.js
@@ -10,11 +10,82 @@ var factory = require( './../lib/factory.js' );
 // TESTS //
 
 tape( 'main export is a function', function test( t ) {
-	t.equal( typeof factory, 'function', 'main export is a function' );
+	t.strictEqual( typeof factory, 'function', 'main export is a function' );
 	t.end();
 });
 
-tape( 'the function throws an error if provided a callback argument which is not a function', function test( t ) {
+tape( 'the function throws an error if provided an options argument which is not an object', function test( t ) {
+	var values;
+	var i;
+
+	values = [
+		'5',
+		5,
+		null,
+		NaN,
+		true,
+		false,
+		void 0,
+		[],
+		function noop() {}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			factory( value );
+		};
+	}
+});
+
+tape( 'the function throws an error if provided an invalid option', function test( t ) {
+	t.throws( foo, TypeError, 'throws type error' );
+	t.end();
+
+	function foo() {
+		factory({
+			'ex': null
+		});
+	}
+});
+
+tape( 'the function returns a function which throws an error if not provided an input string', function test( t ) {
+	var tex2svg;
+	var values;
+	var i;
+
+	values = [
+		5,
+		null,
+		NaN,
+		true,
+		false,
+		void 0,
+		[],
+		{},
+		function noop() {}
+	];
+
+	tex2svg = factory( {} );
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			tex2svg( value, noop );
+		};
+	}
+});
+
+tape( 'the function returns a function which throws an error if provided a callback argument which is not a function', function test( t ) {
+	var tex2svg;
 	var values;
 	var i;
 
@@ -29,32 +100,7 @@ tape( 'the function throws an error if provided a callback argument which is not
 		{}
 	];
 
-	for ( i = 0; i < values.length; i++ ) {
-		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
-	}
-	t.end();
-
-	function badValue( value ) {
-		return function badValue() {
-			factory( {}, value );
-		};
-	}
-});
-
-tape( 'the function throws an error if provided an options argument which is not an object', function test( t ) {
-	var values;
-	var i;
-
-	values = [
-		'5',
-		5,
-		null,
-		NaN,
-		true,
-		void 0,
-		[],
-		function noop() {}
-	];
+	tex2svg = factory( {} );
 
 	for ( i = 0; i < values.length; i++ ) {
 		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
@@ -63,57 +109,16 @@ tape( 'the function throws an error if provided an options argument which is not
 
 	function badValue( value ) {
 		return function badValue() {
-			factory( value, noop );
-		};
-	}
-});
-
-tape( 'the function throws an error if provided an invalid option', function test( t ) {
-	t.throws( foo, TypeError, 'throws type error' );
-	t.end();
-
-	function foo() {
-		factory({
-			'ex': null
-		}, noop );
-	}
-});
-
-tape( 'the function returns a function which throws an error if not provided an input string', function test( t ) {
-	var tex2svg;
-	var values;
-	var i;
-
-	values = [
-		5,
-		null,
-		NaN,
-		true,
-		void 0,
-		[],
-		{},
-		function noop() {}
-	];
-
-	tex2svg = factory( {}, noop );
-
-	for ( i = 0; i < values.length; i++ ) {
-		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
-	}
-	t.end();
-
-	function badValue( value ) {
-		return function badValue() {
-			tex2svg( value );
+			tex2svg( 'y = mx + b', value );
 		};
 	}
 });
 
 tape( 'the function returns a function which returns any errors to a provided callback', function test( t ) {
-	var tex2svg = factory( {}, clbk );
+	var tex2svg = factory( {} );
 
 	// Non-existent LaTeX command...
-	tex2svg( '\\infinity' );
+	tex2svg( '\\infinity', clbk );
 
 	function clbk( error ) {
 		t.ok( error, 'returns an error' );
@@ -122,16 +127,16 @@ tape( 'the function returns a function which returns any errors to a provided ca
 });
 
 tape( 'the function returns a function which converts an input string to an SVG', function test( t ) {
-	var tex2svg = factory( {}, clbk );
-	tex2svg( 'y = mx + b' );
+	var tex2svg = factory( {} );
+	tex2svg( 'y = mx + b', clbk );
 
 	function clbk( error, svg ) {
 		if ( error ) {
 			t.ok( false, error.message );
 		}
-		t.equal( typeof svg, 'string', 'returns a string' );
-		t.equal( svg.substring( 0, 4 ), '<svg', 'svg tag' );
-		t.equal( svg.substring( svg.length-6 ), '</svg>', 'closing svg tag' );
+		t.strictEqual( typeof svg, 'string', 'returns a string' );
+		t.strictEqual( svg.substring( 0, 4 ), '<svg', 'svg tag' );
+		t.strictEqual( svg.substring( svg.length-6 ), '</svg>', 'closing svg tag' );
 		t.end();
 	}
 });
@@ -146,16 +151,16 @@ tape( 'the function returns a function which converts an input string to an SVG 
 		'inline': true,
 		'linebreaks': false
 	};
-	tex2svg = factory( opts, clbk );
-	tex2svg( 'y = mx + b' );
+	tex2svg = factory( opts );
+	tex2svg( 'y = mx + b', clbk );
 
 	function clbk( error, svg ) {
 		if ( error ) {
 			t.ok( false, error.message );
 		}
-		t.equal( typeof svg, 'string', 'returns a string' );
-		t.equal( svg.substring( 0, 4 ), '<svg', 'svg tag' );
-		t.equal( svg.substring( svg.length-6 ), '</svg>', 'closing svg tag' );
+		t.strictEqual( typeof svg, 'string', 'returns a string' );
+		t.strictEqual( svg.substring( 0, 4 ), '<svg', 'svg tag' );
+		t.strictEqual( svg.substring( svg.length-6 ), '</svg>', 'closing svg tag' );
 		t.end();
 	}
 });
