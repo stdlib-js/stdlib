5c53c78310e7c2cb8b3c166555d0fb81a3723f34
Planeshifter <pburckhardt@outlook.com>
2017-07-30 02:30:31 -0400

    Move main entry point to separate file

--- tools/markdown/rawgit-url/lib/index.js
+++ tools/markdown/rawgit-url/lib/index.js
@@ -1,32 +1,9 @@
 'use strict';
 
-// MODULES //
-
-var copy = require( '@stdlib/utils/copy' );
-var defaults = require( './defaults.json' );
-var validate = require( './validate.js' );
-var url = require( './url.js' );
-
-
-// VARIABLES //
-
-// Match any string ending with `/`:
-var DANGLING_SLASH = /\/$/;
-
-// Match any string beginning with `./`:
-var FILEPATH_PREFIX = /^\.\//;
-
-
-// MAIN //
-
 /**
-* Returns a RawGit URL for a file hosted in a public Github repository.
+* Return a RawGit URL for a file hosted in a public Github repository.
 *
-* @param {Object} options - function options
-* @param {string} options.slug - public Github repository slug
-* @param {string} options.file - filepath
-* @param {boolean} [options.cdn=true] - boolean indicating whether to return a CDN URL
-* @returns {string} RawGit URL
+* @module @stdlib/tools/markdown/rawgit-url
 *
 * @example
 * var out = rawgit({
@@ -35,20 +12,10 @@ var FILEPATH_PREFIX = /^\.\//;
 * });
 * // returns 'https://cdn.rawgit.com/stdlib-js/stdlib/develop/README.md'
 */
-function rawgit( options ) {
-	var opts;
-	var err;
 
-	opts = copy( defaults );
-	err = validate( opts, options );
-	if ( err ) {
-		throw err;
-	}
-	opts.slug = opts.slug.replace( DANGLING_SLASH, '' );
-	opts.file = opts.file.replace( FILEPATH_PREFIX, '' );
+// MODULES //
 
-	return url( opts );
-} // end FUNCTION rawgit()
+var rawgit = require( './main.js' );
 
 
 // EXPORTS //
