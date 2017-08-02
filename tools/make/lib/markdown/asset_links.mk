f0347efcae895914cc56471ae73470511f9c2f81
kgryte <kgryte@gmail.com>
2017-08-02 03:37:13 +0200

    Add recipe to generate an asset link

---
+++ tools/make/lib/markdown/asset_links.mk
@@ -0,0 +1,27 @@
+
+# VARIABLES #
+
+# Define the asset file path relative to the project root:
+MARKDOWN_ASSET_PATH ?=
+
+# Get the current Git hash:
+GIT_COMMIT_HASH ?= $(shell $(GIT) rev-parse HEAD)
+
+# Define the path to utility for generating asset links:
+RAWGIT_URL ?= $(TOOLS_DIR)/markdown/rawgit-url/bin/cli
+
+# Define command-line flags to be used when invoking the utility:
+RAWGIT_URL_FLAGS ?= \
+	--slug stdlib-js/stdlib/$(GIT_COMMIT_HASH)
+
+
+# TARGETS #
+
+# Generate an asset link.
+#
+# This target generates an asset link based on the current Git hash for inclusion in a project Markdown file.
+
+markdown-asset-link: $(NODE_MODULES)
+	$(QUIET) $(RAWGIT_URL) $(RAWGIT_URL_FLAGS) $(MARKDOWN_ASSET_PATH)
+
+.PHONY: markdown-asset-link
