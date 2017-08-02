82a45443046aed1c32cd8bdc486c8ed130d86bd1
kgryte <kgryte@gmail.com>
2017-08-02 11:43:32 +0200

    Add makie plugin to generate a base URL for an asset

--- etc/.makie.js
+++ etc/.makie.js
@@ -17,24 +17,25 @@ var plugins = {};
 
 config.plugins = plugins;
 
-plugins[ 'repl' ] = path.join( prefix, 'makie-repl' );
-plugins[ 'notes' ] = path.join( prefix, 'makie-notes' );
-plugins[ 'examples' ] = path.join( prefix, 'makie-examples' );
-plugins[ 'test' ] = path.join( prefix, 'makie-test' );
-plugins[ 'test-summary' ] = path.join( prefix, 'makie-test-summary' );
-plugins[ 'test-cov' ] = path.join( prefix, 'makie-test-cov' );
-plugins[ 'view-cov' ] = path.join( prefix, 'makie-view-cov' );
 plugins[ 'benchmark' ] = path.join( prefix, 'makie-benchmark' );
 plugins[ 'benchmark-lang' ] = path.join( prefix, 'makie-benchmark-lang' );
+plugins[ 'complexity' ] = path.join( prefix, 'makie-complexity' );
+plugins[ 'examples' ] = path.join( prefix, 'makie-examples' );
 plugins[ 'lint-javascript' ] = path.join( prefix, 'makie-lint-javascript' );
 plugins[ 'lint-markdown' ] = path.join( prefix, 'makie-lint-markdown' );
 plugins[ 'lint-python' ] = path.join( prefix, 'makie-lint-python' );
 plugins[ 'lint-r' ] = path.join( prefix, 'makie-lint-r' );
-plugins[ 'complexity' ] = path.join( prefix, 'makie-complexity' );
-plugins[ 'view-complexity' ] = path.join( prefix, 'makie-view-complexity' );
 plugins[ 'list-pkgs' ] = path.join( prefix, 'makie-list-pkgs' );
 plugins[ 'list-pkgs-names' ] = path.join( prefix, 'makie-list-pkgs-names' );
+plugins[ 'markdown-asset-link' ] = path.join( prefix, 'makie-markdown-asset-link' );
+plugins[ 'notes' ] = path.join( prefix, 'makie-notes' );
+plugins[ 'repl' ] = path.join( prefix, 'makie-repl' );
 plugins[ 'stats-list-contributors' ] = path.join( prefix, 'makie-stats-list-contributors' );
+plugins[ 'test' ] = path.join( prefix, 'makie-test' );
+plugins[ 'test-summary' ] = path.join( prefix, 'makie-test-summary' );
+plugins[ 'test-cov' ] = path.join( prefix, 'makie-test-cov' );
+plugins[ 'view-complexity' ] = path.join( prefix, 'makie-view-complexity' );
+plugins[ 'view-cov' ] = path.join( prefix, 'makie-view-cov' );
 
 
 // EXPORTS //
