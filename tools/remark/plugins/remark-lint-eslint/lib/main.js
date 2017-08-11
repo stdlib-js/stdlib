260625ed2d59d88a0333bfa3290995ca425a874e
kgryte <kgryte@gmail.com>
2017-08-11 14:21:46 +0200

    Move function to separate file

--- tools/remark/plugins/remark-lint-eslint/lib/main.js
+++ tools/remark/plugins/remark-lint-eslint/lib/main.js
@@ -8,41 +8,7 @@ var visit = require( 'unist-util-visit' );
 var Engine = require( 'eslint' ).CLIEngine;
 var cwd = require( '@stdlib/utils/cwd' );
 var hasOwnProp = require( '@stdlib/assert/has-own-property' );
-var startsWith = require( '@stdlib/string/starts-with' );
-var endsWith = require( '@stdlib/string/ends-with' );
-var trim = require( '@stdlib/string/trim' );
-
-
-// VARIABLES //
-
-var COMMENT_START = '<!--';
-var COMMENT_END = '-->';
-var ESLINT_PREFIX = 'eslint';
-
-
-// FUNCTIONS //
-
-/**
-* Converts an HTML comment containing ESLint configuration to a JavaScript comment.
-*
-* @private
-* @param {string} html - text content of an HTML AST node
-* @returns {(string|null)} null or a JavaScript comment
-*/
-function transformHTML( html ) {
-	if (
-		!startsWith( html, COMMENT_START ) ||
-		!endsWith( html, COMMENT_END )
-	) {
-		return null;
-	}
-	html = html.slice( COMMENT_START.length, html.length-COMMENT_END.length );
-	html = trim( html );
-	if ( !startsWith( html, ESLINT_PREFIX ) ) {
-		return null;
-	}
-	return '/* ' + html + ' */';
-} // end FUNCTION transformHTML()
+var transformHTML = require( './transform_html.js' );
 
 
 // MAIN //
