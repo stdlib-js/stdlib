cb71d41b686ca812f4de664887d5f3f2774f9328
kgryte <kgryte@gmail.com>
2017-08-17 10:58:08 -0700

    Add recipes for running Fortran benchmarks

---
+++ tools/make/lib/benchmark/fortran.mk
@@ -0,0 +1,35 @@
+
+# TARGETS #
+
+# Run Fortran benchmarks.
+#
+# This target runs a list of Fortran benchmarks in sequential order.
+
+benchmark-fortran:
+	$(QUIET) $(FIND_FORTRAN_BENCHMARKS_CMD) | grep '^[\/]\|^[a-zA-Z]:[/\]' | while read -r file; do \
+		echo ""; \
+		echo "Running benchmark: $$file"; \
+		cd `dirname $$file` && \
+		$(MAKE) clean && \
+		$(MAKE) && \
+		$(MAKE) run || exit 1; \
+	done
+
+.PHONY: benchmark-fortran
+
+
+# Run Fortran benchmarks.
+#
+# This target runs a specified list of Fortran benchmarks in sequential order.
+
+benchmark-fortran-files:
+	$(QUIET) for file in $(FILES); do \
+		echo ""; \
+		echo "Running benchmark: $$file"; \
+		cd `dirname $$file` && \
+		$(MAKE) clean && \
+		$(MAKE) && \
+		$(MAKE) run || exit 1; \
+	done
+
+.PHONY: benchmark-fortran-files
