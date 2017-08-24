55ebd848a5ec7ddf4db6b54bffe2f3ffa15f6729
kgryte <kgryte@gmail.com>
2017-08-24 12:42:47 -0700

    Rename environment variables

--- tools/make/lib/markdown/equations.mk
+++ tools/make/lib/markdown/equations.mk
@@ -26,10 +26,10 @@ EQUATION_RESOURCES_PATH ?= ./docs/img/
 REMARK ?= $(BIN_DIR)/remark
 
 # Define the path to the remark configuration file:
-REMARK_CONF ?= $(CONFIG_DIR)/remark/.remarkrc.js
+REMARK_EQUATIONS_CONF ?= $(CONFIG_DIR)/remark/.remarkrc.js
 
 # Define the path to the remark ignore file:
-REMARK_IGNORE ?= $(CONFIG_DIR)/remark/.remarkignore
+REMARK_EQUATIONS_IGNORE ?= $(CONFIG_DIR)/remark/.remarkignore
 
 # Define the path to the local remark plugins directory:
 REMARK_LOCAL_PLUGINS_DIR ?= $(TOOLS_DIR)/remark/plugins
@@ -52,8 +52,8 @@ REMARK_EQUATION_SRC_URLS_PLUGIN_FLAGS ?= --use $(REMARK_EQUATION_SRC_URLS_PLUGIN
 # Define command-line options when invoking the remark executable:
 REMARK_FLAGS ?= \
 	--ext $(MARKDOWN_FILENAME_EXT) \
-	--rc-path $(REMARK_CONF) \
-	--ignore-path $(REMARK_IGNORE)
+	--rc-path $(REMARK_EQUATIONS_CONF) \
+	--ignore-path $(REMARK_EQUATIONS_IGNORE)
 
 # Define the remark output option:
 REMARK_OUTPUT_FLAG ?= --output
