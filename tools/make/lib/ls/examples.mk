2ef7965e23d3fc01994304611ae5e28b9d79d685
kgryte <kgryte@gmail.com>
2017-08-25 13:03:37 -0700

    Update file search to account for project tools directory

--- tools/make/lib/ls/examples.mk
+++ tools/make/lib/ls/examples.mk
@@ -21,6 +21,7 @@ FIND_EXAMPLES_FLAGS ?= \
 	-not -path "$(ROOT_DIR)/.*" \
 	-not -path "$(NODE_MODULES)/*" \
 	-not -path "$(TOOLS_DIR)/*" \
+	-not -path "$(TOOLS_PKGS_DIR)/*" \
 	-not -path "$(BUILD_DIR)/*" \
 	-not -path "$(DEPS_DIR)/*" \
 	-not -path "$(REPORTS_DIR)/*" \
