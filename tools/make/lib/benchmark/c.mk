fa5a0a2eca26e1b157df622c54de854fbad4c09a
kgryte <kgryte@gmail.com>
2017-09-11 22:25:43 -0700

    Provide Cephes environment variable when compiling C benchmarks

--- tools/make/lib/benchmark/c.mk
+++ tools/make/lib/benchmark/c.mk
@@ -11,7 +11,7 @@ benchmark-c:
 		echo "Running benchmark: $$file"; \
 		cd `dirname $$file` && \
 		$(MAKE) clean && \
-		$(MAKE) && \
+		CEPHES=$(DEPS_CEPHES_BUILD_OUT) $(MAKE) && \
 		$(MAKE) run || exit 1; \
 	done
 
@@ -28,7 +28,7 @@ benchmark-c-files:
 		echo "Running benchmark: $$file"; \
 		cd `dirname $$file` && \
 		$(MAKE) clean && \
-		$(MAKE) && \
+		CEPHES=$(DEPS_CEPHES_BUILD_OUT) $(MAKE) && \
 		$(MAKE) run || exit 1; \
 	done
 
