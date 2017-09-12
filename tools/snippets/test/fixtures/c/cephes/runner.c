b6292cfabc2cda6235e307a984723bd6b5a3d0a9
kgryte <kgryte@gmail.com>
2017-09-11 23:01:43 -0700

    Add Cephes test fixture runner

---
+++ tools/snippets/test/fixtures/c/cephes/runner.c
@@ -0,0 +1,296 @@
+/**
+* Generate Cephes test fixtures.
+*
+* ## Notes
+*
+* -   Run this script from the directory in which fixtures should be written.
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/time.h>
+
+/**
+* Define prototypes for external functions.
+*/
+extern double TODO( double x );
+
+/**
+* Generates a linearly spaced numeric array of doubles.
+*
+* ## Notes
+*
+* -   Assumes that the output array has at least 2 elements.
+* -   Output array is guaranteed to include both the start and end values.
+*
+*
+* @param out    output array
+* @param len    array length
+* @param start  first value
+* @param end    last value
+*/
+void linspace_f64( double *out, const unsigned int len, const double start, const double end ) {
+	unsigned int i;
+	double incr;
+
+	incr = (end-start) / (len-1);
+	for ( i = 0; i < len-1; i++ ) {
+		out[ i ] = start + (incr*i);
+	}
+	out[ i ] = end;
+}
+
+/**
+* Generates a linearly spaced numeric array of integers.
+*
+* ## Notes
+*
+* -   Assumes that the output array has at least 2 elements.
+* -   Output array is guaranteed to include both the start and end values.
+*
+*
+* @param out    output array
+* @param len    array length
+* @param start  first value
+* @param end    last value
+*/
+void linspace_i32( int *out, const unsigned int len, const int start, const int end ) {
+	unsigned int i;
+	int incr;
+
+	incr = (end-start) / (len-1);
+	for ( i = 0; i < len-1; i++ ) {
+		out[ i ] = start + (incr*i);
+	}
+	out[ i ] = end;
+}
+
+/**
+* Generates a random double on the interval [0,1].
+*
+* @return random double
+*/
+double rand_double() {
+	int r = rand();
+	return (double)r / ( (double)RAND_MAX + 1.0 );
+}
+
+/**
+* Generates an array of pseudorandom doubles drawn from a uniform distribution.
+*
+* @param out  output array
+* @param len  array length
+* @param a    lower bound (inclusive)
+* @param b    upper bound (exclusive)
+*/
+void rand_array_f64( double *out, const unsigned int len, const double a, const double b ) {
+	unsigned int i;
+	double delta;
+
+	delta = b - a;
+
+	for ( i = 0; i < len; i++ ) {
+		out[ i ] = a + ( rand_double()*delta );
+	}
+}
+
+/**
+* Generates an array of pseudorandom integers drawn from a uniform distribution.
+*
+* ## Notes
+*
+* * WARNING: the method used here is not particularly robust, as some integer values may be sampled more frequently than others.
+*
+*
+* @param out  output array
+* @param len  array length
+* @param a    lower bound (inclusive)
+* @param b    upper bound (exclusive)
+*/
+void rand_array_i32( int *out, const unsigned int len, const int a, const int b ) {
+	unsigned int i;
+	double delta;
+	int r;
+
+	delta = (double)(b - a);
+
+	for ( i = 0; i < len; i++ ) {
+		r = (int)( delta * rand_double() ); // truncation
+		out[ i ] = a + r;
+	}
+}
+
+/**
+* Casts an array of integers to an array of doubles.
+*
+* @param out  output array
+* @param x    input array
+* @param len  array length
+*/
+void i32_to_f64( double *out, int *x, unsigned int len ) {
+	unsigned int i;
+
+	for ( i = 0; i < len; i++ ) {
+		out[ i ] = (double) x[ i ];
+	}
+}
+
+/**
+* Casts an array of doubles to an array of integers.
+*
+* @param out  output array
+* @param x    input array
+* @param len  array length
+*/
+void f64_to_i32( int *out, double *x, unsigned int len ) {
+	unsigned int i;
+
+	for ( i = 0; i < len; i++ ) {
+		out[ i ] = (int) x[ i ];
+	}
+}
+
+/**
+* Writes an array of doubles to a file as a series of comma-separated values.
+*
+* @param f    file to write to
+* @param x    array of doubles
+* @param len  array length
+*/
+void write_array_f64( FILE *f, const double *x, const unsigned int len ) {
+	unsigned int i;
+
+	for ( i = 0; i < len; i++ ) {
+		fprintf( f, "%.17g", x[ i ] );
+		if ( i < len-1 ) {
+			fprintf( f, "," );
+		}
+	}
+}
+
+/**
+* Writes an array of integers to a file as a series of comma-separated values.
+*
+* @param f    file to write to
+* @param x    array of integers
+* @param len  array length
+*/
+void write_array_i32( FILE *f, const int *x, const unsigned int len ) {
+	unsigned int i;
+
+	for ( i = 0; i < len; i++ ) {
+		fprintf( f, "%d", x[ i ] );
+		if ( i < len-1 ) {
+			fprintf( f, "," );
+		}
+	}
+}
+
+/**
+* Writes a named array of doubles to a file as JSON.
+*
+* @param f     file to write to
+* @param name  array name
+* @param x     data
+* @param len   array length
+*/
+void write_named_array_f64( FILE *f, const char *name, const double *x, const unsigned int len ) {
+	fprintf( f, "\"%s\":[", name );
+	write_array_f64( f, x, len );
+	fprintf( f, "]" );
+}
+
+/**
+* Writes a named array of integers to a file as JSON.
+*
+* @param f     file to write to
+* @param name  array name
+* @param x     data
+* @param len   array length
+*/
+void write_named_array_i32( FILE *f, const char *name, const int *x, const unsigned int len ) {
+	fprintf( f, "\"%s\":[", name );
+	write_array_i32( f, x, len );
+	fprintf( f, "]" );
+}
+
+/**
+* Writes data to a file as JSON.
+*
+* ## Notes
+*
+* -   This function SHOULD be tailored to the input data (e.g., input types, output types, number of arguments, etc) and may vary from use case to use case.
+*
+*
+* @param f    file to write to
+* @param x    domain
+* @param y    results
+* @param len  array length
+*/
+void write_data_as_json( FILE *f, const double *x, const double *y, const unsigned int len ) {
+	fprintf( f, "{" );
+	write_named_array_f64( f, "x", x, len );
+	fprintf( f, "," );
+	write_named_array_f64( f, "expected", y, len ); // TODO: update or erase this comment
+	fprintf( f, "}" );
+}
+
+/**
+* Generates test fixtures.
+*
+* @param x     domain
+* @param len   number of values in the domain
+* @param name  output filename
+*/
+void generate( double *x, const unsigned int len, const char *name ) {
+	unsigned int i;
+	double *y;
+	FILE *f;
+
+	// Allocate an output array:
+	y = (double*) malloc( len * sizeof(double) ); // TODO
+
+	// Generate fixture data:
+	for ( i = 0; i < len; i++ ) {
+		y[ i ] = 3.14; // TODO
+	}
+	// Open a new file:
+	f = fopen( name, "w" );
+	if ( f == NULL ) {
+		printf( "Error opening file.\n" );
+		exit( 1 );
+	}
+	// Write data as JSON...
+	write_data_as_json( f, x, y, len ); // TODO: tailor to fixture data
+
+	// Close the file:
+	fclose( f );
+
+	// Free allocated memory:
+	free( y );
+}
+
+/**
+* Main execution sequence.
+*/
+int main( void ) {
+	unsigned int len;
+	double *x;
+
+	// Define the array length:
+	len = 1000;
+
+	// Allocate an array:
+	x = (double*) malloc( len * sizeof(double) );
+
+	// Generate fixture data:
+	rand_array_f64( x, len, 0.0, 1.0 ); // TODO
+	generate( x, len, "TODO.json" );
+
+	// Free allocated memory:
+	free( x );
+
+	return 0;
+}
