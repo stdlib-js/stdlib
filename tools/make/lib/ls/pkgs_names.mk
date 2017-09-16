4e7a43124242fe2be259b564e68f09e6fef714da
kgryte <kgryte@gmail.com>
2017-09-16 00:50:26 -0700

    Update path

--- tools/make/lib/ls/pkgs_names.mk
+++ tools/make/lib/ls/pkgs_names.mk
@@ -5,7 +5,7 @@
 PACKAGES_DIR ?= $(ROOT_DIR)/lib/node_modules
 
 # Define the path of the executable:
-LIST_PACKAGE_NAMES ?= $(TOOLS_DIR)/pkgs/names/bin/cli
+LIST_PACKAGE_NAMES ?= $(TOOLS_PKGS_DIR)/pkgs/names/bin/cli
 
 # Define the command flags:
 LIST_PACKAGE_NAMES_FLAGS ?=
