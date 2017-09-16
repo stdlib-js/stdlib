a95748a416bbe690ec5cacc2a48d68f6bf984343
kgryte <kgryte@gmail.com>
2017-09-15 23:28:14 -0700

    Update path

--- tools/make/lib/ls/pkgs_addons.mk
+++ tools/make/lib/ls/pkgs_addons.mk
@@ -5,7 +5,7 @@
 PACKAGES_DIR ?= $(ROOT_DIR)/lib/node_modules
 
 # Define the path of the executable:
-LIST_PACKAGE_ADDONS ?= $(TOOLS_DIR)/pkgs/addons/bin/cli
+LIST_PACKAGE_ADDONS ?= $(TOOLS_PKGS_DIR)/pkgs/addons/bin/cli
 
 # Define the command flags:
 LIST_PACKAGE_ADDONS_FLAGS ?=
