f3dfaf0c25861f1c7351f0c3ba2d31c5907a0618
kgryte <kgryte@gmail.com>
2017-09-23 12:58:30 -0700

    Rename targets

--- tools/make/lib/node/npm.mk
+++ tools/make/lib/node/npm.mk
@@ -5,20 +5,20 @@
 #
 # This target runs tasks which should be completed before incrementing the project version.
 
-preversion: dedupe-node-modules
+pre-version: dedupe-node-modules
 
-.PHONY: preversion
+.PHONY: pre-version
 
 
 # Run post-version tasks.
 #
 # This target runs tasks which should be completed after incrementing the project version and committing version changes to the local repository.
 
-postversion:
+post-version:
 	$(QUIET) echo ''
 	$(QUIET) echo 'Incremented version and committed changes. If okay, run:'
 	$(QUIET) echo ''
 	$(QUIET) echo "  $$ git push origin $(shell $(GIT_BRANCH)) && git push origin --tags"
 	$(QUIET) echo ''
 
-.PHONY: postversion
+.PHONY: post-version
