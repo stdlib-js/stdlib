58dcab25cd427af520b96acac4777008679e5961
kgryte <kgryte@gmail.com>
2017-09-27 21:23:22 -0700

    Add recipe to list browser compatible packages

---
+++ tools/make/lib/ls/pkgs_browser_compatible.mk
@@ -0,0 +1,23 @@
+
+# VARIABLES #
+
+# Define the root directory from which to search for packages:
+PACKAGES_DIR ?= $(ROOT_DIR)/lib/node_modules
+
+# Define the path of the executable:
+LIST_BROWSER_COMPATIBLE_PACKAGE_NAMES ?= $(TOOLS_PKGS_DIR)/pkgs/browser-compatible/bin/cli
+
+# Define the command flags:
+LIST_BROWSER_COMPATIBLE_PACKAGE_NAMES_FLAGS ?=
+
+
+# TARGETS #
+
+# List browser compatible package names.
+#
+# This target prints a list of all packages which are compatible with browser environments.
+
+list-pkgs-browser-compatible: $(LIST_BROWSER_COMPATIBLE_PACKAGE_NAMES) $(NODE_MODULES)
+	$(QUIET) $(NODE) $(LIST_BROWSER_COMPATIBLE_PACKAGE_NAMES) $(LIST_BROWSER_COMPATIBLE_PACKAGE_NAMES_FLAGS) $(PACKAGES_DIR)
+
+.PHONY: list-pkgs-browser-compatible
