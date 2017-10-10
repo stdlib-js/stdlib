82b639d4030665ccedf1e01fcf25572a056006fa
kgryte <kgryte@gmail.com>
2017-10-09 21:51:01 -0700

    Add recipe to compute the number of bytes per package

--- tools/make/lib/stats/git.mk
+++ tools/make/lib/stats/git.mk
@@ -520,6 +520,17 @@ stats-bytes-per-file-type-per-pkg:
 .PHONY: stats-bytes-per-file-type-per-pkg
 
 
+# Compute bytes per package.
+#
+# This target computes number of bytes per package.
+
+stats-bytes-per-pkg:
+	$(QUIET) $(MAKE_EXECUTABLE) $(GIT_SCRIPTS_DIR)/bytes_per_pkg
+	$(QUIET) PACKAGES_FILTER=$(PACKAGES_FILTER) FILES_PATTERN=$(FILES_PATTERN) $(GIT_SCRIPTS_DIR)/bytes_per_pkg
+
+.PHONY: stats-bytes-per-pkg
+
+
 # Compute basic COCOMO 81.
 #
 # This target computes a basic constructive cost model (COCOMO 81).
