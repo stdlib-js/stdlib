a535bbb601038c14c223a7f27ea342d3869b5cc5
kgryte <kgryte@gmail.com>
2017-11-25 05:46:32 +0900

    Add source file snippet

---
+++ tools/snippets/src/todo.c
@@ -0,0 +1,16 @@
+/**
+* TODO.
+*/
+#include <math.h>
+#include "TODO.h"
+
+/**
+* TODO.
+*
+* @param x       number
+* @param y       number
+* @return        output value
+*/
+double stdlib_TODO( const double x, const double y ) {
+	return TODO;
+}
