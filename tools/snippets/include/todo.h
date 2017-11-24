615867d7d14e627e125a2402b4b23bde6af3e061
kgryte <kgryte@gmail.com>
2017-11-25 05:41:23 +0900

    Add header file snippet

---
+++ tools/snippets/include/todo.h
@@ -0,0 +1,23 @@
+/**
+* Header file containing function declarations for TODO (summary sentence).
+*/
+#ifndef STDLIB_TODO_H
+#define STDLIB_TODO_H
+
+/*
+* If C++, prevent name mangling so that the compiler emits a binary file having undecorated names, thus mirroring the behavior of a C compiler.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+* TODO.
+*/
+double stdlib_TODO( const double TODO );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !STDLIB_TODO_H
