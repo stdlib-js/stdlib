8e4e84c3916121bdc3f50e1a9e3ae66b3134a46f
kgryte <kgryte@gmail.com>
2017-11-28 14:54:53 +0900

    Add env vars for WebAssembly utilities

--- tools/make/common.mk
+++ tools/make/common.mk
@@ -344,6 +344,15 @@ DEPS_EMSDK_BINARYEN_VERSION ?= master
 # Define the output path when building the WebAssembly Binary Toolkit (WABT):
 DEPS_WABT_BUILD_OUT ?= $(DEPS_BUILD_DIR)/wabt
 
+# Define the path to the utility for converting WebAssembly binary files to the WebAssembly text format:
+DEPS_WABT_WASM2WAT ?= $(DEPS_WABT_BUILD_OUT)/wasm2wat
+
+# Define the path to the utility for converting WebAssembly text format files to the WebAssembly binary format:
+DEPS_WABT_WAT2WASM ?= $(DEPS_WABT_BUILD_OUT)/wat2wasm
+
+# Define the path to the utility for linking (merging) multiple WebAssembly files:
+DEPS_WABT_WASM_LINK ?= $(DEPS_WABT_BUILD_OUT)/wasm-link
+
 # Define the Cephes distribution to build (netlib, moshier, cephes-2.8):
 DEPS_CEPHES_DIST ?= moshier
 
