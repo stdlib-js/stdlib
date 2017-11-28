c82de703aacf0bc82b61aff0960ca4c147b7aafa
kgryte <kgryte@gmail.com>
2017-11-28 22:56:01 +0900

    Add plugin to compile Node.js native add-ons

---
+++ tools/makie/plugins/makie-install-node-addons/lib/index.js
@@ -0,0 +1,93 @@
+'use strict';
+
+// MODULES //
+
+var spawn = require( 'child_process' ).spawn;
+
+
+// FUNCTIONS //
+
+/**
+* Callback invoked upon encountering an error.
+*
+* @private
+* @param {Error} error - error object
+*/
+function onError( error ) {
+	process.exitCode = 1;
+	console.error( error.message ); // eslint-disable-line no-console
+} // end FUNCTION onError()
+
+/**
+* Callback invoked upon child process close.
+*
+* @private
+* @param {number} code - exit code
+* @returns {void}
+*/
+function onFinish( code ) {
+	if ( code !== 0 ) {
+		process.exitCode = code;
+		return console.error( '`make` process exited with code `'+code + '`.' ); // eslint-disable-line no-console
+	}
+} // end FUNCTION onFinish()
+
+/**
+* Callback invoked upon receiving data from `stdout`.
+*
+* @private
+* @param {Buffer} data - standard output
+*/
+function stdout( data ) {
+	process.stdout.write( data );
+} // end FUNCTION stdout()
+
+/**
+* Callback invoked upon receiving data from `stderr`.
+*
+* @private
+* @param {Buffer} data - standard error
+*/
+function stderr( data ) {
+	process.stderr.write( data );
+} // end FUNCTION stderr()
+
+
+// MAIN //
+
+/**
+* Plugin to compile Node.js native add-ons.
+*
+* @param {string} dir - Makefile directory
+* @param {string} cwd - current working directory
+* @param {string} subpath - subdirectory path
+*/
+function plugin( dir, cwd, subpath ) {
+	var opts;
+	var args;
+	var proc;
+
+	opts = {};
+	opts.cwd = dir;
+
+	args = [];
+
+	// Environment variables:
+	if ( subpath ) {
+		subpath = subpath.substring( 17 ); // removes 'lib/node_modules/'
+		args.push( 'NODE_ADDONS_PATTERN='+subpath );
+	}
+	// Target:
+	args.push( 'install-node-addons' );
+
+	proc = spawn( 'make', args, opts );
+	proc.on( 'error', onError );
+	proc.stdout.on( 'data', stdout );
+	proc.stderr.on( 'data', stderr );
+	proc.on( 'close', onFinish );
+} // end FUNCTION plugin()
+
+
+// EXPORTS //
+
+module.exports = plugin;
