d81e5ce15738235f557511bcb21367dc6ad73461
kgryte <kgryte@gmail.com>
2017-11-28 22:57:48 +0900

    Register add-ons plugin

--- etc/.makie.js
+++ etc/.makie.js
@@ -21,6 +21,7 @@ plugins[ 'benchmark' ] = path.join( prefix, 'makie-benchmark' );
 plugins[ 'benchmark-lang' ] = path.join( prefix, 'makie-benchmark-lang' );
 plugins[ 'complexity' ] = path.join( prefix, 'makie-complexity' );
 plugins[ 'examples' ] = path.join( prefix, 'makie-examples' );
+plugins[ 'install-node-addons' ] = path.join( prefix, 'makie-install-node-addons' );
 plugins[ 'lint-javascript' ] = path.join( prefix, 'makie-lint-javascript' );
 plugins[ 'lint-markdown' ] = path.join( prefix, 'makie-lint-markdown' );
 plugins[ 'lint-python' ] = path.join( prefix, 'makie-lint-python' );
