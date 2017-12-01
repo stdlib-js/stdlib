56d66c4a7af9b8c88c4277959b924313c98d24b3
kgryte <kgryte@gmail.com>
2017-12-01 12:29:00 +0900

    Update recipes for running C benchmarks to support resolving deps

--- tools/make/lib/benchmark/c.mk
+++ tools/make/lib/benchmark/c.mk
@@ -1,4 +1,10 @@
 
+# VARIABLES
+
+# Define the path to a script for compiling a C benchmark:
+compile_c_benchmark_bin := $(TOOLS_DIR)/scripts/compile_c_benchmark
+
+
 # TARGETS #
 
 # Run C benchmarks.
@@ -9,9 +15,16 @@ benchmark-c:
 	$(QUIET) $(FIND_C_BENCHMARKS_CMD) | grep '^[\/]\|^[a-zA-Z]:[/\]' | while read -r file; do \
 		echo ""; \
 		echo "Running benchmark: $$file"; \
-		cd `dirname $$file` && \
-		$(MAKE) clean && \
-		CEPHES=$(DEPS_CEPHES_BUILD_OUT) CEPHES_SRC="$(DEPS_CEPHES_SRC)" $(MAKE) && \
+		cd `dirname $$file` && $(MAKE) clean && \
+		OS="$(OS)" \
+		NODE="$(NODE)" \
+		NODE_PATH="$(NODE_PATH)" \
+		C_COMPILER="$(CC)" \
+		BLAS="$(BLAS)" \
+		BLAS_DIR="$(BLAS_DIR)" \
+		CEPHES="$(DEPS_CEPHES_BUILD_OUT)" \
+		CEPHES_SRC="$(CEPHES_SRC)" \
+		"${compile_c_benchmark_bin}" $$file && \
 		$(MAKE) run || exit 1; \
 	done
 
@@ -26,9 +39,16 @@ benchmark-c-files:
 	$(QUIET) for file in $(FILES); do \
 		echo ""; \
 		echo "Running benchmark: $$file"; \
-		cd `dirname $$file` && \
-		$(MAKE) clean && \
-		CEPHES=$(DEPS_CEPHES_BUILD_OUT) CEPHES_SRC="$(DEPS_CEPHES_SRC)" $(MAKE) && \
+		cd `dirname $$file` && $(MAKE) clean && \
+		OS="$(OS)" \
+		NODE="$(NODE)" \
+		NODE_PATH="$(NODE_PATH)" \
+		C_COMPILER="$(CC)" \
+		BLAS="$(BLAS)" \
+		BLAS_DIR="$(BLAS_DIR)" \
+		CEPHES="$(DEPS_CEPHES_BUILD_OUT)" \
+		CEPHES_SRC="$(CEPHES_SRC)" \
+		"${compile_c_benchmark_bin}" $$file && \
 		$(MAKE) run || exit 1; \
 	done
 
