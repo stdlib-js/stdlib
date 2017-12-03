286e9e26a08608f9c0b87c488b3e9c3e6d93c470
kgryte <kgryte@gmail.com>
2017-12-03 07:30:55 -0800

    Add benchmarks

--- docs/migration-guides/lodash/benchmark/benchmark.js
+++ docs/migration-guides/lodash/benchmark/benchmark.js
@@ -4,6 +4,9 @@
 
 var bench = require( '@stdlib/bench' );
 var isnan = require( '@stdlib/math/base/assert/is-nan' );
+var randu = require( '@stdlib/random/base/randu' );
+var floor = require( '@stdlib/math/base/special/floor' );
+var isBoolean = require( '@stdlib/assert/is-boolean' ).isPrimitive;
 var tryRequire = require( '@stdlib/utils/try-require' );
 var pkg = require( './../package.json' ).name;
 
@@ -18,7 +21,69 @@ var opts = {
 
 // MAIN //
 
-bench( pkg+'::lodash:each', opts, function benchmark( b ) {
+bench( pkg+'::lodash:countBy', opts, function benchmark( b ) {
+	var vals;
+	var arr;
+	var len;
+	var o;
+	var i;
+	var j;
+
+	function indicator( v ) {
+		return v[ 0 ];
+	}
+
+	vals = [ 'beep', 'boop', 'foo', 'bar', 'woot' ];
+
+	arr = new Array( 100 );
+	len = arr.length;
+	for ( i = 0; i < len; i++ ) {
+		j = floor( randu()*vals.length );
+		arr[ i ] = vals[ j ];
+	}
+	b.tic();
+	for ( i = 0; i < b.iterations; i++ ) {
+		j = floor( randu()*vals.length );
+		arr[ 0 ] = vals[ j ];
+		o = _.countBy( arr, indicator );
+		if ( typeof o !== 'object' ) {
+			b.fail( 'should return an object' );
+		}
+	}
+	b.toc();
+	if ( typeof o !== 'object' ) {
+		b.fail( 'should return an object' );
+	}
+	b.pass( 'benchmark finished' );
+	b.end();
+});
+
+bench( pkg+'::lodash:every', opts, function benchmark( b ) {
+	var bool;
+	var arr;
+	var i;
+
+	function predicate( v ) {
+		return !isnan( v );
+	}
+
+	b.tic();
+	for ( i = 0; i < b.iterations; i++ ) {
+		arr = [ i, i+1, i+2, i+3, i+4 ];
+		bool = _.every( arr, predicate );
+		if ( !isBoolean( bool ) ) {
+			b.fail( 'should return a boolean' );
+		}
+	}
+	b.toc();
+	if ( !isBoolean( bool ) ) {
+		b.fail( 'should return a boolean' );
+	}
+	b.pass( 'benchmark finished' );
+	b.end();
+});
+
+bench( pkg+'::lodash:forEach', opts, function benchmark( b ) {
 	var arr;
 	var i;
 
@@ -31,7 +96,33 @@ bench( pkg+'::lodash:each', opts, function benchmark( b ) {
 	b.tic();
 	for ( i = 0; i < b.iterations; i++ ) {
 		arr = [ i, i+1, i+2, i+3, i+4 ];
-		_.each( arr, onItem );
+		_.forEach( arr, onItem );
+		if ( arr.length !== 5 ) {
+			b.fail( 'should not change the array length' );
+		}
+	}
+	b.toc();
+	if ( arr.length !== 5 ) {
+		b.fail( 'should not change the array length' );
+	}
+	b.pass( 'benchmark finished' );
+	b.end();
+});
+
+bench( pkg+'::lodash:forEachRight', opts, function benchmark( b ) {
+	var arr;
+	var i;
+
+	function onItem( v ) {
+		if ( isnan( v ) ) {
+			b.fail( 'should not be NaN' );
+		}
+	}
+
+	b.tic();
+	for ( i = 0; i < b.iterations; i++ ) {
+		arr = [ i, i+1, i+2, i+3, i+4 ];
+		_.forEachRight( arr, onItem );
 		if ( arr.length !== 5 ) {
 			b.fail( 'should not change the array length' );
 		}
