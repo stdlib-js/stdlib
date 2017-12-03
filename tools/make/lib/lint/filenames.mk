5d586e4fb9aa9f986e744d63e9bfbe11ce4b4f6c
kgryte <kgryte@gmail.com>
2017-12-02 22:09:28 -0800

    Add recipe for ensuring header file basename uniqueness

--- tools/make/lib/lint/filenames.mk
+++ tools/make/lib/lint/filenames.mk
@@ -7,6 +7,12 @@ FILENAME_LINTER ?= $(TOOLS_DIR)/lint/filenames/bin/cli
 # Define the command-line options to be used when invoking the executable:
 FILENAME_LINTER_FLAGS ?=
 
+# Define the path of the linter executable for header filenames:
+HEADER_FILENAME_LINTER ?= $(TOOLS_PKGS_DIR)/lint/header-filenames/bin/cli
+
+# Define the command-line options to be used when invoking the executable:
+HEADER_FILENAME_LINTER_FLAGS ?=
+
 
 # TARGETS #
 
@@ -19,3 +25,14 @@ lint-filenames:
 	$(QUIET) $(FILENAME_LINTER) $(FILENAME_LINTER_FLAGS) $(ROOT_DIR)
 
 .PHONY: lint-filenames
+
+
+# Lint header filenames.
+#
+# This target lints header filenames for basename uniqueness.
+
+lint-header-filenames:
+	$(QUIET) $(MAKE_EXECUTABLE) $(HEADER_FILENAME_LINTER)
+	$(QUIET) $(HEADER_FILENAME_LINTER) $(HEADER_FILENAME_LINTER_FLAGS) $(ROOT_DIR)
+
+.PHONY: lint-header-filenames
