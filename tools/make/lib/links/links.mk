d3d23b29e88fa7c34e2068ebdc5aa750337ae865
Planeshifter <pburckhardt@outlook.com>
2017-12-18 17:07:06 -0500

    Fix Makefile paths

--- tools/make/lib/links/links.mk
+++ tools/make/lib/links/links.mk
@@ -2,21 +2,21 @@
 # VARIABLES #
 
 # Define the command for inserting a link into the database:
-LINKS_INSERT ?= $(NODE) $(TOOLS_DIR)/links/create/bin/cli
+LINKS_INSERT ?= $(NODE) $(TOOLS_PKGS_DIR)/links/create/bin/cli
 
 # Define the command-line options to be used when executing the command:
 LINKS_INSERT_FLAGS ?= \
 	--database $(ROOT_DIR)/docs/links/database.json
 
 # Define the command for resolving an id from a URI:
-LINKS_URI2ID ?= $(NODE) $(TOOLS_DIR)/links/uri2id/bin/cli
+LINKS_URI2ID ?= $(NODE) $(TOOLS_PKGS_DIR)/links/uri2id/bin/cli
 
 # Define the command-line options to be used when executing the command:
 LINKS_URI2ID_FLAGS ?= \
 	--database $(ROOT_DIR)/docs/links/database.json
 
 # Define the command for resolving a URI from an id:
-LINKS_ID2URI ?= $(NODE) $(TOOLS_DIR)/links/id2uri/bin/cli
+LINKS_ID2URI ?= $(NODE) $(TOOLS_PKGS_DIR)/links/id2uri/bin/cli
 
 # Define the command-line options to be used when executing the command:
 LINKS_ID2URI_FLAGS ?= \
