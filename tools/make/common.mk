dc06bb182ba5543823e2252bdda1f69056caf33d
kgryte <kgryte@gmail.com>
2017-12-19 01:15:54 -0800

    Upgrade electron

--- tools/make/common.mk
+++ tools/make/common.mk
@@ -414,7 +414,7 @@ endif
 endif
 
 # Define the Electron version (NOTE: whenever updated, update the `david` configuration file):
-DEPS_ELECTRON_VERSION ?= 1.7.9
+DEPS_ELECTRON_VERSION ?= 1.7.10
 
 # Generate a version slug:
 deps_electron_version_slug := $(subst .,_,$(DEPS_ELECTRON_VERSION))
