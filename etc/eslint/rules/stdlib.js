45c3abf268a080ce898fa0ccbf490ec0f7e2c562
kgryte <kgryte@gmail.com>
2017-12-30 11:46:09 -0800

    Enable rule to prevent duplicate headings

--- etc/eslint/rules/stdlib.js
+++ etc/eslint/rules/stdlib.js
@@ -788,6 +788,67 @@ rules[ 'stdlib/jsdoc-no-blockquote-without-marker' ] = 'error';
 rules[ 'stdlib/jsdoc-no-duplicate-definitions' ] = 'error';
 
 /**
+* Do not allow duplicate headings.
+*
+* @name jsdoc-no-duplicate-headings
+* @memberof rules
+* @type {string}
+* @default 'error'
+* @see [list-item-bullet-indent]{@link https://github.com/wooorm/remark-lint/tree/master/packages/remark-lint-no-duplicate-headings}
+*
+* @example
+* // Bad...
+*
+* /**
+* * Squares a number.
+* *
+* * ## Methods
+* *
+* * Discuss methods.
+* *
+* * ## Methods
+* *
+* * Discuss methods.
+* *
+* * @arg {number} x - input number
+* * @return {number} x squared
+* *
+* * @examples
+* * var y = square( 2.0 );
+* * // returns 4.0
+* *\/
+* function square( x ) {
+*     return x*x;
+* }
+*
+* @example
+* // Good...
+*
+* /**
+* * Squares a number.
+* *
+* * ## Methods
+* *
+* * Discuss methods.
+* *
+* * ## Notes
+* *
+* * Discuss notes.
+* *
+* * @arg {number} x - input number
+* * @return {number} x squared
+* *
+* * @examples
+* * var y = square( 2.0 );
+* * // returns 4.0
+* *\/
+* function square( x ) {
+*     return x*x;
+* }
+*/
+rules[ 'stdlib/jsdoc-no-duplicate-headings' ] = 'error';
+
+/**
 * Require that only allowed JSDoc tags are used.
 *
 * @name jsdoc-tag-names
