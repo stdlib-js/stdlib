b08ef9566b0891ec48d8155d5b1d8365dee61f91
Planeshifter <pburckhardt@outlook.com>
2018-01-07 22:13:10 +0100

    Enable rule to require table pipes to be aligned

--- etc/eslint/rules/stdlib.js
+++ etc/eslint/rules/stdlib.js
@@ -1009,6 +1009,54 @@ rules[ 'stdlib/jsdoc-no-duplicate-headings-in-section' ] = 'error';
 rules[ 'stdlib/jsdoc-no-table-indentation' ] = 'error';
 
 /**
+* Require table pipes to be aligned.
+*
+* @name jsdoc-table-pipe-alignment
+* @memberof rules
+* @type {string}
+* @default 'error'
+*
+* @example
+* // Bad...
+*
+* /**
+* * Boop beep.
+* *
+* * | x | y |
+* * | any | boop |
+* *
+* * @return {string} a value
+* *
+* * @examples
+* * var str = beep();
+* * // returns 'boop'
+* *\/
+* function beep() {
+*     return 'boop';
+* }
+*
+* @example
+* // Good...
+*
+* /**
+* * Boop beep.
+* *
+* * | x   | y    |
+* * | any | boop |
+* *
+* * @return {string} a value
+* *
+* * @examples
+* * var str = beep();
+* * // returns 'boop'
+* *\/
+* function beep() {
+*     return 'boop';
+* }
+*/
+rules[ 'stdlib/jsdoc-table-pipe-alignment' ] = 'error';
+
+/**
 * Require pipes as fences for table rows.
 *
 * @name jsdoc-table-pipes
