7b0b16dae4749c83dd0dc5ea3a3439113fdf30c9
Planeshifter <pburckhardt@outlook.com>
2018-01-08 14:10:27 +0100

    Enable rule to prevent heading content indentation

--- etc/eslint/rules/stdlib.js
+++ etc/eslint/rules/stdlib.js
@@ -961,6 +961,57 @@ rules[ 'stdlib/jsdoc-no-duplicate-headings' ] = 'error';
 rules[ 'stdlib/jsdoc-no-duplicate-headings-in-section' ] = 'error';
 
 /**
+* Prevent indentation of heading content.
+*
+* @name jsdoc-no-heading-content-indent
+* @memberof rules
+* @type {string}
+* @default 'error'
+*
+* @example
+* // Bad...
+*
+* /**
+* * Boop beep.
+* *
+* * ##    Boop
+* *
+* * Beep.
+* *
+* * @return {string} a value
+* *
+* * @examples
+* * var str = beep();
+* * // returns 'boop'
+* *\/
+* function beep() {
+*     return 'boop';
+* }
+*
+*
+* @example
+* // Good...
+*
+* /**
+* * Boop beep.
+* *
+* * ## Boop
+* *
+* * Beep.
+* *
+* * @return {string} a value
+* *
+* * @examples
+* * var str = beep();
+* * // returns 'boop'
+* *\/
+* function beep() {
+*     return 'boop';
+* }
+*/
+rules[ 'stdlib/jsdoc-no-heading-content-indent' ] = 'error';
+
+/**
 * Prevent indentation of headings.
 *
 * @name jsdoc-no-heading-indent
