1587b95d36d35c735a2f12ff9d52f08a3549e020
Planeshifter <pburckhardt@outlook.com>
2018-01-09 09:01:38 +0100

    Enable rule

--- etc/eslint/rules/stdlib.js
+++ etc/eslint/rules/stdlib.js
@@ -1114,6 +1114,57 @@ rules[ 'stdlib/jsdoc-no-heading-content-indent' ] = 'error';
 rules[ 'stdlib/jsdoc-no-heading-indent' ] = 'error';
 
 /**
+* Prevent indentation of paragraph content.
+*
+* @name jsdoc-no-paragraph-content-indent
+* @memberof rules
+* @type {string}
+* @default 'error'
+*
+* @example
+* // Bad...
+*
+* /**
+* * Boop beep.
+* *
+* * ## Boop
+* *
+* *    Beep.
+* *
+* * @return {string} a value
+* *
+* * @examples
+* * var str = beep();
+* * // returns 'boop'
+* *\/
+* function beep() {
+*     return 'boop';
+* }
+*
+*
+* @example
+* // Good...
+*
+* /**
+* * Boop beep.
+* *
+* * ## Boop
+* *
+* * Beep.
+* *
+* * @return {string} a value
+* *
+* * @examples
+* * var str = beep();
+* * // returns 'boop'
+* *\/
+* function beep() {
+*     return 'boop';
+* }
+*/
+rules[ 'stdlib/jsdoc-no-paragraph-content-indent' ] = 'error';
+
+/**
 * Prevent unneeded indentation before tables.
 *
 * @name jsdoc-no-table-indentation
