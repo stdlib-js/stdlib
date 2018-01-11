5f7aadba98dc531db41eec2a0cd8819769f3aee2
kgryte <kgryte@gmail.com>
2018-01-11 09:45:47 -0800

    Reformat comments based on JSDoc

--- tools/make/lib/bundle/browser.mk
+++ tools/make/lib/bundle/browser.mk
@@ -19,10 +19,12 @@ UPDATE_DIST_README_BROWSER_BUNDLE_STATS_FLAGS ?=
 
 # TARGETS #
 
-# Generate browser bundles.
+#/
+# Generates distributable browser bundles.
 #
-# This target generates distributable browser bundles.
-
+# @example
+# make dist-browser-bundles
+#/
 dist-browser-bundles: $(NODE_MODULES)
 	$(QUIET) echo 'Generating distributable browser bundles...'
 	$(QUIET) $(DIST_BROWSER_BUNDLES) $(DIST_BROWSER_BUNDLES_FLAGS)
@@ -33,11 +35,12 @@ dist-browser-bundles: $(NODE_MODULES)
 
 .PHONY: dist-browser-bundles
 
-
-# Update browser bundle stats.
+#/
+# Updates a README file documenting distributable browser bundles to include the most recent bundle statistics.
 #
-# This target updates a README file documenting distributable browser bundles to include the most recent bundle statistics.
-
+# @example
+# make update-dist-readme-browser-bundle-stats
+#/
 update-dist-readme-browser-bundle-stats: $(NODE_MODULES)
 	$(QUIET) $(UPDATE_DIST_README_BROWSER_BUNDLE_STATS) $(UPDATE_DIST_README_BROWSER_BUNDLE_STATS_FLAGS)
 
