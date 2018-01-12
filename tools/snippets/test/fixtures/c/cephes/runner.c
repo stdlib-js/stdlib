82a5dab7d37a861fc7158152d8178913fbc9b44e
kgryte <kgryte@gmail.com>
2018-01-11 22:42:50 -0800

    Add license header

--- tools/snippets/test/fixtures/c/cephes/runner.c
+++ tools/snippets/test/fixtures/c/cephes/runner.c
@@ -1,4 +1,22 @@
 /**
+* @license Apache-2.0
+*
+* Copyright (c) 2017 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
 * Generate Cephes test fixtures.
 *
 * ## Notes
@@ -101,7 +119,7 @@ void rand_array_f64( double *out, const unsigned int len, const double a, const 
 *
 * ## Notes
 *
-* * WARNING: the method used here is not particularly robust, as some integer values may be sampled more frequently than others.
+* -   WARNING: the method used here is not particularly robust, as some integer values may be sampled more frequently than others.
 *
 *
 * @param out  output array
