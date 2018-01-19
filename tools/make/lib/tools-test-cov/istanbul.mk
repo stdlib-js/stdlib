f838b7cbc1339478187858687371e7cd92220c6e
kgryte <kgryte@gmail.com>
2018-01-18 22:25:22 -0800

    Set env vars

--- tools/make/lib/tools-test-cov/istanbul.mk
+++ tools/make/lib/tools-test-cov/istanbul.mk
@@ -23,8 +23,8 @@
 # This target instruments source code, runs unit tests, and outputs a test coverage report.
 
 tools-test-istanbul:
-	$(QUIET) NODE_ENV=$(NODE_ENV_TEST) \
-	NODE_PATH=$(NODE_PATH_TEST) \
+	$(QUIET) NODE_ENV="$(NODE_ENV_TEST)" \
+	NODE_PATH="$(NODE_PATH_TEST)" \
 	$(ISTANBUL_COVER) \
 		--dir $(COVERAGE_DIR) \
 		--report $(ISTANBUL_COVER_REPORT_FORMAT) \
