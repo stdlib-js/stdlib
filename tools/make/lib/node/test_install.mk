43ac6823e17e352886db01d13d28f52b57c39f06
kgryte <kgryte@gmail.com>
2018-04-18 16:41:50 +0200

    Add rule for testing whether a pkg installs from npm

---
+++ tools/make/lib/node/test_install.mk
@@ -0,0 +1,56 @@
+#/
+# @license Apache-2.0
+#
+# Copyright (c) 2018 The Stdlib Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#/
+
+# VARIABLES #
+
+# Define the directory path where the package should be installed:
+TEST_NPM_INSTALL_DIR ?= $(BUILD_DIR)/test-npm-install
+
+# Define the package to be test:
+TEST_NPM_INSTALL_PKG ?= $(shell $(PROJECT_NAME))
+
+# Define the package version to install:
+TEST_NPM_INSTALL_VERSION ?= latest
+
+
+# RULES #
+
+#/
+# Tests whether a package can be successfully installed via `npm install`.
+#
+# @example
+# make test-npm-install
+#/
+test-npm-install: clean-test-npm-install
+	$(QUIET) $(MKDIR_RECURSIVE) $(TEST_NPM_INSTALL_DIR) && \
+		cd $(TEST_NPM_INSTALL_DIR) && \
+		echo '{"name":"test-npm-install","version":"0.0.0","private":true}' > $(TEST_NPM_INSTALL_DIR)/package.json && \
+		$(NPM) install "$(TEST_NPM_INSTALL_PKG)@$(TEST_NPM_INSTALL_VERSION)"
+
+.PHONY: test-npm-install
+
+#/
+# Removes the test installation.
+#
+# @example
+# make clean-test-npm-install
+#/
+clean-test-npm-install:
+	$(QUIET) $(DELETE) $(DELETE_FLAGS) $(TEST_NPM_INSTALL_DIR)
+
+.PHONY: clean-test-npm-install
