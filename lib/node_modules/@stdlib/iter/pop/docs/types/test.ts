10deb1b52d097fcacd359c240e8044bb9202a833
kgryte <kgryte@gmail.com>
2019-02-01 00:53:22 -0800

    Add TypeScript declaration file

---
+++ lib/node_modules/@stdlib/iter/pop/docs/types/test.ts
@@ -0,0 +1,101 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import iterPop = require( './index' );
+
+/**
+* Returns an iterator protocol-compliant object.
+*
+* @returns iterator protocol-compliant object
+*/
+function iterator() {
+	return {
+		'next': next
+	};
+
+	/**
+	* Implements the iterator protocol `next` method.
+	*
+	* @returns iterator protocol-compliant object
+	*/
+	function next() {
+		return {
+			'value': true,
+			'done': false
+		};
+	}
+}
+
+/**
+* Callback function.
+*
+* @param v - iterated value
+*/
+function clbk( v: any ) {
+	if ( v !== v ) {
+		throw new Error( 'something went wrong' );
+	}
+}
+
+
+// TESTS //
+
+// The function returns an iterator...
+{
+	iterPop( iterator() ); // $ExpectType Iterator
+	iterPop( iterator(), clbk ); // $ExpectType Iterator
+	iterPop( iterator(), clbk, {} ); // $ExpectType Iterator
+	iterPop( iterator(), clbk, null ); // $ExpectType Iterator
+}
+
+// The compiler throws an error if the function is provided a first argument which is not an iterator protocol-compliant object...
+{
+	iterPop( '5', clbk ); // $ExpectError
+	iterPop( 5, clbk ); // $ExpectError
+	iterPop( true, clbk ); // $ExpectError
+	iterPop( false, clbk ); // $ExpectError
+	iterPop( null, clbk ); // $ExpectError
+	iterPop( undefined, clbk ); // $ExpectError
+	iterPop( [], clbk ); // $ExpectError
+	iterPop( {}, clbk ); // $ExpectError
+	iterPop( ( x: number ): number => x, clbk ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a second argument which is not a callback function...
+{
+	iterPop( iterator(), '5' ); // $ExpectError
+	iterPop( iterator(), 5 ); // $ExpectError
+	iterPop( iterator(), true ); // $ExpectError
+	iterPop( iterator(), false ); // $ExpectError
+	iterPop( iterator(), null ); // $ExpectError
+	iterPop( iterator(), [] ); // $ExpectError
+	iterPop( iterator(), {} ); // $ExpectError
+
+	iterPop( iterator(), '5', {} ); // $ExpectError
+	iterPop( iterator(), 5, {} ); // $ExpectError
+	iterPop( iterator(), true, {} ); // $ExpectError
+	iterPop( iterator(), false, {} ); // $ExpectError
+	iterPop( iterator(), null, {} ); // $ExpectError
+	iterPop( iterator(), [], {} ); // $ExpectError
+	iterPop( iterator(), {}, {} ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided insufficient arguments...
+{
+	iterPop(); // $ExpectError
+}
