0bf7b006eb4293ce70069d21b5ee194cae8cb78f
Planeshifter <pburckhardt@outlook.com>
2019-02-02 11:09:24 -0500

    Add Typescript definition file

---
+++ lib/node_modules/@stdlib/math/base/special/spence/docs/types/index.d.ts
@@ -0,0 +1,48 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Evaluates Spence’s function, which is also known as the dilogarithm.
+*
+* @param x - input value
+* @returns function value
+*
+* @example
+* var v = spence( 3.0 );
+* // returns ~-1.437
+*
+* @example
+* var v = spence( 0.0 );
+* // returns ~1.645
+*
+* @example
+* var v = spence( -9.0 );
+* // returns NaN
+*
+* @example
+* var v = spence( NaN );
+* // returns NaN
+*/
+declare function spence( x: number ): number;
+
+
+// EXPORTS //
+
+export = spence;
