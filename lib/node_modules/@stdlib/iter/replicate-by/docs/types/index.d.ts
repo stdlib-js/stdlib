cfd8974fae2108fd54ffaec1bd8ea6db18d7c26d
kgryte <kgryte@gmail.com>
2019-02-02 01:04:30 -0800

    Add TypeScript declaration file

---
+++ lib/node_modules/@stdlib/iter/replicate-by/docs/types/index.d.ts
@@ -0,0 +1,127 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+import { Iterator as Iter, IterableIterator } from '@stdlib/types/iter';
+
+// Define a union type representing both iterable and non-iterable iterators:
+type Iterator = Iter | IterableIterator;
+
+/**
+* Returns the number of times an iterated value should be replicated.
+*
+* @returns number of times an iterated value should be replicated
+*/
+type Nullary = () => number;
+
+/**
+* Returns the number of times an iterated value should be replicated.
+*
+* @param value - iterated value
+* @returns number of times an iterated value should be replicated
+*/
+type Unary = ( value: any ) => number;
+
+/**
+* Returns the number of times an iterated value should be replicated.
+*
+* @param value - iterated value
+* @param index - source iteration index (zero-based)
+* @returns number of times an iterated value should be replicated
+*/
+type Binary = ( value: any, index: number ) => number;
+
+/**
+* Returns the number of times an iterated value should be replicated.
+*
+* @param value - iterated value
+* @param index - source iteration index (zero-based)
+* @param n - iteration index (zero-based)
+* @returns number of times an iterated value should be replicated
+*/
+type Ternary = ( value: any, index: number, n: number ) => number;
+
+/**
+* Returns the number of times an iterated value should be replicated.
+*
+* @param value - iterated value
+* @param index - source iteration index (zero-based)
+* @param n - iteration index (zero-based)
+* @returns number of times an iterated value should be replicated
+*/
+type Callback = Nullary | Unary | Binary | Ternary;
+
+/**
+* Returns an iterator which invokes a function for each iterated value.
+*
+* ## Notes
+*
+* -   The callback function is provided three arguments:
+*
+*     -   `value`: iterated value
+*     -   `index`: source iteration index (zero-based)
+*     -   `n`: iteration index (zero-based)
+*
+* -   The callback function is invoked **once** per iterated value of the provided iterator.
+*
+* -   The callback function **must** return an integer value. If the return value is less than or equal to zero, the returned iterator skips an iterated value and invokes the callback for the next iterated value of the provided iterator.
+*
+* -   If an environment supports `Symbol.iterator` **and** a provided iterator is iterable, the returned iterator is iterable.
+*
+* @param iterator - input iterator
+* @param fcn - callback function which returns the number of times an iterated value should be replicated
+* @param thisArg - execution context
+* @returns iterator
+*
+* @example
+* var array2iterator = require( `@stdlib/array/to-iterator` );
+*
+* function fcn( v, i ) {
+*     return i + 1;
+* }
+*
+* var src = array2iterator( [ 1, 2, 3, 4 ] );
+* var iter = iterReplicateBy( src, fcn );
+*
+* var v = iter.next().value;
+* // returns 1
+*
+* v = iter.next().value;
+* // returns 2
+*
+* v = iter.next().value;
+* // returns 2
+*
+* v = iter.next().value;
+* // returns 3
+*
+* v = iter.next().value;
+* // returns 3
+*
+* v = iter.next().value;
+* // returns 3
+*/
+declare function iterReplicateBy( iterator: Iterator, fcn: Callback, thisArg?: any ): Iterator; // tslint:disable-line:max-line-length
+
+
+// EXPORTS //
+
+export = iterReplicateBy;
---
+++ lib/node_modules/@stdlib/iter/replicate-by/docs/types/test.ts
@@ -0,0 +1,94 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import iterReplicateBy = require( './index' );
+
+/**
+* Returns an iterator protocol-compliant object.
+*
+* @returns iterator protocol-compliant object
+*/
+function iterator() {
+	return {
+		'next': next
+	};
+
+	/**
+	* Implements the iterator protocol `next` method.
+	*
+	* @returns iterator protocol-compliant object
+	*/
+	function next() {
+		return {
+			'value': true,
+			'done': false
+		};
+	}
+}
+
+/**
+* Returns the number of times an iterated value should be replicated.
+*
+* @param _v - iterated value
+* @param i - source iteration index
+* @returns callback result
+*/
+function fcn( _v: any, i: number ): number { // tslint:disable-line:variable-name
+	return i + 1;
+}
+
+
+// TESTS //
+
+// The function returns an iterator...
+{
+	iterReplicateBy( iterator(), fcn ); // $ExpectType Iterator
+	iterReplicateBy( iterator(), fcn, {} ); // $ExpectType Iterator
+	iterReplicateBy( iterator(), fcn, null ); // $ExpectType Iterator
+}
+
+// The compiler throws an error if the function is provided a first argument which is not an iterator protocol-compliant object...
+{
+	iterReplicateBy( '5', fcn ); // $ExpectError
+	iterReplicateBy( 5, fcn ); // $ExpectError
+	iterReplicateBy( true, fcn ); // $ExpectError
+	iterReplicateBy( false, fcn ); // $ExpectError
+	iterReplicateBy( null, fcn ); // $ExpectError
+	iterReplicateBy( undefined, fcn ); // $ExpectError
+	iterReplicateBy( [], fcn ); // $ExpectError
+	iterReplicateBy( {}, fcn ); // $ExpectError
+	iterReplicateBy( ( x: number ): number => x, fcn ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a second argument which is not a valid callback function...
+{
+	iterReplicateBy( iterator(), '5' ); // $ExpectError
+	iterReplicateBy( iterator(), 5 ); // $ExpectError
+	iterReplicateBy( iterator(), true ); // $ExpectError
+	iterReplicateBy( iterator(), false ); // $ExpectError
+	iterReplicateBy( iterator(), null ); // $ExpectError
+	iterReplicateBy( iterator(), undefined ); // $ExpectError
+	iterReplicateBy( iterator(), [] ); // $ExpectError
+	iterReplicateBy( iterator(), {} ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided insufficient arguments...
+{
+	iterReplicateBy(); // $ExpectError
+	iterReplicateBy( iterator() ); // $ExpectError
+}
