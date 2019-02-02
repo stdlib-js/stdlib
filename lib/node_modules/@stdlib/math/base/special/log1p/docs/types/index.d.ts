81e8b0e702a752bc55941be6fd566c6c17e0f826
Planeshifter <pburckhardt@outlook.com>
2019-02-02 10:15:39 -0500

    Add Typescript definition file

---
+++ lib/node_modules/@stdlib/math/base/special/log1p/docs/types/index.d.ts
@@ -0,0 +1,60 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Evaluates the natural logarithm of `1+x`.
+*
+* ## Notes
+*
+* -   For `x < -1`, the function returns `NaN`, as the natural logarithm is not defined for negative numbers.
+*
+* @param x - input value
+* @returns the natural logarithm of `1+x`
+*
+* @example
+* var v = log1p( 4.0 );
+* // returns ~1.609
+*
+* @example
+* var v = log1p( -1.0 );
+* // returns -Infinity
+*
+* @example
+* var v = log1p( 0.0 );
+* // returns 0.0
+*
+* @example
+* var v = log1p( -0.0 );
+* // returns -0.0
+*
+* @example
+* var v = log1p( -2.0 );
+* // returns NaN
+*
+* @example
+* var v = log1p( NaN );
+* // returns NaN
+*/
+declare function log1p( x: number ): number;
+
+
+// EXPORTS //
+
+export = log1p;
