a4861cb0836551f0be9681a06c790e9da834be00
Planeshifter <pburckhardt@outlook.com>
2019-02-13 20:19:22 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/stats/base/dists/geometric/variance/docs/types/index.d.ts
@@ -0,0 +1,52 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns the variance of a geometric distribution.
+*
+* ## Notes
+*
+* -   If `p < 0` or `p > 1`, the function returns `NaN`.
+*
+* @param p - success probability
+* @returns variance
+*
+* @example
+* var v = variance( 0.1 );
+* // returns ~90.0
+*
+* @example
+* var v = variance( 0.5 );
+* // returns 2.0
+*
+* @example
+* var v = variance( 1.1 );
+* // returns NaN
+*
+* @example
+* var v = variance( NaN );
+* // returns NaN
+*/
+declare function variance( p: number ): number;
+
+
+// EXPORTS //
+
+export = variance;
