2c972949f80f6fb4c13ef89c21b4fe6f166eabcb
Planeshifter <pburckhardt@outlook.com>
2019-02-14 20:44:26 -0500

    Add Typescript definition file

---
+++ lib/node_modules/@stdlib/stats/base/dists/poisson/skewness/docs/types/index.d.ts
@@ -0,0 +1,52 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns the skewness of a Poisson distribution.
+*
+* ## Notes
+*
+* -   If provided a non-positive value for `λ`, the function returns `NaN`.
+*
+* @param lambda - mean parameter
+* @returns skewness
+*
+* @example
+* var v = skewness( 9.0 );
+* // returns ~0.33
+*
+* @example
+* var v = skewness( 1.0 );
+* // returns 1.0
+*
+* @example
+* var v = skewness( -0.2 );
+* // returns NaN
+*
+* @example
+* var v = skewness( NaN );
+* // returns NaN
+*/
+declare function skewness( lambda: number ): number;
+
+
+// EXPORTS //
+
+export = skewness;
