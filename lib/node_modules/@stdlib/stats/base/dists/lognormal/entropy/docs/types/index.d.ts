525db584bbd0703428970a53e017f22b09cea1d9
Planeshifter <pburckhardt@outlook.com>
2019-02-24 20:45:10 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/stats/base/dists/lognormal/entropy/docs/types/index.d.ts
@@ -0,0 +1,57 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns the differential entropy for a lognormal distribution with location `mu` and scale `sigma`.
+*
+* ## Notes
+*
+* -   If provided `sigma <= 0`, the function returns `NaN`.
+*
+* @param mu - location parameter
+* @param sigma - scale parameter
+* @returns entropy
+*
+* @example
+* var y = entropy( 0.0, 1.0 );
+* // returns ~1.419
+*
+* @example
+* var y = entropy( 5.0, 2.0 );
+* // returns ~7.112
+*
+* @example
+* var y = entropy( NaN, 1.0 );
+* // returns NaN
+*
+* @example
+* var y = entropy( 0.0, NaN );
+* // returns NaN
+*
+* @example
+* var y = entropy( 0.0, 0.0 );
+* // returns NaN
+*/
+declare function entropy( mu: number, sigma: number ): number;
+
+
+// EXPORTS //
+
+export = entropy;
