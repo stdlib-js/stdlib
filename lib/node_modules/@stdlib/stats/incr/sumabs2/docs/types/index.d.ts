bf05aa15f16fb92b6401427f8691ec433c2ba206
kgryte <kgryte@gmail.com>
2019-02-27 02:14:20 -0800

    Add TypeScript declaration file

---
+++ lib/node_modules/@stdlib/stats/incr/sumabs2/docs/types/index.d.ts
@@ -0,0 +1,63 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+/**
+* If provided a value, returns an updated sum of squared absolute values; otherwise, returns the current sum of squared absolute values.
+*
+* ## Notes
+*
+* -   If provided `NaN` or a value which, when used in computations, results in `NaN`, the accumulated value is `NaN` for all future invocations.
+*
+* @param x - value
+* @returns sum of squared absolute values
+*/
+type accumulator = ( x?: number ) => number | null;
+
+/**
+* Returns an accumulator function which incrementally computes a sum of squared absolute values.
+*
+* @returns accumulator function
+*
+* @example
+* var accumulator = incrsumabs2();
+*
+* var v = accumulator();
+* // returns null
+*
+* v = accumulator( 2.0 );
+* // returns 4.0
+*
+* v = accumulator( -3.0 );
+* // returns 13.0
+*
+* v = accumulator( -4.0 );
+* // returns 29.0
+*
+* v = accumulator();
+* // returns 29.0
+*/
+declare function incrsumabs2(): accumulator;
+
+
+// EXPORTS //
+
+export = incrsumabs2;
