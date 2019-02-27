6fa585421328adb520da8f3105e38b02cf6c5cb1
Planeshifter <pburckhardt@outlook.com>
2019-02-26 22:38:48 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/stats/base/dists/discrete-uniform/stdev/docs/types/index.d.ts
@@ -0,0 +1,66 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns the standard deviation of a discrete uniform distribution.
+*
+* ## Notes
+*
+* -   If `a` or `b` is not an integer value, the function returns `NaN`.
+* -   If provided `a >= b`, the function returns `NaN`.
+*
+* @param a - minimum support
+* @param b - maximum support
+* @returns standard deviation
+*
+* @example
+* var v = stdev( 0, 1 );
+* // returns ~0.5
+*
+* @example
+* var v = stdev( 4, 12 );
+* // returns ~2.582
+*
+* @example
+* var v = stdev( -4, 4 );
+* // returns ~2.582
+*
+* @example
+* var v = stdev( 1, -0.1 );
+* // returns NaN
+*
+* @example
+* var v = stdev( 0.5, 1 );
+* // returns NaN
+*
+* @example
+* var v = stdev( 2, NaN );
+* // returns NaN
+*
+* @example
+* var v = stdev( NaN, 2 );
+* // returns NaN
+*/
+declare function stdev( a: number, b: number ): number;
+
+
+// EXPORTS //
+
+export = stdev;
