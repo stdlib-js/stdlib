43e13579a3479e895a9cfc4e9a58e7a79ae98cb2
Planeshifter <pburckhardt@outlook.com>
2019-03-04 10:38:42 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/assert/is-array-like-object/docs/types/index.d.ts
@@ -0,0 +1,48 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Tests if a value is an array-like object.
+*
+* ## Notes
+*
+* -   If provided a string, the function returns `false`.
+*
+* @param value - value to test
+* @returns boolean indicating if a value is an array-like object
+*
+* @example
+* var bool = isArrayLikeObject( [] );
+* // returns true
+*
+* @example
+* var bool = isArrayLikeObject( { 'length':10 } );
+* // returns true
+*
+* @example
+* var bool = isArrayLikeObject( 'beep' );
+* // returns false
+*/
+declare function isArrayLike( value: any ): boolean;
+
+
+// EXPORTS //
+
+export = isArrayLike;
