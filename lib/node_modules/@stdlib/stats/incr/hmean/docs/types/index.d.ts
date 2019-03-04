20522dd696a7095787cf4b9a7bac5076cc7be181
kgryte <kgryte@gmail.com>
2019-03-03 23:54:57 -0800

    Add TypeScript declaration

---
+++ lib/node_modules/@stdlib/stats/incr/hmean/docs/types/index.d.ts
@@ -0,0 +1,60 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+/**
+* If provided a value, returns an updated harmonic mean; otherwise, returns the current harmonic mean.
+*
+* ## Notes
+*
+* -   If provided `NaN` or a value which, when used in computations, results in `NaN`, the accumulated value is `NaN` for all future invocations.
+*
+* @param x - value
+* @returns harmonic mean
+*/
+type accumulator = ( x?: number ) => number | null;
+
+/**
+* Returns an accumulator function which incrementally computes a harmonic mean.
+*
+* @returns accumulator function
+*
+* @example
+* var accumulator = incrhmean();
+*
+* var v = accumulator();
+* // returns null
+*
+* v = accumulator( 2.0 );
+* // returns 2.0
+*
+* v = accumulator( 5.0 );
+* // returns ~2.86
+*
+* v = accumulator();
+* // returns ~2.86
+*/
+declare function incrhmean(): accumulator;
+
+
+// EXPORTS //
+
+export = incrhmean;
