87163e5faaa3aa7700408d47a5d88f75d1f7dbf6
Planeshifter <pburckhardt@outlook.com>
2019-03-04 21:55:29 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/assert/has-uint8clampedarray-support/docs/types/index.d.ts
@@ -0,0 +1,35 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Tests for native `Uint8ClampedArray` support.
+*
+* @returns boolean indicating if an environment has `Uint8ClampedArray` support
+*
+* @example
+* var bool = hasUint8ClampedArraySupport();
+* // returns <boolean>
+*/
+declare function hasUint8ClampedArraySupport(): boolean;
+
+
+// EXPORTS //
+
+export = hasUint8ClampedArraySupport;
