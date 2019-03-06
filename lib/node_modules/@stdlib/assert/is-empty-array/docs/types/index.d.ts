1f1e84830419da1dad5a3622b139293296837e48
Planeshifter <pburckhardt@outlook.com>
2019-03-05 22:33:14 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/assert/is-empty-array/docs/types/index.d.ts
@@ -0,0 +1,44 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Tests if a value is an empty array.
+*
+* @param value - value to test
+* @returns boolean indicating whether value is an empty array
+*
+* @example
+* var bool = isEmptyArray( [] );
+* // returns true
+*
+* @example
+* var bool = isEmptyArray( [ 1, 2, 3 ] );
+* // returns false
+*
+* @example
+* var bool = isEmptyArray( {} );
+* // returns false
+*/
+declare function isEmptyArray( value: any ): boolean;
+
+
+// EXPORTS //
+
+export = isEmptyArray;
