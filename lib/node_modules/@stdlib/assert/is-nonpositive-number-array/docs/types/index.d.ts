e99f739f6b1d875f2cdf657f3bc2ecfbe0690ef7
Planeshifter <pburckhardt@outlook.com>
2019-03-08 10:26:07 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/assert/is-nonpositive-number-array/docs/types/index.d.ts
@@ -0,0 +1,109 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Interface defining `isNonPositiveNumberArray` with methods for testing for primitive and object arrays, respectively.
+*/
+interface IsNonPositiveNumberArray {
+	/**
+	* Tests if a value is an array-like object containing only nonpositive numbers.
+	*
+	* @param value - value to test
+	* @returns boolean indicating whether value is an array-like object containing only nonpositive numbers
+	*
+	* @example
+	* var bool = isNonPositiveNumberArray( [ -3.0, new Number(-3.0) ] );
+	* // returns true
+	*
+	* @example
+	* var bool = isNonPositiveNumberArray( [ -3.0, '-3.0' ] );
+	* // returns false
+	*/
+	( value: any ): boolean;
+
+	/**
+	* Tests if a value is an array-like object containing only nonpositive primitive integer values.
+	*
+	* @param value - value to test
+	* @returns boolean indicating whether value is an array-like object containing only nonpositive primitive integer values
+	*
+	* @example
+	* var bool = isNonPositiveNumberArray.primitives( [ -1.0, -10.0 ] );
+	* // returns true
+	*
+	* @example
+	* var bool = isNonPositiveNumberArray.primitives( [ -1.0, 0.0, -10.0 ] );
+	* // returns true
+	*
+	* @example
+	* var bool = isNonPositiveNumberArray.primitives( [ -3.0, new Number(-1.0) ] );
+	* // returns false
+	*/
+	primitives( value: any ): boolean;
+
+	/**
+	* Tests if a value is an array-like object containing only number objects having nonpositive integer values.
+	*
+	* @param value - value to test
+	* @returns boolean indicating whether value is an array-like object containing only number objects having nonpositive integer values
+	*
+	* @example
+	* var bool = isNonPositiveNumberArray.objects( [ new Number(-1.0), new Number(-10.0) ] );
+	* // returns true
+	*
+	* @example
+	* var bool = isNonPositiveNumberArray.objects( [ -1.0, 0.0, -10.0 ] );
+	* // returns false
+	*
+	* @example
+	* var bool = isNonPositiveNumberArray.objects( [ -3.0, new Number(-1.0) ] );
+	* // returns false
+	*/
+	objects( value: any ): boolean;
+}
+
+/**
+* Tests if a value is an array-like object containing only nonpositive numbers.
+*
+* @param value - value to test
+* @returns boolean indicating whether value is an array-like object containing only nonpositive numbers
+*
+* @example
+* var bool = isNonPositiveNumberArray( [ -3.0, new Number(-3.0) ] );
+* // returns true
+*
+* @example
+* var bool = isNonPositiveNumberArray( [ -3.0, '-3.0' ] );
+* // returns false
+*
+* @example
+* var bool = isNonPositiveNumberArray.primitives( [ -1.0, -10.0 ] );
+* // returns true
+*
+* @example
+* var bool = isNonPositiveNumberArray.objects( [ new Number(-1.0), new Number(-10.0) ] );
+* // returns true
+*/
+declare var isNonPositiveNumberArray: IsNonPositiveNumberArray;
+
+
+// EXPORTS //
+
+export = isNonPositiveNumberArray;
