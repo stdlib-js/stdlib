c4ee75853d5c3687e5139d775a122ddf4971c9a4
kgryte <kgryte@gmail.com>
2019-03-15 19:05:54 -0700

    Add utility to map an alias to a package name

---
+++ lib/node_modules/@stdlib/namespace/alias2pkg/docs/types/index.d.ts
@@ -0,0 +1,36 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns a package name associated with a specified alias.
+*
+* @param alias - alias
+* @returns package name
+*
+* @example
+* var v = alias2pkg( 'base.sin' );
+* // returns '@stdlib/math/base/special/sin'
+*/
+declare function alias2pkg( alias: string ): string | null;
+
+
+// EXPORTS //
+
+export = alias2pkg;
