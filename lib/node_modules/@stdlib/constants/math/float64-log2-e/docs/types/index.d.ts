8096d087f91f97a2a44f78b22981233b2a9b42d6
Planeshifter <pburckhardt@outlook.com>
2019-04-06 14:38:54 -0400

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/constants/math/float64-log2-e/docs/types/index.d.ts
@@ -0,0 +1,33 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Base 2 logarithm of Euler's number.
+*
+* @example
+* var val = LOG2E;
+* // returns 1.4426950408889634
+*/
+declare const LOG2E: number;
+
+
+// EXPORTS //
+
+export = LOG2E;
