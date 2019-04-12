bcfbdda61db1fcc53861dbb42f4529a114e3377d
Planeshifter <pburckhardt@outlook.com>
2019-04-11 20:13:48 -0400

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/process/getuid/docs/types/index.d.ts
@@ -0,0 +1,38 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns the numeric user identity of the calling process.
+*
+* ## Notes
+*
+* -   The function only returns a user identity on POSIX platforms. For all other platforms (e.g., Windows and Android), the function returns `null`.
+*
+* @returns numeric user identity or null
+*
+* @example
+* var uid = getuid();
+*/
+declare function getuid(): number | null;
+
+
+// EXPORTS //
+
+export = getuid;
