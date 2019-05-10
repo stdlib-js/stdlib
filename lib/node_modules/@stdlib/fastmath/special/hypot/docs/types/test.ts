1bda26fdc30879f22ccb34dc038c938f380168bd
Planeshifter <pburckhardt@outlook.com>
2019-05-10 17:52:21 -0400

    Add Typescript annotations

---
+++ lib/node_modules/@stdlib/fastmath/special/hypot/docs/types/test.ts
@@ -0,0 +1,56 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import hypot = require( './index' );
+
+
+// TESTS //
+
+// The function returns a number...
+{
+	hypot( 8, 2 ); // $ExpectType number
+}
+
+// The function does not compile if provided values other than two numbers...
+{
+	hypot( true, 3 ); // $ExpectError
+	hypot( false, 2 ); // $ExpectError
+	hypot( '5', 1 ); // $ExpectError
+	hypot( [], 1 ); // $ExpectError
+	hypot( {}, 2 ); // $ExpectError
+	hypot( ( x: number ): number => x, 2 ); // $ExpectError
+
+	hypot( 9, true ); // $ExpectError
+	hypot( 9, false ); // $ExpectError
+	hypot( 5, '5' ); // $ExpectError
+	hypot( 8, [] ); // $ExpectError
+	hypot( 9, {} ); // $ExpectError
+	hypot( 8, ( x: number ): number => x ); // $ExpectError
+
+	hypot( [], true ); // $ExpectError
+	hypot( {}, false ); // $ExpectError
+	hypot( false, '5' ); // $ExpectError
+	hypot( {}, [] ); // $ExpectError
+	hypot( '5', ( x: number ): number => x ); // $ExpectError
+}
+
+// The function does not compile if provided insufficient arguments...
+{
+	hypot(); // $ExpectError
+	hypot( 3 ); // $ExpectError
+}
---
+++ lib/node_modules/@stdlib/fastmath/special/max/docs/types/index.d.ts
@@ -0,0 +1,57 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns the maximum value.
+*
+* ## Notes
+*
+* -   The function ignores the sign of `0` and does not check for `NaN` arguments.
+*
+* @param x - first number
+* @param y - second number
+* @returns maximum value
+*
+* @example
+* var v = max( 3.14, 4.2 );
+* // returns 4.2
+*
+* @example
+* var v = max( 3.14, NaN );
+* // returns NaN
+*
+* @example
+* var v = max( NaN, 3.14 );
+* // returns 3.14
+*
+* @example
+* var v = max( -0.0, +0.0 );
+* // returns +0.0
+*
+* @example
+* var v = max( +0.0, -0.0 );
+* // returns -0.0
+*/
+declare function max( x: number, y: number ): number;
+
+
+// EXPORTS //
+
+export = max;
