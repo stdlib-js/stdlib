6b947ad783c7d0a045636c746f00fab13c090094
Planeshifter <pburckhardt@outlook.com>
2019-06-07 17:22:57 +0200

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/utils/uppercase-keys/docs/types/index.d.ts
@@ -0,0 +1,46 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Converts each object key to uppercase.
+*
+* ## Notes
+*
+* -   The function only transforms own properties. Hence, the function does not transform inherited properties.
+* -   The function shallow copies key values.
+*
+* @param obj - source object
+* @returns new object
+*
+* @example
+* var obj1 = {
+*     'a': 1,
+*     'b': 2
+* };
+*
+* var obj2 = uppercaseKeys( obj1 );
+* // returns { 'A': 1, 'B': 2 }
+*/
+declare function uppercaseKeys( obj: Object ): Object;
+
+
+// EXPORTS //
+
+export = uppercaseKeys;
