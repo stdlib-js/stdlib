faec60e52898ed53bfb4b3da12cbcfd6f984461d
Planeshifter <pburckhardt@outlook.com>
2019-06-08 14:05:38 +0200

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/utils/property-symbols-in/docs/types/index.d.ts
@@ -0,0 +1,39 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns an array of an object's own and inherited symbol properties.
+*
+* ## Notes
+*
+* -   If provided `null` or `undefined`, the function returns an empty array.
+*
+* @param value - input object
+* @returns a list of own and inherited symbol properties
+*
+* @example
+* var symbols = propertySymbolsIn( [] );
+*/
+declare function propertySymbolsIn( value: any ): Array<symbol>;
+
+
+// EXPORTS //
+
+export = propertySymbolsIn;
