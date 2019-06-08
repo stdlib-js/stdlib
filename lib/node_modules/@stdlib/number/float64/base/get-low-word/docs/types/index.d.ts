5b01d18e10b109664fdf8d074ffc19ea09fcc4af
Planeshifter <pburckhardt@outlook.com>
2019-06-08 16:36:25 +0200

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/number/float64/base/get-low-word/docs/types/index.d.ts
@@ -0,0 +1,36 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns a 32-bit unsigned integer corresponding to the less significant 32 bits of a double-precision floating-point number.
+*
+* @param x - input value
+* @returns lower order word (unsigned 32-bit integer)
+*
+* @example
+* var w = getLowWord( 3.14e201 ); // => 10010011110010110101100010000010
+* // returns 2479577218
+*/
+declare function getLowWord( x: number ): number;
+
+
+// EXPORTS //
+
+export = getLowWord;
