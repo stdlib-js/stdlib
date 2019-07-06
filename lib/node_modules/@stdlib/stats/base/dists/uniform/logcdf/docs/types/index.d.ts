4881e34b49638f31a6b95a1e61992cdb3f799f1c
Planeshifter <pburckhardt@outlook.com>
2019-07-05 21:44:53 -0400

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/stats/base/dists/uniform/logcdf/docs/types/index.d.ts
@@ -0,0 +1,121 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Evaluates the natural logarithm of the cumulative distribution function (CDF) for a uniform distribution.
+*
+* @param x - input value
+* @returns evaluated logCDF
+*/
+type Unary = ( x: number ) => number;
+
+/**
+* Interface for the logarithm of the cumulative distribution function (CDF) of a uniform distribution.
+*/
+interface LogCDF {
+	/**
+	* Evaluates the logarithm of the cumulative distribution function (CDF) for a uniform distribution with minimum support `a` and maximum support `b` at a value `x`.
+	*
+	* ## Notes
+	*
+	* -   If provided `a >= b`, the function returns `NaN`.
+	*
+	* @param x - input value
+	* @param a - minimum support
+	* @param b - maximum support
+	* @returns evaluated logCDF
+	*
+	* @example
+	* var y = logcdf( 9.0, 0.0, 10.0 );
+	* // returns ~-0.105
+	*
+	* @example
+	* var y = logcdf( 0.5, 0.0, 2.0 );
+	* // returns ~-1.386
+	*
+	* @example
+	* var y = logcdf( +Infinity, 2.0, 4.0 );
+	* // returns 0.0
+	*
+	* @example
+	* var y = logcdf( -Infinity, 2.0, 4.0 );
+	* // returns -Infinity
+	*
+	* @example
+	* var y = logcdf( NaN, 0.0, 1.0 );
+	* // returns NaN
+	*
+	* @example
+	* var y = logcdf( 0.0, NaN, 1.0 );
+	* // returns NaN
+	*
+	* @example
+	* var y = logcdf( 0.0, 0.0, NaN );
+	* // returns NaN
+	*
+	* @example
+	* var y = logcdf( 2.0, 1.0, 0.0 );
+	* // returns NaN
+	*/
+	( x: number, a: number, b: number ): number;
+
+	/**
+	* Returns a function for evaluating the logarithm of the cumulative distribution function (CDF) for a uniform distribution with minimum support `a` and maximum support `b`.
+	*
+	* @param a - minimum support
+	* @param b - maximum support
+	* @returns logCDF
+	*
+	* @example
+	* var mylogcdf = logcdf.factory( 0.0, 10.0 );
+	* var y = mylogcdf( 0.5 );
+	* // returns ~-2.996
+	*
+	* y = mylogcdf( 8.0 );
+	* // returns ~-0.223
+	*/
+	factory( a: number, b: number ): Unary;
+}
+
+/**
+* Uniform distribution logarithm of cumulative distribution function (CDF).
+*
+* @param x - input value
+* @param a - minimum support
+* @param b - maximum support
+* @returns evaluated logCDF
+*
+* @example
+* var y = logcdf( 5.0, 0.0, 4.0 );
+* // returns 0.0
+*
+* var mylogcdf = logcdf.factory( 0.0, 10.0 );
+* y = mylogcdf( 0.5 );
+* // returns ~-2.996
+*
+* y = mylogcdf( 8.0 );
+* // returns ~-0.223
+*/
+declare var logCDF: LogCDF;
+
+
+// EXPORTS //
+
+export = logCDF;
