9d99e7ae6f6cb492af28ff613e2d15c150232126
Planeshifter <pburckhardt@outlook.com>
2019-09-01 22:55:20 -0400

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/utils/property-descriptor/docs/types/index.d.ts
@@ -0,0 +1,48 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns a property descriptor for an object's own property.
+*
+* ## Notes
+*
+* -   In contrast to the built-in `Object.getOwnPropertyDescriptor()`, this function returns `null` if provided `undefined` or `null`, rather than throwing an error.
+* -   In contrast to the built-in `Object.getOwnPropertyDescriptor()`, this function returns `null` if an object does not have a provided property, rather than `undefined`.
+*
+*
+* @param value - input object
+* @param  property - property
+* @returns property descriptor or null
+*
+* @example
+* var obj = {
+*     'beep': 'boop',
+*     'foo': 3.14
+* };
+*
+* var desc = getOwnPropertyDescriptor( obj, 'foo' );
+* // returns {'configurable':true,'enumerable':true,'writable':true,'value':3.14}
+*/
+declare function getOwnPropertyDescriptor( value: any, property: string | symbol ): TypedPropertyDescriptor<any> | null; // tslint-disable-line max-line-length
+
+
+// EXPORTS //
+
+export = getOwnPropertyDescriptor;
