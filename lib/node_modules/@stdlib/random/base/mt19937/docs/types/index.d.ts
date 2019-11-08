d05c6a3c0ea45bda0a108f09e90385d51062ccb7
Planeshifter <pburckhardt@outlook.com>
2019-11-08 15:46:30 -0500

    Add Typescript definition

---
+++ lib/node_modules/@stdlib/random/base/mt19937/docs/types/index.d.ts
@@ -0,0 +1,182 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+import * as random from '@stdlib/types/random';
+
+/**
+* Interface defining `factory` options.
+*/
+interface Options {
+	/**
+	* Pseudorandom number generator seed.
+	*/
+	seed?: random.PRNGSeedMT19937;
+
+	/**
+	* Pseudorandom number generator state.
+	*/
+	state?: random.PRNGStateMT19937;
+
+	/**
+	* Specifies whether to copy a provided pseudorandom number generator state.
+	*/
+	copy?: boolean;
+}
+
+/**
+* Interface for PRNG properties and methods.
+*/
+interface PRNG {
+	/**
+	* Generator name.
+	*/
+	readonly NAME: string;
+
+	/**
+	* Minimum possible value.
+	*/
+	readonly MIN: number;
+
+	/**
+	* Maximum possible value.
+	*/
+	readonly MAX: number;
+
+	/**
+	* PRNG seed.
+	*/
+	readonly seed: random.PRNGSeedMT19937;
+
+	/**
+	* PRNG seed length.
+	*/
+	readonly seedLength: number;
+
+	/**
+	* PRNG state.
+	*/
+	state: random.PRNGStateMT19937;
+
+	/**
+	* PRNG state length.
+	*/
+	readonly stateLength: number;
+
+	/**
+	* PRNG state size (in bytes).
+	*/
+	readonly byteLength: number;
+
+	/**
+	* Serializes the pseudorandom number generator as a JSON object.
+	*
+	* @returns JSON representation
+	*/
+	toJSON(): string;
+}
+
+/**
+* Interface for generating pseudorandom integers on the interval `[1, 4294967295]`.
+*/
+interface NullaryFunction extends PRNG {
+	/**
+	* Returns a pseudorandom integer on the interval `[1, 4294967295]`.
+	*
+	* @returns pseudorandom number
+	*/
+	(): number;
+}
+
+/**
+* Interface for generating pseudorandom integers on the interval `[1, 4294967295]`.
+*/
+interface Random extends PRNG {
+	/**
+	* Returns a pseudorandom integer on the interval `[1, 4294967295]`.
+	*
+	* ## Notes
+	*
+	* -   This pseudorandom number generator (PRNG) is a 32-bit Mersenne Twister pseudorandom number generator.
+	* -   The PRNG is *not* a cryptographically secure PRNG.
+	* -   The PRNG has a period of 2^19937 - 1.
+	*
+	* @returns pseudorandom number
+	*
+	* @example
+	* var v = mt19937();
+	* // returns <number>
+	*/
+	(): number;
+
+	/**
+	* Returns a 32-bit Mersenne Twister pseudorandom number generator.
+	*
+	* @param options - function options
+	* @param options.seed - pseudorandom number generator seed
+	* @param options.state - pseudorandom number generator state
+	* @param options.copy - boolean indicating whether to copy a provided pseudorandom number generator state (default: true)
+	* @throws must provide valid options
+	* @returns pseudorandom number generator
+	*
+	* @example
+	* var rand = mt19937.factory();
+	* var v = rand();
+	* // returns <number>
+	*
+	* @example
+	* var rand = mt19937.factory({
+	*     'seed': 12345
+	* });
+	* var v = rand();
+	* // returns <number>
+	*/
+	factory( options?: Options ): NullaryFunction;
+}
+
+/**
+* Returns a pseudorandom integer on the interval `[1, 4294967295]`.
+*
+* ## Notes
+*
+* -   This pseudorandom number generator (PRNG) is a 32-bit Mersenne Twister pseudorandom number generator.
+* -   The PRNG is *not* a cryptographically secure PRNG.
+* -   The PRNG has a period of 2^19937 - 1.
+*
+* @returns pseudorandom number
+*
+* @example
+* var v = mt19937();
+* // returns <number>
+*
+* @example
+* var rand = mt19937.factory({
+*     'seed': 12345
+* });
+* var v = rand();
+* // returns <number>
+*/
+declare var mt19937: Random;
+
+
+// EXPORTS //
+
+export = mt19937;
