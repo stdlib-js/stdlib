71d190482d36774d027bd4e60851241801b22f3a
Planeshifter <pburckhardt@outlook.com>
2019-11-16 01:15:22 -0500

    Remove SASS support

--- tools/docs/www/config/webpack.config.js
+++ tools/docs/www/config/webpack.config.js
@@ -72,8 +72,6 @@ const STDLIB_PATH = './../../../../lib/node_modules';
 // style files regexes
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
-const sassRegex = /\.(scss|sass)$/;
-const sassModuleRegex = /\.module\.(scss|sass)$/;
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
@@ -488,39 +486,6 @@ module.exports = function(webpackEnv) {
 								modules: true,
 								getLocalIdent: getCSSModuleLocalIdent,
 							}),
-						},
-						// Opt-in support for SASS (using .scss or .sass extensions).
-						// By default we support SASS Modules with the
-						// extensions .module.scss or .module.sass
-						{
-							test: sassRegex,
-							exclude: sassModuleRegex,
-							use: getStyleLoaders(
-								{
-									importLoaders: 2,
-									sourceMap: isEnvProduction && shouldUseSourceMap,
-								},
-								'sass-loader'
-							),
-							// Don't consider CSS imports dead code even if the
-							// containing package claims to have no side effects.
-							// Remove this when webpack adds a warning or an error for this.
-							// See https://github.com/webpack/webpack/issues/6571
-							sideEffects: true,
-						},
-						// Adds support for CSS Modules, but using SASS
-						// using the extension .module.scss or .module.sass
-						{
-							test: sassModuleRegex,
-							use: getStyleLoaders(
-								{
-									importLoaders: 2,
-									sourceMap: isEnvProduction && shouldUseSourceMap,
-									modules: true,
-									getLocalIdent: getCSSModuleLocalIdent,
-								},
-								'sass-loader'
-							),
 						},
 						// "file" loader makes sure those assets get served by WebpackDevServer.
 						// When you `import` an asset, you get its (virtual) filename.
