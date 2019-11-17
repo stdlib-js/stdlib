01b13474557a62c95217d7a7dad1f92311128d68
Planeshifter <pburckhardt@outlook.com>
2019-11-17 14:56:48 -0500

    Remove service worker script

--- tools/docs/www/src/index.jsx
+++ tools/docs/www/src/index.jsx
@@ -21,14 +21,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './client.jsx';
-import * as serviceWorker from './service_worker.js';
 
 
 // MAIN //
 
 ReactDOM.hydrate( <App />, document.getElementById( 'root' ) );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
