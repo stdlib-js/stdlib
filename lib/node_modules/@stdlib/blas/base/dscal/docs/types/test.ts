4797c34f8401357371df79d4d543972487bc21b2
kgryte <kgryte@gmail.com>
2020-03-14 15:39:05 -0700

    Add TypeScript declaration

---
+++ lib/node_modules/@stdlib/blas/base/dscal/docs/types/test.ts
@@ -0,0 +1,187 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import dscal = require( './index' );
+
+
+// TESTS //
+
+// The function returns a Float64Array...
+{
+	const x = new Float64Array( 10 );
+
+	dscal( x.length, 5.0, x, 1 ); // $ExpectType Float64Array
+}
+
+// The compiler throws an error if the function is provided a first argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	dscal( '10', 5.0, x, 1 ); // $ExpectError
+	dscal( true, 5.0, x, 1 ); // $ExpectError
+	dscal( false, 5.0, x, 1 ); // $ExpectError
+	dscal( null, 5.0, x, 1 ); // $ExpectError
+	dscal( undefined, 5.0, x, 1 ); // $ExpectError
+	dscal( [], 5.0, x, 1 ); // $ExpectError
+	dscal( {}, 5.0, x, 1 ); // $ExpectError
+	dscal( ( x: number ): number => x, 5.0, x, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a second argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	dscal( x.length, '10', x, 1 ); // $ExpectError
+	dscal( x.length, true, x, 1 ); // $ExpectError
+	dscal( x.length, false, x, 1 ); // $ExpectError
+	dscal( x.length, null, x, 1 ); // $ExpectError
+	dscal( x.length, undefined, x, 1 ); // $ExpectError
+	dscal( x.length, [], x, 1 ); // $ExpectError
+	dscal( x.length, {}, x, 1 ); // $ExpectError
+	dscal( x.length, ( x: number ): number => x, x, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a third argument which is not a Float64Array...
+{
+	const x = new Float64Array( 10 );
+
+	dscal( x.length, 5.0, 10, 1 ); // $ExpectError
+	dscal( x.length, 5.0, '10', 1 ); // $ExpectError
+	dscal( x.length, 5.0, true, 1 ); // $ExpectError
+	dscal( x.length, 5.0, false, 1 ); // $ExpectError
+	dscal( x.length, 5.0, null, 1 ); // $ExpectError
+	dscal( x.length, 5.0, undefined, 1 ); // $ExpectError
+	dscal( x.length, 5.0, [], 1 ); // $ExpectError
+	dscal( x.length, 5.0, {}, 1 ); // $ExpectError
+	dscal( x.length, 5.0, ( x: number ): number => x, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a fourth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	dscal( x.length, 5.0, x, '10' ); // $ExpectError
+	dscal( x.length, 5.0, x, true ); // $ExpectError
+	dscal( x.length, 5.0, x, false ); // $ExpectError
+	dscal( x.length, 5.0, x, null ); // $ExpectError
+	dscal( x.length, 5.0, x, undefined ); // $ExpectError
+	dscal( x.length, 5.0, x, [] ); // $ExpectError
+	dscal( x.length, 5.0, x, {} ); // $ExpectError
+	dscal( x.length, 5.0, x, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided an unsupported number of arguments...
+{
+	const x = new Float64Array( 10 );
+
+	dscal(); // $ExpectError
+	dscal( x.length ); // $ExpectError
+	dscal( x.length, 5.0 ); // $ExpectError
+	dscal( x.length, 5.0, x ); // $ExpectError
+	dscal( x.length, 5.0, x, 1, 10 ); // $ExpectError
+}
+
+// Attached to main export is an `ndarray` method which returns a Float64Array...
+{
+	const x = new Float64Array( 10 );
+
+	dscal.ndarray( x.length, 5.0, x, 1, 0 ); // $ExpectType Float64Array
+}
+
+// The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	dscal.ndarray( '10', 5.0, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( true, 5.0, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( false, 5.0, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( null, 5.0, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( undefined, 5.0, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( [], 5.0, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( {}, 5.0, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( ( x: number ): number => x, 5.0, x, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a second argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	dscal.ndarray( x.length, '10', x, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, true, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, false, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, null, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, undefined, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, [], x, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, {}, x, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, ( x: number ): number => x, x, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a third argument which is not a Float64Array...
+{
+	const x = new Float64Array( 10 );
+
+	dscal.ndarray( x.length, 5.0, 10, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, '10', 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, true, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, false, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, null, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, undefined, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, [], 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, {}, 1, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, ( x: number ): number => x, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a fourth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	dscal.ndarray( x.length, 5.0, x, '10', 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, true, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, false, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, null, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, undefined, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, [], 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, {}, 0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, ( x: number ): number => x, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a fifth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	dscal.ndarray( x.length, 5.0, x, 1, '10' ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1, true ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1, false ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1, null ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1, undefined ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1, [] ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1, {} ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided an unsupported number of arguments...
+{
+	const x = new Float64Array( 10 );
+
+	dscal.ndarray(); // $ExpectError
+	dscal.ndarray( x.length ); // $ExpectError
+	dscal.ndarray( x.length, 5.0 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1 ); // $ExpectError
+	dscal.ndarray( x.length, 5.0, x, 1, 0, 10 ); // $ExpectError
+}
