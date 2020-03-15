37cdbad9473fd050c8f7185c122a33d0f680b6c6
kgryte <kgryte@gmail.com>
2020-03-15 00:11:43 -0700

    Add BLAS level 1 routine `sdsdot`
    Resolves #275.

---
+++ lib/node_modules/@stdlib/blas/base/sdsdot/include/stdlib/blas/base/sdsdot_cblas.h
@@ -0,0 +1,41 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+* Header file containing function declarations for the C interface to the CBLAS Level 1 routine `cblas_sdsdot`.
+*/
+#ifndef SDSDOT_CBLAS_H
+#define SDSDOT_CBLAS_H
+
+/*
+* If C++, prevent name mangling so that the compiler emits a binary file having undecorated names, thus mirroring the behavior of a C compiler.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+* Computes the dot product of two single-precision floating-point vectors with extended accumulation.
+*/
+float cblas_sdsdot( const int N, const float scalar, const float *X, const int strideX, const float *Y, const int strideY );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !SDSDOT_CBLAS_H
