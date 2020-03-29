149456f143eb696cf404d9c688d40f6ffde67761
kgryte <kgryte@gmail.com>
2020-03-28 18:51:57 -0700

    Add iterator to compute the inverse coversed sine

---
+++ lib/node_modules/@stdlib/math/iter/special/acoversin/docs/types/test.ts
@@ -0,0 +1,68 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import iterAcoversin = require( './index' );
+
+/**
+* Returns an iterator protocol-compliant object.
+*
+* @returns iterator protocol-compliant object
+*/
+function iterator() {
+	return {
+		'next': next
+	};
+
+	/**
+	* Implements the iterator protocol `next` method.
+	*
+	* @returns iterator protocol-compliant object
+	*/
+	function next() {
+		return {
+			'value': true,
+			'done': false
+		};
+	}
+}
+
+
+// TESTS //
+
+// The function returns an iterator...
+{
+	iterAcoversin( iterator() ); // $ExpectType Iterator
+}
+
+// The compiler throws an error if the function is provided a first argument which is not an iterator protocol-compliant object...
+{
+	iterAcoversin( '5' ); // $ExpectError
+	iterAcoversin( 5 ); // $ExpectError
+	iterAcoversin( true ); // $ExpectError
+	iterAcoversin( false ); // $ExpectError
+	iterAcoversin( null ); // $ExpectError
+	iterAcoversin( undefined ); // $ExpectError
+	iterAcoversin( [] ); // $ExpectError
+	iterAcoversin( {} ); // $ExpectError
+	iterAcoversin( ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided insufficient arguments...
+{
+	iterAcoversin(); // $ExpectError
+}
