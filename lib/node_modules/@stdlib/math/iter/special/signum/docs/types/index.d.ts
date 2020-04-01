3f1739b19b458fcf7cc1ff913675c68bb781051e
kgryte <kgryte@gmail.com>
2020-03-31 23:11:30 -0700

    Add iterator to evaluate the signum function

---
+++ lib/node_modules/@stdlib/math/iter/special/signum/docs/types/index.d.ts
@@ -0,0 +1,59 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+import { Iterator as Iter, IterableIterator } from '@stdlib/types/iter';
+
+// Define a union type representing both iterable and non-iterable iterators:
+type Iterator = Iter | IterableIterator;
+
+/**
+* Returns an iterator which iteratively evaluates the signum function.
+*
+* ## Notes
+*
+* -   If an environment supports `Symbol.iterator` **and** a provided iterator is iterable, the returned iterator is iterable.
+*
+* @param iterator - input iterator
+* @returns iterator
+*
+* @example
+* var randu = require( `@stdlib/random/iter/randu` );
+*
+* var iter = iterSignum( randu() );
+*
+* var r = iter.next().value;
+* // returns <number>
+*
+* r = iter.next().value;
+* // returns <number>
+*
+* r = iter.next().value;
+* // returns <number>
+*
+* // ...
+*/
+declare function iterSignum( iterator: Iterator ): Iterator;
+
+
+// EXPORTS //
+
+export = iterSignum;
