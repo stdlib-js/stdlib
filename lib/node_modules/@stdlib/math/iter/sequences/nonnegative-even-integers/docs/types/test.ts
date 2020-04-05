1c0edeb4ee7f2456cebf9e4d5df3b471fab2b9aa
kgryte <kgryte@gmail.com>
2020-04-05 01:25:30 -0700

    Add iterator to generate a sequence of nonnegative even integers

---
+++ lib/node_modules/@stdlib/math/iter/sequences/nonnegative-even-integers/docs/types/test.ts
@@ -0,0 +1,45 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import iterNonNegativeEvenIntegersSeq = require( './index' );
+
+
+// TESTS //
+
+// The function returns an iterator...
+{
+	iterNonNegativeEvenIntegersSeq(); // $ExpectType Iterator
+	iterNonNegativeEvenIntegersSeq( {} ); // $ExpectType Iterator
+	iterNonNegativeEvenIntegersSeq( { 'iter': 10 } ); // $ExpectType Iterator
+}
+
+// The compiler throws an error if the function is provided a first argument which is not an options object...
+{
+	iterNonNegativeEvenIntegersSeq( null ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided an `iter` option which is not a number...
+{
+	iterNonNegativeEvenIntegersSeq( { 'iter': '5' } ); // $ExpectError
+	iterNonNegativeEvenIntegersSeq( { 'iter': true } ); // $ExpectError
+	iterNonNegativeEvenIntegersSeq( { 'iter': false } ); // $ExpectError
+	iterNonNegativeEvenIntegersSeq( { 'iter': null } ); // $ExpectError
+	iterNonNegativeEvenIntegersSeq( { 'iter': [] } ); // $ExpectError
+	iterNonNegativeEvenIntegersSeq( { 'iter': {} } ); // $ExpectError
+	iterNonNegativeEvenIntegersSeq( { 'iter': ( x: number ): number => x } ); // $ExpectError
+}
