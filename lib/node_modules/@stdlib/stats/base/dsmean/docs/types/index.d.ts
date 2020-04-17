2a9fcbe031910c73ff13145983cc0669201ed252
kgryte <kgryte@gmail.com>
2020-04-17 09:31:21 -0700

    Add strided interface to compute the arithmetic mean with extended accumulation and result

---
+++ lib/node_modules/@stdlib/stats/base/dsmean/docs/types/index.d.ts
@@ -0,0 +1,92 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Interface describing `dsmean`.
+*/
+interface Routine {
+	/**
+	* Computes the arithmetic mean of a single-precision floating-point strided array using extended accumulation and returning an extended precision result.
+	*
+	* @param N - number of indexed elements
+	* @param x - input array
+	* @param stride - stride length
+	* @returns arithmetic mean
+	*
+	* @example
+	* var Float32Array = require( `@stdlib/array/float32` );
+	*
+	* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+	*
+	* var v = dsmean( x.length, x, 1 );
+	* // returns ~0.3333
+	*/
+	( N: number, x: Float32Array, stride: number ): number;
+
+	/**
+	* Computes the arithmetic mean of a single-precision floating-point strided array using extended accumulation and alternative indexing semantics and returning an extended precision result.
+	*
+	* @param N - number of indexed elements
+	* @param x - input array
+	* @param stride - stride length
+	* @param offset - starting index
+	* @returns arithmetic mean
+	*
+	* @example
+	* var Float32Array = require( `@stdlib/array/float32` );
+	*
+	* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+	*
+	* var v = dsmean.ndarray( x.length, x, 1, 0 );
+	* // returns ~0.3333
+	*/
+	ndarray( N: number, x: Float32Array, stride: number, offset: number ): number; // tslint:disable-line:max-line-length
+}
+
+/**
+* Computes the arithmetic mean of a single-precision floating-point strided array using extended accumulation and returning an extended precision result.
+*
+* @param N - number of indexed elements
+* @param x - input array
+* @param stride - stride length
+* @returns arithmetic mean
+*
+* @example
+* var Float32Array = require( `@stdlib/array/float32` );
+*
+* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+*
+* var v = dsmean( x.length, x, 1 );
+* // returns ~0.3333
+*
+* @example
+* var Float32Array = require( `@stdlib/array/float32` );
+*
+* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+*
+* var v = dsmean.ndarray( x.length, x, 1, 0 );
+* // returns ~0.3333
+*/
+declare var dsmean: Routine;
+
+
+// EXPORTS //
+
+export = dsmean;
