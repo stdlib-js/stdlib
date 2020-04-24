f6b06425e211c0ae9b4365b7dcf5e6eee2657d64
kgryte <kgryte@gmail.com>
2020-04-23 18:35:21 -0700

    Refactor to N-API and clean-up

---
+++ lib/node_modules/@stdlib/blas/base/dcopy/examples/c/example.c
@@ -0,0 +1,42 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/blas/base/dcopy.h"
+#include <stdio.h>
+
+int main() {
+	// Create strided arrays:
+	double x[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 };
+	double y[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+
+	// Specify the number of elements:
+	int N = 4;
+
+	// Specify stride lengths:
+	int strideX = 2;
+	int strideY = -2;
+
+	// Copy elements:
+	c_dcopy( N, x, strideX, y, strideY );
+
+	// Print the result:
+	for ( int i = 0; i < 8; i++ ) {
+		printf( "y[ %i ] = %lf", i, y[ i ] );
+		printf( "\n" );
+	}
+}
