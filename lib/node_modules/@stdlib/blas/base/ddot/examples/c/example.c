83f3f42db0b62571b3abdd57b326dee99d34446c
kgryte <kgryte@gmail.com>
2020-04-23 20:14:05 -0700

    Add N-API bindings, fix index bug, and clean-up

---
+++ lib/node_modules/@stdlib/blas/base/ddot/examples/c/example.c
@@ -0,0 +1,40 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/blas/base/ddot.h"
+#include <stdio.h>
+
+int main() {
+	// Create strided arrays:
+	double x[] = { 1.0, -2.0, 3.0, -4.0, 5.0, -6.0, 7.0, -8.0 };
+	double y[] = { 1.0, -2.0, 3.0, -4.0, 5.0, -6.0, 7.0, -8.0 };
+
+	// Specify the number of elements:
+	int N = 8;
+
+	// Specify strides:
+	int strideX = 1;
+	int strideY = -1;
+
+	// Compute the dot product:
+	double d = c_ddot( N, x, strideX, y, strideY );
+
+	// Print the result:
+	printf( "dot product: %lf", d );
+	printf( "\n" );
+}
