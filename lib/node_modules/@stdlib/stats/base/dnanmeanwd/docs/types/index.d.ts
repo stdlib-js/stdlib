43d789bc0f899d8e613c03cad8477d6f783f3d90
kgryte <kgryte@gmail.com>
2020-05-02 00:10:16 -0700

    Add strided interface to compute the arithmetic mean ignoring `NaN` values and using Welford's algorithm

---
+++ lib/node_modules/@stdlib/stats/base/dnanmeanwd/docs/types/index.d.ts
@@ -0,0 +1,92 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Interface describing `dnanmeanwd`.
+*/
+interface Routine {
+	/**
+	* Computes the arithmetic mean of a double-precision floating-point strided array, using Welford's algorithm and ignoring `NaN` values.
+	*
+	* @param N - number of indexed elements
+	* @param x - input array
+	* @param stride - stride length
+	* @returns arithmetic mean
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = new Float64Array( [ 1.0, -2.0, NaN, 2.0 ] );
+	*
+	* var v = dnanmeanwd( x.length, x, 1 );
+	* // returns ~0.3333
+	*/
+	( N: number, x: Float64Array, stride: number ): number;
+
+	/**
+	* Computes the arithmetic mean of a double-precision floating-point strided array, ignoring `NaN` values and using Welford's algorithm and alternative indexing semantics.
+	*
+	* @param N - number of indexed elements
+	* @param x - input array
+	* @param stride - stride length
+	* @param offset - starting index
+	* @returns arithmetic mean
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = new Float64Array( [ 1.0, -2.0, NaN, 2.0 ] );
+	*
+	* var v = dnanmeanwd.ndarray( x.length, x, 1, 0 );
+	* // returns ~0.3333
+	*/
+	ndarray( N: number, x: Float64Array, stride: number, offset: number ): number; // tslint:disable-line:max-line-length
+}
+
+/**
+* Computes the arithmetic mean of a double-precision floating-point strided array, using Welford's algorithm and ignoring `NaN` values.
+*
+* @param N - number of indexed elements
+* @param x - input array
+* @param stride - stride length
+* @returns arithmetic mean
+*
+* @example
+* var Float64Array = require( `@stdlib/array/float64` );
+*
+* var x = new Float64Array( [ 1.0, -2.0, NaN, 2.0 ] );
+*
+* var v = dnanmeanwd( x.length, x, 1 );
+* // returns ~0.3333
+*
+* @example
+* var Float64Array = require( `@stdlib/array/float64` );
+*
+* var x = new Float64Array( [ 1.0, -2.0, NaN, 2.0 ] );
+*
+* var v = dnanmeanwd.ndarray( x.length, x, 1, 0 );
+* // returns ~0.3333
+*/
+declare var dnanmeanwd: Routine;
+
+
+// EXPORTS //
+
+export = dnanmeanwd;
