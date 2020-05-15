d5964bd0612c1c1a8e533bd849e01e58433d9dcf
kgryte <kgryte@gmail.com>
2020-05-15 11:34:46 -0700

    Add strided interface to compute the standard deviation using Youngs' and Cramer's algorithm

---
+++ lib/node_modules/@stdlib/stats/base/sstdevyc/docs/types/index.d.ts
@@ -0,0 +1,95 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Interface describing `sstdevyc`.
+*/
+interface Routine {
+	/**
+	* Computes the standard deviation of a single-precision floating-point strided array using a one-pass algorithm proposed by Youngs and Cramer.
+	*
+	* @param N - number of indexed elements
+	* @param correction - degrees of freedom adjustment
+	* @param x - input array
+	* @param stride - stride length
+	* @returns standard deviation
+	*
+	* @example
+	* var Float32Array = require( `@stdlib/array/float32` );
+	*
+	* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+	*
+	* var v = sstdevyc( x.length, 1, x, 1 );
+	* // returns ~2.0817
+	*/
+	( N: number, correction: number, x: Float32Array, stride: number ): number;
+
+	/**
+	* Computes the standard deviation of a single-precision floating-point strided array using a one-pass algorithm proposed by Youngs and Cramer and alternative indexing semantics.
+	*
+	* @param N - number of indexed elements
+	* @param correction - degrees of freedom adjustment
+	* @param x - input array
+	* @param stride - stride length
+	* @param offset - starting index
+	* @returns standard deviation
+	*
+	* @example
+	* var Float32Array = require( `@stdlib/array/float32` );
+	*
+	* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+	*
+	* var v = sstdevyc.ndarray( x.length, 1, x, 1, 0 );
+	* // returns ~2.0817
+	*/
+	ndarray( N: number, correction: number, x: Float32Array, stride: number, offset: number ): number; // tslint:disable-line:max-line-length
+}
+
+/**
+* Computes the standard deviation of a single-precision floating-point strided array using a one-pass algorithm proposed by Youngs and Cramer.
+*
+* @param N - number of indexed elements
+* @param correction - degrees of freedom adjustment
+* @param x - input array
+* @param stride - stride length
+* @returns standard deviation
+*
+* @example
+* var Float32Array = require( `@stdlib/array/float32` );
+*
+* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+*
+* var v = sstdevyc( x.length, 1, x, 1 );
+* // returns ~2.0817
+*
+* @example
+* var Float32Array = require( `@stdlib/array/float32` );
+*
+* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+*
+* var v = sstdevyc.ndarray( x.length, 1, x, 1, 0 );
+* // returns ~2.0817
+*/
+declare var sstdevyc: Routine;
+
+
+// EXPORTS //
+
+export = sstdevyc;
