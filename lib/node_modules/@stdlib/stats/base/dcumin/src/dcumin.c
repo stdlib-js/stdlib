99de9945d80fc7340774aaf5756c4fe12d3805e5
kgryte <kgryte@gmail.com>
2020-05-17 21:12:03 -0700

    Add strided interface to compute a cumulative minimum value

---
+++ lib/node_modules/@stdlib/stats/base/dcumin/src/dcumin.c
@@ -0,0 +1,80 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dcumin.h"
+#include "stdlib/math/base/assert/is_nan.h"
+#include "stdlib/math/base/assert/is_negative_zero.h"
+#include <stdint.h>
+
+/**
+* Computes the cumulative minimum of double-precision floating-point strided array elements.
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  X stride length
+* @param Y        output array
+* @param strideY  Y stride length
+*/
+void stdlib_strided_dcumin( const int64_t N, const double *X, const int64_t strideX, double *Y, const int64_t strideY ) {
+	double min;
+	int64_t ix;
+	int64_t iy;
+	int64_t i;
+	double v;
+
+	if ( N <= 0 ) {
+		return;
+	}
+	if ( strideX < 0 ) {
+		ix = (1-N) * strideX;
+	} else {
+		ix = 0;
+	}
+	if ( strideY < 0 ) {
+		iy = (1-N) * strideY;
+	} else {
+		iy = 0;
+	}
+	min = X[ ix ];
+	Y[ iy ] = min;
+
+	iy += strideY;
+	i = 1;
+	if ( !stdlib_base_is_nan( min ) ) {
+		for (; i < N; i++ ) {
+			ix += strideX;
+			v = X[ ix ];
+			if ( stdlib_base_is_nan( v ) ) {
+				min = v;
+				break;
+			}
+			if ( v < min || ( v == min && stdlib_base_is_negative_zero( v ) ) ) {
+				min = v;
+			}
+			Y[ iy ] = min;
+			iy += strideY;
+		}
+	}
+	if ( stdlib_base_is_nan( min ) ) {
+		for (; i < N; i++ ) {
+			Y[ iy ] = min;
+			iy += strideY;
+		}
+	}
+	return;
+}
