2862f2c92b06212c3a985f1eedefecc584f65002
kgryte <kgryte@gmail.com>
2020-05-26 10:22:51 -0700

    Add strided interface to compute the cumulative minimum absolute value

---
+++ lib/node_modules/@stdlib/stats/base/cuminabs/docs/types/test.ts
@@ -0,0 +1,248 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import cuminabs = require( './index' );
+
+
+// TESTS //
+
+// The function returns a numeric array...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs( x.length, x, 1, y, 1 ); // $ExpectType NumericArray
+}
+
+// The compiler throws an error if the function is provided a first argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs( '10', x, 1, y, 1 ); // $ExpectError
+	cuminabs( true, x, 1, y, 1 ); // $ExpectError
+	cuminabs( false, x, 1, y, 1 ); // $ExpectError
+	cuminabs( null, x, 1, y, 1 ); // $ExpectError
+	cuminabs( undefined, x, 1, y, 1 ); // $ExpectError
+	cuminabs( [], x, 1, y, 1 ); // $ExpectError
+	cuminabs( {}, x, 1, y, 1 ); // $ExpectError
+	cuminabs( ( x: number ): number => x, x, 1, y, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a second argument which is not a numeric array...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs( x.length, 10, 1, y, 1 ); // $ExpectError
+	cuminabs( x.length, '10', 1, y, 1 ); // $ExpectError
+	cuminabs( x.length, true, 1, y, 1 ); // $ExpectError
+	cuminabs( x.length, false, 1, y, 1 ); // $ExpectError
+	cuminabs( x.length, null, 1, y, 1 ); // $ExpectError
+	cuminabs( x.length, undefined, 1, y, 1 ); // $ExpectError
+	cuminabs( x.length, [ '1' ], 1, y, 1 ); // $ExpectError
+	cuminabs( x.length, {}, 1, y, 1 ); // $ExpectError
+	cuminabs( x.length, ( x: number ): number => x, 1, y, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a third argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs( x.length, x, '10', y, 1 ); // $ExpectError
+	cuminabs( x.length, x, true, y, 1 ); // $ExpectError
+	cuminabs( x.length, x, false, y, 1 ); // $ExpectError
+	cuminabs( x.length, x, null, y, 1 ); // $ExpectError
+	cuminabs( x.length, x, undefined, y, 1 ); // $ExpectError
+	cuminabs( x.length, x, [], y, 1 ); // $ExpectError
+	cuminabs( x.length, x, {}, y, 1 ); // $ExpectError
+	cuminabs( x.length, x, ( x: number ): number => x, y, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a fourth argument which is not a numeric array...
+{
+	const x = new Float64Array( 10 );
+
+	cuminabs( x.length, x, 1, 10, 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, '10', 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, true, 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, false, 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, null, 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, undefined, 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, [ '1' ], 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, {}, 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, ( x: number ): number => x, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a fifth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs( x.length, x, 1, y, '10' ); // $ExpectError
+	cuminabs( x.length, x, 1, y, true ); // $ExpectError
+	cuminabs( x.length, x, 1, y, false ); // $ExpectError
+	cuminabs( x.length, x, 1, y, null ); // $ExpectError
+	cuminabs( x.length, x, 1, y, undefined ); // $ExpectError
+	cuminabs( x.length, x, 1, y, [] ); // $ExpectError
+	cuminabs( x.length, x, 1, y, {} ); // $ExpectError
+	cuminabs( x.length, x, 1, y, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided an unsupported number of arguments...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs(); // $ExpectError
+	cuminabs( x.length ); // $ExpectError
+	cuminabs( x.length, x ); // $ExpectError
+	cuminabs( x.length, x, 1 ); // $ExpectError
+	cuminabs( x.length, x, 1, y ); // $ExpectError
+	cuminabs( x.length, x, 1, y, 1, 10 ); // $ExpectError
+}
+
+// Attached to main export is an `ndarray` method which returns a numeric array...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, 0 ); // $ExpectType NumericArray
+}
+
+// The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs.ndarray( '10', x, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( true, x, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( false, x, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( null, x, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( undefined, x, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( [], x, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( {}, x, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( ( x: number ): number => x, x, 1, 0, y, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a second argument which is not a numeric array...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs.ndarray( x.length, 10, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, '10', 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, true, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, false, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, null, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, undefined, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, [ '1' ], 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, {}, 1, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, ( x: number ): number => x, 1, 0, y, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a third argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs.ndarray( x.length, x, '10', 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, true, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, false, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, null, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, undefined, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, [], 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, {}, 0, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, ( x: number ): number => x, 0, y, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a fourth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs.ndarray( x.length, x, 1, '10', y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, true, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, false, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, null, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, undefined, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, [], y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, {}, y, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, ( x: number ): number => x, y, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a fifth argument which is not a numeric array...
+{
+	const x = new Float64Array( 10 );
+
+	cuminabs.ndarray( x.length, x, 1, 0, 10, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, '10', 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, true, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, false, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, null, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, undefined, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, [ '1' ], 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, {}, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, ( x: number ): number => x, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a sixth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs.ndarray( x.length, x, 1, 0, y, '10', 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, true, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, false, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, null, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, undefined, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, [], 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, {}, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, ( x: number ): number => x, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a seventh argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, '10' ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, true ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, false ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, null ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, undefined ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, [] ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, {} ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided an unsupported number of arguments...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	cuminabs.ndarray(); // $ExpectError
+	cuminabs.ndarray( x.length ); // $ExpectError
+	cuminabs.ndarray( x.length, x ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1 ); // $ExpectError
+	cuminabs.ndarray( x.length, x, 1, 0, y, 1, 0, 10 ); // $ExpectError
+}
