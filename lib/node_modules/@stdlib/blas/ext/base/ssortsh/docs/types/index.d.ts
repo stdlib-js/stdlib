c0d8799b26564828695026b9cf08374712fc1473
kgryte <kgryte@gmail.com>
2020-06-11 00:19:31 -0700

    Add strided interface to sort a strided array using Shellsort

---
+++ lib/node_modules/@stdlib/blas/ext/base/ssortsh/docs/types/index.d.ts
@@ -0,0 +1,95 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Interface describing `ssortsh`.
+*/
+interface Routine {
+	/**
+	* Sorts a single-precision floating-point strided array using Shellsort.
+	*
+	* @param N - number of indexed elements
+	* @param order - sort order
+	* @param x - input array
+	* @param stride - stride length
+	* @returns `x`
+	*
+	* @example
+	* var Float32Array = require( `@stdlib/array/float32` );
+	*
+	* var x = new Float32Array( [ 1.0, -2.0, 3.0, -4.0 ] );
+	*
+	* ssortsh( x.length, 1, x, 1 );
+	* // x => <Float32Array>[ -4.0, -2.0, 1.0, 3.0 ]
+	*/
+	( N: number, order: number, x: Float32Array, stride: number ): Float32Array;
+
+	/**
+	* Sorts a single-precision floating-point strided array using Shellsort and alternative indexing semantics.
+	*
+	* @param N - number of indexed elements
+	* @param order - sort order
+	* @param x - input array
+	* @param stride - stride length
+	* @param offset - starting index
+	* @returns `x`
+	*
+	* @example
+	* var Float32Array = require( `@stdlib/array/float32` );
+	*
+	* var x = new Float32Array( [ 1.0, -2.0, 3.0, -4.0 ] );
+	*
+	* ssortsh.ndarray( x.length, 1, x, 1, 0 );
+	* // x => <Float32Array>[ -4.0, -2.0, 1.0, 3.0 ]
+	*/
+	ndarray( N: number, order: number, x: Float32Array, stride: number, offset: number ): Float32Array; // tslint:disable-line:max-line-length
+}
+
+/**
+* Sorts a single-precision floating-point strided array using Shellsort.
+*
+* @param N - number of indexed elements
+* @param order - sort order
+* @param x - input array
+* @param stride - stride length
+* @returns `x`
+*
+* @example
+* var Float32Array = require( `@stdlib/array/float32` );
+*
+* var x = new Float32Array( [ 1.0, -2.0, 3.0, -4.0 ] );
+*
+* ssortsh( x.length, 1, x, 1 );
+* // x => <Float32Array>[ -4.0, -2.0, 1.0, 3.0 ]
+*
+* @example
+* var Float32Array = require( `@stdlib/array/float32` );
+*
+* var x = new Float32Array( [ 1.0, -2.0, 3.0, -4.0 ] );
+*
+* ssortsh.ndarray( x.length, 1, x, 1, 0 );
+* // x => <Float32Array>[ -4.0, -2.0, 1.0, 3.0 ]
+*/
+declare var ssortsh: Routine;
+
+
+// EXPORTS //
+
+export = ssortsh;
