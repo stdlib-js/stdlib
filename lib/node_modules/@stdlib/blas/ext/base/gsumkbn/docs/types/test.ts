21a060905402f22de08702d67a77b71b48c87840
kgryte <kgryte@gmail.com>
2020-06-16 11:53:20 -0700

    Add strided interface to compute the sum using an improved Kahan-Babuska algorithm

---
+++ lib/node_modules/@stdlib/blas/ext/base/gsumkbn/docs/types/test.ts
@@ -0,0 +1,155 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import gsumkbn = require( './index' );
+
+
+// TESTS //
+
+// The function returns a number...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn( x.length, x, 1 ); // $ExpectType number
+}
+
+// The compiler throws an error if the function is provided a first argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn( '10', x, 1 ); // $ExpectError
+	gsumkbn( true, x, 1 ); // $ExpectError
+	gsumkbn( false, x, 1 ); // $ExpectError
+	gsumkbn( null, x, 1 ); // $ExpectError
+	gsumkbn( undefined, x, 1 ); // $ExpectError
+	gsumkbn( [], x, 1 ); // $ExpectError
+	gsumkbn( {}, x, 1 ); // $ExpectError
+	gsumkbn( ( x: number ): number => x, x, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a second argument which is not a numeric array...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn( x.length, 10, 1 ); // $ExpectError
+	gsumkbn( x.length, '10', 1 ); // $ExpectError
+	gsumkbn( x.length, true, 1 ); // $ExpectError
+	gsumkbn( x.length, false, 1 ); // $ExpectError
+	gsumkbn( x.length, null, 1 ); // $ExpectError
+	gsumkbn( x.length, undefined, 1 ); // $ExpectError
+	gsumkbn( x.length, {}, 1 ); // $ExpectError
+	gsumkbn( x.length, ( x: number ): number => x, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a third argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn( x.length, x, '10' ); // $ExpectError
+	gsumkbn( x.length, x, true ); // $ExpectError
+	gsumkbn( x.length, x, false ); // $ExpectError
+	gsumkbn( x.length, x, null ); // $ExpectError
+	gsumkbn( x.length, x, undefined ); // $ExpectError
+	gsumkbn( x.length, x, [] ); // $ExpectError
+	gsumkbn( x.length, x, {} ); // $ExpectError
+	gsumkbn( x.length, x, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided an unsupported number of arguments...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn(); // $ExpectError
+	gsumkbn( x.length ); // $ExpectError
+	gsumkbn( x.length, x ); // $ExpectError
+	gsumkbn( x.length, x, 1, 10 ); // $ExpectError
+}
+
+// Attached to main export is an `ndarray` method which returns a number...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn.ndarray( x.length, x, 1, 0 ); // $ExpectType number
+}
+
+// The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn.ndarray( '10', x, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( true, x, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( false, x, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( null, x, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( undefined, x, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( [], x, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( {}, x, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( ( x: number ): number => x, x, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a second argument which is not a numeric array...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn.ndarray( x.length, 10, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, '10', 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, true, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, false, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, null, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, undefined, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, {}, 1, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, ( x: number ): number => x, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a third argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn.ndarray( x.length, x, '10', 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, true, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, false, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, null, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, undefined, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, [], 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, {}, 0 ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, ( x: number ): number => x, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a fourth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn.ndarray( x.length, x, 1, '10' ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1, true ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1, false ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1, null ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1, undefined ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1, [] ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1, {} ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided an unsupported number of arguments...
+{
+	const x = new Float64Array( 10 );
+
+	gsumkbn.ndarray(); // $ExpectError
+	gsumkbn.ndarray( x.length ); // $ExpectError
+	gsumkbn.ndarray( x.length, x ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1 ); // $ExpectError
+	gsumkbn.ndarray( x.length, x, 1, 0, 10 ); // $ExpectError
+}
