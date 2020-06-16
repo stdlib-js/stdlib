1b07cc3c15d0dec012453400db107a4d88a34ef9
kgryte <kgryte@gmail.com>
2020-06-16 14:11:12 -0700

    Add strided interface to add a constant and compute the sum using an improved Kahan-Babuska algorithm

---
+++ lib/node_modules/@stdlib/blas/ext/base/sapxsumkbn/docs/types/index.d.ts
@@ -0,0 +1,95 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Interface describing `sapxsumkbn`.
+*/
+interface Routine {
+	/**
+	* Adds a constant to each single-precision floating-point strided array element and computes the sum using an improved Kahan–Babuška algorithm.
+	*
+	* @param N - number of indexed elements
+	* @param alpha - constant
+	* @param x - input array
+	* @param stride - stride length
+	* @returns sum
+	*
+	* @example
+	* var Float32Array = require( `@stdlib/array/float32` );
+	*
+	* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+	*
+	* var v = sapxsumkbn( x.length, 5.0, x, 1 );
+	* // returns 16.0
+	*/
+	( N: number, alpha: number, x: Float32Array, stride: number ): number;
+
+	/**
+	* Adds a constant to each single-precision floating-point strided array element and computes the sum using an improved Kahan–Babuška algorithm and alternative indexing semantics.
+	*
+	* @param N - number of indexed elements
+	* @param alpha - constant
+	* @param x - input array
+	* @param stride - stride length
+	* @param offset - starting index
+	* @returns sum
+	*
+	* @example
+	* var Float32Array = require( `@stdlib/array/float32` );
+	*
+	* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+	*
+	* var v = sapxsumkbn.ndarray( x.length, 5.0, x, 1, 0 );
+	* // returns 16.0
+	*/
+	ndarray( N: number, alpha: number, x: Float32Array, stride: number, offset: number ): number; // tslint:disable-line:max-line-length
+}
+
+/**
+* Adds a constant to each single-precision floating-point strided array element and computes the sum using an improved Kahan–Babuška algorithm.
+*
+* @param N - number of indexed elements
+* @param alpha - constant
+* @param x - input array
+* @param stride - stride length
+* @returns sum
+*
+* @example
+* var Float32Array = require( `@stdlib/array/float32` );
+*
+* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+*
+* var v = sapxsumkbn( x.length, 5.0, x, 1 );
+* // returns 16.0
+*
+* @example
+* var Float32Array = require( `@stdlib/array/float32` );
+*
+* var x = new Float32Array( [ 1.0, -2.0, 2.0 ] );
+*
+* var v = sapxsumkbn.ndarray( x.length, 5.0, x, 1, 0 );
+* // returns 16.0
+*/
+declare var sapxsumkbn: Routine;
+
+
+// EXPORTS //
+
+export = sapxsumkbn;
