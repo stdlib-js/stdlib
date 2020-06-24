08ca32895957967bd760a4fe02d61762432a0b72
kgryte <kgryte@gmail.com>
2020-06-24 15:52:36 -0700

    Add strided interface to compute the standard deviation

---
+++ lib/node_modules/@stdlib/stats/base/stdev/docs/types/index.d.ts
@@ -0,0 +1,91 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+import { NumericArray } from '@stdlib/types/array';
+
+/**
+* Interface describing `stdev`.
+*/
+interface Routine {
+	/**
+	* Computes the standard deviation of a strided array.
+	*
+	* @param N - number of indexed elements
+	* @param correction - degrees of freedom adjustment
+	* @param x - input array
+	* @param stride - stride length
+	* @returns standard deviation
+	*
+	* @example
+	* var x = [ 1.0, -2.0, 2.0 ];
+	*
+	* var v = stdev( x.length, 1, x, 1 );
+	* // returns ~2.0817
+	*/
+	( N: number, correction: number, x: NumericArray, stride: number ): number;
+
+	/**
+	* Computes the standard deviation of a strided array using alternative indexing semantics.
+	*
+	* @param N - number of indexed elements
+	* @param correction - degrees of freedom adjustment
+	* @param x - input array
+	* @param stride - stride length
+	* @param offset - starting index
+	* @returns standard deviation
+	*
+	* @example
+	* var x = [ 1.0, -2.0, 2.0 ];
+	*
+	* var v = stdev.ndarray( x.length, 1, x, 1, 0 );
+	* // returns ~2.0817
+	*/
+	ndarray( N: number, correction: number, x: NumericArray, stride: number, offset: number ): number; // tslint:disable-line:max-line-length
+}
+
+/**
+* Computes the standard deviation of a strided array.
+*
+* @param N - number of indexed elements
+* @param correction - degrees of freedom adjustment
+* @param x - input array
+* @param stride - stride length
+* @returns standard deviation
+*
+* @example
+* var x = [ 1.0, -2.0, 2.0 ];
+*
+* var v = stdev( x.length, 1, x, 1 );
+* // returns ~2.0817
+*
+* @example
+* var x = [ 1.0, -2.0, 2.0 ];
+*
+* var v = stdev.ndarray( x.length, 1, x, 1, 0 );
+* // returns ~2.0817
+*/
+declare var stdev: Routine;
+
+
+// EXPORTS //
+
+export = stdev;
