db280f6437b654ec9c88631c52b279095beb8af4
kgryte <kgryte@gmail.com>
2020-06-24 13:46:39 -0700

    Refactor rules for installing a dev custom ESLint plugin

--- tools/make/lib/init/eslint.mk
+++ tools/make/lib/init/eslint.mk
@@ -18,26 +18,54 @@
 
 # VARIABLES #
 
-# Define the entry point of the plugin for custom ESLint rules:
-STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_ENTRY ?= $(TOOLS_PKGS_DIR)/eslint/rules/scripts/plugin.js
+# Define the entry point of the plugin for project-specific custom ESLint rules:
+stdlib_custom_eslint_rules_plugin_entry := $(TOOLS_PKGS_DIR)/eslint/rules/scripts/plugin.js
 
-# Define the plugin name for custom ESLint rules:
-STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_NAME ?= eslint-plugin-stdlib
+# Define the plugin name for project-specific custom ESLint rules:
+stdlib_custom_eslint_rules_plugin_name := eslint-plugin-stdlib
 
-# Define the output directory for the custom ESLint rules plugin bundle:
-STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_OUT ?= $(NODE_MODULES)/$(STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_NAME)
+# Define the output directory for the project-specific custom ESLint rules plugin:
+stdlib_custom_eslint_rules_plugin_out := $(NODE_MODULES)/$(stdlib_custom_eslint_rules_plugin_name)
 
-# Define the output filename for the custom ESLint rules plugin bundle:
-STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_BUNDLE ?= $(STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_OUT)/index.js
+# Define the contents of the custom ESLint rules plugin `package.json`:
+stdlib_custom_eslint_rules_plugin_package_json := '{"name":"@stdlib/PLUGIN_NAME","version":"0.0.0","main":"index.js"}'
+stdlib_custom_eslint_rules_plugin_package_json := $(subst PLUGIN_NAME,$(stdlib_custom_eslint_rules_plugin_name),$(stdlib_custom_eslint_rules_plugin_package_json))
 
-# Define command-line options to be used when invoking the browserify executable:
-BROWSERIFY_STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_FLAGS ?= \
-	--node \
-	--outfile $(STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_BUNDLE) \
-	--standalone $(STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_NAME)
+# Define the build output prerequisites:
+stdlib_custom_eslint_rules_plugin_prereqs := \
+	$(stdlib_custom_eslint_rules_plugin_out)/index.js \
+	$(stdlib_custom_eslint_rules_plugin_out)/package.json
 
 
 # RULES #
+
+#/
+# Creates an output directory for installing a custom ESLint rules plugin.
+#
+# @private
+#/
+$(stdlib_custom_eslint_rules_plugin_out):
+	$(QUIET) $(MKDIR_RECURSIVE) $@
+
+#/
+# Creates the main entry point for a custom ESLint rules plugin.
+#
+# @private
+#/
+$(stdlib_custom_eslint_rules_plugin_out)/index.js: $(NODE_MODULES) $(stdlib_custom_eslint_rules_plugin_entry) $(stdlib_custom_eslint_rules_plugin_out)
+	$(QUIET) NODE_PATH="$(NODE_PATH)" \
+	$(BROWSERIFY) $(stdlib_custom_eslint_rules_plugin_entry) \
+		--node \
+		--outfile $@ \
+		--standalone $(stdlib_custom_eslint_rules_plugin_name)
+
+#/
+# Creates a `package.json` for a custom ESLint rules plugin.
+#
+# @private
+#/
+$(stdlib_custom_eslint_rules_plugin_out)/package.json: $(stdlib_custom_eslint_rules_plugin_out)
+	$(QUIET) printf $(stdlib_custom_eslint_rules_plugin_package_json) > $@
 
 #/
 # Initializes custom [ESLint][eslint] rules.
@@ -51,12 +79,7 @@ BROWSERIFY_STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_FLAGS ?= \
 # @example
 # make init-stdlib-custom-eslint-rules-plugin
 #/
-init-stdlib-custom-eslint-rules-plugin: $(NODE_MODULES)
-	$(QUIET) $(MKDIR_RECURSIVE) $(STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_OUT)
-	$(QUIET) NODE_PATH="$(NODE_PATH)" \
-	$(BROWSERIFY) \
-		$(STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_ENTRY) \
-		$(BROWSERIFY_STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_FLAGS)
+init-stdlib-custom-eslint-rules-plugin: clean-stdlib-custom-eslint-rules-plugin $(NODE_MODULES) $(stdlib_custom_eslint_rules_plugin_prereqs)
 
 .PHONY: init-stdlib-custom-eslint-rules-plugin
 
@@ -81,7 +104,7 @@ init-stdlib-custom-eslint-plugins: init-stdlib-custom-eslint-rules-plugin
 # make clean-stdlib-custom-eslint-rules-plugin
 #/
 clean-stdlib-custom-eslint-rules-plugin:
-	$(QUIET) $(DELETE) $(DELETE_FLAGS) $(STDLIB_CUSTOM_ESLINT_RULES_PLUGIN_OUT)
+	$(QUIET) $(DELETE) $(DELETE_FLAGS) $(stdlib_custom_eslint_rules_plugin_out)
 
 .PHONY: clean-stdlib-custom-eslint-rules-plugin
 
