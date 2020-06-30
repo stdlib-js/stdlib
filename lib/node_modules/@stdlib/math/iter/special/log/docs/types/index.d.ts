ef4c6e5900e8bd56de79741725f43348ce08ebe5
kgryte <kgryte@gmail.com>
2020-06-30 13:54:19 -0700

    Add iterator to compute the base `b` logarithm

---
+++ lib/node_modules/@stdlib/math/iter/special/log/docs/types/index.d.ts
@@ -0,0 +1,64 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+import { Iterator as Iter, IterableIterator } from '@stdlib/types/iter';
+
+// Define a union type representing both iterable and non-iterable iterators:
+type Iterator = Iter | IterableIterator;
+
+/**
+* Returns an iterator which iteratively computes the base `b` logarithm.
+*
+* ## Notes
+*
+* -   For negative `b` or `x` iterated values, the returned iterator returns `NaN`.
+* -   If provided a numeric value as an iterator argument, the value is broadcast as an **infinite** iterator which **always** returns the provided value.
+* -   If an iterated value is non-numeric (including `NaN`), the returned iterator returns `NaN`. If non-numeric iterated values are possible, you are advised to provide an iterator which type checks and handles non-numeric values accordingly.
+* -   The length of the returned iterator is equal to the length of the shortest provided iterator. In other words, the returned iterator ends once **one** of the provided iterators ends.
+* -   If an environment supports `Symbol.iterator` and all provided iterators are iterable, the returned iterator is iterable.
+*
+* @param x - input iterator
+* @param b - input iterator
+* @returns iterator
+*
+* @example
+* var uniform = require( `@stdlib/random/iter/uniform` );
+*
+* var iter = iterLog( uniform( 0.0, 100.0 ), uniform( 0.0, 10.0 ) );
+*
+* var r = iter.next().value;
+* // returns <number>
+*
+* r = iter.next().value;
+* // returns <number>
+*
+* r = iter.next().value;
+* // returns <number>
+*
+* // ...
+*/
+declare function iterLog( x: Iterator | number, b: Iterator | number ): Iterator; // tslint:disable-line:max-line-length
+
+
+// EXPORTS //
+
+export = iterLog;
