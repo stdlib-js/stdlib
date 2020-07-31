b580d7ca5d4851b7164b04c5ef9ea03c47259161
kgryte <kgryte@gmail.com>
2020-07-31 14:35:32 -0700

    Add strided interface to compute the range via a callback function

---
+++ lib/node_modules/@stdlib/stats/base/range-by/docs/types/index.d.ts
@@ -0,0 +1,199 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+import { Collection } from '@stdlib/types/object';
+
+/**
+* Returns an accessed value.
+*
+* @returns accessed value
+*/
+type Nullary = () => number | void;
+
+/**
+* Returns an accessed value.
+*
+* @param value - array element
+* @returns accessed value
+*/
+type Unary = ( value: any ) => number | void;
+
+/**
+* Returns an accessed value.
+*
+* @param value - array element
+* @param aidx - array index
+* @returns accessed value
+*/
+type Binary = ( value: any, aidx: number ) => number | void;
+
+/**
+* Returns an accessed value.
+*
+* @param value - array element
+* @param aidx - array index
+* @param sidx - strided index (offset + aidx*stride)
+* @returns accessed value
+*/
+type Tertiary = ( value: any, aidx: number, sidx: number ) => number | void;
+
+/**
+* Returns an accessed value.
+*
+* @param value - array element
+* @param aidx - array index
+* @param sidx - strided index (offset + aidx*stride)
+* @param array - input array
+* @returns accessed value
+*/
+type Quaternary = ( value: any, aidx: number, sidx: number, array: Collection ) => number | void; // tslint-disable-line max-line-length
+
+/**
+* Returns an accessed value.
+*
+* @param value - array element
+* @param aidx - array index
+* @param sidx - strided index (offset + aidx*stride)
+* @param array - input array
+* @returns accessed value
+*/
+type Callback = Nullary | Unary | Binary | Tertiary | Quaternary;
+
+/**
+* Interface describing `rangeBy`.
+*/
+interface Routine {
+	/**
+	* Calculates the range of a strided array via a callback function.
+	*
+	* ## Notes
+	*
+	* -   The callback function is provided four arguments:
+	*
+	*     -   `value`: array element
+	*     -   `aidx`: array index
+	*     -   `sidx`: strided index (offset + aidx*stride)
+	*     -   `array`: input array
+	*
+	* -   The callback function should return a numeric value. If the callback function does not return any value (or equivalently, explicitly returns `undefined`), the value is ignored.
+	*
+	* @param N - number of indexed elements
+	* @param x - input array
+	* @param stride - stride length
+	* @param clbk - callback
+	* @param thisArg - execution context
+	* @returns range
+	*
+	* @example
+	* var x = [ -2.0, 1.0, 3.0, -5.0, 4.0, 0.0, -1.0, -3.0 ];
+	*
+	* function accessor( v ) {
+	*     return v * 2.0;
+	* }
+	*
+	* var v = rangeBy( x.length, x, 1, accessor );
+	* // returns 18.0
+	*/
+	( N: number, x: Collection, stride: number, clbk: Callback, thisArg?: any ): number; // tslint:disable-line:max-line-length
+
+	/**
+	* Calculates the range of a strided array via a callback function and using alternative indexing semantics.
+	*
+	* ## Notes
+	*
+	* -   The callback function is provided four arguments:
+	*
+	*     -   `value`: array element
+	*     -   `aidx`: array index
+	*     -   `sidx`: strided index (offset + aidx*stride)
+	*     -   `array`: input array
+	*
+	* -   The callback function should return a numeric value. If the callback function does not return any value (or equivalently, explicitly returns `undefined`), the value is ignored.
+	*
+	* @param N - number of indexed elements
+	* @param x - input array
+	* @param stride - stride length
+	* @param offset - starting index
+	* @param clbk - callback
+	* @param thisArg - execution context
+	* @returns range
+	*
+	* @example
+	* var x = [ -2.0, 1.0, 3.0, -5.0, 4.0, 0.0, -1.0, -3.0 ];
+	*
+	* function accessor( v ) {
+	*     return v * 2.0;
+	* }
+	*
+	* var v = rangeBy.ndarray( x.length, x, 1, 0, accessor );
+	* // returns 18.0
+	*/
+	ndarray( N: number, x: Collection, stride: number, offset: number, clbk: Callback, thisArg?: any ): number; // tslint:disable-line:max-line-length
+}
+
+/**
+* Calculates the range of a strided array via a callback function.
+*
+* ## Notes
+*
+* -   The callback function is provided four arguments:
+*
+*     -   `value`: array element
+*     -   `aidx`: array index
+*     -   `sidx`: strided index (offset + aidx*stride)
+*     -   `array`: input array
+*
+* -   The callback function should return a numeric value. If the callback function does not return any value (or equivalently, explicitly returns `undefined`), the value is ignored.
+*
+* @param N - number of indexed elements
+* @param x - input array
+* @param stride - stride length
+* @param clbk - callback
+* @param thisArg - execution context
+* @returns range
+*
+* @example
+* var x = [ -2.0, 1.0, 3.0, -5.0, 4.0, 0.0, -1.0, -3.0 ];
+*
+* function accessor( v ) {
+*     return v * 2.0;
+* }
+*
+* var v = rangeBy( x.length, x, 1, accessor );
+* // returns 18.0
+*
+* @example
+* var x = [ -2.0, 1.0, 3.0, -5.0, 4.0, 0.0, -1.0, -3.0 ];
+*
+* function accessor( v ) {
+*     return v * 2.0;
+* }
+*
+* var v = rangeBy.ndarray( x.length, x, 1, 0, accessor );
+* // returns 18.0
+*/
+declare var rangeBy: Routine;
+
+
+// EXPORTS //
+
+export = rangeBy;
