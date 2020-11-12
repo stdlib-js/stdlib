6ba6c84f001006ab8e369aa2fd321ca38cb8b91b
kgryte <kgryte@gmail.com>
2020-11-12 17:18:39 -0600

    Add pkg to return the platform float word order

---
+++ lib/node_modules/@stdlib/os/float-word-order/examples/c/example.c
@@ -0,0 +1,33 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/os/byte_order.h"
+#include "stdlib/os/float_word_order.h"
+#include <stdio.h>
+
+int main() {
+#if defined(STDLIB_OS_FLOAT_WORD_ORDER)
+#if STDLIB_OS_FLOAT_WORD_ORDER == STDLIB_OS_ORDER_LITTLE_ENDIAN
+	printf( "Least significant word comes first...\n" );
+#elif STDLIB_OS_FLOAT_WORD_ORDER == STDLIB_OS_ORDER_BIG_ENDIAN
+	printf( "Most significant word comes first...\n" );
+#else
+	printf( "Platform float word order is unknown...\n" )
+#endif
+#endif
+}
