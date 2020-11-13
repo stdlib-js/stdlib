ec094cf15916f7e6a7c56f503a82b54ee3558e05
kgryte <kgryte@gmail.com>
2020-11-13 14:16:05 -0600

    Add strided interface to compute the cube root (float64)

---
+++ lib/node_modules/@stdlib/math/strided/special/dcbrt/docs/types/index.d.ts
@@ -0,0 +1,103 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Interface describing `dcbrt`.
+*/
+interface Routine {
+	/**
+	* Computes the cube root of each element in a double-precision floating-point strided array `x` and assigns the results to elements in a double-precision floating-point strided array `y`.
+	*
+	* @param N - number of indexed elements
+	* @param x - input array
+	* @param strideX - `x` stride length
+	* @param y - destination array
+	* @param strideY - `y` stride length
+	* @returns `y`
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = new Float64Array( [ 0.0, 1.0, 8.0, 27.0, 64.0 ] );
+	* var y = new Float64Array( [ 0.0, 0.0, 0.0, 0.0, 0.0 ] );
+	*
+	* dcbrt( x.length, x, 1, y, 1 );
+	* // y => <Float64Array>[ 0.0, 1.0, 2.0, 3.0, 4.0 ]
+	*/
+	( N: number, x: Float64Array, strideX: number, y: Float64Array, strideY: number ): Float64Array; // tslint:disable-line:max-line-length
+
+	/**
+	* Computes the cube root of each element in a double-precision floating-point strided array `x` and assigns the results to elements in a double-precision floating-point strided array `y` using alternative indexing semantics.
+	*
+	* @param N - number of indexed elements
+	* @param x - input array
+	* @param strideX - `x` stride length
+	* @param offsetX - starting index for `x`
+	* @param y - destination array
+	* @param strideY - `y` stride length
+	* @param offsetY - starting index for `y`
+	* @returns `y`
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = new Float64Array( [ 0.0, 1.0, 8.0, 27.0, 64.0 ] );
+	* var y = new Float64Array( [ 0.0, 0.0, 0.0, 0.0, 0.0 ] );
+	*
+	* dcbrt.ndarray( x.length, x, 1, 0, y, 1, 0 );
+	* // y => <Float64Array>[ 0.0, 1.0, 2.0, 3.0, 4.0 ]
+	*/
+	ndarray( N: number, x: Float64Array, strideX: number, offsetX: number, y: Float64Array, strideY: number, offsetY: number ): Float64Array; // tslint:disable-line:max-line-length
+}
+
+/**
+* Computes the cube root of each element in a double-precision floating-point strided array `x` and assigns the results to elements in a double-precision floating-point strided array `y`.
+*
+* @param N - number of indexed elements
+* @param x - input array
+* @param strideX - `x` stride length
+* @param y - destination array
+* @param strideY - `y` stride length
+* @returns `y`
+*
+* @example
+* var Float64Array = require( `@stdlib/array/float64` );
+*
+* var x = new Float64Array( [ 0.0, 1.0, 8.0, 27.0, 64.0 ] );
+* var y = new Float64Array( [ 0.0, 0.0, 0.0, 0.0, 0.0 ] );
+*
+* dcbrt( x.length, x, 1, y, 1 );
+* // y => <Float64Array>[ 0.0, 1.0, 2.0, 3.0, 4.0 ]
+*
+* @example
+* var Float64Array = require( `@stdlib/array/float64` );
+*
+* var x = new Float64Array( [ 0.0, 1.0, 8.0, 27.0, 64.0 ] );
+* var y = new Float64Array( [ 0.0, 0.0, 0.0, 0.0, 0.0 ] );
+*
+* dcbrt.ndarray( x.length, x, 1, 0, y, 1, 0 );
+* // y => <Float64Array>[ 0.0, 1.0, 2.0, 3.0, 4.0 ]
+*/
+declare var dcbrt: Routine;
+
+
+// EXPORTS //
+
+export = dcbrt;
---
+++ lib/node_modules/@stdlib/math/strided/special/dcbrt/docs/types/test.ts
@@ -0,0 +1,248 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2020 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import dcbrt = require( './index' );
+
+
+// TESTS //
+
+// The function returns a Float64Array...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt( x.length, x, 1, y, 1 ); // $ExpectType Float64Array
+}
+
+// The compiler throws an error if the function is provided a first argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt( '10', x, 1, y, 1 ); // $ExpectError
+	dcbrt( true, x, 1, y, 1 ); // $ExpectError
+	dcbrt( false, x, 1, y, 1 ); // $ExpectError
+	dcbrt( null, x, 1, y, 1 ); // $ExpectError
+	dcbrt( undefined, x, 1, y, 1 ); // $ExpectError
+	dcbrt( [], x, 1, y, 1 ); // $ExpectError
+	dcbrt( {}, x, 1, y, 1 ); // $ExpectError
+	dcbrt( ( x: number ): number => x, x, 1, y, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a second argument which is not a Float64Array...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt( x.length, 10, 1, y, 1 ); // $ExpectError
+	dcbrt( x.length, '10', 1, y, 1 ); // $ExpectError
+	dcbrt( x.length, true, 1, y, 1 ); // $ExpectError
+	dcbrt( x.length, false, 1, y, 1 ); // $ExpectError
+	dcbrt( x.length, null, 1, y, 1 ); // $ExpectError
+	dcbrt( x.length, undefined, 1, y, 1 ); // $ExpectError
+	dcbrt( x.length, [ '1' ], 1, y, 1 ); // $ExpectError
+	dcbrt( x.length, {}, 1, y, 1 ); // $ExpectError
+	dcbrt( x.length, ( x: number ): number => x, 1, y, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a third argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt( x.length, x, '10', y, 1 ); // $ExpectError
+	dcbrt( x.length, x, true, y, 1 ); // $ExpectError
+	dcbrt( x.length, x, false, y, 1 ); // $ExpectError
+	dcbrt( x.length, x, null, y, 1 ); // $ExpectError
+	dcbrt( x.length, x, undefined, y, 1 ); // $ExpectError
+	dcbrt( x.length, x, [], y, 1 ); // $ExpectError
+	dcbrt( x.length, x, {}, y, 1 ); // $ExpectError
+	dcbrt( x.length, x, ( x: number ): number => x, y, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a fourth argument which is not a Float64Array...
+{
+	const x = new Float64Array( 10 );
+
+	dcbrt( x.length, x, 1, 10, 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, '10', 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, true, 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, false, 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, null, 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, undefined, 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, [ '1' ], 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, {}, 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, ( x: number ): number => x, 1 ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided a fifth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt( x.length, x, 1, y, '10' ); // $ExpectError
+	dcbrt( x.length, x, 1, y, true ); // $ExpectError
+	dcbrt( x.length, x, 1, y, false ); // $ExpectError
+	dcbrt( x.length, x, 1, y, null ); // $ExpectError
+	dcbrt( x.length, x, 1, y, undefined ); // $ExpectError
+	dcbrt( x.length, x, 1, y, [] ); // $ExpectError
+	dcbrt( x.length, x, 1, y, {} ); // $ExpectError
+	dcbrt( x.length, x, 1, y, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided an unsupported number of arguments...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt(); // $ExpectError
+	dcbrt( x.length ); // $ExpectError
+	dcbrt( x.length, x ); // $ExpectError
+	dcbrt( x.length, x, 1 ); // $ExpectError
+	dcbrt( x.length, x, 1, y ); // $ExpectError
+	dcbrt( x.length, x, 1, y, 1, 10 ); // $ExpectError
+}
+
+// Attached to main export is an `ndarray` method which returns a Float64Array...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, 0 ); // $ExpectType Float64Array
+}
+
+// The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt.ndarray( '10', x, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( true, x, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( false, x, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( null, x, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( undefined, x, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( [], x, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( {}, x, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( ( x: number ): number => x, x, 1, 0, y, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a second argument which is not a Float64Array...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt.ndarray( x.length, 10, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, '10', 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, true, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, false, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, null, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, undefined, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, [ '1' ], 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, {}, 1, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, ( x: number ): number => x, 1, 0, y, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a third argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt.ndarray( x.length, x, '10', 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, true, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, false, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, null, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, undefined, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, [], 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, {}, 0, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, ( x: number ): number => x, 0, y, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a fourth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt.ndarray( x.length, x, 1, '10', y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, true, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, false, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, null, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, undefined, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, [], y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, {}, y, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, ( x: number ): number => x, y, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a fifth argument which is not a Float64Array...
+{
+	const x = new Float64Array( 10 );
+
+	dcbrt.ndarray( x.length, x, 1, 0, 10, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, '10', 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, true, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, false, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, null, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, undefined, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, [ '1' ], 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, {}, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, ( x: number ): number => x, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a sixth argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt.ndarray( x.length, x, 1, 0, y, '10', 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, true, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, false, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, null, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, undefined, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, [], 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, {}, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, ( x: number ): number => x, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a seventh argument which is not a number...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, '10' ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, true ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, false ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, null ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, undefined ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, [] ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, {} ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided an unsupported number of arguments...
+{
+	const x = new Float64Array( 10 );
+	const y = new Float64Array( 10 );
+
+	dcbrt.ndarray(); // $ExpectError
+	dcbrt.ndarray( x.length ); // $ExpectError
+	dcbrt.ndarray( x.length, x ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1 ); // $ExpectError
+	dcbrt.ndarray( x.length, x, 1, 0, y, 1, 0, 10 ); // $ExpectError
+}
