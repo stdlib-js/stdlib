3e1d7c2161baced237caac1f97c50014bd2a04d4
Planeshifter <pburckhardt@outlook.com>
2021-01-13 02:56:41 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/array/same-kind-casts/docs/types/index.d.ts
@@ -0,0 +1,41 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns a list of array data types to which a provided array data type can be safely cast or cast within the same "kind".
+*
+* ## Notes
+*
+* -   If not provided an array data type, the function returns a casting table.
+* -   If provided an unrecognized array data type, the function returns `null`.
+*
+* @param dtype - array data type
+* @returns list of array data types or null
+*
+* @example
+* var list = sameKindCasts( 'float32' );
+* // returns [...]
+*/
+declare function sameKindCasts( dtype?: string ): Object | Array<string> | null;
+
+
+// EXPORTS //
+
+export = sameKindCasts;
