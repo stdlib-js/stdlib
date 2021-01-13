86fbe70dad608c25dfb7b87e6bfd43958d4c8335
Planeshifter <pburckhardt@outlook.com>
2021-01-13 02:38:36 -0500

    Add Typescript definitions

---
+++ lib/node_modules/@stdlib/array/typed-dtypes/docs/types/index.d.ts
@@ -0,0 +1,35 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns a list of typed array data types.
+*
+* @returns list of typed array data types
+*
+* @example
+* var list = dtypes();
+* // returns [ 'float32', 'float64', 'int16', 'int32', 'int8', 'uint16', 'uint32', 'uint8', 'uint8c' ]
+*/
+declare function dtypes(): Array<string>;
+
+
+// EXPORTS //
+
+export = dtypes;
