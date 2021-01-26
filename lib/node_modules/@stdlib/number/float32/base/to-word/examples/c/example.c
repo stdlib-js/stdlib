2f434a9829c9a393c21dfba28e38449732744935
kgryte <kgryte@gmail.com>
2021-01-25 16:00:07 -0800

    Add C implementation

---
+++ lib/node_modules/@stdlib/number/float32/base/to-word/examples/c/example.c
@@ -0,0 +1,32 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/number/float32/base/to_word.h"
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+	float x[] = { 3.14f, -3.14f, 0.0f, 0.0f/0.0f };
+
+	uint32_t word;
+	int i;
+	for ( i = 0; i < 4; i++ ) {
+		stdlib_base_float32_to_word( x[ i ], &word );
+		printf( "%f => word: %u\n", x[ i ], word );
+	}
+}
