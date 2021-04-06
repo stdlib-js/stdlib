9232f9fa6da26490ca892362127c962f18b27e61
Planeshifter <pburckhardt@outlook.com>
2021-04-05 20:08:16 -0400

    Add Typescript definition

---
+++ lib/node_modules/@stdlib/stats/base/dists/cosine/ctor/docs/types/index.d.ts
@@ -0,0 +1,157 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Cosine distribution.
+*/
+declare class Cosine {
+	/**
+	* Cosine distribution constructor.
+	*
+	* @param mu - location parameter (default: 0.0)
+	* @param s - scale parameter (default: 1.0)
+	* @throws `s` must be a positive number
+	* @returns distribution instance
+	*
+	* @example
+	* var cosine = new Cosine( 1.0, 1.0 );
+	*
+	* var y = cosine.cdf( 1.5 );
+	* // returns ~0.909
+	*
+	* var mu = cosine.mean;
+	* // returns 1.0
+	*/
+	constructor( mu: number, s: number );
+
+	/**
+	* Cosine distribution constructor.
+	*
+	* @returns distribution instance
+	*
+	* @example
+	* var cosine = new Cosine();
+	*
+	* var y = cosine.cdf( 1.5 );
+	* // returns 1.0
+	*
+	* var mu = cosine.mean;
+	* // returns 0.0
+	*/
+	constructor();
+
+	/**
+	*  Location parameter.
+	*/
+	mu: number;
+
+	/**
+	* Scale parameter. If set, the value must be greater than `0`.
+	*/
+	s: number;
+
+	/**
+	* Read-only property which returns the excess kurtosis.
+	*/
+	readonly kurtosis: number;
+
+	/**
+	* Read-only property which returns the expected value.
+	*/
+	readonly mean: number;
+
+	/**
+	* Read-only property which returns the median.
+	*/
+	readonly median: number;
+
+	/**
+	* Read-only property which returns the mode.
+	*/
+	readonly mode: number;
+
+	/**
+	* Read-only property which returns the skewness.
+	*/
+	readonly skewness: number;
+
+	/**
+	* Read-only property which returns the standard deviation.
+	*/
+	readonly stdev: number;
+
+	/**
+	* Read-only property which returns the variance.
+	*/
+	readonly variance: number;
+
+	/**
+	* Evaluates the cumulative distribution function (CDF).
+	*
+	* @param x - input value
+	* @returns evaluated CDF
+	*/
+	cdf( x: number ): number;
+
+	/**
+	* Evaluates the natural logarithm of the cumulative distribution function (CDF).
+	*
+	* @param x - input value
+	* @returns evaluated logCDF
+	*/
+	logcdf( x: number ): number;
+
+	/**
+	* Evaluates the natural logarithm of the probability density function (PDF).
+	*
+	* @param x - input value
+	* @returns evaluated logPDF
+	*/
+	logpdf( x: number ): number;
+
+	/**
+	* Evaluates the moment-generating function (MGF).
+	*
+	* @param t - input value
+	* @returns evaluated MGF
+	*/
+	mgf( t: number ): number;
+
+	/**
+	* Evaluates the probability density function (PDF).
+	*
+	* @param x - input value
+	* @returns evaluated PDF
+	*/
+	pdf( x: number ): number;
+
+	/**
+	* Evaluates the quantile function at probability `p`.
+	*
+	* @param p - input probability
+	* @returns evaluated quantile function
+	*/
+	quantile( p: number ): number;
+}
+
+
+// EXPORTS //
+
+export = Cosine;
