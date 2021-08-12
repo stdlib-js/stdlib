4c3291cf00c9f092d1512adb8e4ea55e9a67f9d1
kgryte <kgryte@gmail.com>
2021-08-12 13:53:26 -0700

    Refactor node modules installation due to npm v7 behavior

--- tools/make/lib/node/modules.mk
+++ tools/make/lib/node/modules.mk
@@ -21,8 +21,19 @@
 # Define a path to a utility for printing the list of node module dependencies to install:
 print_npm_install_deps := $(TOOLS_PKGS_DIR)/scripts/print_npm_install_deps
 
+# Define the file path for storing the list of node module dependencies to install:
+npm_install_deps_file := $(CONFIG_DIR)/npm/deps.txt
+
 
 # RULES #
+
+#/
+# Generates a file containing a list of node module dependencies to install.
+#
+# @private
+#/
+$(npm_install_deps_file): $(ROOT_PACKAGE_JSON) $(print_npm_install_deps)
+	$(QUIET) $(NODE) $(print_npm_install_deps) > $(npm_install_deps_file)
 
 #/
 # Installs node module dependencies.
@@ -34,8 +45,9 @@ print_npm_install_deps := $(TOOLS_PKGS_DIR)/scripts/print_npm_install_deps
 # @example
 # make install-node-modules
 #/
-install-node-modules: $(ROOT_PACKAGE_JSON)
-	$(QUIET) $(NODE) $(print_npm_install_deps) | xargs $(NPM) install --no-save
+install-node-modules: $(npm_install_deps_file)
+	$(QUIET) $(MV) "$(ROOT_PACKAGE_JSON)" "$(ROOT_PACKAGE_JSON).copy"
+	$(QUIET) { cat $(npm_install_deps_file) | xargs $(NPM) install --no-save && $(MV) "$(ROOT_PACKAGE_JSON).copy" "$(ROOT_PACKAGE_JSON)"; } || { $(MV) "$(ROOT_PACKAGE_JSON).copy" "$(ROOT_PACKAGE_JSON)" && exit 1; }
 
 .PHONY: install-node-modules
 
