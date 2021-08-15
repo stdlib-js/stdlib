4d852a9b769ad74c5ff369fa1f78df430f571a39
kgryte <kgryte@gmail.com>
2021-08-15 14:53:39 -0700

    Add recipe for publishing to npm

--- tools/make/lib/dist/package.mk
+++ tools/make/lib/dist/package.mk
@@ -18,206 +18,25 @@
 
 # VARIABLES #
 
+# Define the path to a script for releasing a new project version:
+NPM_RELEASE ?= $(TOOLS_DIR)/scripts/npm_publish
+
 # Define the version bump type (e.g., pre-patch, patch, pre-minor, minor, pre-major, major, pre-release):
-NPM_VERSION ?=
+NPM_RELEASE_TYPE ?= patch
 
 # Define a Git commit message when incrementing the project version:
-NPM_VERSION_COMMIT_MESSAGE ?= 'Increment version'
-
-# Specify the output build directory when generating a gzipped archive:
-NPM_TARBALL_BUILD_OUT ?= $(BUILD_DIR)/npm
-
-# Specify the gzipped archive basename:
-npm_tarball_basename := "stdlib_v$(shell $(CURRENT_PROJECT_VERSION)).tgz"
-
-# Specify the output gzipped archive basename:
-NPM_TARBALL ?= $(npm_tarball_basename)
-
-# Specify the output gzipped archive file path:
-npm_tarball := $(NPM_TARBALL_BUILD_OUT)/$(NPM_TARBALL)
-
-# Define the version upgrade prerequisite...
-ifeq ($(NPM_VERSION), pre-patch)
-	npm_version_target := npm-version-pre-patch
-else
-ifeq ($(NPM_VERSION), patch)
-	npm_version_target := npm-version-patch
-else
-ifeq ($(NPM_VERSION), pre-minor)
-	npm_version_target := npm-version-pre-minor
-else
-ifeq ($(NPM_VERSION), minor)
-	npm_version_target := npm-version-minor
-else
-ifeq ($(NPM_VERSION), pre-major)
-	npm_version_target := npm-version-pre-major
-else
-ifeq ($(NPM_VERSION), major)
-	npm_version_target := npm-version-major
-else
-ifeq ($(NPM_VERSION), pre-release)
-	npm_version_target := npm-version-pre-release
-else
-	npm_version_target :=
-endif
-endif
-endif
-endif
-endif
-endif
-endif
+NPM_RELEASE_COMMIT_MESSAGE ?= 'Features updates and bug fixes'
 
 
 # RULES #
 
 #/
-# Generates a gzipped archive for publishing to npm.
-#
-# @private
-#/
-$(npm_tarball):
-	$(QUIET) $(MKDIR_RECURSIVE) $(NPM_TARBALL_BUILD_OUT)
-	$(QUIET) $(NPM_PACK) $(ROOT_DIR) >/dev/null
-	$(QUIET) mv $(ROOT_DIR)/$(npm_tarball_basename) $@
-
-#/
-# Generates a gzipped archive for publishing to npm.
+# Publishes a new project version to the npm package registry.
 #
 # @example
-# make npm-tarball
+# make npm-publish
 #/
-npm-tarball: $(npm_tarball)
+npm-publish: $(NODE_MODULES) assert-clean-working-directory
+	$(QUIET) $(NPM_RELEASE) $(NPM_RELEASE_TYPE) $(NPM_RELEASE_COMMIT_MESSAGE)
 
-.PHONY: npm-tarball
-
-#/
-# Performs checks to prevent unintended behavior when attempting to publish to npm.
-#
-# TODO: once we have a `master` branch, swap `develop` for `master`
-#
-# @example
-# make npm-publish-check
-#/
-npm-publish-check:
-	$(QUIET) if [[ "$(shell $(GIT_BRANCH))" != "develop" ]]; then \
-		echo 'Error: invalid operation. New versions should only be performed on the `develop`'; \
-		echo 'branch.'; \
-		exit 1; \
-	fi
-
-.PHONY: npm-publish-check
-
-
-# Run pre-patch tasks.
-#
-# This target runs tasks which should be completed when publishing a "pre-patch" version.
-
-npm-version-pre-patch: npm-pre-version
-	$(QUIET) $(NPM) version pre-patch $(npm_version_flags)
-
-.PHONY: npm-version-pre-patch
-
-
-# Run patch tasks.
-#
-# This target runs tasks which should be completed when publishing a "patch" version.
-
-npm-version-patch: npm-pre-version
-	$(QUIET) $(NPM) version patch $(npm_version_flags)
-
-.PHONY: npm-version-patch
-
-
-# Run pre-minor tasks.
-#
-# This target runs tasks which should be completed when publishing a "pre-minor" version.
-
-npm-version-pre-minor: npm-pre-version
-	$(QUIET) $(NPM) version pre-minor $(npm_version_flags)
-
-.PHONY: npm-version-pre-minor
-
-
-# Run minor version tasks.
-#
-# This target runs tasks which should be completed when publishing a "minor" version.
-
-npm-version-minor: npm-pre-version
-	$(QUIET) $(NPM) version minor $(npm_version_flags)
-
-.PHONY: npm-version-minor
-
-
-# Run pre-major tasks.
-#
-# This target runs tasks which should be completed when publishing a "pre-major" version.
-
-npm-version-pre-major: npm-pre-version
-	$(QUIET) $(NPM) version pre-major $(npm_version_flags)
-
-.PHONY: npm-version-pre-major
-
-
-# Run major version tasks.
-#
-# This target runs tasks which should be completed when publishing a "major" version.
-
-npm-version-major: npm-pre-version
-	$(QUIET) $(NPM) version major $(npm_version_flags)
-
-.PHONY: npm-version-major
-
-
-# Run pre-release tasks.
-#
-# This target runs tasks which should be completed when publishing a "pre-release" version.
-
-npm-version-pre-release: npm-pre-version
-	$(QUIET) $(NPM) version pre-release $(npm_version_flags)
-
-.PHONY: npm-version-pre-release
-
-
-# Run version tasks.
-#
-# This target runs tasks which should be completed when incrementing the project version and committing version changes to the local repository.
-#
-# TODO: move recipe guts to a script
-# TODO: on version, regenerate SVG equations and update README equation elements
-
-npm-version: dist-browser-bundles
-	$(QUIET) $(GIT_ADD) -A dist && \
-		$(GIT_COMMIT) -m 'Update distributable browser bundles' && \
-		$(MAKE) -f $(this_file) $(npm_version_target) && \
-		$(MAKE) -f $(this_file) npm-post-version
-
-.PHONY: npm-version
-
-
-# Run post-version tasks.
-#
-# This target runs tasks which should be completed after incrementing the project version and committing version changes to the local repository.
-
-npm-post-version:
-	$(QUIET) echo ''
-	$(QUIET) echo 'Incremented version and committed changes.'
-	$(QUIET) echo ''
-	$(QUIET) echo "Tarball size: $(shell $(MAKE) -f $(this_file) stats-npm-tarball-size)"
-	$(QUIET) echo ''
-	$(QUIET) echo 'If okay to publish, run:'
-	$(QUIET) echo ''
-	$(QUIET) echo '  $$ make npm-publish'
-	$(QUIET) echo "  $$ git push origin $(shell $(GIT_BRANCH)) && git push origin --tags"
-	$(QUIET) echo ''
-
-.PHONY: npm-post-version
-
-#/
-# Remove npm tarball(s).
-#
-# This target removes npm gzipped archives.
-#/
-clean-npm-tarball:
-	$(QUIET) $(DELETE) $(DELETE_FLAGS) $(NPM_TARBALL_BUILD_OUT)/*.tgz
-
-.PHONY: clean-npm-tarball
+.PHONY: npm-publish
