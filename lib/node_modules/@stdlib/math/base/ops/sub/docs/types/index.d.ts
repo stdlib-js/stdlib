c24ed8d80e377ef80a47c74463f1eac7c87a6d63
Athan Reines <kgryte@gmail.com>
2021-10-08 11:07:57 -0700

    Add pkg to subtract two double-precision floating-point numbers

---
+++ lib/node_modules/@stdlib/math/base/ops/sub/docs/types/index.d.ts
@@ -0,0 +1,53 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Subtracts two double-precision floating-point numbers `x` and `y`.
+*
+* @param x - first input value
+* @param y - second input value
+* @returns result
+*
+* @example
+* var v = sub( -1.0, 5.0 );
+* // returns -6.0
+*
+* @example
+* var v = sub( 2.0, 5.0 );
+* // returns -3.0
+*
+* @example
+* var v = sub( 0.0, 5.0 );
+* // returns -5.0
+*
+* @example
+* var v = sub( -0.0, 0.0 );
+* // returns -0.0
+*
+* @example
+* var v = sub( NaN, NaN );
+* // returns NaN
+*/
+declare function sub( x: number, y: number ): number;
+
+
+// EXPORTS //
+
+export = sub;
