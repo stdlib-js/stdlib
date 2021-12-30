730e105a3fa744cfbacd6f1de50e6caacd865c15
kgryte <kgryte@gmail.com>
2021-12-30 15:14:47 -0800

    Add utility to apply a function to a three-dimensional nested array

---
+++ lib/node_modules/@stdlib/utils/map3d/docs/types/index.d.ts
@@ -0,0 +1,60 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/// <reference types="@stdlib/types"/>
+
+import { ArrayLike } from '@stdlib/types/array';
+
+/**
+* Applies a function to each nested element in a three-dimensional nested array and assigns the result to a nested element in a new three-dimensional nested array.
+*
+* ## Notes
+*
+* -   The applied function is provided the following arguments:
+*
+*     -   **value**: array element.
+*     -   **i**: index of the first dimension.
+*     -   **j**: index of the second dimension.
+*     -   **k**: index of the third dimension.
+*     -   **arr**: input array.
+*
+* @param arr - three-dimensional nested array
+* @param fcn - function to apply
+* @param thisArg - input function context
+* @returns three-dimensional nested array
+*
+* @example
+* var naryFunction = require( `@stdlib/utils/nary-function` );
+* var abs = require( `@stdlib/math/base/special/abs` );
+*
+* var arr = [
+*     [ [ -1, -2, -3 ] ],
+*     [ [ -4, -5, -6 ] ]
+* ];
+*
+* var out = map3d( arr, naryFunction( abs, 1 ) );
+* // returns [ [ [ 1, 2, 3 ] ], [ [ 4, 5, 6 ] ] ]
+*/
+declare function map3d( arr: ArrayLike<ArrayLike<ArrayLike<any>>>, fcn: Function, thisArg?: any ): Array<Array<Array<any>>>; // tslint-disable-line max-line-length
+
+
+// EXPORTS //
+
+export = map3d;
