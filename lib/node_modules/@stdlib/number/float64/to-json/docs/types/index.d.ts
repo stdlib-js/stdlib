56475658cb8df9350b154ede6231cb75540b92bf
Pranav <85227306+Pranavchiku@users.noreply.github.com>
2022-10-06 10:40:51 +0530

    Add support for serializing `NaN` and `Infinity` to JSON (#527)
    * created to-json and an alias number2json for number/float64
    
    * added examples
    
    * performed suggested changes
    
    * created examples
    
    * created docs and added index.d.ts in types
    
    * added test.ts
    
    * Include blank line per project conventions.
    
    * Fix description
    
    * Fix specifier
    
    * Fix path
    
    * Fix description
    
    * Remove blank line
    
    * Update description to be consistent with elsewhere
    
    * Remove import
    
    * Added package.json
    
    * created benchmark and added benchmark.js
    
    * added benchmark for large numbers
    
    * Fix keyword
    
    * performed suggested changes
    
    * add README.md
    
    * created test and added test.js
    
    * Update copyright year
    
    * performed suggested changes
    
    * Update lib/node_modules/@stdlib/number/float64/to-json/docs/types/index.d.ts
    
    Co-authored-by: Athan <kgryte@gmail.com>
    
    * add repl.txt
    
    * Add hyperlink in README.md
    
    * Remove section content
    
    * Remove unrelated content
    
    * Removing leftover comment
    
    * Update lib/node_modules/@stdlib/number/float64/examples/index.js
    
    Co-authored-by: Athan <kgryte@gmail.com>
    
    * Update lib/node_modules/@stdlib/number/float64/examples/index.js
    
    Co-authored-by: Athan <kgryte@gmail.com>
    
    Co-authored-by: Athan <kgryte@gmail.com>

---
+++ lib/node_modules/@stdlib/number/float64/to-json/docs/types/index.d.ts
@@ -0,0 +1,36 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2022 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Returns a JSON representation of a number.
+*
+* @param x - input value
+* @returns JSON representation
+*
+* @example
+* var str = JSON.stringify( number2json( NaN ) );
+* // returns '{"type":"float64","value":"NaN"}'
+*/
+declare function number2json( x: number ): any;
+
+
+// EXPORTS //
+
+export = number2json;
