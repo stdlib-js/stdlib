76da4643fce15ad49ef8b3ed043c54f69a8ff2c1
Philipp Burckhardt <pburckhardt@outlook.com>
2022-10-08 16:09:06 -0400

    Try using single quotes

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,49 +1,50 @@
-name=""
+ name=""
   if [ "true" != "false" ]; then
-    name="${name} -name *.js"
-  fi
-  if [ "true" != "false" ]; then
-    if [ -n "${name}" ]; then
-      name="${name} -o"
-    fi
-    name="${name} -name *.md"
+    name= name + "tru
   fi
   if [ "true" != "false" ]; then
     if [ -n "${name}" ]; then
       name="${name} -o"
     fi
-    name="${name} -name *.json"
+    name="${name} -name '*.md'"
   fi
-  if [ "false" != "false" ]; then
+  if [ "true" != "false" ]; then
     if [ -n "${name}" ]; then
       name="${name} -o"
     fi
-    name="${name} -name *.repl"
+    name="${name} -name '*.json'"
   fi
-  if [ "false" != "false" ]; then
+  if [ "true" != "false" ]; then
     if [ -n "${name}" ]; then
       name="${name} -o"
     fi
-    name="${name} -name *.R"
+    name="${name} -name '*.repl'"
   fi
-  if [ "false" != "false" ]; then
+  if [ "true" != "false" ]; then
     if [ -n "${name}" ]; then
       name="${name} -o"
     fi
-    name="${name} -name *.c"
+    name="${name} -name '*.R'"
   fi
-  if [ "false" != "false" ]; then
+  if [ "true" != "false" ]; then
     if [ -n "${name}" ]; then
       name="${name} -o"
     fi
-    name="${name} -name *.ts"
+    name="${name} -name '*.c'"
   fi
-  if [ "false" != "false" ]; then
+  if [ "true" != "false" ]; then
     if [ -n "${name}" ]; then
       name="${name} -o"
     fi
-    name="${name} -name *.py"
+    name="${name} -name '*.ts'"
+  fi
+  if [ "true" != "false" ]; then
+    if [ -n "${name}" ]; then
+      name="${name} -o"
+    fi
+    name="${name} -name '*.py'"
   fi
   files=$(find . -type f \( ${name} \) |
     grep -E ".*" |
     shuf -n 100 | tr '\n' ' ')
+
