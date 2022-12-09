e8fca33a225c52960e80783508adbde72f801e55
Stephannie Jimenez <steff456@hotmail.com>
2022-12-09 15:27:16 -0500

    Update minmax typescript declarations

--- lib/node_modules/@stdlib/math/base/special/minmax/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/math/base/special/minmax/docs/types/index.d.ts
@@ -137,9 +137,66 @@ import { Collection } from '@stdlib/types/object';
 	/**
 	* Returns the minimum and maximum values and assigns results to a provided output array.
 	*
+	* @param out - output object
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns minimum and maximum values
+	*
+	* @example
+	* var out = [ 0.0, 0.0 ];
+	* var v = minmax( out, 1, 0 );
+	* // returns [ Infinity, -Infinity ]
+	*
+	* var bool = ( v === out );
+	* // returns true
+	*/
+	assign( out: Collection, stride: number, offset: number ): Collection;
+
+	/**
+	* Returns the minimum and maximum values and assigns results to a provided output array.
+	*
+	* @param x - first number
+	* @param out - output object
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns minimum and maximum values
+	*
+	* @example
+	* var out = [ 0.0, 0.0 ];
+	* var v = minmax( 5.9, out, 1, 0 );
+	* // returns [ 5.9, 5.9 ]
+	*
+	* var bool = ( v === out );
+	* // returns true
+	*/
+	assign( x: number, out: Collection, stride: number, offset: number ): Collection; // tslint-disable-line max-line-length
+
+	/**
+	* Returns the minimum and maximum values and assigns results to a provided output array.
+	*
 	* @param x - first number
 	* @param y - second number
-	* @param args - numbers
+	* @param out - output object
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns minimum and maximum values
+	*
+	* @example
+	* var out = [ 0.0, 0.0 ];
+	* var v = minmax( 5.9, 3.14, out, 1, 0 );
+	* // returns [ 3.14, 5.9 ]
+	*
+	* var bool = ( v === out );
+	* // returns true
+	*/
+	assign( x: number, y: number, out: Collection, stride: number, offset: number ): Collection; // tslint-disable-line max-line-length
+
+	/**
+	* Returns the minimum and maximum values and assigns results to a provided output array.
+	*
+	* @param x - first number
+	* @param y - second number
+	* @param z - third number
 	* @param out - output object
 	* @param stride - output array stride
 	* @param offset - output array index offset
@@ -153,7 +210,73 @@ import { Collection } from '@stdlib/types/object';
 	* var bool = ( v === out );
 	* // returns true
 	*/
-	assign( x: number, y?: number, ...args: Array<number>, out: ArrayLike<any>, stride: number, offset: number ): Collection; // tslint-disable-line max-line-length
+	assign( x: number, y: number, z: number, out: Collection, stride: number, offset: number ): Collection; // tslint-disable-line max-line-length
+
+	/**
+	* Returns the minimum and maximum values and assigns results to a provided output array.
+	*
+	* @param x - first number
+	* @param y - second number
+	* @param z - third number
+	* @param w - fourth number
+	* @param out - output object
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns minimum and maximum values
+	*
+	* @example
+	* var out = [ 0.0, 0.0 ];
+	* var v = minmax( 5.9, 3.14, 4.2, 3.9, out, 1, 0 );
+	* // returns [ 3.14, 5.9 ]
+	*
+	* var bool = ( v === out );
+	* // returns true
+	*/
+	assign( x: number, y: number, z: number, w: number, out: Collection, stride: number, offset: number ): Collection; // tslint-disable-line max-line-length
+
+	/**
+	* Returns the minimum and maximum values and assigns results to a provided output array.
+	*
+	* @param x - first number
+	* @param y - second number
+	* @param z - third number
+	* @param w - fourth number
+	* @param v - fifth number
+	* @param out - output object
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns minimum and maximum values
+	*
+	* @example
+	* var out = [ 0.0, 0.0 ];
+	* var v = minmax( 5.9, 3.14, 4.2, 3.9, 3.8, out, 1, 0 );
+	* // returns [ 3.14, 5.9 ]
+	*
+	* var bool = ( v === out );
+	* // returns true
+	*/
+	assign( x: number, y: number, z: number, w: number, v: number, out: Collection, stride: number, offset: number ): Collection; // tslint-disable-line max-line-length
+
+	/**
+	* Returns the minimum and maximum values and assigns results to a provided output array.
+	*
+	* @param x - first number
+	* @param y - second number
+	* @param args - numbers
+	* @param out - output object
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns minimum and maximum values
+	*
+	* @example
+	* var out = [ 0.0, 0.0 ];
+	* var v = minmax( 5.9, 3.14, 4.2, 3.9, 3.8, 3.7, out, 1, 0 );
+	* // returns [ 3.14, 5.9 ]
+	*
+	* var bool = ( v === out );
+	* // returns true
+	*/
+	assign( x: number, y: number, ...args: Array<number|Collection> ): Collection; // tslint-disable-line max-line-length
 }
 
 /**
