1b17097195a8c1e0a49df068a9ce41b91d5ad021
Stephannie Jimenez <steff456@hotmail.com>
2022-12-13 12:23:18 -0500

    Update benchmark and docs

--- lib/node_modules/@stdlib/math/base/special/minmaxabs/docs/types/test.ts
+++ lib/node_modules/@stdlib/math/base/special/minmaxabs/docs/types/test.ts
@@ -21,15 +21,17 @@ import minmaxabs = require( './index' );
 
 // TESTS //
 
-// The function returns a collection...
+// The function returns an array of numbers...
 {
-	minmaxabs( -0.2 ); // $ExpectType Collection
-	minmaxabs( 3, -0.2 ); // $ExpectType Collection
-	minmaxabs( 3, -0.2, -1.2, -4.0 ); // $ExpectType Collection
-	minmaxabs( [], -4.0 ); // $ExpectType Collection
+	minmaxabs(); // $ExpectType number[]
+	minmaxabs( -0.2 ); // $ExpectType number[]
+	minmaxabs( 3.0, -0.2 ); // $ExpectType number[]
+	minmaxabs( 3.0, -0.2, -1.2 ); // $ExpectType number[]
+	minmaxabs( 3.0, -0.2, -1.2, -4.0 ); // $ExpectType number[]
+	minmaxabs( 3.0, -0.2, -1.2, -4.0, 5.0 ); // $ExpectType number[]
 }
 
-// The compiler throws an error if the function is provided non-numbers for all but the first argument...
+// The compiler throws an error if the function is provided an argument which is not a number...
 {
 	minmaxabs( true ); // $ExpectError
 	minmaxabs( false ); // $ExpectError
@@ -45,17 +47,112 @@ import minmaxabs = require( './index' );
 	minmaxabs( 1.2, 'abc' ); // $ExpectError
 	minmaxabs( 1.2, ( x: number ): number => x ); // $ExpectError
 
-	minmaxabs( 1.2, 3, true ); // $ExpectError
-	minmaxabs( 1.2, 3, false ); // $ExpectError
-	minmaxabs( 1.2, 3, [] ); // $ExpectError
-	minmaxabs( 1.2, 3, {} ); // $ExpectError
-	minmaxabs( 1.2, 3, 'abc' ); // $ExpectError
-	minmaxabs( 1.2, 3, ( x: number ): number => x ); // $ExpectError
+	minmaxabs( 1.2, 3.0, true ); // $ExpectError
+	minmaxabs( 1.2, 3.0, false ); // $ExpectError
+	minmaxabs( 1.2, 3.0, [] ); // $ExpectError
+	minmaxabs( 1.2, 3.0, {} ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 'abc' ); // $ExpectError
+	minmaxabs( 1.2, 3.0, ( x: number ): number => x ); // $ExpectError
+
+	minmaxabs( 1.2, 3.0, 4.0, true ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, false ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, [] ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, {} ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 'abc' ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, ( x: number ): number => x ); // $ExpectError
+
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, true ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, false ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, [] ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, {} ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 'abc' ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, ( x: number ): number => x ); // $ExpectError
+
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, true ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, false ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, [] ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, {} ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, 'abc' ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, ( x: number ): number => x ); // $ExpectError
+
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, 7.0, true ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, 7.0, false ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, 7.0, [] ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, 7.0, {} ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, 7.0, 'abc' ); // $ExpectError
+	minmaxabs( 1.2, 3.0, 4.0, 5.0, 6.0, 7.0, ( x: number ): number => x ); // $ExpectError
 }
 
-// The compiler throws an error if the function is not provided a number or collection as its first argument...
+// Attached to the main export is an `assign` method which returns an array-like object containing numbers...
 {
-	minmaxabs( true, 3 ); // $ExpectError
-	minmaxabs( false, 3 ); // $ExpectError
-	minmaxabs( {}, 3 ); // $ExpectError
+	const out = [ 0.0, 0.0 ];
+
+	minmaxabs.assign( out, 1, 0 ); // $ExpectType Collection
+	minmaxabs.assign( 3.0, out, 1, 0 ); // $ExpectType Collection
+	minmaxabs.assign( 3.0, -0.2, out, 1, 0 ); // $ExpectType Collection
+	minmaxabs.assign( 3.0, -0.2, 1.0, out, 1, 0 ); // $ExpectType Collection
+	minmaxabs.assign( 3.0, -0.2, -1.2, -4.0, out, 1, 0 ); // $ExpectType Collection
+	minmaxabs.assign( 3.0, -0.2, -1.2, -4.0, 5.0, out, 1, 0 ); // $ExpectType Collection
+	minmaxabs.assign( 3.0, -0.2, -1.2, -4.0, 5.0, 6.0, out, 1, 0 ); // $ExpectType Collection
+}
+
+// The compiler throws an error if the `assign` method is provided a first argument which is not a number...
+{
+	const out = [ 0.0, 0.0 ];
+
+	minmaxabs.assign( true, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( false, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( '5', -0.2, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( null, -0.2, 1.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( [], -0.2, -1.2, -4.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( {}, -0.2, -1.2, -4.0, 5.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( ( x: number ): number => x, -0.2, -1.2, -4.0, 5.0, 6.0, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a second argument which is not a number...
+{
+	const out = [ 0.0, 0.0 ];
+
+	minmaxabs.assign( 1.0, false, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, '5', -0.2, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, null, -0.2, 1.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, [], -0.2, -1.2, -4.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, {}, -0.2, -1.2, -4.0, 5.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, ( x: number ): number => x, -0.2, -1.2, -4.0, 5.0, 6.0, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided an invalid third argument...
+{
+	const out = [ 0.0, 0.0 ];
+
+	minmaxabs.assign( 1.0, 2.0, false, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, 2.0, null, -0.2, 1.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, 2.0, {}, -0.2, -1.2, -4.0, 5.0, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided an invalid fourth argument...
+{
+	const out = [ 0.0, 0.0 ];
+
+	minmaxabs.assign( 1.0, 2.0, 3.0, false, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, 2.0, 3.0, null, -0.2, 1.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, 2.0, 3.0, {}, -0.2, -1.2, -4.0, 5.0, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided an invalid fifth argument...
+{
+	const out = [ 0.0, 0.0 ];
+
+	minmaxabs.assign( 1.0, 2.0, 3.0, 4.0, false, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, 2.0, 3.0, 4.0, null, -0.2, 1.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, 2.0, 3.0, 4.0, {}, -0.2, -1.2, -4.0, 5.0, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided an invalid sixth argument...
+{
+	const out = [ 0.0, 0.0 ];
+
+	minmaxabs.assign( 1.0, 2.0, 3.0, 4.0, 5.0, false, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, 2.0, 3.0, 4.0, 5.0, null, -0.2, 1.0, out, 1, 0 ); // $ExpectError
+	minmaxabs.assign( 1.0, 2.0, 3.0, 4.0, 5.0, {}, -0.2, -1.2, -4.0, 5.0, out, 1, 0 ); // $ExpectError
 }
