8e3f1e42225b291c0874de91e0deeddd57187c84
Athan Reines <kgryte@gmail.com>
2022-12-17 15:23:09 -0800

    Remove semicolon

--- lib/node_modules/@stdlib/strided/napi/zmap/include/stdlib/strided/napi/zmap.h
+++ lib/node_modules/@stdlib/strided/napi/zmap/include/stdlib/strided/napi/zmap.h
@@ -40,7 +40,7 @@
 * // ...
 *
 * // Register a Node-API module:
-* STDLIB_STRIDED_NAPI_MODULE_ZMAP( scale );
+* STDLIB_STRIDED_NAPI_MODULE_ZMAP( scale )
 */
 #define STDLIB_STRIDED_NAPI_MODULE_ZMAP( clbk )                                \
 	static napi_value stdlib_strided_napi_zmap_wrapper(                        \
