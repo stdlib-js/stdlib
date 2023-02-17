319d420e00b7e4c2cfdca6878ed3e4864f6a2ee3
Athan Reines <kgryte@gmail.com>
2023-02-17 02:05:15 -0800

    Add missing declaration tests

--- lib/node_modules/@stdlib/random/strided/mt19937/docs/types/test.ts
+++ lib/node_modules/@stdlib/random/strided/mt19937/docs/types/test.ts
@@ -228,3 +228,211 @@ import random = require( './index' );
 	random.ndarray( out.length, out, 1 ); // $ExpectError
 	random.ndarray( out.length, out, 1, 0, out ); // $ExpectError
 }
+
+// Attached to main export is a `normalized` method which returns a collection...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized( out.length, out, 1 ); // $ExpectType Collection
+	random.normalized( out.length, out, 1, {} ); // $ExpectType Collection
+}
+
+// The compiler throws an error if the `normalized` method is provided a first argument which is not a number...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized( '10', out, 1 ); // $ExpectError
+	random.normalized( true, out, 1 ); // $ExpectError
+	random.normalized( false, out, 1 ); // $ExpectError
+	random.normalized( null, out, 1 ); // $ExpectError
+	random.normalized( undefined, out, 1 ); // $ExpectError
+	random.normalized( [], out, 1 ); // $ExpectError
+	random.normalized( {}, out, 1 ); // $ExpectError
+	random.normalized( ( x: number ): number => x, out, 1 ); // $ExpectError
+
+	random.normalized( '10', out, 1, {} ); // $ExpectError
+	random.normalized( true, out, 1, {} ); // $ExpectError
+	random.normalized( false, out, 1, {} ); // $ExpectError
+	random.normalized( null, out, 1, {} ); // $ExpectError
+	random.normalized( undefined, out, 1, {} ); // $ExpectError
+	random.normalized( [], out, 1, {} ); // $ExpectError
+	random.normalized( {}, out, 1, {} ); // $ExpectError
+	random.normalized( ( x: number ): number => x, out, 1, {} ); // $ExpectError
+}
+
+// The compiler throws an error if the `normalized` method is provided a second argument which is not a collection...
+{
+	random.normalized( 10, 10, 1 ); // $ExpectError
+	random.normalized( 10, true, 1 ); // $ExpectError
+	random.normalized( 10, false, 1 ); // $ExpectError
+	random.normalized( 10, null, 1 ); // $ExpectError
+	random.normalized( 10, undefined, 1 ); // $ExpectError
+	random.normalized( 10, {}, 1 ); // $ExpectError
+
+	random.normalized( 10, 10, 1, {} ); // $ExpectError
+	random.normalized( 10, true, 1, {} ); // $ExpectError
+	random.normalized( 10, false, 1, {} ); // $ExpectError
+	random.normalized( 10, null, 1, {} ); // $ExpectError
+	random.normalized( 10, undefined, 1, {} ); // $ExpectError
+	random.normalized( 10, {}, 1, {} ); // $ExpectError
+}
+
+// The compiler throws an error if the `normalized` method is provided a third argument which is not a number...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized( out.length, out, '10' ); // $ExpectError
+	random.normalized( out.length, out, true ); // $ExpectError
+	random.normalized( out.length, out, false ); // $ExpectError
+	random.normalized( out.length, out, null ); // $ExpectError
+	random.normalized( out.length, out, undefined ); // $ExpectError
+	random.normalized( out.length, out, [] ); // $ExpectError
+	random.normalized( out.length, out, {} ); // $ExpectError
+	random.normalized( out.length, out, ( x: number ): number => x ); // $ExpectError
+
+	random.normalized( out.length, out, '10', {} ); // $ExpectError
+	random.normalized( out.length, out, true, {} ); // $ExpectError
+	random.normalized( out.length, out, false, {} ); // $ExpectError
+	random.normalized( out.length, out, null, {} ); // $ExpectError
+	random.normalized( out.length, out, undefined, {} ); // $ExpectError
+	random.normalized( out.length, out, [], {} ); // $ExpectError
+	random.normalized( out.length, out, {}, {} ); // $ExpectError
+	random.normalized( out.length, out, ( x: number ): number => x, {} ); // $ExpectError
+}
+
+// The compiler throws an error if the `normalized` method is provided an invalid option...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized( out.length, out, 1, 0, { 'copy': '10' } ); // $ExpectError
+	random.normalized( out.length, out, 1, 0, { 'copy': null } ); // $ExpectError
+	random.normalized( out.length, out, 1, 0, { 'copy': [] } ); // $ExpectError
+	random.normalized( out.length, out, 1, 0, { 'copy': {} } ); // $ExpectError
+	random.normalized( out.length, out, 1, 0, { 'copy': ( x: number ): number => x } ); // $ExpectError
+}
+
+// The compiler throws an error if the `normalized` method is provided an unsupported number of arguments...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized(); // $ExpectError
+	random.normalized( out.length ); // $ExpectError
+	random.normalized( out.length, out ); // $ExpectError
+	random.normalized( out.length, out, 1, {}, {} ); // $ExpectError
+}
+
+// Attached to the `normalized` method is an `ndarray` method which returns a collection...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized.ndarray( out.length, out, 1, 0 ); // $ExpectType Collection
+	random.normalized.ndarray( out.length, out, 1, 0, {} ); // $ExpectType Collection
+}
+
+// The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized.ndarray( '10', out, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( true, out, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( false, out, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( null, out, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( undefined, out, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( [], out, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( {}, out, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( ( x: number ): number => x, out, 1, 0 ); // $ExpectError
+
+	random.normalized.ndarray( '10', out, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( true, out, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( false, out, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( null, out, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( undefined, out, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( [], out, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( {}, out, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( ( x: number ): number => x, out, 1, 0, {} ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a second argument which is not a collection...
+{
+	random.normalized.ndarray( 10, 10, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( 10, true, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( 10, false, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( 10, null, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( 10, undefined, 1, 0 ); // $ExpectError
+	random.normalized.ndarray( 10, {}, 1, 0 ); // $ExpectError
+
+	random.normalized.ndarray( 10, 10, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( 10, true, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( 10, false, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( 10, null, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( 10, undefined, 1, 0, {} ); // $ExpectError
+	random.normalized.ndarray( 10, {}, 1, 0, {} ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a third argument which is not a number...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized.ndarray( out.length, out, '10', 0 ); // $ExpectError
+	random.normalized.ndarray( out.length, out, true, 0 ); // $ExpectError
+	random.normalized.ndarray( out.length, out, false, 0 ); // $ExpectError
+	random.normalized.ndarray( out.length, out, null, 0 ); // $ExpectError
+	random.normalized.ndarray( out.length, out, undefined, 0 ); // $ExpectError
+	random.normalized.ndarray( out.length, out, [], 0 ); // $ExpectError
+	random.normalized.ndarray( out.length, out, {}, 0 ); // $ExpectError
+	random.normalized.ndarray( out.length, out, ( x: number ): number => x, 0 ); // $ExpectError
+
+	random.normalized.ndarray( out.length, out, '10', 0, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, true, 0, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, false, 0, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, null, 0, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, undefined, 0, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, [], 0, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, {}, 0, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, ( x: number ): number => x, 0, {} ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided a fourth argument which is not a number...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized.ndarray( out.length, out, 1, '10' ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, true ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, false ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, null ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, undefined ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, [] ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, ( x: number ): number => x ); // $ExpectError
+
+	random.normalized.ndarray( out.length, out, 1, '10', {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, true, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, false, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, null, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, undefined, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, [], {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, {}, {} ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, ( x: number ): number => x, {} ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided an invalid option...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized.ndarray( out.length, out, 1, 0, { 'copy': '10' } ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, 0, { 'copy': null } ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, 0, { 'copy': [] } ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, 0, { 'copy': {} } ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, 0, { 'copy': ( x: number ): number => x } ); // $ExpectError
+}
+
+// The compiler throws an error if the `ndarray` method is provided an unsupported number of arguments...
+{
+	const out = new Float64Array( 10 );
+
+	random.normalized.ndarray(); // $ExpectError
+	random.normalized.ndarray( out.length ); // $ExpectError
+	random.normalized.ndarray( out.length, out ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1 ); // $ExpectError
+	random.normalized.ndarray( out.length, out, 1, 0, out ); // $ExpectError
+}
