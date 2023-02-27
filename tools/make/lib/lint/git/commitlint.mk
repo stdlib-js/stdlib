85927042e2855e0932a316cbe556c12ccd485c70
Athan Reines <kgryte@gmail.com>
2023-02-27 00:58:11 -0800

    chore: add recipes for linting Git commit messages

--- tools/make/lib/lint/git/commitlint.mk
+++ tools/make/lib/lint/git/commitlint.mk
@@ -33,13 +33,13 @@ COMMITLINT ?= $(BIN_DIR)/commitlint
 COMMITLINT_FLAGS ?= \
 	--cwd "$(ROOT_DIR)" \
 	--config "$(CONFIG_DIR)/commitlint/.commitlintrc.js" \
-	--edit
+	--verbose
 
 
 # RULES #
 
 #/
-# Lints a commit message.
+# Lints a commit.
 #
 # ## Notes
 #
@@ -52,7 +52,60 @@ COMMITLINT_FLAGS ?= \
 #/
 commitlint: $(NODE_MODULES)
 	$(QUIET) mv $(ROOT_DIR)/tsconfig.json $(ROOT_DIR)/tsconfig.json.tmp
-	$(QUIET) NODE_PATH="$(NODE_PATH)" $(NODE) "$(COMMITLINT)" $(COMMITLINT_FLAGS) || (mv $(ROOT_DIR)/tsconfig.json.tmp $(ROOT_DIR)/tsconfig.json && exit 1)
+	$(QUIET) NODE_PATH="$(NODE_PATH)" $(NODE) "$(COMMITLINT)" $(COMMITLINT_FLAGS) --edit || ( mv $(ROOT_DIR)/tsconfig.json.tmp $(ROOT_DIR)/tsconfig.json && exit 1 )
 	$(QUIET) mv $(ROOT_DIR)/tsconfig.json.tmp $(ROOT_DIR)/tsconfig.json
 
 .PHONY: commitlint
+
+#/
+# Lints a commit message.
+#
+# ## Notes
+#
+# -   We have to temporarily move the `tsconfig` file, as `commitlint` (erroneously) attempts to use the file for compiling TypeScript.
+#
+# @private
+#
+# @example
+# make commitlint-message
+#/
+commitlint-message: $(NODE_MODULES)
+	$(QUIET) mv $(ROOT_DIR)/tsconfig.json $(ROOT_DIR)/tsconfig.json.tmp
+	$(QUIET) ( printf "$(GIT_COMMIT_MESSAGE)" | NODE_PATH="$(NODE_PATH)" $(NODE) "$(COMMITLINT)" $(COMMITLINT_FLAGS) ) || ( mv $(ROOT_DIR)/tsconfig.json.tmp $(ROOT_DIR)/tsconfig.json && exit 1 )
+	$(QUIET) mv $(ROOT_DIR)/tsconfig.json.tmp $(ROOT_DIR)/tsconfig.json
+
+.PHONY: commitlint-message
+
+#/
+# Lints a list of files, each containing a commit message.
+#
+# ## Notes
+#
+# -   We have to temporarily move the `tsconfig` file, as `commitlint` (erroneously) attempts to use the file for compiling TypeScript.
+#
+# @private
+#
+# @param {string} FILES - list of file paths
+# @param {*} [FAST_FAIL] - flag indicating whether to stop linting upon encountering a lint error
+#
+# @example
+# make commitlint-files FILES='/foo/commit.txt'
+#/
+commitlint-files: $(NODE_MODULES)
+	$(QUIET) mv $(ROOT_DIR)/tsconfig.json $(ROOT_DIR)/tsconfig.json.tmp
+ifeq ($(FAIL_FAST), true)
+	$(QUIET) for file in $(FILES); do \
+		echo ''; \
+		echo "Linting commit message:"; \
+		( cat $$file | grep -v '^#' | $(NODE) "$(COMMITLINT)" $(COMMITLINT_FLAGS) ) || ( mv $(ROOT_DIR)/tsconfig.json.tmp $(ROOT_DIR)/tsconfig.json && exit 1 ); \
+	done
+else
+	$(QUIET) for file in $(FILES); do \
+		echo ''; \
+		echo "Linting commit message"; \
+		( cat $$file | grep -v '^#' | $(NODE) "$(COMMITLINT)" $(COMMITLINT_FLAGS) ) || echo 'Linting failed.'; \
+	done
+endif
+	$(QUIET) mv $(ROOT_DIR)/tsconfig.json.tmp $(ROOT_DIR)/tsconfig.json
+
+.PHONY: commitlint-files
