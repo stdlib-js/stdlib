41d3972e82c51bdd6522708d9446fce378e30c8a
Athan Reines <kgryte@gmail.com>
2023-04-01 17:47:35 -0700

    feat: add `ndarray/base/assert/is-real-floating-point-data-type`

---
+++ lib/node_modules/@stdlib/ndarray/base/assert/is-real-floating-point-data-type/docs/types/index.d.ts
@@ -0,0 +1,69 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2023 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Tests whether an input value is a supported ndarray real-valued floating-point data type.
+*
+* @param v - value to test
+* @returns boolean indicating whether an input value is a supported ndarray real-valued floating-point data type
+*
+* @example
+* var bool = isRealFloatingPointDataType( 'binary' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'float32' );
+* // returns true
+*
+* bool = isRealFloatingPointDataType( 'float64' );
+* // returns true
+*
+* bool = isRealFloatingPointDataType( 'generic' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'int16' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'int32' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'int8' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'uint16' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'uint32' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'uint8' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'uint8c' );
+* // returns false
+*
+* bool = isRealFloatingPointDataType( 'foo' );
+* // returns false
+*/
+declare function isRealFloatingPointDataType( v: any ): boolean;
+
+
+// EXPORTS //
+
+export = isRealFloatingPointDataType;
