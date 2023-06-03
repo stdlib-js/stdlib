c40a1396adb204bf1f9c6d550d99884a8004b424
Naresh Jagadeesan <37257700+Infinage@users.noreply.github.com>
2023-06-04 01:38:00 +0530

    feat: add `string/base/distances/levenshtein`
    PR-URL: #1030 
    Closes: #835 
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/string/base/distances/levenshtein/docs/types/index.d.ts
@@ -0,0 +1,46 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2023 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 2.0
+
+/**
+* Calculates the Levenshtein (edit) distance between two strings.
+*
+* @param str1 - first string value
+* @param str2 - second string value
+* @returns Levenshtein distance
+*
+* @example
+* var dist = levenshteinDistance( 'fly', 'ant' );
+* // returns 3
+*
+* @example
+* var dist = levenshteinDistance( 'algorithm', 'altruistic' );
+* // returns 6
+*
+*
+* @example
+* var dist = levenshteinDistance( 'hippo', 'elephant' );
+* // returns 7
+*/
+declare function levenshteinDistance( str1: string, str2: string ): number;
+
+
+// EXPORTS //
+
+export = levenshteinDistance;
