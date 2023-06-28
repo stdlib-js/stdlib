886d7f9cd3bb190f49e3d08c20f5b817b4b249f4
Philipp Burckhardt <pburckhardt@outlook.com>
2023-06-27 23:21:53 -0400

    feat: use type predicates for narrowing

--- lib/node_modules/@stdlib/assert/is-nonpositive-integer-array/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/assert/is-nonpositive-integer-array/docs/types/index.d.ts
@@ -36,7 +36,7 @@ interface IsNonPositiveIntegerArray {
 	* var bool = isNonPositiveIntegerArray( [ -3.0, '-3.0' ] );
 	* // returns false
 	*/
-	( value: any ): boolean;
+	( value: any ): value is ArrayLike<number | Number>;
 
 	/**
 	* Tests if a value is an array-like object containing only nonpositive primitive integer values.
@@ -56,7 +56,7 @@ interface IsNonPositiveIntegerArray {
 	* var bool = isNonPositiveIntegerArray.primitives( [ -3.0, new Number(-1.0) ] );
 	* // returns false
 	*/
-	primitives( value: any ): boolean;
+	primitives( value: any ): value is ArrayLike<number>;
 
 	/**
 	* Tests if a value is an array-like object containing only number objects having nonpositive integer values.
@@ -76,7 +76,7 @@ interface IsNonPositiveIntegerArray {
 	* var bool = isNonPositiveIntegerArray.objects( [ -3.0, new Number(-1.0) ] );
 	* // returns false
 	*/
-	objects( value: any ): boolean;
+	objects( value: any ): value is ArrayLike<Number>;
 }
 
 /**
--- lib/node_modules/@stdlib/assert/is-nonpositive-number/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/assert/is-nonpositive-number/docs/types/index.d.ts
@@ -48,7 +48,7 @@ interface IsNonPositiveNumber {
 	* var bool = isNonPositiveNumber( null );
 	* // returns false
 	*/
-	( value: any ): boolean;
+	( value: any ): value is number | Number;
 
 	/**
 	* Tests if a value is a number primitive having a nonpositive value.
@@ -64,7 +64,7 @@ interface IsNonPositiveNumber {
 	* var bool = isNonPositiveNumber.isPrimitive( new Number( -3.0 ) );
 	* // returns false
 	*/
-	isPrimitive( value: any ): boolean;
+	isPrimitive( value: any ): value is number;
 
 	/**
 	* Tests if a value is a number object having a nonpositive value.
@@ -80,7 +80,7 @@ interface IsNonPositiveNumber {
 	* var bool = isNonPositiveNumber.isObject( new Number( -3.0 ) );
 	* // returns true
 	*/
-	isObject( value: any ): boolean;
+	isObject( value: any ): value is Number;
 }
 
 /**
