fc23d36b095bd1848edee25a4b08b303833a5b6e
Athan Reines <kgryte@gmail.com>
2023-07-23 12:57:38 -0700

    feat: add an `assign` method

--- lib/node_modules/@stdlib/array/base/flatten2d/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/array/base/flatten2d/docs/types/index.d.ts
@@ -28,6 +28,71 @@ import { Collection } from '@stdlib/types/object';
 type Array2D<T> = Array<Collection<T>>;
 
 /**
+* Interface describing `flatten2d`.
+*/
+interface Flatten2d {
+	/**
+	* Flattens a two-dimensional nested array.
+	*
+	* ## Notes
+	*
+	* -   The function assumes that all nested arrays have the same length (i.e., the input array is **not** a ragged array).
+	*
+	* @param x - input array
+	* @param shape - array shape
+	* @param colexicographic - specifies whether to flatten array values in colexicographic order
+	* @returns flattened array
+	*
+	* @example
+	* var x = [ [ 1, 2 ], [ 3, 4 ] ];
+	*
+	* var out = flatten2d( x, [ 2, 2 ], false );
+	* // returns [ 1, 2, 3, 4 ]
+	*
+	* @example
+	* var x = [ [ 1, 2 ], [ 3, 4 ] ];
+	*
+	* var out = flatten2d( x, [ 2, 2 ], true );
+	* // returns [ 1, 3, 2, 4 ]
+	*/
+	<T = unknown>( x: Array2D<T>, shape: Collection<number>, colexicographic: boolean ): Array<T>;
+
+
+	/**
+	* Flattens a two-dimensional nested array and assigns elements to a provided output array.
+	*
+	* ## Notes
+	*
+	* -   The function assumes that all nested arrays have the same length (i.e., the input array is **not** a ragged array).
+	*
+	* @param x - input array
+	* @param shape - array shape
+	* @param colexicographic - specifies whether to flatten array values in colexicographic order
+	* @param out - output array
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns output array
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = [ [ 1, 2 ], [ 3, 4 ] ];
+	*
+	* var out = flatten2d.assign( x, [ 2, 2 ], false, new Float64Array( 4 ), 1, 0 );
+	* // returns <Float64Array>[ 1, 2, 3, 4 ]
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = [ [ 1, 2 ], [ 3, 4 ] ];
+	*
+	* var out = flatten2d.assign( x, [ 2, 2 ], true, new Float64Array( 4 ), 1, 0 );
+	* // returns <Float64Array>[ 1, 3, 2, 4 ]
+	*/
+	assign<T = unknown, U = unknown>( x: Array2D<T>, shape: Collection<number>, colexicographic: boolean, out: Collection<U>, stride: number, offset: number ): Collection<T | U>;
+}
+
+/**
 * Flattens a two-dimensional nested array.
 *
 * ## Notes
@@ -51,7 +116,7 @@ type Array2D<T> = Array<Collection<T>>;
 * var out = flatten2d( x, [ 2, 2 ], true );
 * // returns [ 1, 3, 2, 4 ]
 */
-declare function flatten2d<T = unknown>( x: Array2D<T>, shape: Collection<number>, colexicographic: boolean ): Array<T>;
+declare var flatten2d: Flatten2d;
 
 
 // EXPORTS //
