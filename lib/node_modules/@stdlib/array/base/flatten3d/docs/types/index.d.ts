e1d0385aea002003e5198f124b2f6c5388ec85da
Athan Reines <kgryte@gmail.com>
2023-07-26 16:27:00 -0700

    feat: add support for assigning results to an output array

--- lib/node_modules/@stdlib/array/base/flatten3d/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/array/base/flatten3d/docs/types/index.d.ts
@@ -28,6 +28,70 @@ import { Collection } from '@stdlib/types/object';
 type Array3D<T> = Array<Array<Collection<T>>>;
 
 /**
+* Interface describing `flatten3d`.
+*/
+interface Flatten3d {
+	/**
+	* Flattens a three-dimensional nested array.
+	*
+	* ## Notes
+	*
+	* -   The function assumes that all nested arrays have the same length (i.e., the input array is **not** a ragged array).
+	*
+	* @param x - input array
+	* @param shape - array shape
+	* @param colexicographic - specifies whether to flatten array values in colexicographic order
+	* @returns flattened array
+	*
+	* @example
+	* var x = [ [ [ 1, 2 ] ], [ [ 3, 4 ] ] ];
+	*
+	* var out = flatten3d( x, [ 2, 1, 2 ], false );
+	* // returns [ 1, 2, 3, 4 ]
+	*
+	* @example
+	* var x = [ [ [ 1, 2 ] ], [ [ 3, 4 ] ] ];
+	*
+	* var out = flatten3d( x, [ 2, 1, 2 ], true );
+	* // returns [ 1, 3, 2, 4 ]
+	*/
+	<T = unknown>( x: Array3D<T>, shape: Collection<number>, colexicographic: boolean ): Array<T>;
+
+	/**
+	* Flattens a three-dimensional nested array and assigns elements to a provided output array.
+	*
+	* ## Notes
+	*
+	* -   The function assumes that all nested arrays have the same length (i.e., the input array is **not** a ragged array).
+	*
+	* @param x - input array
+	* @param shape - array shape
+	* @param colexicographic - specifies whether to flatten array values in colexicographic order
+	* @param out - output array
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns output array
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = [ [ [ 1, 2 ] ], [ [ 3, 4 ] ] ];
+	*
+	* var out = flatten3d.assign( x, [ 2, 1, 2 ], false, new Float64Array( 4 ), 1, 0 );
+	* // returns <Float64Array>[ 1, 2, 3, 4 ]
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = [ [ [ 1, 2 ] ], [ [ 3, 4 ] ] ];
+	*
+	* var out = flatten3d.assign( x, [ 2, 1, 2 ], true, new Float64Array( 4 ), 1, 0 );
+	* // returns <Float64Array>[ 1, 3, 2, 4 ]
+	*/
+	assign<T = unknown, U = unknown>( x: Array3D<T>, shape: Collection<number>, colexicographic: boolean, out: Collection<U>, stride: number, offset: number ): Collection<T | U>;
+}
+
+/**
 * Flattens a three-dimensional nested array.
 *
 * ## Notes
@@ -51,7 +115,7 @@ type Array3D<T> = Array<Array<Collection<T>>>;
 * var out = flatten3d( x, [ 2, 1, 2 ], true );
 * // returns [ 1, 3, 2, 4 ]
 */
-declare function flatten3d<T = unknown>( x: Array3D<T>, shape: Collection<number>, colexicographic: boolean ): Array<T>;
+declare var flatten3d: Flatten3d;
 
 
 // EXPORTS //
