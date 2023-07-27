841d3453e96db2a7ff7a0ee7936023aea8ec3d1b
Athan Reines <kgryte@gmail.com>
2023-07-26 17:12:06 -0700

    feat: add support for assigning results to an output array

--- lib/node_modules/@stdlib/array/base/flatten4d/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/array/base/flatten4d/docs/types/index.d.ts
@@ -28,6 +28,70 @@ import { Collection } from '@stdlib/types/object';
 type Array4D<T> = Array<Array<Array<Collection<T>>>>;
 
 /**
+* Interface describing `flatten4d`.
+*/
+interface Flatten4d {
+	/**
+	* Flattens a four-dimensional nested array.
+	*
+	* ## Notes
+	*
+	* -   The function assumes that all nested arrays have the same length (i.e., the input array is **not** a ragged array).
+	*
+	* @param x - input array
+	* @param shape - array shape
+	* @param colexicographic - specifies whether to flatten array values in colexicographic order
+	* @returns flattened array
+	*
+	* @example
+	* var x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	*
+	* var out = flatten4d( x, [ 2, 1, 1, 2 ], false );
+	* // returns [ 1, 2, 3, 4 ]
+	*
+	* @example
+	* var x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	*
+	* var out = flatten4d( x, [ 2, 1, 1, 2 ], true );
+	* // returns [ 1, 3, 2, 4 ]
+	*/
+	<T = unknown>( x: Array4D<T>, shape: Collection<number>, colexicographic: boolean ): Array<T>;
+
+	/**
+	* Flattens a four-dimensional nested array and assigns elements to a provided output array.
+	*
+	* ## Notes
+	*
+	* -   The function assumes that all nested arrays have the same length (i.e., the input array is **not** a ragged array).
+	*
+	* @param x - input array
+	* @param shape - array shape
+	* @param colexicographic - specifies whether to flatten array values in colexicographic order
+	* @param out - output array
+	* @param stride - output array stride
+	* @param offset - output array index offset
+	* @returns output array
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	*
+	* var out = flatten4d.assign( x, [ 2, 2 ], false, new Float64Array( 4 ), 1, 0 );
+	* // returns <Float64Array>[ 1, 2, 3, 4 ]
+	*
+	* @example
+	* var Float64Array = require( `@stdlib/array/float64` );
+	*
+	* var x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	*
+	* var out = flatten4d.assign( x, [ 2, 2 ], true, new Float64Array( 4 ), 1, 0 );
+	* // returns <Float64Array>[ 1, 3, 2, 4 ]
+	*/
+	assign<T = unknown, U = unknown>( x: Array4D<T>, shape: Collection<number>, colexicographic: boolean, out: Collection<U>, stride: number, offset: number ): Collection<T | U>;
+}
+
+/**
 * Flattens a four-dimensional nested array.
 *
 * ## Notes
@@ -51,7 +115,7 @@ type Array4D<T> = Array<Array<Array<Collection<T>>>>;
 * var out = flatten4d( x, [ 2, 1, 1, 2 ], true );
 * // returns [ 1, 3, 2, 4 ]
 */
-declare function flatten4d<T = unknown>( x: Array4D<T>, shape: Collection<number>, colexicographic: boolean ): Array<T>;
+declare var flatten4d: Flatten4d;
 
 
 // EXPORTS //
--- lib/node_modules/@stdlib/array/base/flatten4d/docs/types/test.ts
+++ lib/node_modules/@stdlib/array/base/flatten4d/docs/types/test.ts
@@ -82,3 +82,119 @@ import flatten4d = require( './index' );
 	flatten4d( x, [ 2, 1, 1, 2 ] ); // $ExpectError
 	flatten4d( x, [ 2, 1, 1, 2 ], false, {} ); // $ExpectError
 }
+
+// Attached to the main export is an `assign` method which returns a collection...
+{
+	const x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+
+	const out1 = [ 0, 0, 0, 0 ];
+
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out1, 1, 0 ); // $ExpectType Collection<number>
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], true, out1, 1, 0 ); // $ExpectType Collection<number>
+
+	flatten4d.assign<number, number>( x, [ 2, 1, 1, 2 ], false, out1, 1, 0 ); // $ExpectType Collection<number>
+	flatten4d.assign<number, number>( x, [ 2, 1, 1, 2 ], true, out1, 1, 0 ); // $ExpectType Collection<number>
+
+	const out2 = [ '', '', '', '' ];
+
+	flatten4d.assign<string, string>( [ [ [ [ '1', '2' ] ] ], [ [ [ '3', '4' ] ] ] ], [ 2, 1, 1, 2 ], false, out2, 1, 0 ); // $ExpectType Collection<string>
+	flatten4d.assign<string, string>( [ [ [ [ '1', '2' ] ] ], [ [ [ '3', '4' ] ] ] ], [ 2, 1, 1, 2 ], true, out2, 1, 0 ); // $ExpectType Collection<string>
+}
+
+// The compiler throws an error if the `assign` method is provided a first argument which is not an array-like object...
+{
+	const out = [ 0, 0, 0, 0 ];
+
+	flatten4d.assign( 1, [ 2, 1, 1, 2 ], false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( true, [ 2, 1, 1, 2 ], false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( false, [ 2, 1, 1, 2 ], false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( null, [ 2, 1, 1, 2 ], false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( void 0, [ 2, 1, 1, 2 ], false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( {}, [ 2, 1, 1, 2 ], false, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a second argument which is not an array-like object containing numbers...
+{
+	const x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	const out = [ 0, 0, 0, 0 ];
+
+	flatten4d.assign( x, '', false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, 1, false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, true, false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, false, false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, null, false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, void 0, false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, {}, false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 1, '2', 3 ], false, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, ( x: number ): number => x, false, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a third argument which is not a boolean...
+{
+	const x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	const out = [ 0, 0, 0, 0 ];
+
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], '', out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], 1, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], null, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], void 0, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], {}, out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], [], out, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], ( x: number ): number => x, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a fourth argument which is not an array-like object...
+{
+	const x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, 1, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, true, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, false, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, null, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, void 0, 1, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, {}, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a fifth argument which is not a number...
+{
+	const x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	const out = [ 0, 0, 0, 0 ];
+
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, '1', 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, true, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, false, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, null, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, void 0, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, {}, 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, [], 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, ( x: number ): number => x, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a sixth argument which is not a number...
+{
+	const x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	const out = [ 0, 0, 0, 0 ];
+
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, '1' ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, true ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, false ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, null ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, void 0 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, {} ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, [] ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided an unsupported number of arguments...
+{
+	const x = [ [ [ [ 1, 2 ] ] ], [ [ [ 3, 4 ] ] ] ];
+	const out = [ 0, 0, 0, 0 ];
+
+	flatten4d.assign(); // $ExpectError
+	flatten4d.assign( x ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ] ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1 ); // $ExpectError
+	flatten4d.assign( x, [ 2, 1, 1, 2 ], false, out, 1, 0, {} ); // $ExpectError
+}
