ec92052c39a967222bcc4d81bb80a412e87eb725
Athan Reines <kgryte@gmail.com>
2023-08-18 01:31:49 -0700

    fix: update import path for `Collection` type definition
    Ref: https://github.com/stdlib-js/stdlib/commit/bde4671201dfa6b510f88bcb60d455f44c0842e1

--- lib/node_modules/@stdlib/math/strided/special/sqrt-by/docs/types/test.ts
+++ lib/node_modules/@stdlib/math/strided/special/sqrt-by/docs/types/test.ts
@@ -35,8 +35,8 @@ function accessor(): number {
 	const x = new Float64Array( 10 );
 	const y = new Float64Array( 10 );
 
-	sqrtBy( x.length, x, 1, y, 1, accessor ); // $ExpectType Collection
-	sqrtBy( x.length, x, 1, y, 1, accessor, {} ); // $ExpectType Collection
+	sqrtBy( x.length, x, 1, y, 1, accessor ); // $ExpectType Collection<number>
+	sqrtBy( x.length, x, 1, y, 1, accessor, {} ); // $ExpectType Collection<number>
 }
 
 // The compiler throws an error if the function is provided a first argument which is not a number...
@@ -143,8 +143,8 @@ function accessor(): number {
 	const x = new Float64Array( 10 );
 	const y = new Float64Array( 10 );
 
-	sqrtBy.ndarray( x.length, x, 1, 0, y, 1, 0, accessor ); // $ExpectType Collection
-	sqrtBy.ndarray( x.length, x, 1, 0, y, 1, 0, accessor, {} ); // $ExpectType Collection
+	sqrtBy.ndarray( x.length, x, 1, 0, y, 1, 0, accessor ); // $ExpectType Collection<number>
+	sqrtBy.ndarray( x.length, x, 1, 0, y, 1, 0, accessor, {} ); // $ExpectType Collection<number>
 }
 
 // The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
