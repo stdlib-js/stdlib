a2a771469fb7ed35e48c034e1bf4a490449be15f
Athan Reines <kgryte@gmail.com>
2023-08-18 01:37:28 -0700

    fix: update import path for `Collection` type definition
    Ref: https://github.com/stdlib-js/stdlib/commit/bde4671201dfa6b510f88bcb60d455f44c0842e1

--- lib/node_modules/@stdlib/utils/any/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/utils/any/docs/types/index.d.ts
@@ -20,7 +20,7 @@
 
 /// <reference types="@stdlib/types"/>
 
-import { Collection } from '@stdlib/types/object';
+import { Collection } from '@stdlib/types/array';
 
 /**
 * Tests whether at least one element in a collection is truthy.
