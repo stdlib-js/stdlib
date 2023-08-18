2de03325135024e1a1922d8bb166a95d6efde18d
Athan Reines <kgryte@gmail.com>
2023-08-18 01:54:53 -0700

    fix: update import path for `Collection` type definition
    Ref: https://github.com/stdlib-js/stdlib/commit/bde4671201dfa6b510f88bcb60d455f44c0842e1

--- lib/node_modules/@stdlib/utils/mask-arguments/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/utils/mask-arguments/docs/types/index.d.ts
@@ -20,7 +20,7 @@
 
 /// <reference types="@stdlib/types"/>
 
-import { Collection } from '@stdlib/types/object';
+import { Collection } from '@stdlib/types/array';
 
 /**
 * Returns a function that applies arguments to a provided function according to a specified mask.
