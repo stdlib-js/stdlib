efe050dbf3d17bd6929da255fa079c0324afc213
Philipp Burckhardt <pburckhardt@outlook.com>
2023-08-19 20:20:34 -0400

    feat!: update minimum TypeScript version
    To leverage new TypeScript features, we need to update the minimum TypeScript version to 4.1. Doing so allows us to take advantage of the various features across the stdlib code base: constructs like Record, Omit, or Partial, the keyof operator, type predicates, conditional types, and more. TypeScript versions older than 4.1 are not in widespread use, so this change should not affect most users, especially those who upgrade with each new TypeScript version.
    
    BREAKING CHANGE: update minimum TypeScript version to 4.1
    
    To migrate, users should upgrade their TypeScript version to at least version 4.1.

--- lib/node_modules/@stdlib/process/exec-path/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/process/exec-path/docs/types/index.d.ts
@@ -16,7 +16,7 @@
 * limitations under the License.
 */
 
-// TypeScript Version: 2.0
+// TypeScript Version: 4.1
 
 /**
 * Absolute pathname of the executable which started the current Node.js process.
