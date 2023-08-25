36d8cb755436aafd5b7bb085212de5e629b47457
Stephannie Jimenez Gacha <steff456@users.noreply.github.com>
2023-08-25 15:10:38 -0500

    feat!: remove support for out argument and add C implementation
    This commit refactors the `cpolar` API to no longer support the `out` argument and the API now accepts a complex number object as an input argument.
    
    BREAKING CHANGE: remove support for out argument
    
    In order to migrate, users should provide a complex number object as an input argument and handle a complex number object as a return value.
    
    PR-URL: 	https://github.com/stdlib-js/stdlib/pull/1067
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com> 
    Private-ref: https://github.com/stdlib-js/todo/issues/1454

--- lib/node_modules/@stdlib/math/base/special/cpolar/docs/types/test.ts
+++ lib/node_modules/@stdlib/math/base/special/cpolar/docs/types/test.ts
@@ -16,6 +16,7 @@
 * limitations under the License.
 */
 
+import Complex128 = require( '@stdlib/complex/float64' );
 import cpolar = require( './index' );
 
 
@@ -23,46 +24,89 @@ import cpolar = require( './index' );
 
 // The function returns an array of numbers...
 {
-	cpolar( -4, 3 ); // $ExpectType ArrayLike<number>
-	cpolar( [], 5, 3 ); // $ExpectType ArrayLike<number>
+	cpolar( new Complex128( 5.0, 3.0 ) ); // $ExpectType number[]
 }
 
-// The compiler throws an error if the function is provided a real component which is not a number...
+// The compiler throws an error if the function is provided a complex number...
 {
-	cpolar( true, 3 ); // $ExpectError
-	cpolar( false, 3 ); // $ExpectError
-	cpolar( null, 3 ); // $ExpectError
-	cpolar( undefined, 3 ); // $ExpectError
-	cpolar( '5', 3 ); // $ExpectError
-	cpolar( [], 3 ); // $ExpectError
-	cpolar( {}, 3 ); // $ExpectError
-	cpolar( ( x: number ): number => x, 3 ); // $ExpectError
-}
-
-// The compiler throws an error if the function is provided an imaginary component which is not a number...
-{
-	cpolar( 5, true ); // $ExpectError
-	cpolar( 5, false ); // $ExpectError
-	cpolar( 5, null ); // $ExpectError
-	cpolar( 5, undefined ); // $ExpectError
-	cpolar( 5, '5' ); // $ExpectError
-	cpolar( 5, [] ); // $ExpectError
-	cpolar( 5, {} ); // $ExpectError
-	cpolar( 5, ( x: number ): number => x ); // $ExpectError
-}
-
-// The compiler throws an error if the function is provided an output array which is not array-like...
-{
-	cpolar( true, 5, 3 ); // $ExpectError
-	cpolar( false, 5, 3 ); // $ExpectError
-	cpolar( 'abc', 5, 3 ); // $ExpectError
-	cpolar( {}, 5, 3 ); // $ExpectError
-	cpolar( ( x: number ): number => x, 5, 3 ); // $ExpectError
-	cpolar( 123, 5, 3 ); // $ExpectError
+	cpolar( true ); // $ExpectError
+	cpolar( false ); // $ExpectError
+	cpolar( null ); // $ExpectError
+	cpolar( undefined ); // $ExpectError
+	cpolar( '5' ); // $ExpectError
+	cpolar( [] ); // $ExpectError
+	cpolar( {} ); // $ExpectError
+	cpolar( ( x: number ): number => x ); // $ExpectError
 }
 
 // The compiler throws an error if the function is provided insufficient arguments...
 {
 	cpolar(); // $ExpectError
-	cpolar( 2 ); // $ExpectError
+}
+
+// Attached to the main export is an `assign` method which returns an array-like object containing numbers...
+{
+	const out = [ 0.0, 0.0 ];
+
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, 0 ); // $ExpectType Collection<number>
+	cpolar.assign<number>( new Complex128( 5.0, 3.0 ), out, 1, 0 ); // $ExpectType Collection<number>
+}
+
+// The compiler throws an error if the `assign` method is provided a first argument which is not a number...
+{
+	const out = [ 0.0, 0.0 ];
+
+	cpolar.assign( true, out, 1, 0 ); // $ExpectError
+	cpolar.assign( false, out, 1, 0 ); // $ExpectError
+	cpolar.assign( '5', out, 1, 0 ); // $ExpectError
+	cpolar.assign( null, out, 1, 0 ); // $ExpectError
+	cpolar.assign( [], out, 1, 0 ); // $ExpectError
+	cpolar.assign( {}, out, 1, 0 ); // $ExpectError
+	cpolar.assign( ( x: number ): number => x, out, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a second argument which is not an array-like object...
+{
+	cpolar.assign( new Complex128( 5.0, 3.0 ), 1, 1, 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), true, 1, 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), false, 1, 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), null, 1, 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), {}, 1, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a third argument which is not a number...
+{
+	const out = [ 0.0, 0 ];
+
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, '5', 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, true, 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, false, 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, null, 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, [], 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, {}, 0 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, ( x: number ): number => x, 0 ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided a fourth argument which is not a number...
+{
+	const out = [ 0.0, 0 ];
+
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, '5' ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, true ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, false ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, null ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, [] ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, {} ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the `assign` method is provided an unsupported number of arguments...
+{
+	const out = [ 0.0, 0 ];
+
+	cpolar.assign(); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ) ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1 ); // $ExpectError
+	cpolar.assign( new Complex128( 5.0, 3.0 ), out, 1, 0, 1 ); // $ExpectError
 }
