b23ab2d3dc27228f3abf291769af012fd6599d06
Athan Reines <kgryte@gmail.com>
2023-08-27 11:50:55 -0700

    bench: add benchmarks

--- docs/migration-guides/mathjs/benchmark/benchmark.abs.matrix.3d.js
+++ docs/migration-guides/mathjs/benchmark/benchmark.abs.matrix.3d.js
@@ -24,8 +24,11 @@ var resolve = require( 'path' ).resolve;
 var bench = require( '@stdlib/bench' );
 var isnan = require( '@stdlib/math/base/assert/is-nan' );
 var filled3dBy = require( '@stdlib/array/base/filled3d-by' );
+var unary3d = require( '@stdlib/array/base/unary3d' );
+var zeros3d = require( '@stdlib/array/base/zeros3d' );
 var array = require( '@stdlib/ndarray/array' );
 var uniform = require( '@stdlib/random/base/uniform' ).factory;
+var base = require( '@stdlib/math/base/special/abs' );
 var abs = require( '@stdlib/math/special/abs' );
 var tryRequire = require( '@stdlib/utils/try-require' );
 var pkg = require( './../package.json' ).name;
@@ -59,6 +62,31 @@ bench( pkg+'::stdlib:math/special/abs:value=ndarray,dtype=generic,size=100,shape
 	}
 	b.toc();
 	if ( isnan( y.get( 0, 0, 0 ) ) || isnan( y.get( 3, 4, 4 ) ) ) {
+		b.fail( 'should not return NaN' );
+	}
+	b.pass( 'benchmark finished' );
+	b.end();
+});
+
+bench( pkg+'::stdlib:array/base/unary3d:value=nested_array,dtype=generic,size=100,shape=(4,5,5)', opts, function benchmark( b ) {
+	var sh;
+	var x;
+	var y;
+	var i;
+
+	sh = [ 4, 5, 5 ];
+	x = filled3dBy( sh, uniform( -100.0, 100.0 ) );
+
+	b.tic();
+	for ( i = 0; i < b.iterations; i++ ) {
+		y = zeros3d( sh );
+		unary3d( [ x, y ], sh, base );
+		if ( isnan( y[ 0 ][ 0 ][ 0 ] ) || isnan( y[ 3 ][ 4 ][ 4 ] ) ) {
+			b.fail( 'should not return NaN' );
+		}
+	}
+	b.toc();
+	if ( isnan( y[ 1 ][ 1 ][ 1 ] ) || isnan( y[ 2 ][ 3 ][ 3 ] ) ) {
 		b.fail( 'should not return NaN' );
 	}
 	b.pass( 'benchmark finished' );
