7de06d8787ac72365d3ba9d29ac70d8ccbb069a0
Athan Reines <kgryte@gmail.com>
2023-09-10 16:21:47 -0700

    fix: update import path for `Collection` type definition and use generics
    Ref: https://github.com/stdlib-js/stdlib/commit/bde4671201dfa6b510f88bcb60d455f44c0842e1

--- lib/node_modules/@stdlib/utils/async/none-by-right/docs/types/test.ts
+++ lib/node_modules/@stdlib/utils/async/none-by-right/docs/types/test.ts
@@ -83,8 +83,8 @@ const done = ( error: Error | null, bool: boolean ) => {
 
 // Attached to main export is a `factory` method which returns a function...
 {
-	noneByRightAsync.factory( isPositive ); // $ExpectType FactoryFunction
-	noneByRightAsync.factory( { 'series': true }, isPositive ); // $ExpectType FactoryFunction
+	noneByRightAsync.factory( isPositive ); // $ExpectType FactoryFunction<number>
+	noneByRightAsync.factory( { 'series': true }, isPositive ); // $ExpectType FactoryFunction<number>
 }
 
 // The compiler throws an error if the `factory` method is provided an options argument which is not an object...
