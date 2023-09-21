982de7581326be5be20dda30103779ac5ae5767f
Stephannie Jimenez Gacha <steff456@users.noreply.github.com>
2023-09-21 16:40:45 -0500

    feat: refactor `string/remove-last`
    This commit adds 3 new base string packages for removing code units, code points, and grapheme clusters, respectively. This commit subsequently refactors `string/remove-last` to depend on those base packages. As a consequence, a new option has been added to `string/remove-last` to select which processing "mode" is desired in order to balance performance considerations.
    
    Additionally, this commit fixes a bug in `string/remove-first` due to an off-by-one indexing error.
    
    Lastly, this commit fixes the `name` field in `string/base/remove-first*` `package.json` files.
    
    PR-URL: 	https://github.com/stdlib-js/stdlib/pull/1079
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com> 
    Ref: https://github.com/stdlib-js/stdlib/issues/1062

--- lib/node_modules/@stdlib/string/remove-last/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/string/remove-last/docs/types/index.d.ts
@@ -18,6 +18,69 @@
 
 // TypeScript Version: 4.1
 
+// tslint:disable:unified-signatures
+
+/**
+* Interface describing function options.
+*/
+interface Options {
+	/**
+	* Specifies the type of characters to remove (default: 'grapheme').
+	*
+	* ## Notes
+	*
+	* -   The following option values are supported:
+	*
+	*     -   `'grapheme'`: grapheme clusters. Appropriate for strings containing visual characters which can span multiple Unicode code points (e.g., emoji).
+	*     -   `'code_point'`: Unicode code points. Appropriate for strings containing visual characters which are comprised of more than one Unicode code unit (e.g., ideographic symbols and punctuation and mathematical alphanumerics).
+	*     -   `'code_unit'`: UTF-16 code units. Appropriate for strings containing visual characters drawn from the basic multilingual plane (BMP) (e.g., common characters, such as those from the Latin, Greek, and Cyrillic alphabets).
+	*/
+	mode?: 'grapheme' | 'code_point' | 'code_unit';
+}
+
+/**
+* Removes the last character(s) of a string.
+*
+* @param str - input string
+* @param n - number of characters to remove (default: 1)
+* @param options - options
+* @returns updated string
+*
+* @example
+* var out = removeLast( 'last man standing', 1, {
+*     'mode': 'code_unit'
+* });
+* // returns 'last man standin'
+*
+* @example
+* var out = removeLast( '🐶🐮🐷🐰🐸', 2, {
+*     'mode': 'grapheme'
+* });
+* // returns '🐶🐮🐷'
+*/
+declare function removeLast( str: string, n: number, options?: Options ): string;
+
+/**
+* Removes the last character of a string.
+*
+* @param str - input string
+* @param options - options
+* @returns updated string
+*
+* @example
+* var out = removeLast( 'last man standing', {
+*     'mode': 'code_unit'
+* });
+* // returns 'last man standin'
+*
+* @example
+* var out = removeFirst( '🐶🐮🐷🐰🐸', {
+*     'mode': 'grapheme'
+* });
+* // returns '🐶🐮🐷🐰'
+*/
+declare function removeLast( str: string, options?: Options ): string;
+
 /**
 * Removes the last character(s) of a string.
 *
