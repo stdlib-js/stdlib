eff3cdd2245ee7c9265d38f2d31b49bc7f4400d8
Athan Reines <kgryte@gmail.com>
2023-10-14 02:26:26 -0700

    feat: add `ndarray/mostly-safe-casts`

---
+++ lib/node_modules/@stdlib/ndarray/mostly-safe-casts/docs/types/index.d.ts
@@ -0,0 +1,41 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2023 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/**
+* Returns a list of ndarray data types to which a provided ndarray data type can be safely cast and, for floating-point data types, can be downcast.
+*
+* ## Notes
+*
+* -   If not provided an ndarray data type, the function returns a casting table.
+* -   If provided an unrecognized ndarray data type, the function returns `null`.
+*
+* @param dtype - ndarray data type value
+* @returns list of ndarray data types or null
+*
+* @example
+* var list = mostlySafeCasts( 'float32' );
+* // returns [...]
+*/
+declare function mostlySafeCasts( dtype?: any ): any; // FIXME: use overloads to improve this definition
+
+
+// EXPORTS //
+
+export = mostlySafeCasts;
