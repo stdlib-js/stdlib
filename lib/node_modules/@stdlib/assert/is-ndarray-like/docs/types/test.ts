8b7e0325d5c3040171114c332e23a1e932302cb7
Philipp Burckhardt <pburckhardt@outlook.com>
2023-11-12 13:33:01 -0500

    build: replace tslint directives

--- lib/node_modules/@stdlib/assert/is-ndarray-like/docs/types/test.ts
+++ lib/node_modules/@stdlib/assert/is-ndarray-like/docs/types/test.ts
@@ -16,7 +16,7 @@
 * limitations under the License.
 */
 
-/* tslint:disable:no-invalid-this */
+/* eslint-disable @typescript-eslint/no-invalid-this */
 
 import isndarrayLike = require( './index' );
 
