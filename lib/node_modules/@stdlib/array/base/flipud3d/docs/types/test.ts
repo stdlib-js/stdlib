acd8e3a1b0419cad357bf39d3f315c5166f40be5
Athan Reines <kgryte@gmail.com>
2023-12-26 16:11:58 -0800

    feat: add `array/base/flipud3d`

---
+++ lib/node_modules/@stdlib/array/base/flipud3d/docs/types/test.ts
@@ -0,0 +1,48 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2023 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import flipud3d = require( './index' );
+
+
+// TESTS //
+
+// The function returns a nested array...
+{
+	const x = [ [ [ 1, 2 ], [ 3, 4 ] ] ];
+
+	flipud3d( x ); // $ExpectType Array3D<number>
+}
+
+// The compiler throws an error if the function is provided a first argument which is not a nested array...
+{
+	flipud3d( 'abc' ); // $ExpectError
+	flipud3d( 1 ); // $ExpectError
+	flipud3d( true ); // $ExpectError
+	flipud3d( false ); // $ExpectError
+	flipud3d( null ); // $ExpectError
+	flipud3d( {} ); // $ExpectError
+	flipud3d( ( x: number ): number => x ); // $ExpectError
+}
+
+// The compiler throws an error if the function is provided an unsupported number of arguments...
+{
+	const x = [ [ [ 1, 2 ], [ 3, 4 ] ] ];
+
+	flipud3d(); // $ExpectError
+	flipud3d( x, 2 ); // $ExpectError
+}
