e747f12e436656778635f822213af6f8fd122fd7
Athan Reines <kgryte@gmail.com>
2024-02-06 23:55:47 -0800

    refactor: reduce dtype string duplication

--- lib/node_modules/@stdlib/array/typed-dtypes/docs/types/test.ts
+++ lib/node_modules/@stdlib/array/typed-dtypes/docs/types/test.ts
@@ -23,7 +23,7 @@ import dtypes = require( './index' );
 
 // The function returns a string array..
 {
-	dtypes(); // $ExpectType NumericDataType[]
+	dtypes(); // $ExpectType TypedDataType[]
 }
 
 // The compiler throws an error if the function is provided arguments...
