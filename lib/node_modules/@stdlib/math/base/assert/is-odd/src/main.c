33b4f70fd3104b62eab54372eda93b8095721785
Shashank Shekhar Singh <123410790+Shashankss1205@users.noreply.github.com>
2024-03-05 17:51:55 +0530

    feat: add C implementation for `math/base/assert/is-odd`
    Closes: https://github.com/stdlib-js/stdlib/issues/1668
    PR-URL: 	https://github.com/stdlib-js/stdlib/pull/1670
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Co-authored-by: Pranav Goswami <goswami.4@iitj.ac.in>
    Reviewed-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Pranav Goswami <goswami.4@iitj.ac.in>
    Signed-off-by: Pranav Goswami <goswami.4@iitj.ac.in>
    Signed-off-by: Athan Reines <kgryte@gmail.com>

--- lib/node_modules/@stdlib/math/base/assert/is-odd/src/main.c
+++ lib/node_modules/@stdlib/math/base/assert/is-odd/src/main.c
@@ -1,7 +1,7 @@
 /**
 * @license Apache-2.0
 *
-* Copyright (c) 2018 The Stdlib Authors.
+* Copyright (c) 2024 The Stdlib Authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,34 +16,25 @@
 * limitations under the License.
 */
 
-'use strict';
+#include "stdlib/math/base/assert/is_odd.h"
+#include "stdlib/math/base/assert/is_even.h"
 
 /**
 * Test if a finite double-precision floating-point number is an odd number.
 *
-* @module @stdlib/math/base/assert/is-odd
+* @param x    input value
+* @return	  output value
 *
 * @example
-* var isOdd = require( '@stdlib/math/base/assert/is-odd' );
+* #include <stdbool.h>
 *
-* var bool = isOdd( 5.0 );
+* bool out = stdlib_base_is_odd( 3.0 );
 * // returns true
-*
-* bool = isOdd( -2.0 );
-* // returns false
-*
-* bool = isOdd( 0.0 );
-* // returns false
-*
-* bool = isOdd( NaN );
-* // returns false
 */
-
-// MODULES //
-
-var main = require( './main.js' );
-
-
-// EXPORTS //
-
-module.exports = main;
+bool stdlib_base_is_odd( const double x ) {
+    // Check sign to prevent overflow...
+    if ( x > 0.0 ) {
+        return stdlib_base_is_even( x - 1.0 );
+    }
+    return stdlib_base_is_even( x + 1.0 );
+}
