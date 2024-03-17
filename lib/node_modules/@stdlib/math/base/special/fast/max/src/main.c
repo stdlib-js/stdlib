bd53ca9f58d0481edff05d43323561dcbcdde189
Rejoan Sardar <119718513+Rejoan-Sardar@users.noreply.github.com>
2024-03-17 07:16:13 +0530

    feat: add C implementation for `math/base/special/fast/max`
    PR-URL: #1912
    
    ---------
    
    Signed-off-by: Pranav Goswami <goswami.4@iitj.ac.in>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: Pranav Goswami <goswami.4@iitj.ac.in>
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Reviewed-by: Pranav Goswami <goswami.4@iitj.ac.in>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/math/base/special/fast/max/src/main.c
@@ -0,0 +1,53 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/math/base/special/fast/max.h"
+
+/**
+* Returns the maximum value.
+*
+* @param x       first number
+* @param y       second number
+* @return        maximum value
+*
+* @example
+* double v = stdlib_base_fast_max( 3.14, 4.2 );
+* // returns 4.2
+*
+* @example
+* double v = stdlib_base_fast_max( 3.14, NaN );
+* // returns NaN
+*
+* @example
+* double v = stdlib_base_fast_max( NaN, 3.14 );
+* // returns 3.14
+*
+* @example
+* double v = stdlib_base_fast_max( -0.0, +0.0 );
+* // returns +0.0
+*
+* @example
+* double v = stdlib_base_fast_max( +0.0, -0.0 );
+* // returns -0.0
+*/
+double stdlib_base_fast_max( const double x, const double y ) {
+	if ( x > y ) {
+		return x;
+	}
+	return y;
+}
