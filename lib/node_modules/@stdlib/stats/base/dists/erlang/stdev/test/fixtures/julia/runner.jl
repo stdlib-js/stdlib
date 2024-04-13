41d41e959b4eaad3c631e6898e3144a4015a5458
Philipp Burckhardt <pburckhardt@outlook.com>
2024-04-12 20:33:24 -0400

    test: include trailing newlines in Julia-generated JSON fixtures

--- lib/node_modules/@stdlib/stats/base/dists/erlang/stdev/test/fixtures/julia/runner.jl
+++ lib/node_modules/@stdlib/stats/base/dists/erlang/stdev/test/fixtures/julia/runner.jl
@@ -58,6 +58,7 @@ function gen( k, lambda, name )
 	# Write the data to the output filepath as JSON:
 	outfile = open( filepath, "w" );
 	write( outfile, JSON.json(data) );
+	write( outfile, "\n" );
 	close( outfile );
 end
 
