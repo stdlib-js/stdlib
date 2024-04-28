17c703258149f4d66f7f828e46e4a5972f2b0ef5
Athan Reines <kgryte@gmail.com>
2024-04-28 02:11:52 -0700

    feat: add `blas/base/xerbla`

---
+++ lib/node_modules/@stdlib/blas/base/xerbla/src/addon.c
@@ -0,0 +1,36 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/blas/base/xerbla.h"
+#include "stdlib/napi/export.h"
+#include <node_api.h>
+
+/**
+* Receives JavaScript callback invocation data.
+*
+* @private
+* @param env    environment under which the function is invoked
+* @param info   callback data
+* @return       Node-API value
+*/
+static napi_value addon( napi_env env, napi_callback_info info ) {
+	c_xerbla( 1, "dgemm", "" ); // test values
+	return NULL;
+}
+
+STDLIB_NAPI_MODULE_EXPORT_FCN( addon )
