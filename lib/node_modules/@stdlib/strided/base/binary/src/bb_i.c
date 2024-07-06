599f5e4435bf9ae550a80162851521095b2410fc
Jaysukh Makvana <111515433+Jaysukh-409@users.noreply.github.com>
2024-07-07 00:58:50 +0530

    feat: add boolean dtype support to `strided/base/binary`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/2525
    Ref: https://github.com/stdlib-js/stdlib/issues/2500
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com>

--- lib/node_modules/@stdlib/strided/base/binary/src/bb_i.c
+++ lib/node_modules/@stdlib/strided/base/binary/src/bb_i.c
@@ -1,7 +1,7 @@
 /**
 * @license Apache-2.0
 *
-* Copyright (c) 2023 The Stdlib Authors.
+* Copyright (c) 2024 The Stdlib Authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -51,12 +51,12 @@
 * int64_t shape[] = { 3 };
 *
 * // Define a callback:
-* static uint8_t add( uint8_t x, uint8_t y ) {
+* static uint8_t fcn( uint8_t x, uint8_t y ) {
 *     return x + y;
 * }
 *
 * // Apply the callback:
-* stdlib_strided_bb_i( arrays, shape, strides, (void *)add );
+* stdlib_strided_bb_i( arrays, shape, strides, (void *)fcn );
 */
 void stdlib_strided_bb_i( uint8_t *arrays[], const int64_t *shape, const int64_t *strides, void *fcn ) {
 	typedef uint8_t func_type( const uint8_t x, const uint8_t y );
