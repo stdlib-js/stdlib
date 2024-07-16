cecc11af413b6065ed9a61d30d547fc07f656173
Athan Reines <kgryte@gmail.com>
2024-07-16 01:08:36 -0700

    feat: add `complex/float32/conj`
    Ref: https://github.com/stdlib-js/stdlib/issues/2260

---
+++ lib/node_modules/@stdlib/complex/float32/conj/examples/c/example.c
@@ -0,0 +1,41 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/complex/float32/conj.h"
+#include "stdlib/complex/realf.h"
+#include "stdlib/complex/imagf.h"
+#include "stdlib/complex/float32/ctor.h"
+#include <stdio.h>
+
+int main( void ) {
+	const stdlib_complex64_t x[] = {
+		stdlib_complex64( 5.0f, 2.0f ),
+		stdlib_complex64( -2.0f, 1.0f ),
+		stdlib_complex64( 0.0f, -0.0f ),
+		stdlib_complex64( 0.0f/0.0f, 0.0f/0.0f )
+	};
+
+	stdlib_complex64_t z;
+	stdlib_complex64_t v;
+	int i;
+	for ( i = 0; i < 4; i++ ) {
+		z = x[ i ];
+		v = stdlib_complex64_conj( z );
+		printf( "conj(%f + %fi) = %f + %fi\n", stdlib_realf( z ), stdlib_imagf( z ), stdlib_realf( v ), stdlib_imagf( v ) );
+	}
+}
