be600732b2732b2cac2f7cee16b3a0e7723c6ed8
GUNJ JOSHI <gunjjoshi8372@gmail.com>
2024-07-20 22:50:11 +0530

    feat!: add C implementation for `math/base/special/ceilsd`
    PR-URL: #2631
    Ref: #649 
    
    ---------
    
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

--- lib/node_modules/@stdlib/math/base/special/ceilsd/src/addon.c
+++ lib/node_modules/@stdlib/math/base/special/ceilsd/src/addon.c
@@ -1,7 +1,7 @@
 /**
 * @license Apache-2.0
 *
-* Copyright (c) 2018 The Stdlib Authors.
+* Copyright (c) 2024 The Stdlib Authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,17 +16,8 @@
 * limitations under the License.
 */
 
-'use strict';
+#include "stdlib/math/base/special/ceilsd.h"
+#include "stdlib/math/base/napi/ternary.h"
 
-var randu = require( '@stdlib/random/base/randu' );
-var ceilsd = require( './../lib' );
-
-var x;
-var y;
-var i;
-
-for ( i = 0; i < 100; i++ ) {
-	x = (randu()*10000.0) - 5000.0;
-	y = ceilsd( x, 5, 10 );
-	console.log( 'x: %d. Rounded: %d.', x, y );
-}
+// cppcheck-suppress shadowFunction
+STDLIB_MATH_BASE_NAPI_MODULE_DII_D( stdlib_base_ceilsd )
