16464e11f5d79e7114bbbece51efb50d51efc0f5
Athan Reines <kgryte@gmail.com>
2024-09-08 14:55:19 -0700

    feat: update namespace

--- lib/node_modules/@stdlib/napi/include/stdlib/napi.h
+++ lib/node_modules/@stdlib/napi/include/stdlib/napi.h
@@ -35,6 +35,7 @@
 #include "stdlib/napi/argv_int32array.h"
 #include "stdlib/napi/argv_int64.h"
 #include "stdlib/napi/argv_strided_complex64array.h"
+#include "stdlib/napi/argv_strided_complex64array2d.h"
 #include "stdlib/napi/argv_strided_complex128array.h"
 #include "stdlib/napi/argv_strided_complex128array2d.h"
 #include "stdlib/napi/argv_strided_float32array.h"
