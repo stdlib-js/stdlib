ae54d13ecd92912dea70b2d31525d7f54c3a26f1
Athan Reines <kgryte@gmail.com>
2024-09-10 00:47:23 -0700

    feat: add C "ndarray" API

--- lib/node_modules/@stdlib/blas/base/daxpy/src/daxpy_cblas.c
+++ lib/node_modules/@stdlib/blas/base/daxpy/src/daxpy_cblas.c
@@ -33,3 +33,40 @@
 void API_SUFFIX(c_daxpy)( const CBLAS_INT N, const double alpha, const double *X, const CBLAS_INT strideX, double *Y, const CBLAS_INT strideY ) {
 	API_SUFFIX(cblas_daxpy)( N, alpha, X, strideX, Y, strideY );
 }
+
+/**
+* Multiplies a vector `X` by a constant and adds the result to `Y` using alternative indexing semantics.
+*
+* @param N        number of indexed elements
+* @param alpha    scalar
+* @param X        input array
+* @param strideX  X stride length
+* @param offsetX  starting X index
+* @param Y        output array
+* @param strideY  Y stride length
+* @param offsetY  starting Y index
+*/
+void API_SUFFIX(c_daxpy_ndarray)( const CBLAS_INT N, const double alpha, const double *X, const CBLAS_INT strideX, const CBLAS_INT offsetX, double *Y, const CBLAS_INT strideY, const CBLAS_INT offsetY ) {
+	CBLAS_INT ox;
+	CBLAS_INT oy;
+
+	double *x = X;
+	double *y = Y;
+
+	// TODO: replace with @stdlib/strided/base/min-view-buffer-index
+	if ( N > 0 && strideX < 0 ) {
+		ox = offsetX + ( (N-1)*strideX ); // decrements the offset
+	} else {
+		ox = offsetX;
+	}
+	if ( N > 0 && strideY < 0 ) {
+		oy = offsetY + ( (N-1)*strideY ); // decrements the offset
+	} else {
+		oy = offsetY;
+	}
+	// Adjust the array pointers:
+	x += ox;
+	y += oy;
+
+	API_SUFFIX(cblas_daxpy)( N, alpha, x, strideX, y, strideY );
+}
