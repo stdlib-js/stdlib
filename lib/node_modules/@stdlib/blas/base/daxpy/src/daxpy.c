18deecf00e8d7a8069c0ebfe69a4479c249c98d4
Athan Reines <kgryte@gmail.com>
2024-09-11 13:40:39 -0700

    refactor: reduce code duplication by calling "ndarray" API

--- lib/node_modules/@stdlib/blas/base/daxpy/src/daxpy.c
+++ lib/node_modules/@stdlib/blas/base/daxpy/src/daxpy.c
@@ -18,6 +18,7 @@
 
 #include "stdlib/blas/base/daxpy.h"
 #include "stdlib/blas/base/shared.h"
+#include "stdlib/strided/base/stride2offset.h"
 
 /**
 * Multiplies a vector `X` by a constant and adds the result to `Y`.
@@ -30,54 +31,9 @@
 * @param strideY  Y stride length
 */
 void API_SUFFIX(c_daxpy)( const CBLAS_INT N, const double alpha, const double *X, const CBLAS_INT strideX, double *Y, const CBLAS_INT strideY ) {
-	CBLAS_INT ix;
-	CBLAS_INT iy;
-	CBLAS_INT i;
-	CBLAS_INT m;
-
-	if ( N <= 0 ) {
-		return;
-	}
-	// If `alpha` is `0`, then `y` is unchanged...
-	if ( alpha == 0.0 ) {
-		return;
-	}
-	// If both strides are equal to `1`, use unrolled loops...
-	if ( strideX == 1 && strideY == 1 ) {
-		m = N % 4;
-
-		// If we have a remainder, do a clean-up loop...
-		if ( m > 0 ) {
-			for ( i = 0; i < m; i++ ) {
-				Y[ i ] += alpha * X[ i ];
-			}
-			if ( N < 4 ) {
-				return;
-			}
-		}
-		for ( i = m; i < N; i += 4 ) {
-			Y[ i ] += alpha * X[ i ];
-			Y[ i+1 ] += alpha * X[ i+1 ];
-			Y[ i+2 ] += alpha * X[ i+2 ];
-			Y[ i+3 ] += alpha * X[ i+3 ];
-		}
-		return;
-	}
-	if ( strideX < 0 ) {
-		ix = (1-N) * strideX;
-	} else {
-		ix = 0;
-	}
-	if ( strideY < 0 ) {
-		iy = (1-N) * strideY;
-	} else {
-		iy = 0;
-	}
-	for ( i = 0; i < N; i++ ) {
-		Y[ iy ] += alpha * X[ ix ];
-		ix += strideX;
-		iy += strideY;
-	}
+	CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	CBLAS_INT oy = stdlib_strided_stride2offset( N, strideY );
+	API_SUFFIX(c_daxpy_ndarray)( N, alpha, X, strideX, ox, Y, strideY, oy );
 	return;
 }
 
