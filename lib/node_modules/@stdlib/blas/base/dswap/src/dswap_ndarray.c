edcea4761bc3065f9c5218c162b38ebec4a6c423
Aman Bhansali <92033532+aman-095@users.noreply.github.com>
2024-09-16 14:58:06 +0530

    feat: add C `ndarray` implementation for `blas/base/sswap` and `blas/base/dswap`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/2905
    Ref: https://github.com/stdlib-js/stdlib/issues/2039
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com> 
    Signed-off-by: Athan Reines <kgryte@gmail.com>

--- lib/node_modules/@stdlib/blas/base/dswap/src/dswap_ndarray.c
+++ lib/node_modules/@stdlib/blas/base/dswap/src/dswap_ndarray.c
@@ -17,25 +17,12 @@
 */
 
 #include "stdlib/blas/base/dswap.h"
-#include "stdlib/blas/base/dswap_fortran.h"
 #include "stdlib/blas/base/shared.h"
-#include "stdlib/strided/base/min_view_buffer_index.h"
+
+static const CBLAS_INT M = 3;
 
 /**
-* Interchanges two double-precision floating-point vectors.
-*
-* @param N        number of indexed elements
-* @param X        first input array
-* @param strideX  X stride length
-* @param Y        second input array
-* @param strideY  Y stride length
-*/
-void API_SUFFIX(c_dswap)( const CBLAS_INT N, double *X, const CBLAS_INT strideX, double *Y, const CBLAS_INT strideY ) {
-	dswap( &N, X, &strideX, Y, &strideY );
-}
-
-/**
-* Interchanges two double-precision floating-point vectors using alternative indexing semantics.
+* Interchanges two single-precision floating-point vectors using alternative indexing semantics.
 *
 * @param N        number of indexed elements
 * @param X        first input array
@@ -46,7 +33,59 @@ void API_SUFFIX(c_dswap)( const CBLAS_INT N, double *X, const CBLAS_INT strideX,
 * @param offsetY  starting index for Y
 */
 void API_SUFFIX(c_dswap_ndarray)( const CBLAS_INT N, double *X, const CBLAS_INT strideX, const CBLAS_INT offsetX, double *Y, const CBLAS_INT strideY, const CBLAS_INT offsetY ) {
-	X += stdlib_strided_min_view_buffer_index( N, strideX, offsetX ); // adjust array pointer
-	Y += stdlib_strided_min_view_buffer_index( N, strideY, offsetY ); // adjust array pointer
-	dswap( &N, X, &strideX, Y, &strideY );
+	CBLAS_INT ix;
+	CBLAS_INT iy;
+	CBLAS_INT i;
+	CBLAS_INT m;
+	double tmp;
+
+	if ( N <= 0 ) {
+		return;
+	}
+	ix = offsetX;
+	iy = offsetY;
+
+	// If both strides are equal to `1`, use unrolled loops...
+	if ( strideX == 1 && strideY == 1 ) {
+		m = N % M;
+
+		// If we have a remainder, do a clean-up loop...
+		if ( m > 0 ) {
+			for ( i = 0; i < m; i++ ) {
+				tmp = X[ ix ];
+				X[ ix ] = Y[ iy ];
+				Y[ iy ] = tmp;
+				ix += strideX;
+				iy += strideY;
+			}
+			if ( N < M ) {
+				return;
+			}
+		}
+		for ( i = m; i < N; i += M ) {
+			tmp = X[ ix ];
+			X[ ix ] = Y[ iy ];
+			Y[ iy ] = tmp;
+
+			tmp = X[ ix+1 ];
+			X[ ix+1 ] = Y[ iy+1 ];
+			Y[ iy+1 ] = tmp;
+
+			tmp = X[ ix+2 ];
+			X[ ix+2 ] = Y[ iy+2 ];
+			Y[ iy+2 ] = tmp;
+
+			ix += M;
+			iy += M;
+		}
+		return;
+	}
+	for ( i = 0; i < N; i++ ) {
+		tmp = X[ ix ];
+		X[ ix ] = Y[ iy ];
+		Y[ iy ] = tmp;
+		ix += strideX;
+		iy += strideY;
+	}
+	return;
 }
