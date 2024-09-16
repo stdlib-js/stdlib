c82b0678e53dc5b0b411cb7410dd06a2283cbe0c
Gunj Joshi <gunjjoshi8372@gmail.com>
2024-09-16 13:15:48 +0530

    feat: add `I_F` macro to `math/base/napi/unary`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/2903
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com> 
    Signed-off-by: Athan Reines <kgryte@gmail.com>

--- lib/node_modules/@stdlib/math/base/napi/unary/src/main.c
+++ lib/node_modules/@stdlib/math/base/napi/unary/src/main.c
@@ -647,3 +647,51 @@ napi_value stdlib_math_base_napi_f_i( napi_env env, napi_callback_info info, int
 
 	return v;
 }
+
+/**
+* Invokes a unary function accepting a signed 32-bit integer and returning a single-precision floating-point number.
+*
+* ## Notes
+*
+* -   This function expects that the callback `info` argument provides access to the following JavaScript arguments:
+*
+*     -   `x`: input value.
+*
+* @param env    environment under which the function is invoked
+* @param info   callback data
+* @param fcn    unary function
+* @return       function return value as a Node-API double-precision floating-point number
+*/
+napi_value stdlib_math_base_napi_i_f( napi_env env, napi_callback_info info, float (*fcn)( int32_t ) ) {
+	napi_status status;
+
+	size_t argc = 1;
+	napi_value argv[ 1 ];
+	status = napi_get_cb_info( env, info, &argc, argv, NULL, NULL );
+	assert( status == napi_ok );
+
+	if ( argc < 1 ) {
+		status = napi_throw_error( env, NULL, "invalid invocation. Must provide a number." );
+		assert( status == napi_ok );
+		return NULL;
+	}
+
+	napi_valuetype vtype0;
+	status = napi_typeof( env, argv[ 0 ], &vtype0 );
+	assert( status == napi_ok );
+	if ( vtype0 != napi_number ) {
+		status = napi_throw_type_error( env, NULL, "invalid argument. Must provide a number." );
+		assert( status == napi_ok );
+		return NULL;
+	}
+
+	int32_t x;
+	status = napi_get_value_int32( env, argv[ 0 ], &x );
+	assert( status == napi_ok );
+
+	napi_value v;
+	status = napi_create_double( env, (double)fcn( x ), &v );
+	assert( status == napi_ok );
+
+	return v;
+}
