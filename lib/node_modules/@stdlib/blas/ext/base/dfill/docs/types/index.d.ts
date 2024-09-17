040a33583774b120b5e486a072cff397fdb1d273
Athan Reines <kgryte@gmail.com>
2024-09-17 14:19:17 -0700

    feat: add a C `ndarray` API, refactor, and clean-up

--- lib/node_modules/@stdlib/blas/ext/base/dfill/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/blas/ext/base/dfill/docs/types/index.d.ts
@@ -28,7 +28,7 @@ interface Routine {
 	* @param N - number of indexed elements
 	* @param alpha - constant
 	* @param x - input array
-	* @param stride - stride length
+	* @param strideX - stride length
 	* @returns input array
 	*
 	* @example
@@ -39,7 +39,7 @@ interface Routine {
 	* dfill( x.length, 5.0, x, 1 );
 	* // x => <Float64Array>[ 5.0, 5.0, 5.0, 0.0, 5.0, 5.0, 5.0, 5.0 ]
 	*/
-	( N: number, alpha: number, x: Float64Array, stride: number ): Float64Array;
+	( N: number, alpha: number, x: Float64Array, strideX: number ): Float64Array;
 
 	/**
 	* Fills a double-precision floating-point strided array with a specified scalar value using alternative indexing semantics.
@@ -47,8 +47,8 @@ interface Routine {
 	* @param N - number of indexed elements
 	* @param alpha - constant
 	* @param x - input array
-	* @param stride - stride length
-	* @param offset - starting index
+	* @param strideX - stride length
+	* @param offsetX - starting index
 	* @returns input array
 	*
 	* @example
@@ -59,7 +59,7 @@ interface Routine {
 	* dfill.ndarray( x.length, 5.0, x, 1, 0 );
 	* // x => <Float64Array>[ 5.0, 5.0, 5.0, 0.0, 5.0, 5.0, 5.0, 5.0 ]
 	*/
-	ndarray( N: number, alpha: number, x: Float64Array, stride: number, offset: number ): Float64Array;
+	ndarray( N: number, alpha: number, x: Float64Array, strideX: number, offsetX: number ): Float64Array;
 }
 
 /**
@@ -68,7 +68,7 @@ interface Routine {
 * @param N - number of indexed elements
 * @param alpha - constant
 * @param x - input array
-* @param stride - stride length
+* @param strideX - stride length
 * @returns input array
 *
 * @example
