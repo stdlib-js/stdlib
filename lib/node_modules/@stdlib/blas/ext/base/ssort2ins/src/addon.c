d9e45f2b3607c04357ed10204aa068a90c35a8bd
Philipp Burckhardt <pburckhardt@outlook.com>
2024-09-20 17:27:09 -0400

    fix: fix includes and types

--- lib/node_modules/@stdlib/blas/ext/base/ssort2ins/src/addon.c
+++ lib/node_modules/@stdlib/blas/ext/base/ssort2ins/src/addon.c
@@ -34,7 +34,7 @@
 static napi_value addon( napi_env env, napi_callback_info info ) {
     STDLIB_NAPI_ARGV( env, info, argv, argc, 6 );
     STDLIB_NAPI_ARGV_INT64( env, N, argv, 0 );
-	STDLIB_NAPI_ARGV_DOUBLE( env, order, argv, 1 );
+	STDLIB_NAPI_ARGV_FLOAT( env, order, argv, 1 );
     STDLIB_NAPI_ARGV_INT64( env, strideX, argv, 3 );
     STDLIB_NAPI_ARGV_INT64( env, strideY, argv, 5 );
     STDLIB_NAPI_ARGV_STRIDED_FLOAT32ARRAY( env, X, N, strideX, argv, 2 );
