29222c97df48d8aa8b7bcdaee6fdcbfc6f75fbdb
headlessNode <harriskhan047@outlook.com>
2024-10-02 18:58:39 +0500

    feat: add C ndarray API and refactor

--- lib/node_modules/@stdlib/blas/ext/base/dcusumpw/src/main.c
+++ lib/node_modules/@stdlib/blas/ext/base/dcusumpw/src/main.c
@@ -17,10 +17,27 @@
 */
 
 #include "stdlib/blas/ext/base/dcusumpw.h"
-#include <stdint.h>
+#include "stdlib/strided/base/stride2offset.h"
+#include "stdlib/blas/base/shared.h"
 
 /**
 * Computes the cumulative sum of double-precision floating-point strided array elements using pairwise summation.
+*
+* @param N        number of indexed elements
+* @param sum      initial sum
+* @param X        input array
+* @param strideX  X stride length
+* @param Y        output array
+* @param strideY  Y stride length
+*/
+void API_SUFFIX(stdlib_strided_dcusumpw)( const CBLAS_INT N, const double sum, const double *X, const CBLAS_INT strideX, double *Y, const CBLAS_INT strideY ) {
+	const CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	const CBLAS_INT oy = stdlib_strided_stride2offset( N, strideY );
+	API_SUFFIX(stdlib_strided_dcusumpw_ndarray)( N, sum, X, strideX, ox, Y, strideY, oy );
+}
+
+/**
+* Computes the cumulative sum of double-precision floating-point strided array elements using pairwise summation and indexing semantics.
 *
 * ## Method
 *
@@ -33,34 +50,24 @@
 * @param N        number of indexed elements
 * @param sum      initial sum
 * @param X        input array
-* @param strideX  X stride length
+* @param strideX  X index increment
+* @param offsetX  X starting index
 * @param Y        output array
-* @param strideY  Y stride length
+* @param strideY  Y index increment
+* @param offsetY  Y starting index
 */
-void stdlib_strided_dcusumpw( const int64_t N, const double sum, const double *X, const int64_t strideX, double *Y, const int64_t strideY ) {
-	double *xp1;
-	double *xp2;
-	double *yp1;
-	double *yp2;
-	int64_t ix;
-	int64_t iy;
-	int64_t i;
-	int64_t n;
+void API_SUFFIX(stdlib_strided_dcusumpw_ndarray)( const CBLAS_INT N, const double sum, const double *X, const CBLAS_INT strideX, const CBLAS_INT offsetX, double *Y, const CBLAS_INT strideY, const CBLAS_INT offsetY ) {
+	CBLAS_INT ix;
+	CBLAS_INT iy;
+	CBLAS_INT i;
+	CBLAS_INT n;
 	double s;
 
 	if ( N <= 0 ) {
 		return;
 	}
-	if ( strideX < 0 ) {
-		ix = (1-N) * strideX;
-	} else {
-		ix = 0;
-	}
-	if ( strideY < 0 ) {
-		iy = (1-N) * strideY;
-	} else {
-		iy = 0;
-	}
+	ix = offsetX;
+	iy = offsetY;
 	// Blocksize for pairwise summation...
 	if ( N <= 128 ) {
 		s = 0.0;
@@ -73,22 +80,8 @@ void stdlib_strided_dcusumpw( const int64_t N, const double sum, const double *X
 		return;
 	}
 	n = N / 2;
-	if ( strideX < 0 ) {
-		xp1 = (double *)X + ( (n-N)*strideX );
-		xp2 = (double *)X;
-	} else {
-		xp1 = (double *)X;
-		xp2 = (double *)X + ( n*strideX );
-	}
-	if ( strideY < 0 ) {
-		yp1 = Y + ( (n-N)*strideY );
-		yp2 = Y;
-	} else {
-		yp1 = Y;
-		yp2 = Y + ( n*strideY );
-	}
-	stdlib_strided_dcusumpw( n, sum, xp1, strideX, yp1, strideY );
+	API_SUFFIX(stdlib_strided_dcusumpw_ndarray)( n, sum, X, strideX, ix, Y, strideY, iy );
 	iy += (n-1) * strideY;
-	stdlib_strided_dcusumpw( N-n, Y[ iy ], xp2, strideX, yp2, strideY );
+	API_SUFFIX(stdlib_strided_dcusumpw_ndarray)( N-n, Y[ iy ], X, strideX, ix+(n*strideX), Y, strideY, iy+strideY );
 	return;
 }
