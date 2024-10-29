3ee55a3faeded02cd02f7787dacb614b5613a230
Aayush Khanna <96649223+aayush0325@users.noreply.github.com>
2024-10-29 18:48:09 +0530

    feat: add `math/base/special/acosdf`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/3015
    Ref: https://github.com/stdlib-js/stdlib/issues/649
    
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Signed-off-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com> 
    Reviewed-by: Gunj Joshi <gunjjoshi8372@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com>

---
+++ lib/node_modules/@stdlib/math/base/special/acosdf/docs/types/index.d.ts
@@ -0,0 +1,56 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/**
+* Computes the arccosine (in degrees) of a single-precision floating-point number.
+*
+* @param x - input value
+* @returns arccosine (in degrees)
+*
+* @example
+* var v = acosdf( 0.0 );
+* // returns 0.0
+*
+* @example
+* var v = acosdf( 0.5 );
+* // returns ~60.0
+*
+* @example
+* var sqrtf = require( '@stdlib/math/base/special/sqrtf' );
+*
+* var v = acosdf( sqrtf( 2 ) / 2 );
+* // returns ~45.0
+*
+* @example
+* var sqrtf = require( '@stdlib/math/base/special/sqrtf' );
+*
+* var v = acosdf( sqrtf( 3 ) / 2 );
+* // returns ~30.0
+*
+* @example
+* var v = acosdf( NaN );
+* // returns NaN
+*/
+declare function acosdf( x: number ): number;
+
+
+// EXPORTS //
+
+export = acosdf;
