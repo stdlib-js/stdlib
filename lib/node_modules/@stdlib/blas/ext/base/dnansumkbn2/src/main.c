8792db561cbf2f98a1b01aded5d5ea225520c63e
Muhammad Haris <101793258+headlessNode@users.noreply.github.com>
2024-10-30 06:05:06 +0500

    feat: add C `ndarray` API and refactor `blas/ext/base/dnansumkbn2`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/3000
    
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>

--- lib/node_modules/@stdlib/blas/ext/base/dnansumkbn2/src/main.c
+++ lib/node_modules/@stdlib/blas/ext/base/dnansumkbn2/src/main.c
@@ -17,9 +17,10 @@
 */
 
 #include "stdlib/blas/ext/base/dnansumkbn2.h"
+#include "stdlib/strided/base/stride2offset.h"
 #include "stdlib/math/base/assert/is_nan.h"
-#include <stdint.h>
-#include <math.h>
+#include "stdlib/math/base/special/abs.h"
+#include "stdlib/blas/base/shared.h"
 
 /**
 * Computes the sum of double-precision floating-point strided array elements, ignoring `NaN` values and using a second-order iterative Kahan–Babuška algorithm.
@@ -32,51 +33,69 @@
 *
 * -   Klein, Andreas. 2005. "A Generalized Kahan-Babuška-Summation-Algorithm." _Computing_ 76 (3): 279–93. doi:[10.1007/s00607-005-0139-x](https://doi.org/10.1007/s00607-005-0139-x).
 *
-* @param N       number of indexed elements
-* @param X       input array
-* @param stride  stride length
-* @return        output value
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @return         output value
 */
-double stdlib_strided_dnansumkbn2( const int64_t N, const double *X, const int64_t stride ) {
+double API_SUFFIX(stdlib_strided_dnansumkbn2)( const CBLAS_INT N, const double *X, const CBLAS_INT strideX ) {
+	CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	return API_SUFFIX(stdlib_strided_dnansumkbn2_ndarray)( N, X, strideX, ox );
+}
+
+/**
+* Computes the sum of double-precision floating-point strided array elements, ignoring `NaN` values and using a second-order iterative Kahan–Babuška algorithm and alternative indexing semantics.
+*
+* ## Method
+*
+* -   This implementation uses a second-order iterative Kahan–Babuška algorithm, as described by Klein (2005).
+*
+* ## References
+*
+* -   Klein, Andreas. 2005. "A Generalized Kahan-Babuška-Summation-Algorithm." _Computing_ 76 (3): 279–93. doi:[10.1007/s00607-005-0139-x](https://doi.org/10.1007/s00607-005-0139-x).
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @param offsetX  starting index
+* @return         output value
+*/
+double API_SUFFIX(stdlib_strided_dnansumkbn2_ndarray)( const CBLAS_INT N, const double *X, const CBLAS_INT strideX, const CBLAS_INT offsetX ) {
 	double sum;
 	double ccs;
-	int64_t ix;
-	int64_t i;
+	CBLAS_INT ix;
+	CBLAS_INT i;
 	double cs;
 	double cc;
 	double v;
 	double t;
 	double c;
 
-	if ( N <= 0 ) {
-		return 0.0;
-	}
-	if ( N == 1 || stride == 0 ) {
-		if ( stdlib_base_is_nan( X[ 0 ] ) ) {
-			return 0.0;
-		}
-		return X[ 0 ];
-	}
-	if ( stride < 0 ) {
-		ix = (1-N) * stride;
-	} else {
-		ix = 0;
-	}
 	sum = 0.0;
+	if ( N <= 0 ) {
+		return sum;
+	}
+	ix = offsetX;
+	if ( strideX == 0 ) {
+		if ( stdlib_base_is_nan( X[ ix ] ) ) {
+			return sum;
+		}
+		return X[ ix ] * N;
+	}
 	ccs = 0.0; // second order correction term for lost lower order bits
 	cs = 0.0; // first order correction term for lost low order bits
 	for ( i = 0; i < N; i++ ) {
 		v = X[ ix ];
 		if ( !stdlib_base_is_nan( v ) ) {
 			t = sum + v;
-			if ( fabs( sum ) >= fabs( v ) ) {
+			if ( stdlib_base_abs( sum ) >= stdlib_base_abs( v ) ) {
 				c = (sum-t) + v;
 			} else {
 				c = (v-t) + sum;
 			}
 			sum = t;
 			t = cs + c;
-			if ( fabs( cs ) >= fabs( c ) ) {
+			if ( stdlib_base_abs( cs ) >= stdlib_base_abs( c ) ) {
 				cc = (cs-t) + c;
 			} else {
 				cc = (c-t) + cs;
@@ -84,7 +103,7 @@ double stdlib_strided_dnansumkbn2( const int64_t N, const double *X, const int64
 			cs = t;
 			ccs += cc;
 		}
-		ix += stride;
+		ix += strideX;
 	}
 	return sum + cs + ccs;
 }
