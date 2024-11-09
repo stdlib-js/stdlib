60bad94d411dc5e13296c96758b4445139788281
aman-095 <bhansali.1@iitj.ac.in>
2024-11-09 14:04:30 +0530

    feat: add C ndarray API for zcopy

--- lib/node_modules/@stdlib/blas/base/zcopy/src/zcopy_ndarray.c
+++ lib/node_modules/@stdlib/blas/base/zcopy/src/zcopy_ndarray.c
@@ -17,23 +17,7 @@
 */
 
 #include "stdlib/blas/base/zcopy.h"
-#include "stdlib/blas/base/zcopy_fortran.h"
 #include "stdlib/blas/base/shared.h"
-#include "stdlib/complex/float64/ctor.h"
-#include "stdlib/strided/base/min_view_buffer_index.h"
-
-/**
-* Copies values from one complex double-precision floating-point vector to another complex double-precision floating-point vector.
-*
-* @param N        number of indexed elements
-* @param X        input array
-* @param strideX  X stride length
-* @param Y        output array
-* @param strideY  Y stride length
-*/
-void API_SUFFIX(c_zcopy)( const CBLAS_INT N, const void *X, const CBLAS_INT strideX, void *Y, const CBLAS_INT strideY ) {
-	zcopy( &N, X, &strideX, Y, &strideY );
-}
 
 /**
 * Copies values from one complex double-precision floating-point vector to another complex double-precision floating-point vector using alternative indexing semantics.
@@ -47,10 +31,26 @@ void API_SUFFIX(c_zcopy)( const CBLAS_INT N, const void *X, const CBLAS_INT stri
 * @param offsetY  starting index for Y
 */
 void API_SUFFIX(c_zcopy_ndarray)( const CBLAS_INT N, const void *X, const CBLAS_INT strideX, const CBLAS_INT offsetX, void *Y, const CBLAS_INT strideY, const CBLAS_INT offsetY ) {
-	stdlib_complex128_t *zx = (stdlib_complex128_t *)X;
-	stdlib_complex128_t *zy = (stdlib_complex128_t *)Y;
+	double *x = (double *)X;
+	double *y = (double *)Y;
+	CBLAS_INT ix;
+	CBLAS_INT iy;
+	CBLAS_INT sx;
+	CBLAS_INT sy;
+	CBLAS_INT i;
 
-	zx += stdlib_strided_min_view_buffer_index( N, strideX, offsetX );
-	zy += stdlib_strided_min_view_buffer_index( N, strideY, offsetY );
-	zcopy( &N, (void *)zx, &strideX, (void *)zy, &strideY );
+	if ( N <= 0 ) {
+		return;
+	}
+	sx = strideX * 2;
+	sy = strideY * 2;
+	ix = offsetX * 2;
+	iy = offsetY * 2;
+	for ( i = 0; i < N; i++ ) {
+		y[ iy ] = x[ ix ];
+		y[ iy+1 ] = x[ ix+1 ];
+		ix += sx;
+		iy += sy;
+	}
+	return;
 }
