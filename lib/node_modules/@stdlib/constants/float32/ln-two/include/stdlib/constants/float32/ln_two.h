54929cbf64b75da47bdfe7b324a72828b9ad5120
GUNJ JOSHI <gunjjoshi8372@gmail.com>
2024-11-09 11:23:41 +0530

    feat: add constants/float32/ln-two

---
+++ lib/node_modules/@stdlib/constants/float32/ln-two/include/stdlib/constants/float32/ln_two.h
@@ -0,0 +1,27 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef STDLIB_CONSTANTS_FLOAT32_LN2_H
+#define STDLIB_CONSTANTS_FLOAT32_LN2_H
+
+/**
+* Macro for the natural logarithm of `2` as a single-precision floating-point number.
+*/
+#define STDLIB_CONSTANT_FLOAT32_LN2 0.6931471824645996
+
+#endif // !STDLIB_CONSTANTS_FLOAT32_LN2_H
