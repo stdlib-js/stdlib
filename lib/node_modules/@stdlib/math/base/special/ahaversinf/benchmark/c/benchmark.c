53ebcc8502a34c5de8e7be86741a7f91add788b5
Gunj Joshi <gunjjoshi8372@gmail.com>
2024-11-17 14:58:38 +0530

    refactor: update benchmarks and test fixtures in `math/base/special/ahaversinf`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/3119
    Ref: https://github.com/stdlib-js/stdlib/commit/545a05093d99910b39773f57d3df2da9232b24f5
    Private-ref: https://github.com/stdlib-js/todo/issues/2299
    Reviewed-by: Athan Reines <kgryte@gmail.com>
    Signed-off-by: Gunj Joshi <gunjjoshi8372@gmail.com>

--- lib/node_modules/@stdlib/math/base/special/ahaversinf/benchmark/c/benchmark.c
+++ lib/node_modules/@stdlib/math/base/special/ahaversinf/benchmark/c/benchmark.c
@@ -1,7 +1,7 @@
 /**
 * @license Apache-2.0
 *
-* Copyright (c) 2018 The Stdlib Authors.
+* Copyright (c) 2024 The Stdlib Authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -90,15 +90,18 @@ static float rand_float( void ) {
 */
 static double benchmark( void ) {
 	double elapsed;
-	float x;
-	float y;
+	float x[ 100 ];
 	double t;
+	float y;
 	int i;
+
+	for ( i = 0; i < 100; i++ ) {
+		x[ i ] = rand_float();
+	}
 
 	t = tic();
 	for ( i = 0; i < ITERATIONS; i++ ) {
-		x = rand_float();
-		y = 2.0f * asinf( sqrtf( x ) );
+		y = 2.0f * asinf( sqrtf( x[ i % 100 ] ) );
 		if ( y != y ) {
 			printf( "should not return NaN\n" );
 			break;
