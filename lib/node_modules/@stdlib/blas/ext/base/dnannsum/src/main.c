d8a47755e3d48bac9274d97766eab0e7bc5e9750
Muhammad Haris <101793258+headlessNode@users.noreply.github.com>
2024-11-20 08:57:40 +0000

    feat: add C ndarray API and refactor

--- lib/node_modules/@stdlib/blas/ext/base/dnannsum/src/main.c
+++ lib/node_modules/@stdlib/blas/ext/base/dnannsum/src/main.c
@@ -18,7 +18,8 @@
 
 #include "stdlib/blas/ext/base/dnannsum.h"
 #include "stdlib/blas/ext/base/dnannsumkbn.h"
-#include <stdint.h>
+#include "stdlib/strided/base/stride2offset.h"
+#include "stdlib/blas/base/shared.h"
 
 /**
 * Computes the sum of double-precision floating-point strided array elements, ignoring `NaN` values.
@@ -29,6 +30,21 @@
 * @param n       pointer for storing the number of non-NaN elements
 * @return        output value
 */
-double stdlib_strided_dnannsum( const int64_t N, const double *X, const int64_t stride, int64_t *n ) {
-	return stdlib_strided_dnannsumkbn( N, X, stride, n );
+double API_SUFFIX(stdlib_strided_dnannsum)( const CBLAS_INT N, const double *X, const CBLAS_INT strideX, CBLAS_INT *n ) {
+	CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	return API_SUFFIX(stdlib_strided_dnannsum_ndarray)( N, X, strideX, ox, n );
+}
+
+/**
+* Computes the sum of double-precision floating-point strided array elements, ignoring `NaN` values and using alternative indexing semantics.
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @param offsetX  starting index
+* @param n        pointer for storing the number of non-NaN elements
+* @return         output value
+*/
+double API_SUFFIX(stdlib_strided_dnannsum_ndarray)( const CBLAS_INT N, const double *X, const CBLAS_INT strideX, const CBLAS_INT offsetX, CBLAS_INT *n ) {
+	return API_SUFFIX(stdlib_strided_dnannsumkbn_ndarray)( N, X, strideX, offsetX, n );
 }
