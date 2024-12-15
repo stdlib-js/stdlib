753354ce538d0554de18dc80041b8c2d1b29e611
Gururaj Gurram <143020143+gururaj1512@users.noreply.github.com>
2024-12-15 09:08:24 +0530

    feat: add `math/base/special/sqrtpif`
    PR-URL: #3116
    Ref: #649
    
    ---------
    
    Signed-off-by: Gunj Joshi <gunjjoshi8372@gmail.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: stdlib-bot <82920195+stdlib-bot@users.noreply.github.com>
    Co-authored-by: Gunj Joshi <gunjjoshi8372@gmail.com>
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Reviewed-by: Gunj Joshi <gunjjoshi8372@gmail.com>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/math/base/special/sqrtpif/src/main.c
@@ -0,0 +1,35 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/math/base/special/sqrtpif.h"
+#include "stdlib/math/base/special/sqrtf.h"
+#include "stdlib/constants/float32/pi.h"
+
+/**
+* Compute the principal square root of the product of π and a positive single-precision floating-point number.
+*
+* @param x    input value
+* @return     output value
+*
+* @example
+* float out = stdlib_base_sqrtpif( 4.0f );
+* // returns ~3.5449f
+*/
+float stdlib_base_sqrtpif( const float x ) {
+	return stdlib_base_sqrtf( x * STDLIB_CONSTANT_FLOAT32_PI );
+}
