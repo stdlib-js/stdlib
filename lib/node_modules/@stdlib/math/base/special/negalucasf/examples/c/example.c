1a72a3ef64f46ffd29f413ffc1a1163bc749861b
Aayush Khanna <96649223+aayush0325@users.noreply.github.com>
2024-12-15 09:10:50 +0530

    feat: add `math/base/special/negalucasf`
    PR-URL: #3375
    Ref: #649
    
    ---------
    
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: stdlib-bot <82920195+stdlib-bot@users.noreply.github.com>
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/math/base/special/negalucasf/examples/c/example.c
@@ -0,0 +1,31 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/math/base/special/negalucasf.h"
+#include <stdio.h>
+#include <stdint.h>
+
+int main( void ) {
+    int32_t i;
+    float v;
+
+    for ( i = 0; i > -35; i-- ) {
+        v = stdlib_base_negalucasf( i );
+        printf( "negalucasf(%d) = %f\n", i, v );
+    }
+}
