dc47c94d1fc9281f10beb8ff006118ec9ef714d1
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2024-12-20 06:42:55 +0530

    feat: add C implementation for `stats/base/dists/rayleigh/mean`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4026
    Closes: https://github.com/stdlib-js/stdlib/issues/3803
    
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/rayleigh/mean/src/main.c
@@ -0,0 +1,41 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/rayleigh/mean.h"
+#include "stdlib/math/base/assert/is_nan.h"
+#include "stdlib/constants/float64/sqrt_half_pi.h"
+
+/**
+* Returns the mean of a Rayleigh distribution.
+*
+* @param sigma    scale parameter
+* @return         mean
+*
+* @example
+* double y = stdlib_base_rayleigh_mean( 9.0 );
+* // returns ~11.253
+*/
+double stdlib_base_dists_rayleigh_mean( const double sigma ) {
+	if (
+		stdlib_base_is_nan( sigma ) ||
+		sigma < 0
+	) {
+		return 0.0 / 0.0; // NaN
+	}
+	return sigma * STDLIB_CONSTANT_FLOAT64_SQRT_HALF_PI;
+}
