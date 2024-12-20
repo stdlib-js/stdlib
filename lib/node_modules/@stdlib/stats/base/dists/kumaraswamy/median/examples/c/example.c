9ba7c554685fb64bc1df214abf1e56288c335bbf
Desh Deepak Kant <118960904+DeshDeepakKant@users.noreply.github.com>
2024-12-20 06:43:21 +0530

    feat: add C implementation for `stats/base/dists/kumaraswamy/median`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4020
    Closes: https://github.com/stdlib-js/stdlib/issues/3701
    
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/kumaraswamy/median/examples/c/example.c
@@ -0,0 +1,43 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#include "stdlib/stats/base/dists/kumaraswamy/median.h"
+#include "stdlib/constants/float64/eps.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+static double random_uniform( const double min, const double max ) {
+	double v = (double)rand() / ( (double)RAND_MAX + 1.0 );
+	return min + ( v*(max-min) );
+}
+
+int main( void ) {
+	double a;
+	double b;
+	double y;
+	int i;
+
+	for ( i = 0; i < 25; i++ ) {
+		a = random_uniform( 0.0, 10.0 ) + STDLIB_CONSTANT_FLOAT64_EPS;
+		b = random_uniform( 0.0, 10.0 ) + STDLIB_CONSTANT_FLOAT64_EPS;
+
+		y = stdlib_base_dists_kumaraswamy_median( a, b );
+		printf( "a: %lf, b: %lf, Median(a,b): %lf\n", a, b, y );
+	}
+
+	return 0;
+}
---
+++ lib/node_modules/@stdlib/stats/base/dists/kumaraswamy/median/src/main.c
@@ -0,0 +1,43 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/kumaraswamy/median.h"
+#include "stdlib/math/base/assert/is_nan.h"
+#include "stdlib/math/base/special/pow.h"
+
+/**
+* Returns the median of a Kumaraswamy distribution.
+*
+* @param a       first shape parameter
+* @param b       second shape parameter
+* @return        evaluated median
+*
+* @example
+* double y = stdlib_base_kumaraswamy_median( 2.0, 3.0 );
+* // returns 1.0
+*/
+double stdlib_base_dists_kumaraswamy_median( const double a, const double b ) {
+    if (
+        stdlib_base_is_nan( a ) ||
+        stdlib_base_is_nan( b ) ||
+        a <= 0.0 || b <= 0.0
+    ) {
+        return 0.0/0.0; // NaN
+    }
+    return 1.0 - stdlib_base_pow( 0.5, 1.0 / b );
+}
