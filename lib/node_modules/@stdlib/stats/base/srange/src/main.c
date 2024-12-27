44ffc2eda9e1885017820e2abd46d4cf6ed4062b
Aayush Khanna <96649223+aayush0325@users.noreply.github.com>
2024-12-28 02:50:09 +0530

    feat: add C ndarray interface and refactor implementation for `stats/base/srange`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4297
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com>

--- lib/node_modules/@stdlib/stats/base/srange/src/main.c
+++ lib/node_modules/@stdlib/stats/base/srange/src/main.c
@@ -18,19 +18,34 @@
 
 #include "stdlib/stats/base/srange.h"
 #include "stdlib/math/base/assert/is_nanf.h"
-#include <stdint.h>
+#include "stdlib/blas/base/shared.h"
+#include "stdlib/strided/base/stride2offset.h"
 
 /**
 * Computes the range of a single-precision floating-point strided array.
 *
-* @param N       number of indexed elements
-* @param X       input array
-* @param stride  stride length
-* @return        output value
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @return         output value
 */
-float stdlib_strided_srange( const int64_t N, const float *X, const int64_t stride ) {
-	int64_t ix;
-	int64_t i;
+float API_SUFFIX(stdlib_strided_srange)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX ) {
+	const CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	return API_SUFFIX(stdlib_strided_srange_ndarray)( N, X, strideX, ox );
+}
+
+/**
+* Computes the range of a single-precision floating-point strided array using alternative indexing semantics.
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @param offsetX  starting index for X
+* @return         output value
+*/
+float API_SUFFIX(stdlib_strided_srange_ndarray)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX, const CBLAS_INT offsetX ) {
+	CBLAS_INT ix;
+	CBLAS_INT i;
 	float max;
 	float min;
 	float v;
@@ -38,21 +53,17 @@ float stdlib_strided_srange( const int64_t N, const float *X, const int64_t stri
 	if ( N <= 0 ) {
 		return 0.0f / 0.0f; // NaN
 	}
-	if ( N == 1 || stride == 0 ) {
+	if ( N == 1 || strideX == 0 ) {
 		if ( stdlib_base_is_nanf( X[ 0 ] ) ) {
-			return X[ 0 ];
+			return X[ offsetX ];
 		}
 		return 0.0f;
 	}
-	if ( stride < 0 ) {
-		ix = (1-N) * stride;
-	} else {
-		ix = 0;
-	}
+	ix = offsetX;
 	min = X[ ix ];
 	max = min;
 	for ( i = 1; i < N; i++ ) {
-		ix += stride;
+		ix += strideX;
 		v = X[ ix ];
 		if ( stdlib_base_is_nanf( v ) ) {
 			return v;
