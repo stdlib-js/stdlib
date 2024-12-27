03da66e60c08a80a692719ee1b023df6824e2070
aayush0325 <ayush25khanna@gmail.com>
2024-12-27 17:01:03 +0000

    feat: add C ndarray interface and refactor implementation
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: na
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: passed
      - task: lint_c_examples
        status: passed
      - task: lint_c_benchmarks
        status: passed
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: na
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/stats/base/dnanrange/src/main.c
+++ lib/node_modules/@stdlib/stats/base/dnanrange/src/main.c
@@ -18,43 +18,54 @@
 
 #include "stdlib/stats/base/dnanrange.h"
 #include "stdlib/math/base/assert/is_nan.h"
-#include <stdint.h>
+#include "stdlib/blas/base/shared.h"
+#include "stdlib/strided/base/stride2offset.h"
 
 /**
 * Computes the range of a double-precision floating-point strided array, ignoring `NaN` values.
 *
-* @param N       number of indexed elements
-* @param X       input array
-* @param stride  stride length
-* @return        output value
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @return         output value
 */
-double stdlib_strided_dnanrange( const int64_t N, const double *X, const int64_t stride ) {
+double API_SUFFIX(stdlib_strided_dnanrange)( const CBLAS_INT N, const double *X, const CBLAS_INT strideX ) {
+	const CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	return API_SUFFIX(stdlib_strided_dnanrange_ndarray)( N, X, strideX, ox );
+}
+
+/**
+* Computes the range of a double-precision floating-point strided array, ignoring `NaN` values.
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @param offsetX  starting index for X
+* @return         output value
+*/
+double API_SUFFIX(stdlib_strided_dnanrange_ndarray)( const CBLAS_INT N, const double *X, const CBLAS_INT strideX, const CBLAS_INT offsetX ) {
+	CBLAS_INT ix;
+	CBLAS_INT i;
 	double max;
 	double min;
-	int64_t ix;
-	int64_t i;
 	double v;
 
 	if ( N <= 0 ) {
 		return 0.0 / 0.0; // NaN
 	}
-	if ( N == 1 || stride == 0 ) {
+	if ( N == 1 || strideX == 0 ) {
 		if ( stdlib_base_is_nan( X[ 0 ] ) ) {
-			return X[ 0 ];
+			return X[ offsetX ];
 		}
 		return 0.0;
 	}
-	if ( stride < 0 ) {
-		ix = (1-N) * stride;
-	} else {
-		ix = 0;
-	}
+	ix = offsetX;
 	for ( i = 0; i < N; i++ ) {
 		v = X[ ix ];
 		if ( v == v ) {
 			break;
 		}
-		ix += stride;
+		ix += strideX;
 	}
 	if ( i == N ) {
 		return 0.0 / 0.0; // NaN
@@ -63,7 +74,7 @@ double stdlib_strided_dnanrange( const int64_t N, const double *X, const int64_t
 	max = min;
 	i += 1;
 	for (; i < N; i++ ) {
-		ix += stride;
+		ix += strideX;
 		v = X[ ix ];
 		if ( stdlib_base_is_nan( v ) ) {
 			continue;
