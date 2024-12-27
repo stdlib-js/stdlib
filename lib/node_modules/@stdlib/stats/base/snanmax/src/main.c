ba40e8bec5388f288c33455abcacfe590e73cced
Aayush Khanna <96649223+aayush0325@users.noreply.github.com>
2024-12-28 02:52:07 +0530

    feat: add C ndarray interface and refactor implementation for `stats/base/snanmax`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4287
    
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

--- lib/node_modules/@stdlib/stats/base/snanmax/src/main.c
+++ lib/node_modules/@stdlib/stats/base/snanmax/src/main.c
@@ -1,7 +1,7 @@
 /**
 * @license Apache-2.0
 *
-* Copyright (c) 2020 The Stdlib Authors.
+* Copyright (c) 2024 The Stdlib Authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -19,39 +19,50 @@
 #include "stdlib/stats/base/snanmax.h"
 #include "stdlib/math/base/assert/is_nanf.h"
 #include "stdlib/math/base/assert/is_positive_zerof.h"
-#include <stdint.h>
+#include "stdlib/blas/base/shared.h"
+#include "stdlib/strided/base/stride2offset.h"
 
 /**
 * Computes the maximum value of a single-precision floating-point strided array, ignoring `NaN` values.
 *
-* @param N       number of indexed elements
-* @param X       input array
-* @param stride  stride length
-* @return        output value
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @return         output value
 */
-float stdlib_strided_snanmax( const int64_t N, const float *X, const int64_t stride ) {
-	int64_t ix;
-	int64_t i;
+float API_SUFFIX(stdlib_strided_snanmax)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX ) {
+	const CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	return API_SUFFIX(stdlib_strided_snanmax_ndarray)( N, X, strideX, ox );
+}
+
+/**
+* Computes the maximum value of a single-precision floating-point strided array, ignoring `NaN` values using alternative indexing semantics.
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @param offsetX  starting index for X
+* @return         output value
+*/
+float API_SUFFIX(stdlib_strided_snanmax_ndarray)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX, const CBLAS_INT offsetX ) {
+	CBLAS_INT ix;
+	CBLAS_INT i;
 	float max;
 	float v;
 
 	if ( N <= 0 ) {
 		return 0.0f / 0.0f; // NaN
 	}
-	if ( N == 1 || stride == 0 ) {
-		return X[ 0 ];
+	if ( N == 1 || strideX == 0 ) {
+		return X[ offsetX ];
 	}
-	if ( stride < 0 ) {
-		ix = (1-N) * stride;
-	} else {
-		ix = 0;
-	}
+	ix = offsetX;
 	for ( i = 0; i < N; i++ ) {
 		v = X[ ix ];
 		if ( v == v ) {
 			break;
 		}
-		ix += stride;
+		ix += strideX;
 	}
 	if ( i == N ) {
 		return 0.0f / 0.0f; // NaN
@@ -59,7 +70,7 @@ float stdlib_strided_snanmax( const int64_t N, const float *X, const int64_t str
 	max = v;
 	i += 1;
 	for (; i < N; i++ ) {
-		ix += stride;
+		ix += strideX;
 		v = X[ ix ];
 		if ( stdlib_base_is_nanf( v ) ) {
 			continue;
