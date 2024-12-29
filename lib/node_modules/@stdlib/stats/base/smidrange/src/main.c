6769b3e9abfe3cf4b282b9e7a167aff41f693d0b
Aayush Khanna <96649223+aayush0325@users.noreply.github.com>
2024-12-29 21:53:21 +0530

    feat: add C ndarray interface and refactor implementation for `stats/base/smidrange`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4341
    
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

--- lib/node_modules/@stdlib/stats/base/smidrange/src/main.c
+++ lib/node_modules/@stdlib/stats/base/smidrange/src/main.c
@@ -20,38 +20,49 @@
 #include "stdlib/math/base/assert/is_nanf.h"
 #include "stdlib/math/base/assert/is_positive_zerof.h"
 #include "stdlib/math/base/assert/is_negative_zerof.h"
-#include <stdint.h>
+#include "stdlib/blas/base/shared.h"
+#include "stdlib/strided/base/stride2offset.h"
 
 /**
 * Computes the mid-range of a single-precision floating-point strided array.
 *
-* @param N       number of indexed elements
-* @param X       input array
-* @param stride  stride length
-* @return        output value
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @return         output value
 */
-float stdlib_strided_smidrange( const int64_t N, const float *X, const int64_t stride ) {
-	int64_t ix;
-	int64_t i;
+float API_SUFFIX(stdlib_strided_smidrange)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX ) {
+	const CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	return API_SUFFIX(stdlib_strided_smidrange_ndarray)( N, X, strideX, ox );
+}
+
+/**
+* Computes the mid-range of a single-precision floating-point strided array and using alternative indexing semantics.
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @param offsetX  starting index for X
+* @return         output value
+*/
+float API_SUFFIX(stdlib_strided_smidrange_ndarray)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX, const CBLAS_INT offsetX ) {
+	CBLAS_INT ix;
+	CBLAS_INT i;
 	float max;
 	float min;
 	float v;
 
 	if ( N <= 0 ) {
-		return 0.0f / 0.0f; // NaN
+		return 0.0 / 0.0; // NaN
 	}
-	if ( N == 1 || stride == 0 ) {
-		return X[ 0 ];
+	if ( N == 1 || strideX == 0 ) {
+		return X[ offsetX ];
 	}
-	if ( stride < 0 ) {
-		ix = (1-N) * stride;
-	} else {
-		ix = 0;
-	}
+	ix = offsetX;
 	min = X[ ix ];
 	max = min;
 	for ( i = 1; i < N; i++ ) {
-		ix += stride;
+		ix += strideX;
 		v = X[ ix ];
 		if ( stdlib_base_is_nanf( v ) ) {
 			return v;
@@ -62,5 +73,5 @@ float stdlib_strided_smidrange( const int64_t N, const float *X, const int64_t s
 			max = v;
 		}
 	}
-	return ( max+min ) / 2.0f;
+	return ( max+min ) / 2.0;
 }
