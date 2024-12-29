f8b2ee6806df8ede96ec02e32a3ece3e791bc728
Vinit Pandit <106718914+MeastroZI@users.noreply.github.com>
2024-12-29 09:11:20 +0530

    feat: add C implementation of `stats/base/dists/weibull/cdf`
    PR-URL: #4196
    Closes: #3838 
    
    ---------
    
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

--- lib/node_modules/@stdlib/stats/base/dists/weibull/cdf/src/addon.c
+++ lib/node_modules/@stdlib/stats/base/dists/weibull/cdf/src/addon.c
@@ -1,7 +1,7 @@
 /**
 * @license Apache-2.0
 *
-* Copyright (c) 2018 The Stdlib Authors.
+* Copyright (c) 2024 The Stdlib Authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,21 +16,8 @@
 * limitations under the License.
 */
 
-'use strict';
+#include "stdlib/stats/base/dists/weibull/cdf.h"
+#include "stdlib/math/base/napi/ternary.h"
 
-var randu = require( '@stdlib/random/base/randu' );
-var cdf = require( './../lib' );
-
-var lambda;
-var k;
-var x;
-var y;
-var i;
-
-for ( i = 0; i < 10; i++ ) {
-	x = randu() * 10.0;
-	lambda = randu() * 10.0;
-	k = randu() * 10.0;
-	y = cdf( x, k, lambda );
-	console.log( 'x: %d, k: %d, λ: %d, F(x;k,λ): %d', x, k, lambda, y );
-}
+// cppcheck-suppress shadowFunction
+STDLIB_MATH_BASE_NAPI_MODULE_DDD_D( stdlib_base_dists_weibull_cdf )
---
+++ lib/node_modules/@stdlib/stats/base/dists/weibull/cdf/src/main.c
@@ -0,0 +1,50 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2024 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/weibull/cdf.h"
+#include "stdlib/math/base/assert/is_nan.h"
+#include "stdlib/math/base/special/expm1.h"
+#include "stdlib/math/base/special/pow.h"
+
+/**
+* Evaluates the cumulative distribution function (CDF) for a Weibull distribution with scale parameter `k` and shape parameter `lambda` at a value `x`.
+*
+* @param x         input value
+* @param k    	   scale parameter
+* @param lambda    shape parameter
+* @return          evaluated CDF
+*
+* @example
+* double y = stdlib_base_weibull_cdf( 2.0, 1.0, 1.0 );
+* // returns ~0.865
+*/
+double stdlib_base_dists_weibull_cdf( const double x, const double k, const double lambda ) {
+	if (
+		stdlib_base_is_nan( x ) ||
+		stdlib_base_is_nan( k ) ||
+		stdlib_base_is_nan( lambda ) ||
+		k <= 0.0 ||
+		lambda <= 0.0
+	) {
+		return 0.0/0.0; // NaN
+	}
+	if ( x < 0.0 ) {
+		return 0.0;
+	}
+	return -stdlib_base_expm1( -stdlib_base_pow( x / lambda, k ) );
+}
