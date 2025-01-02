e69f31a53cb577904ed4ba634dfdf1b76f81c20d
Jaysukh Makvana <111515433+Jaysukh-409@users.noreply.github.com>
2025-01-02 12:53:39 +0530

    feat: add `stats/base/dists/planck/logcdf`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4227
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com> 
    Co-authored-by: stdlib-bot <82920195+stdlib-bot@users.noreply.github.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/planck/logcdf/docs/types/index.d.ts
@@ -0,0 +1,114 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/**
+* Evaluates the natural logarithm of the cumulative distribution function (CDF) for a Planck distribution.
+*
+* @param x - input value
+* @returns evaluated logCDF
+*/
+type Unary = ( x: number ) => number;
+
+/**
+* Interface for the logarithm of the cumulative distribution function (CDF) of a Planck distribution.
+*/
+interface LogCDF {
+	/**
+	* Evaluates the logarithm of the cumulative distribution function (CDF) for a Planck distribution with shape parameter `lambda` at a value `x`.
+	*
+	* ## Notes
+	*
+	* -   If `lambda <= 0`, the function returns `NaN`.
+	*
+	* @param x - input value
+	* @param lambda - shape parameter
+	* @returns evaluated logCDF
+	*
+	* @example
+	* var y = logcdf( 2.0, 0.5 );
+	* // returns ~-0.2525
+	*
+	* @example
+	* var y = logcdf( 2.0, 1.5 );
+	* // returns ~-0.0112
+	*
+	* @example
+	* var y = logcdf( -1.0, 0.5 );
+	* // returns -Infinity
+	*
+	* @example
+	* var y = logcdf( NaN, 0.5 );
+	* // returns NaN
+	*
+	* @example
+	* var y = logcdf( 0.0, NaN );
+	* // returns NaN
+	*
+	* @example
+	* // Invalid shape parameter
+	* var y = logcdf( 2.0, -1.4 );
+	* // returns NaN
+	*/
+	( x: number, lambda: number ): number;
+
+	/**
+	* Returns a function for evaluating the logarithm of the cumulative distribution function (CDF) for a Planck distribution with shape parameter `lambda`.
+	*
+	* @param lambda - shape parameter
+	* @returns logCDF
+	*
+	* @example
+	* var mylogcdf = logcdf.factory( 1.5 );
+	* var y = mylogcdf( 3.0 );
+	* // returns ~-0.0025
+	*
+	* y = mylogcdf( 1.0 );
+	* // returns ~-0.0511
+	*/
+	factory( lambda: number ): Unary;
+}
+
+/**
+* Evaluates the logarithm of cumulative distribution function (CDF) for a Planck distribution with shape parameter `lambda`.
+*
+* @param x - input value
+* @param lambda - shape parameter
+* @returns evaluated logCDF
+*
+* @example
+* var y = logcdf( 2.0, 0.5 );
+* // returns ~-0.2525
+*
+* y = logcdf( 2.0, 1.5 );
+* // returns ~-0.0112
+*
+* var mylogcdf = logcdf.factory( 1.5 );
+* y = mylogcdf( 3.0 );
+* // returns ~-0.0025
+*
+* y = mylogcdf( 1.0 );
+* // returns ~-0.0511
+*/
+declare var logCDF: LogCDF;
+
+
+// EXPORTS //
+
+export = logCDF;
