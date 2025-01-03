810f68ab9df619907d134931431014c543aeabda
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2025-01-03 09:01:56 +0530

    feat: add C implementation for `stats/base/dists/rayleigh/median`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4027
    Closes: https://github.com/stdlib-js/stdlib/issues/3804
    
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: stdlib-bot <noreply@stdlib.io>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/rayleigh/median/src/main.c
@@ -0,0 +1,39 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/rayleigh/median.h"
+#include "stdlib/math/base/assert/is_nan.h"
+#include "stdlib/math/base/special/sqrt.h"
+#include "stdlib/constants/float64/ln_two.h"
+
+/**
+* Returns the median of a Rayleigh distribution.
+*
+* @param sigma    scale parameter
+* @return         median
+*
+* @example
+* double y = stdlib_base_dists_rayleigh_median( 9.0 );
+* // returns ~10.595
+*/
+double stdlib_base_dists_rayleigh_median( const double sigma ) {
+	if ( stdlib_base_is_nan( sigma ) || sigma < 0 ) {
+		return 0.0 / 0.0; // NaN
+	}
+	return sigma * stdlib_base_sqrt( 2.0 * STDLIB_CONSTANT_FLOAT64_LN2 );
+}
