8b0d5136f07551606c0afbdaad5bf4995c5f79ae
Aayush Khanna <96649223+aayush0325@users.noreply.github.com>
2025-01-03 05:33:37 +0530

    feat: add C ndarray interface and refactor implementation for `stats/base/dcumin`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4144
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com>

--- lib/node_modules/@stdlib/stats/base/dcumin/src/main.c
+++ lib/node_modules/@stdlib/stats/base/dcumin/src/main.c
@@ -19,7 +19,8 @@
 #include "stdlib/stats/base/dcumin.h"
 #include "stdlib/math/base/assert/is_nan.h"
 #include "stdlib/math/base/assert/is_negative_zero.h"
-#include <stdint.h>
+#include "stdlib/blas/base/shared.h"
+#include "stdlib/strided/base/stride2offset.h"
 
 /**
 * Computes the cumulative minimum of double-precision floating-point strided array elements.
@@ -30,26 +31,36 @@
 * @param Y        output array
 * @param strideY  Y stride length
 */
-void stdlib_strided_dcumin( const int64_t N, const double *X, const int64_t strideX, double *Y, const int64_t strideY ) {
+void API_SUFFIX(stdlib_strided_dcumin)( const CBLAS_INT N, const double *X, const CBLAS_INT strideX, double *Y, const CBLAS_INT strideY ) {
+	const CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	const CBLAS_INT oy = stdlib_strided_stride2offset( N, strideY );
+	API_SUFFIX(stdlib_strided_dcumin_ndarray)( N, X, strideX, ox, Y, strideY, oy );
+	return;
+}
+
+/**
+* Computes the cumulative minimum of double-precision floating-point strided array elements using alternative indexing semantics.
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  X stride length
+* @param offsetX  starting index for X
+* @param Y        output array
+* @param strideY  Y stride length
+* @param offsetY  starting index for Y
+*/
+void API_SUFFIX(stdlib_strided_dcumin_ndarray)( const CBLAS_INT N, const double *X, const CBLAS_INT strideX, const CBLAS_INT offsetX, double *Y, const CBLAS_INT strideY, const CBLAS_INT offsetY ) {
+	CBLAS_INT ix;
+	CBLAS_INT iy;
+	CBLAS_INT i;
 	double min;
-	int64_t ix;
-	int64_t iy;
-	int64_t i;
 	double v;
 
 	if ( N <= 0 ) {
 		return;
 	}
-	if ( strideX < 0 ) {
-		ix = (1-N) * strideX;
-	} else {
-		ix = 0;
-	}
-	if ( strideY < 0 ) {
-		iy = (1-N) * strideY;
-	} else {
-		iy = 0;
-	}
+	ix = offsetX;
+	iy = offsetY;
 	min = X[ ix ];
 	Y[ iy ] = min;
 
