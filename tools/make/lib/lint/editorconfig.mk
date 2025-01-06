3fd083f5bff00535203984e4b95261d4e9491a1a
Athan Reines <kgryte@gmail.com>
2025-01-05 20:09:48 -0800

    build: fix missing directory argument
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: na
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: na
      - task: lint_javascript_src
        status: na
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: na
      - task: lint_javascript_tests
        status: na
      - task: lint_javascript_benchmarks
        status: na
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: na
      - task: lint_typescript_tests
        status: na
      - task: lint_license_headers
        status: passed
    ---
    
    ---
    type: pre_push_report
    description: Results of running various checks prior to pushing changes.
    report:
      - task: run_javascript_examples
        status: na
      - task: run_c_examples
        status: na
      - task: run_cpp_examples
        status: na
      - task: run_javascript_readme_examples
        status: na
      - task: run_c_benchmarks
        status: na
      - task: run_cpp_benchmarks
        status: na
      - task: run_fortran_benchmarks
        status: na
      - task: run_javascript_benchmarks
        status: na
      - task: run_julia_benchmarks
        status: na
      - task: run_python_benchmarks
        status: na
      - task: run_r_benchmarks
        status: na
      - task: run_javascript_tests
        status: na
    ---

--- tools/make/lib/lint/editorconfig.mk
+++ tools/make/lib/lint/editorconfig.mk
@@ -57,7 +57,7 @@ lint-editorconfig: $(NODE_MODULES)
 	$(QUIET) $(FIND_PACKAGES_CMD) | grep '^[\/]\|^[a-zA-Z]:[/\]' | while read -r pkg; do \
 		echo ''; \
 		echo "Linting package for basic formatting errors: $$pkg"; \
-		cd "$$pkg" && ( $(NODE) $(EDITORCONFIG_CHECKER) $(EDITORCONFIG_CHECKER_CONF_FLAGS) --config $(EDITORCONFIG_CHECKER_CONF) && $(NODE) $(EDITORCONFIG_CHECKER) $(EDITORCONFIG_CHECKER_CONF_FLAGS) --config $(EDITORCONFIG_CHECKER_MARKDOWN_CONF) ./ && echo 'Success. No detected EditorConfig lint errors.' && echo '' ) || exit 1; \
+		cd "$$pkg" && ( $(NODE) $(EDITORCONFIG_CHECKER) $(EDITORCONFIG_CHECKER_CONF_FLAGS) --config $(EDITORCONFIG_CHECKER_CONF) ./ && $(NODE) $(EDITORCONFIG_CHECKER) $(EDITORCONFIG_CHECKER_CONF_FLAGS) --config $(EDITORCONFIG_CHECKER_MARKDOWN_CONF) ./ && echo 'Success. No detected EditorConfig lint errors.' && echo '' ) || exit 1; \
 	done
 
 .PHONY: lint-editorconfig
