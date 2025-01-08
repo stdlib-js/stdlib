bbc5552557faed064c53840e01ccc4a4d65e817b
Muhammad Haris <101793258+headlessNode@users.noreply.github.com>
2025-01-08 14:20:10 +0500

    feat: add C `ndarray` API and refactor `blas/ext/base/dsumpw`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4329
    Reviewed-by: Athan Reines <kgryte@gmail.com>

--- lib/node_modules/@stdlib/blas/ext/base/dsumpw/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/blas/ext/base/dsumpw/docs/types/index.d.ts
@@ -27,7 +27,7 @@ interface Routine {
 	*
 	* @param N - number of indexed elements
 	* @param x - input array
-	* @param stride - stride length
+	* @param strideX - stride length
 	* @returns sum
 	*
 	* @example
@@ -38,15 +38,15 @@ interface Routine {
 	* var v = dsumpw( x.length, x, 1 );
 	* // returns 1.0
 	*/
-	( N: number, x: Float64Array, stride: number ): number;
+	( N: number, x: Float64Array, strideX: number ): number;
 
 	/**
 	* Computes the sum of double-precision floating-point strided array elements using pairwise summation and alternative indexing semantics.
 	*
 	* @param N - number of indexed elements
 	* @param x - input array
-	* @param stride - stride length
-	* @param offset - starting index
+	* @param strideX - stride length
+	* @param offsetX - starting index
 	* @returns sum
 	*
 	* @example
@@ -57,7 +57,7 @@ interface Routine {
 	* var v = dsumpw.ndarray( x.length, x, 1, 0 );
 	* // returns 1.0
 	*/
-	ndarray( N: number, x: Float64Array, stride: number, offset: number ): number;
+	ndarray( N: number, x: Float64Array, strideX: number, offsetX: number ): number;
 }
 
 /**
@@ -65,7 +65,7 @@ interface Routine {
 *
 * @param N - number of indexed elements
 * @param x - input array
-* @param stride - stride length
+* @param strideX - stride length
 * @returns sum
 *
 * @example
