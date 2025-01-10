832a466b91d610a8d0c191f74ad178944129289f
Athan Reines <kgryte@gmail.com>
2025-01-09 18:14:19 -0800

    feat: add `array/base/mskfilter2`
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: passed
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

---
+++ lib/node_modules/@stdlib/array/base/mskfilter2/docs/types/index.d.ts
@@ -0,0 +1,46 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/// <reference types="@stdlib/types"/>
+
+import { Collection } from '@stdlib/types/array';
+
+/**
+* Returns new arrays by applying a mask to two provided input arrays in a single pass.
+*
+* @param x - first input array
+* @param y - second input array
+* @param mask - mask array
+* @returns output arrays
+*
+* @example
+* var x = [ 1, 2, 3, 4 ];
+* var idx = [ 5, 6, 7, 8 ];
+* var mask = [ 0, 1, 0, 1 ];
+*
+* var out = mskfilter2( x, idx, mask );
+* // returns [ [ 2, 4 ], [ 6, 8 ] ]
+*/
+declare function mskfilter2<T = unknown, U = unknown>( x: Collection<T>, y: Collection<U>, mask: Collection ): [ Array<T>, Array<U> ];
+
+
+// EXPORTS //
+
+export = mskfilter2;
