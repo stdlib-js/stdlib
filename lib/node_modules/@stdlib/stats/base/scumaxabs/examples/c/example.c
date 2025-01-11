767f5781b487933e11dd86a5413ce58cc0b4afbe
Aayush Khanna <96649223+aayush0325@users.noreply.github.com>
2025-01-11 09:27:23 +0530

    docs: update examples in `stats/base/scumaxabs`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4695
    
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

--- lib/node_modules/@stdlib/stats/base/scumaxabs/examples/c/example.c
+++ lib/node_modules/@stdlib/stats/base/scumaxabs/examples/c/example.c
@@ -22,7 +22,7 @@
 int main( void ) {
 	// Create strided arrays:
 	const float x[] = { 1.0f, 2.0f, -3.0f, 4.0f, -5.0f, 6.0f, 7.0f, 8.0f };
-	const float y[] = { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+	float y[] = { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
 
 	// Specify the number of elements:
 	const int N = 4;
