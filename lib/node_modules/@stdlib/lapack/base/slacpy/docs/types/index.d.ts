18ee830fd7ba9f5da9d356849aac0dec37a12ab4
Athan Reines <kgryte@gmail.com>
2025-01-12 12:51:02 -0800

    docs: update examples
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: na
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: na
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: na
      - task: lint_javascript_tests
        status: na
      - task: lint_javascript_benchmarks
        status: na
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: na
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/lapack/base/slacpy/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/lapack/base/slacpy/docs/types/index.d.ts
@@ -69,11 +69,11 @@ interface Routine {
 	* @example
 	* var Float32Array = require( '@stdlib/array/float32' );
 	*
-	* var A = new Float32Array( [ 0.0, 1.0, 2.0, 3.0, 4.0 ] );
-	* var B = new Float32Array( [ 0.0, 0.0, 99.0, 99.0, 99.0, 99.0 ] );
+	* var A = new Float32Array( [ 1.0, 2.0, 3.0, 4.0, 5.0 ] );
+	* var B = new Float32Array( 6 );
 	*
 	* slacpy.ndarray( 'all', 2, 2, A, 2, 1, 1, B, 2, 1, 2 );
-	* // B => <Float32Array>[ 0.0, 0.0, 1.0, 2.0, 3.0, 4.0 ]
+	* // B => <Float32Array>[ 0.0, 0.0, 2.0, 3.0, 4.0, 5.0 ]
 	*/
 	ndarray( uplo: string, M: number, N: number, A: Float32Array, strideA1: number, strideA2: number, offsetA: number, B: Float32Array, strideB1: number, strideB2: number, offsetB: number ): Float32Array;
 }
@@ -103,11 +103,11 @@ interface Routine {
 * @example
 * var Float32Array = require( '@stdlib/array/float32' );
 *
-* var A = new Float32Array( [ 0.0, 1.0, 2.0, 3.0, 4.0 ] );
-* var B = new Float32Array( [ 0.0, 0.0, 99.0, 99.0, 99.0, 99.0 ] );
+* var A = new Float32Array( [ 1.0, 2.0, 3.0, 4.0, 5.0 ] );
+* var B = new Float32Array( 6 );
 *
 * slacpy.ndarray( 'all', 2, 2, A, 2, 1, 1, B, 2, 1, 2 );
-* // B => <Float32Array>[ 0.0, 0.0, 1.0, 2.0, 3.0, 4.0 ]
+* // B => <Float32Array>[ 0.0, 0.0, 2.0, 3.0, 4.0, 5.0 ]
 */
 declare var slacpy: Routine;
 
