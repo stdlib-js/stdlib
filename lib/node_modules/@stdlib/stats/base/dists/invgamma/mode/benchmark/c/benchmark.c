284b54007e8fe22bdc908d7cd31cd28438e80bef
Philipp Burckhardt <pburckhardt@outlook.com>
2025-01-12 17:05:59 -0500

    chore: directly draw from the desired distribution instead of adding constants
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: na
      - task: lint_javascript_src
        status: na
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: na
      - task: lint_javascript_tests
        status: na
      - task: lint_javascript_benchmarks
        status: na
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: passed
      - task: lint_c_benchmarks
        status: passed
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: na
      - task: lint_typescript_tests
        status: na
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/stats/base/dists/invgamma/mode/benchmark/c/benchmark.c
+++ lib/node_modules/@stdlib/stats/base/dists/invgamma/mode/benchmark/c/benchmark.c
@@ -101,8 +101,8 @@ static double benchmark( void ) {
 	int i;
 
 	for ( i = 0; i < 100; i++ ) {
-		alpha[ i ] = random_uniform( 0.0, 10.0 ) + STDLIB_CONSTANT_FLOAT64_EPS;
-		beta[ i ] = random_uniform( 0.0, 10.0 ) + STDLIB_CONSTANT_FLOAT64_EPS;
+		alpha[ i ] = random_uniform( STDLIB_CONSTANT_FLOAT64_EPS, 10.0 );
+		beta[ i ] = random_uniform( STDLIB_CONSTANT_FLOAT64_EPS, 10.0 );
 	}
 
 	t = tic();
