2848cd1dacd0229bd84ff97dd77063cdf03ee60c
Muhammad Haris <101793258+headlessNode@users.noreply.github.com>
2025-01-16 19:31:34 +0000

    feat: add C ndarray API and refactor
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: na
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: missing_dependencies
      - task: lint_c_examples
        status: missing_dependencies
      - task: lint_c_benchmarks
        status: missing_dependencies
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: na
      - task: lint_license_headers
        status: passed
    ---
    
    ---
    type: pre_push_report
    description: Results of running various checks prior to pushing changes.
    report:
      - task: run_javascript_examples
        status: na
      - task: run_c_examples
        status: na
      - task: run_cpp_examples
        status: na
      - task: run_javascript_readme_examples
        status: na
      - task: run_c_benchmarks
        status: na
      - task: run_cpp_benchmarks
        status: na
      - task: run_fortran_benchmarks
        status: na
      - task: run_javascript_benchmarks
        status: na
      - task: run_julia_benchmarks
        status: na
      - task: run_python_benchmarks
        status: na
      - task: run_r_benchmarks
        status: na
      - task: run_javascript_tests
        status: na
    ---

--- lib/node_modules/@stdlib/blas/ext/base/scusumkbn/src/main.c
+++ lib/node_modules/@stdlib/blas/ext/base/scusumkbn/src/main.c
@@ -18,7 +18,8 @@
 
 #include "stdlib/blas/ext/base/scusumkbn.h"
 #include "stdlib/math/base/special/absf.h"
-#include <stdint.h>
+#include "stdlib/strided/base/stride2offset.h"
+#include "stdlib/blas/base/shared.h"
 
 /**
 * Computes the cumulative sum of single-precision floating-point strided array elements using an improved Kahan–Babuška algorithm.
@@ -34,14 +35,40 @@
 * @param N        number of indexed elements
 * @param sum      initial sum
 * @param X        input array
-* @param strideX  X stride length
+* @param strideX  stride length for X
 * @param Y        output array
-* @param strideY  Y stride length
+* @param strideY  stride length for Y
 */
-void stdlib_strided_scusumkbn( const int64_t N, const float sum, const float *X, const int64_t strideX, float *Y, const int64_t strideY ) {
-	int64_t ix;
-	int64_t iy;
-	int64_t i;
+void API_SUFFIX(stdlib_strided_scusumkbn)( const CBLAS_INT N, const float sum, const float *X, const CBLAS_INT strideX, float *Y, const CBLAS_INT strideY ) {
+	const CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	const CBLAS_INT oy = stdlib_strided_stride2offset( N, strideY );
+	API_SUFFIX(stdlib_strided_scusumkbn_ndarray)( N, sum, X, strideX, ox, Y, strideY, oy );
+}
+
+/**
+* Computes the cumulative sum of single-precision floating-point strided array elements using an improved Kahan–Babuška algorithm and alternative indexing semantics.
+*
+* ## Method
+*
+* -   This implementation uses an "improved Kahan–Babuška algorithm", as described by Neumaier (1974).
+*
+* ## References
+*
+* -   Neumaier, Arnold. 1974. "Rounding Error Analysis of Some Methods for Summing Finite Sums." _Zeitschrift Für Angewandte Mathematik Und Mechanik_ 54 (1): 39–51. doi:[10.1002/zamm.19740540106](https://doi.org/10.1002/zamm.19740540106).
+*
+* @param N        number of indexed elements
+* @param sum      initial sum
+* @param X        input array
+* @param strideX  stride length for X
+* @param offsetX  starting index for X
+* @param Y        output array
+* @param strideY  stride length for Y
+* @param offsetY  starting index for Y
+*/
+void API_SUFFIX(stdlib_strided_scusumkbn_ndarray)( const CBLAS_INT N, const float sum, const float *X, const CBLAS_INT strideX, const CBLAS_INT offsetX, float *Y, const CBLAS_INT strideY, const CBLAS_INT offsetY ) {
+	CBLAS_INT ix;
+	CBLAS_INT iy;
+	CBLAS_INT i;
 	float s;
 	float v;
 	float t;
@@ -50,16 +77,8 @@ void stdlib_strided_scusumkbn( const int64_t N, const float sum, const float *X,
 	if ( N <= 0 ) {
 		return;
 	}
-	if ( strideX < 0 ) {
-		ix = (1-N) * strideX;
-	} else {
-		ix = 0;
-	}
-	if ( strideY < 0 ) {
-		iy = (1-N) * strideY;
-	} else {
-		iy = 0;
-	}
+	ix = offsetX;
+	iy = offsetY;
 	s = sum;
 	c = 0.0f;
 	for ( i = 0; i < N; i++ ) {
