02a62bcdeafe5e83e7a7e7bb8933aae76c29bece
Vinit Pandit <106718914+Vinit-Pandit@users.noreply.github.com>
2025-01-18 04:55:35 +0530

    feat: add C ndarray interface and refactor implementation for `stats/base/svarianceyc`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4372
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Co-authored-by: Aayush Khanna <aayushiitbhu23@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com> 
    Reviewed-by: Aayush Khanna <aayushiitbhu23@gmail.com>

--- lib/node_modules/@stdlib/stats/base/svarianceyc/src/main.c
+++ lib/node_modules/@stdlib/stats/base/svarianceyc/src/main.c
@@ -17,7 +17,8 @@
 */
 
 #include "stdlib/stats/base/svarianceyc.h"
-#include <stdint.h>
+#include "stdlib/blas/base/shared.h"
+#include "stdlib/strided/base/stride2offset.h"
 
 /**
 * Computes the variance of a single-precision floating-point strided array using a one-pass algorithm proposed by Youngs and Cramer.
@@ -33,33 +34,45 @@
 * @param N           number of indexed elements
 * @param correction  degrees of freedom adjustment
 * @param X           input array
-* @param stride      stride length
+* @param strideX     stride length
 * @return            output value
 */
-float stdlib_strided_svarianceyc( const int64_t N, const float correction, const float *X, const int64_t stride ) {
-	int64_t ix;
-	int64_t i;
-	double di;
-	float sum;
-	double n;
-	float S;
-	float v;
-	float d;
+float API_SUFFIX(stdlib_strided_svarianceyc)( const CBLAS_INT N, const float correction, const float *X, const CBLAS_INT strideX ) {
+    CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+    return API_SUFFIX(stdlib_strided_svarianceyc_ndarray)( N, correction, X, strideX, ox );
+}
 
-	n = (double)N - (double)correction;
-	if ( N <= 0 || n <= 0.0f ) {
-		return 0.0f / 0.0f; // NaN
-	}
-	if ( N == 1 || stride == 0 ) {
-		return 0.0f;
-	}
-	if ( stride < 0 ) {
-		ix = (1-N) * stride;
-	} else {
-		ix = 0;
-	}
-	sum = X[ ix ];
-	ix += stride;
+
+/**
+* Computes the variance of a single-precision floating-point strided array using a one-pass algorithm proposed by Youngs and Cramer and alternative indexing semantics.
+*
+* @param N           number of indexed elements
+* @param correction  degrees of freedom adjustment
+* @param X           input array
+* @param strideX     stride length
+* @param offsetX     starting index of X
+* @return            output value
+*/
+float API_SUFFIX(stdlib_strided_svarianceyc_ndarray)( const CBLAS_INT N, const float correction, const float *X, const CBLAS_INT strideX, const CBLAS_INT offsetX ) {
+    CBLAS_INT ix;
+    CBLAS_INT i;
+    double di;
+    float sum;
+    double n;
+    float S;
+    float v;
+    float d;
+
+    n = (double)N - (double)correction;
+    if ( N <= 0 || n <= 0.0f ) {
+        return 0.0f / 0.0f; // NaN
+    }
+    if ( N == 1 || strideX == 0 ) {
+        return 0.0f;
+    }
+    ix = offsetX;
+    sum = X[ ix ];
+	ix += strideX;
 	S = 0.0f;
 	for ( i = 2; i <= N; i++ ) {
 		di = (double)i;
@@ -67,7 +80,7 @@ float stdlib_strided_svarianceyc( const int64_t N, const float correction, const
 		sum += v;
 		d = (float)(di*(double)v) - sum;
 		S += (float)(1.0/(di*(di-1.0))) * d * d;
-		ix += stride;
+		ix += strideX;
 	}
 	return (double)S / n;
 }
