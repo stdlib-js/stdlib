ae712e52f17fbfa0c1b4edc6a6f9ab63d8d310a5
Vivek maurya <155618190+vivekmaurya001@users.noreply.github.com>
2025-01-20 03:20:08 +0530

    feat: add C implementation for `stats/base/dists/logistic/stdev`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4189
    Closes: https://github.com/stdlib-js/stdlib/issues/3745
    
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: stdlib-bot <noreply@stdlib.io>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/logistic/stdev/src/main.c
@@ -0,0 +1,44 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/logistic/stdev.h"
+#include "stdlib/math/base/assert/is_nan.h"
+#include "stdlib/constants/float64/pi.h"
+#include "stdlib/constants/float64/sqrt_three.h"
+
+/**
+* Returns the standard deviation for a logistic distribution with location `mu` and scale `s`.
+*
+* @param mu    location parameter
+* @param s     scale parameter
+* @return      standard deviation
+*
+* @example
+* double y = stdlib_base_logistic_stdev( 0.0, 1.0 );
+* // returns ~1.814
+*/
+double stdlib_base_dists_logistic_stdev( const double mu, const double s ) {
+	if (
+		stdlib_base_is_nan( mu ) ||
+		stdlib_base_is_nan( s ) ||
+		s <= 0.0
+	) {
+		return 0.0/0.0; // NaN
+	}
+	return s * STDLIB_CONSTANT_FLOAT64_PI / STDLIB_CONSTANT_FLOAT64_SQRT3;
+}
