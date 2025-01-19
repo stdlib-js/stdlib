7050c3777a7730d1b2de2ca4025754604e842a13
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2025-01-19 20:25:34 +0530

    feat: add C implementation for `stats/base/dists/cosine/logpdf`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4502
    Closes: https://github.com/stdlib-js/stdlib/issues/3517
    
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/cosine/logpdf/examples/c/example.c
@@ -0,0 +1,43 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/cosine/logpdf.h"
+#include "stdlib/constants/float64/eps.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+static double random_uniform( const double min, const double max ) {
+	double v = (double)rand() / ( (double)RAND_MAX + 1.0 );
+	return min + ( v*(max-min) );
+}
+
+int main( void ) {
+	double mu;
+	double s;
+	double x;
+	double y;
+	int i;
+
+	for ( i = 0; i < 10; i++ ) {
+		x = random_uniform( -50.0, 50.0 );
+		mu = random_uniform( -50.0, 50.0 );
+		s = random_uniform( STDLIB_CONSTANT_FLOAT64_EPS, 20.0 );
+		y = stdlib_base_dists_cosine_logpdf( x, mu, s );
+		printf( "x: %lf, µ: %lf, s: %lf, ln(f(x;µ,s)): %lf\n", x, mu, s , y );
+	}
+}
