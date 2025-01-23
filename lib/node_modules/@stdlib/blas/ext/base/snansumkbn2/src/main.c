d9963706c7fb289e5c362577e8b897c46eda045f
Muhammad Haris <101793258+headlessNode@users.noreply.github.com>
2025-01-23 05:57:58 +0500

    feat: add C `ndarray` API and refactor `blas/ext/base/snansumkbn2`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4846
    Reviewed-by: Athan Reines <kgryte@gmail.com>

--- lib/node_modules/@stdlib/blas/ext/base/snansumkbn2/src/main.c
+++ lib/node_modules/@stdlib/blas/ext/base/snansumkbn2/src/main.c
@@ -1,7 +1,7 @@
 /**
 * @license Apache-2.0
 *
-* Copyright (c) 2020 The Stdlib Authors.
+* Copyright (c) 2025 The Stdlib Authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -18,8 +18,9 @@
 
 #include "stdlib/blas/ext/base/snansumkbn2.h"
 #include "stdlib/math/base/assert/is_nanf.h"
-#include <stdint.h>
-#include <math.h>
+#include "stdlib/strided/base/stride2offset.h"
+#include "stdlib/math/base/special/absf.h"
+#include "stdlib/blas/base/shared.h"
 
 /**
 * Computes the sum of single-precision floating-point strided array elements, ignoring `NaN` values and using a second-order iterative Kahan–Babuška algorithm.
@@ -32,14 +33,36 @@
 *
 * -   Klein, Andreas. 2005. "A Generalized Kahan-Babuška-Summation-Algorithm." _Computing_ 76 (3): 279–93. doi:[10.1007/s00607-005-0139-x](https://doi.org/10.1007/s00607-005-0139-x).
 *
-* @param N       number of indexed elements
-* @param X       input array
-* @param stride  stride length
-* @return        output value
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @return         output value
 */
-float stdlib_strided_snansumkbn2( const int64_t N, const float *X, const int64_t stride ) {
-	int64_t ix;
-	int64_t i;
+float API_SUFFIX(stdlib_strided_snansumkbn2)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX ) {
+	CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	return API_SUFFIX(stdlib_strided_snansumkbn2_ndarray)( N, X, strideX, ox );
+}
+
+/**
+* Computes the sum of single-precision floating-point strided array elements, ignoring `NaN` values and using a second-order iterative Kahan–Babuška algorithm and alternative indexing semantics.
+*
+* ## Method
+*
+* -   This implementation uses a second-order iterative Kahan–Babuška algorithm, as described by Klein (2005).
+*
+* ## References
+*
+* -   Klein, Andreas. 2005. "A Generalized Kahan-Babuška-Summation-Algorithm." _Computing_ 76 (3): 279–93. doi:[10.1007/s00607-005-0139-x](https://doi.org/10.1007/s00607-005-0139-x).
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @param offsetX  stride length
+* @return         output value
+*/
+float API_SUFFIX(stdlib_strided_snansumkbn2_ndarray)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX, const CBLAS_INT offsetX ) {
+	CBLAS_INT ix;
+	CBLAS_INT i;
 	float sum;
 	float ccs;
 	float cs;
@@ -52,16 +75,12 @@ float stdlib_strided_snansumkbn2( const int64_t N, const float *X, const int64_t
 	if ( N <= 0 ) {
 		return sum;
 	}
-	if ( N == 1 || stride == 0 ) {
-		if ( stdlib_base_is_nanf( X[ 0 ] ) ) {
+	ix = offsetX;
+	if ( strideX == 0 ) {
+		if ( stdlib_base_is_nanf( X[ ix ] ) ) {
 			return sum;
 		}
-		return X[ 0 ];
-	}
-	if ( stride < 0 ) {
-		ix = (1-N) * stride;
-	} else {
-		ix = 0;
+		return N * X[ ix ];
 	}
 	ccs = 0.0f; // second order correction term for lost lower order bits
 	cs = 0.0f; // first order correction term for lost low order bits
@@ -69,14 +88,14 @@ float stdlib_strided_snansumkbn2( const int64_t N, const float *X, const int64_t
 		v = X[ ix ];
 		if ( !stdlib_base_is_nanf( v ) ) {
 			t = sum + v;
-			if ( fabsf( sum ) >= fabsf( v ) ) {
+			if ( stdlib_base_absf( sum ) >= stdlib_base_absf( v ) ) {
 				c = (sum-t) + v;
 			} else {
 				c = (v-t) + sum;
 			}
 			sum = t;
 			t = cs + c;
-			if ( fabsf( cs ) >= fabsf( c ) ) {
+			if ( stdlib_base_absf( cs ) >= stdlib_base_absf( c ) ) {
 				cc = (cs-t) + c;
 			} else {
 				cc = (c-t) + cs;
@@ -84,7 +103,7 @@ float stdlib_strided_snansumkbn2( const int64_t N, const float *X, const int64_t
 			cs = t;
 			ccs += cc;
 		}
-		ix += stride;
+		ix += strideX;
 	}
 	return sum + cs + ccs;
 }
