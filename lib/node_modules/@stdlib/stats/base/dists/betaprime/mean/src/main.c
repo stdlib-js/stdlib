b9483192e185319ab9f9e36a9793a661d0cf20df
Neeraj Pathak <neerajrpathak710@gmail.com>
2025-02-05 19:41:22 +0530

    feat: add C implementation for `stats/base/dists/betaprime/mean`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4064
    Closes: https://github.com/stdlib-js/stdlib/issues/3441
    
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: stdlib-bot <noreply@stdlib.io>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Signed-off-by: Neeraj Pathak <neerajrpathak710@gmail.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/betaprime/mean/src/main.c
@@ -0,0 +1,38 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/betaprime/mean.h"
+
+/**
+* Returns the expected value for a beta prime distribution with first shape parameter `alpha` and second shape parameter `beta`.
+*
+* @param x        input value
+* @param alpha    first shape parameter
+* @param beta     second shape parameter
+* @returns        expected value
+*
+* @example
+* double y = stdlib_base_dists_betaprime_mean( 1.0, 2.0 );
+* // returns ~1.0
+*/
+double stdlib_base_dists_betaprime_mean( double alpha, const double beta ) {
+	if ( alpha <= 0.0 || beta <= 1.0 ) {
+		return 0.0 / 0.0;
+	}
+	return alpha / ( beta - 1.0 );
+}
