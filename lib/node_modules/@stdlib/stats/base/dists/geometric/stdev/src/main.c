d524634b6add111fccf5e8666f963081494c98e7
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2025-02-09 07:16:02 +0530

    feat: add C implementation for `stats/base/dists/geometric/stdev`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4095
    Closes: https://github.com/stdlib-js/stdlib/issues/3642
    
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: stdlib-bot <noreply@stdlib.io>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/geometric/stdev/src/main.c
@@ -0,0 +1,42 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/geometric/stdev.h"
+#include "stdlib/math/base/special/sqrt.h"
+#include "stdlib/math/base/assert/is_nan.h"
+
+/**
+* Returns the  standard deviation of a geometric distribution.
+*
+* @param p    success probability
+* @return     standard deviation
+*
+* @example
+* double y = stdlib_base_dists_geometric_stdev( 0.1 );
+* // returns 10.0
+*/
+double stdlib_base_dists_geometric_stdev( const double p ) {
+	if (
+		stdlib_base_is_nan( p ) ||
+		p <= 0.0 ||
+		p >= 1.0
+	) {
+		return 0.0 / 0.0; // NaN
+	}
+	return stdlib_base_sqrt( 1.0-p ) / p ;
+}
