db4ba243e10a474bc7faabb09b0adbb4c0af6753
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2025-02-09 07:26:23 +0530

    feat: add C implementation for `stats/base/dists/uniform/kurtosis`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/4132
    Closes: https://github.com/stdlib-js/stdlib/issues/3827
    
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Co-authored-by: stdlib-bot <noreply@stdlib.io>
    Reviewed-by: Philipp Burckhardt <pburckhardt@outlook.com>
    Signed-off-by: Philipp Burckhardt <pburckhardt@outlook.com>

---
+++ lib/node_modules/@stdlib/stats/base/dists/uniform/kurtosis/src/main.c
@@ -0,0 +1,42 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/stats/base/dists/uniform/kurtosis.h"
+#include "stdlib/math/base/assert/is_nan.h"
+
+/**
+* Returns the excess kurtosis of a uniform distribution.
+*
+* @param n   minimum support
+* @param p   maximum support
+* @return    excess kurtosis
+*
+* @example
+* double v = kurtosis( 4.0, 12.0 );
+* // returns -1.2
+*/
+double stdlib_base_dists_uniform_kurtosis( const double a, const double b ) {
+	if (
+		stdlib_base_is_nan( a ) ||
+		stdlib_base_is_nan( b ) ||
+		a >= b
+	) {
+		return 0.0 / 0.0; // NaN
+	}
+	return -1.2;
+}
