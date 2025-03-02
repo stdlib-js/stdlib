9e686c6609ffbaae38757513a349da903ef7f686
Annamalai Prabu <apfossdev@gmail.com>
2025-03-02 10:55:27 +0530

    refactor: add accessor protocol support to stats/base/mean #5629
    Refactor the mean function to add support for the accessor protocol. This
    change ensures that the function can handle array-like objects with custom
    accessors, improving flexibility and compatibility with various data
    structures.
    
    - Refactor mean function in main.js to delegate to ndarray.js
    - Update ndarray.js to handle accessor protocol
    - Add tests for accessor protocol support
    - Fix JSDoc comments and examples for accuracy
    
    Fixes: #5629
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/stats/base/mean/docs/types/test.ts
+++ lib/node_modules/@stdlib/stats/base/mean/docs/types/test.ts
@@ -16,6 +16,7 @@
 * limitations under the License.
 */
 
+import AccessorArray = require( '@stdlib/array/base/accessor' );
 import mean = require( './index' );
 
 
@@ -26,6 +27,7 @@ import mean = require( './index' );
 	const x = new Float64Array( 10 );
 
 	mean( x.length, x, 1 ); // $ExpectType number
+	mean( x.length, new AccessorArray( x ), 1 ); // $ExpectType number
 }
 
 // The compiler throws an error if the function is provided a first argument which is not a number...
@@ -85,6 +87,7 @@ import mean = require( './index' );
 	const x = new Float64Array( 10 );
 
 	mean.ndarray( x.length, x, 1, 0 ); // $ExpectType number
+	mean.ndarray( x.length, new AccessorArray( x ), 1, 0 ); // $ExpectType number
 }
 
 // The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
