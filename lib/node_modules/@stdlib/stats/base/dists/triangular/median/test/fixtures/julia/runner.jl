08c42abb8166fd966e22b19f250b1e3f1c3ec836
jalajk3004 <jalajkumar23989@gmail.com>
2025-03-02 10:40:25 +0530

    edit
    ---
    type: pre_push_report
    description: Results of running various checks prior to pushing changes.
    report:
      - task: run_javascript_examples
        status: na
      - task: run_c_examples
        status: na
      - task: run_cpp_examples
        status: na
      - task: run_javascript_readme_examples
        status: na
      - task: run_c_benchmarks
        status: na
      - task: run_cpp_benchmarks
        status: na
      - task: run_fortran_benchmarks
        status: na
      - task: run_javascript_benchmarks
        status: na
      - task: run_julia_benchmarks
        status: na
      - task: run_python_benchmarks
        status: na
      - task: run_r_benchmarks
        status: na
      - task: run_javascript_tests
        status: na
    ---

--- lib/node_modules/@stdlib/stats/base/dists/triangular/median/test/fixtures/julia/runner.jl
+++ lib/node_modules/@stdlib/stats/base/dists/triangular/median/test/fixtures/julia/runner.jl
@@ -74,9 +74,11 @@ dir = dirname( file );
 # Generate fixtures:
 a = rand( 500 ) .* 10.0;
 b = ( rand( 500 ) .* 10.0 ) .+ a;
+
 # Case: c < (a + b) / 2
 c1 = a .+ ( b .- a ) .* ( 0.5 .* rand( 500 ) );
 gen( a, b, c1, "data1.json" );
+
 # Case: c >= (a + b) / 2
 c2 = ( a .+ b ) ./ 2 .+ ( b .- a ) .* ( 0.5 .* rand( 500 ) );
 gen( a, b, c2, "data2.json" );
