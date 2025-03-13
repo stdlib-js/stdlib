9a9b7cf99a9e6316a3163c5540734594fe82105a
Planeshifter <1913638+Planeshifter@users.noreply.github.com>
2025-03-13 02:32:49 +0000

    feat: update namespace TypeScript declarations
    Signed-off-by: stdlib-bot <82920195+stdlib-bot@users.noreply.github.com>

--- lib/node_modules/@stdlib/stats/base/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/stats/base/docs/types/index.d.ts
@@ -1037,7 +1037,7 @@ interface Namespace {
 	* @param N - number of indexed elements
 	* @param correction - degrees of freedom adjustment
 	* @param x - input array
-	* @param stride - stride length
+	* @param strideX - stride length
 	* @returns standard deviation
 	*
 	* @example
@@ -1702,7 +1702,7 @@ interface Namespace {
 	* @param N - number of indexed elements
 	* @param correction - degrees of freedom adjustment
 	* @param x - input array
-	* @param stride - stride length
+	* @param strideX - stride length
 	* @returns standard deviation
 	*
 	* @example
@@ -5162,7 +5162,7 @@ interface Namespace {
 	* @param N - number of indexed elements
 	* @param correction - degrees of freedom adjustment
 	* @param x - input array
-	* @param stride - stride length
+	* @param strideX - stride length
 	* @returns variance
 	*
 	* @example
