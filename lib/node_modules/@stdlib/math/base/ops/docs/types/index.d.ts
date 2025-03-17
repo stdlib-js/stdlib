38464c5340e81a0967f1a83748b93d25acbc51b6
gururaj1512 <gururajgurram1512@gmail.com>
2025-03-17 21:22:44 +0000

    remove: remove `cmulf` from namespace
    This commit removes the `cmulf` symbol from the `@stdlib/math/base/ops`
    namespace due to a package migration.
    
    BREAKING CHANGE: remove `cmulf`
    
    To migrate, users should access the same symbol via the
    `@stdlib/complex/float32/base` namespace.
    
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: na
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: na
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: na
      - task: lint_javascript_tests
        status: na
      - task: lint_javascript_benchmarks
        status: na
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: na
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/math/base/ops/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/math/base/ops/docs/types/index.d.ts
@@ -20,7 +20,6 @@
 
 /* eslint-disable max-lines */
 
-import cmulf = require( '@stdlib/complex/float32/base/mul' );
 import cadd = require( '@stdlib/complex/float64/base/add' );
 import cmul = require( '@stdlib/complex/float64/base/mul' );
 
@@ -28,54 +27,6 @@ import cmul = require( '@stdlib/complex/float64/base/mul' );
 * Interface describing the `ops` namespace.
 */
 interface Namespace {
-	/**
-	* Multiplies two single-precision complex floating-point numbers.
-	*
-	* @param z1 - complex number
-	* @param z2 - complex number
-	* @returns result
-	*
-	* @example
-	* var Complex64 = require( '@stdlib/complex/float32/ctor' );
-	* var realf = require( '@stdlib/complex/float32/real' );
-	* var imagf = require( '@stdlib/complex/float32/imag' );
-	*
-	* var z1 = new Complex64( 5.0, 3.0 );
-	* // returns <Complex64>
-	*
-	* var z2 = new Complex64( -2.0, 1.0 );
-	* // returns <Complex64>
-	*
-	* var out = ns.cmulf( z1, z2 );
-	* // returns <Complex64>
-	*
-	* var re = realf( out );
-	* // returns -13.0
-	*
-	* var im = imagf( out );
-	* // returns -1.0
-	*
-	* @example
-	* var Float32Array = require( '@stdlib/array/float32' );
-	*
-	* var out = new Float32Array( 2 );
-	* var v = ns.cmulf.assign( 5.0, 3.0, -2.0, 1.0, out, 1, 0 );
-	* // returns <Float32Array>[ -13.0, -1.0 ]
-	*
-	* var bool = ( out === v );
-	* // returns true
-	*
-	* @example
-	* var Float32Array = require( '@stdlib/array/float32' );
-	*
-	* var z1 = new Float32Array( [ 5.0, 3.0 ] );
-	* var z2 = new Float32Array( [ -2.0, 1.0 ] );
-	*
-	* var out = ns.cmulf.strided( z1, 1, 0, z2, 1, 0, new Float32Array( 2 ), 1, 0 );
-	* // returns <Float32Array>[ -13.0, -1.0 ]
-	*/
-	cmulf: typeof cmulf;
-
 	/**
 	* Adds two double-precision complex floating-point numbers.
 	*
