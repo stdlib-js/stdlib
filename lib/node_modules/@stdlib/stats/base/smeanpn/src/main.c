9dfd3f409004243bb631fb497b950a5bd032b814
gururaj1512 <gururajgurram1512@gmail.com>
2025-04-12 16:03:53 +0000

    feat: add C ndarray interface and refactor implementation for `stats/base/smeanpn`
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: missing_dependencies
      - task: lint_c_examples
        status: missing_dependencies
      - task: lint_c_benchmarks
        status: missing_dependencies
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: na
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/stats/base/smeanpn/src/main.c
+++ lib/node_modules/@stdlib/stats/base/smeanpn/src/main.c
@@ -19,7 +19,8 @@
 #include "stdlib/stats/base/smeanpn.h"
 #include "stdlib/blas/ext/base/ssumpw.h"
 #include "stdlib/blas/ext/base/sapxsumpw.h"
-#include <stdint.h>
+#include "stdlib/blas/base/shared.h"
+#include "stdlib/strided/base/stride2offset.h"
 
 /**
 * Computes the arithmetic mean of a single-precision floating-point strided array using a two-pass error correction algorithm.
@@ -33,29 +34,43 @@
 * -   Neely, Peter M. 1966. "Comparison of Several Algorithms for Computation of Means, Standard Deviations and Correlation Coefficients." _Communications of the ACM_ 9 (7). Association for Computing Machinery: 496–99. doi:[10.1145/365719.365958](https://doi.org/10.1145/365719.365958).
 * -   Schubert, Erich, and Michael Gertz. 2018. "Numerically Stable Parallel Computation of (Co-)Variance." In _Proceedings of the 30th International Conference on Scientific and Statistical Database Management_. New York, NY, USA: Association for Computing Machinery. doi:[10.1145/3221269.3223036](https://doi.org/10.1145/3221269.3223036).
 *
-* @param N       number of indexed elements
-* @param X       input array
-* @param stride  stride length
-* @return        output value
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @return         output value
 */
-float stdlib_strided_smeanpn( const int64_t N, const float *X, const int64_t stride ) {
+float API_SUFFIX(stdlib_strided_smeanpn)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX ) {
+	const CBLAS_INT ox = stdlib_strided_stride2offset( N, strideX );
+	return API_SUFFIX(stdlib_strided_smeanpn_ndarray)( N, X, strideX, ox );
+}
+
+/**
+* Computes the arithmetic mean of a single-precision floating-point strided array using a two-pass error correction algorithm and alternative indexing semantics.
+*
+* @param N        number of indexed elements
+* @param X        input array
+* @param strideX  stride length
+* @param offsetX  starting index
+* @return         output value
+*/
+float API_SUFFIX(stdlib_strided_smeanpn_ndarray)( const CBLAS_INT N, const float *X, const CBLAS_INT strideX, const CBLAS_INT offsetX ) {
 	double dN;
 	float mu;
 	float c;
 
 	if ( N <= 0 ) {
-		return 0.0 / 0.0; // NaN
+		return 0.0f / 0.0f; // NaN
 	}
-	if ( N == 1 || stride == 0 ) {
-		return X[ 0 ];
+	if ( N == 1 || strideX == 0 ) {
+		return X[ offsetX ];
 	}
 	dN = (double)N;
 
 	// Compute an estimate for the mean:
-	mu = (double)stdlib_strided_ssumpw( N, X, stride ) / dN;
+	mu = (double)API_SUFFIX(stdlib_strided_ssumpw_ndarray)( N, X, strideX, offsetX ) / dN;
 
 	// Compute an error term:
-	c = (double)stdlib_strided_sapxsumpw( N, -mu, X, stride ) / dN;
+	c = (double)API_SUFFIX(stdlib_strided_sapxsumpw_ndarray)( N, -mu, X, strideX, offsetX ) / dN;
 
 	return mu + c;
 }
