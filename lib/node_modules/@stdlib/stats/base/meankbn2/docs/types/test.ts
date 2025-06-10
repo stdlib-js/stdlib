67c3e4025c97584c53ca10d37d2decf5049f4a77
gururaj1512 <gururajgurram1512@gmail.com>
2025-06-10 14:20:26 +0000

    feat: add support for accessor arrays and refactor stats/base/meankbn2

--- lib/node_modules/@stdlib/stats/base/meankbn2/docs/types/test.ts
+++ lib/node_modules/@stdlib/stats/base/meankbn2/docs/types/test.ts
@@ -16,6 +16,7 @@
 * limitations under the License.
 */
 
+import AccessorArray = require( '@stdlib/array/base/accessor' );
 import meankbn2 = require( './index' );
 
 
@@ -26,6 +27,7 @@ import meankbn2 = require( './index' );
 	const x = new Float64Array( 10 );
 
 	meankbn2( x.length, x, 1 ); // $ExpectType number
+	meankbn2( x.length, new AccessorArray( x ), 1 ); // $ExpectType number
 }
 
 // The compiler throws an error if the function is provided a first argument which is not a number...
@@ -85,6 +87,7 @@ import meankbn2 = require( './index' );
 	const x = new Float64Array( 10 );
 
 	meankbn2.ndarray( x.length, x, 1, 0 ); // $ExpectType number
+	meankbn2.ndarray( x.length, new AccessorArray( x ), 1, 0 ); // $ExpectType number
 }
 
 // The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
