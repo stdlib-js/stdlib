ee11e14656d265e007a364e7f2096d3f15a17b57
gururaj1512 <gururajgurram1512@gmail.com>
2025-06-17 20:34:57 +0000

    feat: add support for accessor arrays and refactor `stats/base/nanvariance`
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/stats/base/nanvariance/docs/types/test.ts
+++ lib/node_modules/@stdlib/stats/base/nanvariance/docs/types/test.ts
@@ -16,6 +16,7 @@
 * limitations under the License.
 */
 
+import AccessorArray = require( '@stdlib/array/base/accessor' );
 import nanvariance = require( './index' );
 
 
@@ -26,6 +27,7 @@ import nanvariance = require( './index' );
 	const x = new Float64Array( 10 );
 
 	nanvariance( x.length, 1, x, 1 ); // $ExpectType number
+	nanvariance( x.length, 1, new AccessorArray( x ), 1 ); // $ExpectType number
 }
 
 // The compiler throws an error if the function is provided a first argument which is not a number...
@@ -101,6 +103,7 @@ import nanvariance = require( './index' );
 	const x = new Float64Array( 10 );
 
 	nanvariance.ndarray( x.length, 1, x, 1, 0 ); // $ExpectType number
+	nanvariance.ndarray( x.length, 1, new AccessorArray( x ), 1, 0 ); // $ExpectType number
 }
 
 // The compiler throws an error if the `ndarray` method is provided a first argument which is not a number...
