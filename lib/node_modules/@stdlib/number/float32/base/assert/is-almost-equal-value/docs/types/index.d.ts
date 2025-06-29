d65aec19343f1632687350f381a008bef9600e11
Karan Anand <anandkarancompsci@gmail.com>
2025-06-29 16:40:02 -0700

    feat: add `number/float32/base/assert/is-almost-equal-value`
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: passed
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

---
+++ lib/node_modules/@stdlib/number/float32/base/assert/is-almost-equal-value/docs/types/index.d.ts
@@ -0,0 +1,63 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2025 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/**
+* Tests if two single-precision floating-point numbers are approximately equal within a specified number of ULPs (units in the last place).
+*
+* ## Notes
+*
+* -   The function returns `false` if either input value is `NaN`.
+* -   The function does not distinguish between `-0` and `+0`, treating them as equal.
+*
+* @param a - first input value
+* @param b - second input value
+* @param maxULP - maximum allowed ULP difference
+* @returns boolean indicating whether two single-precision floating-point numbers are approximately equal within a specified number of ULPs
+*
+* @example
+* var EPS = require( '@stdlib/constants/float32/eps' );
+*
+* var bool = isAlmostEqualValue( 1.0, 1.0+EPS, 1 );
+* // returns true
+*
+* var bool = isAlmostEqualValue( 1.0+EPS, 1.0, 1 );
+* // returns true
+*
+* bool = isAlmostEqualValue( 1.0, 1.0+EPS+EPS, 1 );
+* // returns false
+*
+* bool = isAlmostEqualValue( 1.0, 1.0+EPS, 0 );
+* // returns false
+*
+* bool = isAlmostEqualValue( 0.0, -0.0, 0 );
+* // returns true
+*
+* bool = isAlmostEqualValue( 1.0, NaN, 1 );
+* // returns false
+*
+* bool = isAlmostEqualValue( NaN, NaN, 1 );
+* // returns false
+*/
+declare function isAlmostEqualValue( a: number, b: number, maxULP: number ): boolean;
+
+
+// EXPORTS //
+
+export = isAlmostEqualValue;
