15d191e9aa4de61ce65f67f5fd6442520627643f
Shubham <b4mshubham@gmail.com>
2025-10-03 21:50:11 +0530

    fix eslint error in #8189

--- etc/eslint/rules/stdlib.js
+++ etc/eslint/rules/stdlib.js
@@ -4676,12 +4676,12 @@ rules[ 'stdlib/require-globals' ] = [ 'error', {
 *
 * @example
 * // Bad...
-* var foo = require( 'foo/bar.js' );
+*
 * var baz = require( '../baz.js' );
 *
 * @example
 * // Good...
-* var foo = require( './foo/bar.js' );
+*
 * var baz = require( './../baz.js' );
 */
 rules[ 'stdlib/require-leading-slash' ] = 'error';
