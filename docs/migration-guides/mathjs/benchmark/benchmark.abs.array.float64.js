c08fd5281ee4831cf4859869d47d5abd4e92032a
Athan <kgryte@gmail.com>
2026-01-15 23:51:19 -0800

    bench: remove duplicate import
    Signed-off-by: Athan <kgryte@gmail.com>

--- docs/migration-guides/mathjs/benchmark/benchmark.abs.array.float64.js
+++ docs/migration-guides/mathjs/benchmark/benchmark.abs.array.float64.js
@@ -25,7 +25,6 @@ var bench = require( '@stdlib/bench' );
 var isnan = require( '@stdlib/math/base/assert/is-nan' );
 var filledBy = require( '@stdlib/array/filled-by' );
 var zeros = require( '@stdlib/array/zeros' );
-var format = require( '@stdlib/string/format' );
 var uniform = require( '@stdlib/random/base/uniform' ).factory;
 var strided = require( '@stdlib/math/strided/special/abs' );
 var dabs = require( '@stdlib/math/strided/special/dabs' );
