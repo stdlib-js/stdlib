2814e395383420256abcaa3e73e3c731df225dfe
Mahi <desaihmahi@gmail.com>
2026-01-31 00:06:20 +0530

    tools/snippets: add documentation and basic tests for placeholder snippet function

--- tools/snippets/lib/index.js
+++ tools/snippets/lib/index.js
@@ -1,40 +1,22 @@
-/**
-* @license Apache-2.0
-*
-* Copyright (c) 2026 The Stdlib Authors.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 'use strict';
 
 /**
-* TODO: description
+* Example snippet utility.
 *
-* @module @stdlib/<TODO: module path>
+* @module @stdlib/tools/snippets
 *
 * @example
-* var TODO = require( '@stdlib/<TODO: module path>' );
+* var snippet = require( '@stdlib/tools/snippets' );
 *
-* var v = TODO( 0.0 );
-* // returns TODO
+* var v = snippet( 3.0 );
+* // returns 3.0
 */
 
 // MODULES //
 
-var TODO = require( './main.js' );
+var snippet = require( './main.js' );
 
 
 // EXPORTS //
 
-module.exports = TODO;
+module.exports = snippet;
--- tools/snippets/lib/main.js
+++ tools/snippets/lib/main.js
@@ -1,40 +1,25 @@
-/**
-* @license Apache-2.0
-*
-* Copyright (c) 2026 The Stdlib Authors.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 'use strict';
 
 // MAIN //
 
 /**
-* TODO: description
+* Example snippet function which returns the input value unchanged.
 *
-* @param {<TODO: type>} TODO - TODO: param desc
-* @returns {<TODO: type>} TODO: desc
+* This function exists as a simple demonstration utility for tooling
+* and documentation examples.
+*
+* @param {number} x - input value
+* @returns {number} input value
 *
 * @example
-* var v = TODO( 0.0 );
-* // returns TODO
+* var v = snippet( 2.0 );
+* // returns 2.0
 */
-function TODO() {
-	// TODO: implementation
+function snippet( x ) {
+	return x;
 }
 
 
 // EXPORTS //
 
-module.exports = TODO;
+module.exports = snippet;
