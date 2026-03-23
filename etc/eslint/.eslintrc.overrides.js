160b526c0beb23a29882630dfff4b2cc40e9dacb
Philipp Burckhardt <pburckhardt@outlook.com>
2026-03-23 00:41:03 -0500

    feat: add `jsdoc-example-eslint` ESLint rule
    Add a new custom ESLint rule that extracts JavaScript code from
    JSDoc `@example` blocks and lints it using ESLint's synchronous
    `Linter` API. The rule is a pure mechanism: it accepts a `rules`
    object and lints example code with those rules, reporting violations
    mapped back to the correct source file lines.
    
    The rule is configured in `.eslintrc.overrides.js`, where the
    examples ESLint config is loaded (avoiding a circular dependency
    with `stdlib.js`), filtered to exclude plugin rules and rules
    incompatible with code snippets, and passed to the rule.
    
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: passed
      - task: lint_package_json
        status: passed
      - task: lint_repl_help
        status: na
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: na
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: na
      - task: lint_license_headers
        status: passed
    ---

--- etc/eslint/.eslintrc.overrides.js
+++ etc/eslint/.eslintrc.overrides.js
@@ -16,15 +16,72 @@
 * limitations under the License.
 */
 
+/* eslint-disable vars-on-top, stdlib/empty-line-before-comment */
+
 'use strict';
 
 // MODULES //
 
 // FIXME: remove the next line and uncomment the subsequent line once all remark JSDoc ESLint rules are completed
 var copy = require( './../../lib/node_modules/@stdlib/utils/copy' );
-
 // var copy = require( './utils/copy.js' );
+var objectKeys = require( './../../lib/node_modules/@stdlib/utils/keys' );
 var defaults = require( './.eslintrc.js' );
+var examplesConfig = require( './.eslintrc.examples.js' );
+
+
+// VARIABLES //
+
+/**
+* List of rules to exclude when linting JSDoc code snippets because they are incompatible with code fragments extracted from JSDoc comments.
+*
+* @private
+* @type {Array}
+*/
+var JSDOC_SNIPPET_EXCLUDE = [
+	'no-undef',
+	'no-unused-vars',
+	'strict',
+	'no-var',
+	'eol-last',
+	'indent',
+	'no-restricted-syntax'
+];
+
+
+// FUNCTIONS //
+
+/**
+* Extracts built-in ESLint rules from a configuration object, filtering out plugin rules and rules listed in an exclusion list.
+*
+* @private
+* @param {Object} config - ESLint configuration object
+* @param {Array} exclude - rule names to exclude
+* @returns {Object} filtered rules object
+*/
+function extractSnippetRules( config, exclude ) {
+	var rules;
+	var keys;
+	var key;
+	var i;
+
+	rules = {};
+	keys = objectKeys( config.rules );
+	for ( i = 0; i < keys.length; i++ ) {
+		key = keys[ i ];
+
+		// Skip plugin rules (e.g., "stdlib/foo", "node/bar") since the Linter instance does not have them registered:
+		if ( key.indexOf( '/' ) !== -1 ) {
+			continue;
+		}
+		// Skip rules explicitly excluded as incompatible with code snippets:
+		if ( exclude.indexOf( key ) !== -1 ) {
+			continue;
+		}
+		rules[ key ] = config.rules[ key ];
+	}
+	return rules;
+}
 
 
 // MAIN //
@@ -44,6 +101,13 @@ var eslint = copy( defaults );
 * @type {Array}
 */
 eslint.overrides = require( './overrides' );
+
+/**
+* Configure JSDoc example linting using the examples ESLint config, with rules filtered for use on JSDoc code snippets.
+*/
+eslint.rules[ 'stdlib/jsdoc-example-eslint' ] = [ 'warn', {
+	'rules': extractSnippetRules( examplesConfig, JSDOC_SNIPPET_EXCLUDE )
+}];
 
 
 // EXPORTS //
