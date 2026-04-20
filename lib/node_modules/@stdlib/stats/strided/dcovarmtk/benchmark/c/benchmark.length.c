28604828bd3f91c8d688f8a24fb5a66674c5dd3d
Uday Kakade <udaykakade2525@gmail.com>
2026-04-20 18:29:34 +0530

    bench: refactor to use dynamic memory allocation in stats/strided/dcovarmtk

--- lib/node_modules/@stdlib/stats/strided/dcovarmtk/benchmark/c/benchmark.length.c
+++ lib/node_modules/@stdlib/stats/strided/dcovarmtk/benchmark/c/benchmark.length.c
@@ -96,11 +96,12 @@ static double rand_double( void ) {
 */
 static double benchmark1( int iterations, int len ) {
 	double elapsed;
-	double x[ len ];
+	double *x;
 	double v;
 	double t;
 	int i;
 
+	x = (double *) malloc( len * sizeof( double ) );
 	for ( i = 0; i < len; i++ ) {
 		x[ i ] = ( rand_double() * 20000.0 ) - 10000.0;
 	}
@@ -118,6 +119,7 @@ static double benchmark1( int iterations, int len ) {
 	if ( v != v ) {
 		printf( "should not return NaN\n" );
 	}
+	free( x );
 	return elapsed;
 }
 
