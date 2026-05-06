cb880ecc07157537197b9d47a5119f8144575599
Karan Anand <anandkarancompsci@gmail.com>
2026-05-06 12:12:05 -0700

    docs: remove unused imports in `blas/base/zswap`
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown
        status: na
      - task: lint_package_json
        status: na
      - task: lint_repl_help
        status: na
      - task: lint_javascript_src
        status: na
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: na
      - task: lint_javascript_tests
        status: na
      - task: lint_javascript_benchmarks
        status: na
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/blas/base/zswap/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/blas/base/zswap/docs/types/index.d.ts
@@ -62,8 +62,6 @@ interface Routine {
 	*
 	* @example
 	* var Complex128Array = require( '@stdlib/array/complex128' );
-	* var real = require( '@stdlib/complex/float64/real' );
-	* var imag = require( '@stdlib/complex/float64/imag' );
 	*
 	* var x = new Complex128Array( [ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 ] );
 	* var y = new Complex128Array( [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ] );
