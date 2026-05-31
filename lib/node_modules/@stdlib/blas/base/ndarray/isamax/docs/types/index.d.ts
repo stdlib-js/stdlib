d8bd1c0756c5af8f7008f5aacea82d1b38ccee2f
kaustubh <kaustubh.mp007@gmail.com>
2026-05-31 22:51:51 +0530

    feat: add blas/base/ndarray/isamax
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown_pkg_readmes
        status: passed
      - task: lint_markdown_docs
        status: na
      - task: lint_markdown
        status: na
      - task: lint_package_json
        status: passed
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

---
+++ lib/node_modules/@stdlib/blas/base/ndarray/isamax/docs/types/index.d.ts
@@ -0,0 +1,50 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2026 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/// <reference types="@stdlib/types"/>
+
+import { float32ndarray } from '@stdlib/types/ndarray';
+
+/**
+* Finds the index of the first element having the maximum absolute value for all elements in a one-dimensional single-precision floating-point ndarray.
+*
+* ## Notes
+*
+* -   The function expects the following ndarrays:
+*
+*     -   a one-dimensional input ndarray.
+*
+* @param arrays - array-like object containing ndarrays
+* @returns Index value
+*
+* @example
+* var Float32Vector = require( '@stdlib/ndarray/vector/float32' );
+*
+* var x = new Float32Vector( [ 1.0, -2.0, 3.0, -4.0, 5.0 ] );
+*
+* var y = isamax( [ x ] );
+* // returns 4.0
+*/
+declare function isamax( arrays: [ float32ndarray ] ): number;
+
+
+// EXPORTS //
+
+export = isamax;
