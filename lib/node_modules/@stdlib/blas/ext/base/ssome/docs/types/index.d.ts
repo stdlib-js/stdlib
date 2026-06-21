68db8bc757100bcdb191ef83631342082c6fc44c
headlessNode <harriskhan047@outlook.com>
2026-06-21 20:52:35 +0500

    feat: add blas/ext/base/ssome
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown_pkg_readmes
        status: passed
      - task: lint_markdown_docs
        status: na
      - task: lint_markdown
        status: na
      - task: lint_package_json
        status: passed
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: missing_dependencies
      - task: lint_c_examples
        status: missing_dependencies
      - task: lint_c_benchmarks
        status: missing_dependencies
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

---
+++ lib/node_modules/@stdlib/blas/ext/base/ssome/docs/types/index.d.ts
@@ -0,0 +1,95 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2026 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/**
+* Interface describing `ssome`.
+*/
+interface Routine {
+	/**
+	* Tests whether a single-precision floating-point strided array contains at least `k` truthy elements.
+	*
+	* @param N - number of indexed elements
+	* @param k - minimum number of truthy elements
+	* @param x - input array
+	* @param strideX - stride length
+	* @returns boolean indicating whether the input array contains at least `k` truthy elements
+	*
+	* @example
+	* var Float32Array = require( '@stdlib/array/float32' );
+	*
+	* var x = new Float32Array( [ 0.0, 0.0, 1.0, 2.0 ] );
+	*
+	* var v = ssome( x.length, 2, x, 1 );
+	* // returns true
+	*/
+	( N: number, k: number, x: Float32Array, strideX: number ): boolean;
+
+	/**
+	* Tests whether a single-precision floating-point strided array contains at least `k` truthy elements using alternative indexing semantics.
+	*
+	* @param N - number of indexed elements
+	* @param k - minimum number of truthy elements
+	* @param x - input array
+	* @param strideX - stride length
+	* @param offsetX - starting index
+	* @returns boolean indicating whether the input array contains at least `k` truthy elements
+	*
+	* @example
+	* var Float32Array = require( '@stdlib/array/float32' );
+	*
+	* var x = new Float32Array( [ 0.0, 0.0, 1.0, 2.0 ] );
+	*
+	* var v = ssome.ndarray( x.length, 2, x, 1, 0 );
+	* // returns true
+	*/
+	ndarray( N: number, k: number, x: Float32Array, strideX: number, offsetX: number ): boolean;
+}
+
+/**
+* Tests whether a single-precision floating-point strided array contains at least `k` truthy elements.
+*
+* @param N - number of indexed elements
+* @param k - minimum number of truthy elements
+* @param x - input array
+* @param strideX - stride length
+* @returns boolean indicating whether the input array contains at least `k` truthy elements
+*
+* @example
+* var Float32Array = require( '@stdlib/array/float32' );
+*
+* var x = new Float32Array( [ 0.0, 0.0, 1.0, 2.0 ] );
+*
+* var v = ssome( x.length, 2, x, 1 );
+* // returns true
+*
+* @example
+* var Float32Array = require( '@stdlib/array/float32' );
+*
+* var x = new Float32Array( [ 0.0, 0.0, 1.0, 2.0 ] );
+*
+* var v = ssome.ndarray( x.length, 2, x, 1, 0 );
+* // returns true
+*/
+declare var ssome: Routine;
+
+
+// EXPORTS //
+
+export = ssome;
