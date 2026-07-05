f28ac3779f2e142c7921438cc669113d3f9e5d0d
Athan <kgryte@gmail.com>
2026-07-05 12:16:25 -0700

    chore: clean-up
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown_pkg_readmes
        status: passed
      - task: lint_markdown_docs
        status: na
      - task: lint_markdown
        status: na
      - task: lint_package_json
        status: passed
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: na
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

--- lib/node_modules/@stdlib/stats/incr/nanmrange/docs/types/index.d.ts
+++ lib/node_modules/@stdlib/stats/incr/nanmrange/docs/types/index.d.ts
@@ -40,7 +40,6 @@ type accumulator = ( x?: number ) => number | null;
 *
 * -   The `W` parameter defines the number of values over which to compute the moving range.
 * -   As `W` values are needed to fill the window buffer, the first `W-1` returned values are calculated from smaller sample sizes. Until the window is full, each returned value is calculated from all provided values.
-* -   `NaN` values are ignored.
 *
 * @param W - window size
 * @throws must provide a positive integer
