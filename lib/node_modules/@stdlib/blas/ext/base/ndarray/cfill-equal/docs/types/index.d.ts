a1c263ac5695b0806a344d30563a103e3adaa673
headlessNode <harriskhan047@outlook.com>
2026-08-10 18:57:22 +0500

    feat: add blas/ext/base/ndarray/cfill-equal
    ---
    type: pre_commit_static_analysis_report
    description: Results of running static analysis checks when committing changes.
    report:
      - task: lint_filenames
        status: passed
      - task: lint_editorconfig
        status: passed
      - task: lint_markdown_pkg_readmes
        status: passed
      - task: lint_markdown_docs
        status: na
      - task: lint_markdown
        status: na
      - task: lint_package_json
        status: passed
      - task: lint_repl_help
        status: passed
      - task: lint_javascript_src
        status: passed
      - task: lint_javascript_cli
        status: na
      - task: lint_javascript_examples
        status: passed
      - task: lint_javascript_tests
        status: passed
      - task: lint_javascript_benchmarks
        status: passed
      - task: lint_python
        status: na
      - task: lint_r
        status: na
      - task: lint_c_src
        status: na
      - task: lint_c_examples
        status: na
      - task: lint_c_benchmarks
        status: na
      - task: lint_c_tests_fixtures
        status: na
      - task: lint_shell
        status: na
      - task: lint_typescript_declarations
        status: passed
      - task: lint_typescript_tests
        status: passed
      - task: lint_license_headers
        status: passed
    ---

---
+++ lib/node_modules/@stdlib/blas/ext/base/ndarray/cfill-equal/docs/types/index.d.ts
@@ -0,0 +1,65 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2026 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/// <reference types="@stdlib/types"/>
+
+import { complex64ndarray, typedndarray } from '@stdlib/types/ndarray';
+import { Complex64 } from '@stdlib/types/complex';
+
+/**
+* Replaces elements in a one-dimensional single-precision complex floating-point ndarray equal to a provided search element with a specified scalar constant.
+*
+* ## Notes
+*
+* -   The function expects the following ndarrays:
+*
+*     -   a one-dimensional input ndarray.
+*     -   a zero-dimensional ndarray containing the search element.
+*     -   a zero-dimensional ndarray containing the scalar constant.
+*
+* -   When comparing elements, the function checks for equality of real and imaginary components using the strict equality operator `===`. As a consequence, `NaN` components are considered distinct (i.e., as `NaN === NaN` always evaluates to `false`, elements having one or more `NaN` components are never replaced), and `-0` and `+0` are considered the same.
+*
+* @param arrays - array-like object containing ndarrays
+* @returns input ndarray
+*
+* @example
+* var Complex64Vector = require( '@stdlib/ndarray/vector/complex64' );
+* var scalar2ndarray = require( '@stdlib/ndarray/from-scalar' );
+* var Complex64 = require( '@stdlib/complex/float32/ctor' );
+*
+* var x = new Complex64Vector( [ 0.0, 0.0, -2.0, 3.0, 0.0, 0.0, 4.0, -6.0 ] );
+*
+* var searchElement = scalar2ndarray( new Complex64( 0.0, 0.0 ), {
+*     'dtype': 'complex64'
+* });
+*
+* var alpha = scalar2ndarray( new Complex64( 5.0, 5.0 ), {
+*     'dtype': 'complex64'
+* });
+*
+* var out = cfillEqual( [ x, searchElement, alpha ] );
+* // returns <ndarray>[ <Complex64>[ 5.0, 5.0 ], <Complex64>[ -2.0, 3.0 ], <Complex64>[ 5.0, 5.0 ], <Complex64>[ 4.0, -6.0 ] ]
+*/
+declare function cfillEqual( arrays: [ complex64ndarray, typedndarray<Complex64>, typedndarray<Complex64> ] ): complex64ndarray;
+
+
+// EXPORTS //
+
+export = cfillEqual;
