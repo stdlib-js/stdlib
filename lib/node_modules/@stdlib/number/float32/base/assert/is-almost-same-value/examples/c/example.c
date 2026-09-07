f2e4c4fb00a75ca16b696ec268f0a776117748df
Muhammad Haris <101793258+headlessNode@users.noreply.github.com>
2026-09-08 03:09:35 +0500

    feat: add C implementation to `number/float32/base/assert/is-almost-same-value`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/15036
    Co-authored-by: Athan Reines <kgryte@gmail.com>
    Reviewed-by: Athan Reines <kgryte@gmail.com> 
    Signed-off-by: Athan Reines <kgryte@gmail.com>
    Closes: https://github.com/stdlib-js/metr-issue-tracker/issues/1292

---
+++ lib/node_modules/@stdlib/number/float32/base/assert/is-almost-same-value/examples/c/example.c
@@ -0,0 +1,56 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2026 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "stdlib/number/float32/base/assert/is_almost_same_value.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main( void ) {
+	const float a[] = {
+		5.0f,
+		-2.0f,
+		0.0f,
+		0.0f/0.0f,
+		1.0f,
+		1.0f + 1.1920929e-7f
+	};
+	const float b[] = {
+		5.0f,
+		2.0f,
+		-0.0f,
+		0.0f/0.0f,
+		1.0f + 1.1920929e-7f,
+		1.0f
+	};
+	const int32_t maxULP[] = {
+		0,
+		1,
+		0,
+		1,
+		1,
+		0
+	};
+
+	bool v;
+	int i;
+	for ( i = 0; i < 6; i++ ) {
+		v = stdlib_base_float32_is_almost_same_value( a[ i ], b[ i ], maxULP[ i ] );
+		printf( "Almost same value? %s\n", ( v ) ? "True" : "False" );
+	}
+}
