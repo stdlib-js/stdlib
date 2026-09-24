94dd4d3220eaa17f97c6af61916a5de3c4fbd77f
headlessNode <harriskhan047@outlook.com>
2026-09-24 14:32:52 +0500

    feat: add blas/ext/base/ndarray/swhere

---
+++ lib/node_modules/@stdlib/blas/ext/base/ndarray/swhere/docs/types/index.d.ts
@@ -0,0 +1,57 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2026 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/// <reference types="@stdlib/types"/>
+
+import { float32ndarray, boolndarray } from '@stdlib/types/ndarray';
+
+/**
+* Takes elements from one of two one-dimensional single-precision floating-point ndarrays depending on a condition.
+*
+* ## Notes
+*
+* -   The function expects the following ndarrays:
+*
+*     -   a one-dimensional condition ndarray.
+*     -   first one-dimensional input ndarray.
+*     -   second one-dimensional input ndarray.
+*     -   a one-dimensional output ndarray.
+*
+* @param arrays - array-like object containing ndarrays
+* @returns output ndarray
+*
+* @example
+* var BooleanVector = require( '@stdlib/ndarray/vector/bool' );
+* var Float32Vector = require( '@stdlib/ndarray/vector/float32' );
+*
+* var condition = new BooleanVector( [ true, false, true, false, true ] );
+* var x = new Float32Vector( [ 1.0, 2.0, 3.0, 4.0, 5.0 ] );
+* var y = new Float32Vector( [ 6.0, 7.0, 8.0, 9.0, 10.0 ] );
+* var out = new Float32Vector( 5 );
+*
+* var v = swhere( [ condition, x, y, out ] );
+* // returns <ndarray>[ 1.0, 7.0, 3.0, 9.0, 5.0 ]
+*/
+declare function swhere( arrays: [ boolndarray, float32ndarray, float32ndarray, float32ndarray ] ): float32ndarray;
+
+
+// EXPORTS //
+
+export = swhere;
