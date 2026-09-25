ae1635a24d3239b65ea9603ab445feeb14b2d620
Samarth Kolarkar <samarthkolarkar7777@gmail.com>
2026-09-25 10:12:37 +0530

    feat: add float16 dtype support to ndarray/base/slice-dimension-to

--- lib/node_modules/@stdlib/ndarray/base/slice-dimension-to/docs/types/test.ts
+++ lib/node_modules/@stdlib/ndarray/base/slice-dimension-to/docs/types/test.ts
@@ -30,6 +30,7 @@ import sliceDimensionTo = require( './index' );
 
 	sliceDimensionTo( empty( 'float64', sh, order ), 1, s, false, false ); // $ExpectType float64ndarray
 	sliceDimensionTo( empty( 'float32', sh, order ), 1, s, false, false ); // $ExpectType float32ndarray
+	sliceDimensionTo( empty( 'float16', sh, order ), 1, s, false, false ); // $ExpectType float16ndarray
 	sliceDimensionTo( empty( 'complex128', sh, order ), 1, s, false, false ); // $ExpectType complex128ndarray
 	sliceDimensionTo( empty( 'complex64', sh, order ), 1, s, false, false ); // $ExpectType complex64ndarray
 	sliceDimensionTo( empty( 'int32', sh, order ), 1, s, false, false ); // $ExpectType int32ndarray
@@ -42,6 +43,7 @@ import sliceDimensionTo = require( './index' );
 
 	sliceDimensionTo( empty( 'float64', sh, order ), 1, s, true, true ); // $ExpectType float64ndarray
 	sliceDimensionTo( empty( 'float32', sh, order ), 1, s, true, true ); // $ExpectType float32ndarray
+	sliceDimensionTo( empty( 'float16', sh, order ), 1, s, true, true ); // $ExpectType float16ndarray
 	sliceDimensionTo( empty( 'complex128', sh, order ), 1, s, true, true ); // $ExpectType complex128ndarray
 	sliceDimensionTo( empty( 'complex64', sh, order ), 1, s, true, true ); // $ExpectType complex64ndarray
 	sliceDimensionTo( empty( 'int32', sh, order ), 1, s, true, true ); // $ExpectType int32ndarray
