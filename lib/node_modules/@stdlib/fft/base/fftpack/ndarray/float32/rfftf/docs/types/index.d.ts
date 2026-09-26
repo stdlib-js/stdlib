45e130fcc846626c9335fe8aeba50ab22df37b0b
Gunj Joshi <gunjjoshi8372@gmail.com>
2026-09-26 14:41:03 +0530

    feat: add `fft/base/fftpack/ndarray/float32/rfftf`
    PR-URL: https://github.com/stdlib-js/stdlib/pull/15540
    Reviewed-by: Athan Reines <kgryte@gmail.com>
    Closes: https://github.com/stdlib-js/metr-issue-tracker/issues/1368

---
+++ lib/node_modules/@stdlib/fft/base/fftpack/ndarray/float32/rfftf/docs/types/index.d.ts
@@ -0,0 +1,64 @@
+/*
+* @license Apache-2.0
+*
+* Copyright (c) 2026 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// TypeScript Version: 4.1
+
+/// <reference types="@stdlib/types"/>
+
+import { float32ndarray } from '@stdlib/types/ndarray';
+
+/**
+* Computes the forward discrete Fourier transform (DFT) of a real-valued one-dimensional single-precision floating-point ndarray.
+*
+* ## Notes
+*
+* -   The function expects the following ndarrays:
+*
+*     -   a one-dimensional input ndarray.
+*     -   a one-dimensional workspace ndarray containing pre-computed values.
+*
+* @param arrays - array-like object containing ndarrays
+* @returns input ndarray
+*
+* @example
+* var Float32Vector = require( '@stdlib/ndarray/vector/float32' );
+* var scalar2ndarray = require( '@stdlib/ndarray/from-scalar' );
+* var rffti = require( '@stdlib/fft/base/fftpack/ndarray/float32/rffti' );
+*
+* var N = 4;
+* var len = scalar2ndarray( N, {
+*     'dtype': 'int32'
+* });
+*
+* var w = new Float32Vector( ( 2*N ) + 34 );
+* rffti( [ w, len ] );
+*
+* var r = new Float32Vector( [ 1.0, 2.0, 3.0, 4.0 ] );
+*
+* var out = rfftf( [ r, w ] );
+* // returns <ndarray>[ 10.0, -2.0, 2.0, -2.0 ]
+*
+* var bool = ( out === r );
+* // returns true
+*/
+declare function rfftf( arrays: [ float32ndarray, float32ndarray ] ): float32ndarray;
+
+
+// EXPORTS //
+
+export = rfftf;
